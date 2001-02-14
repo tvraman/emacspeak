3f4a302eec3335438d28e8f2481b294b16e1d519
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2001-02-14 04:47:36 +0000

    updated jde completion.
    git-svn-id: https://emacspeak.googlecode.com/svn@53 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/emacspeak/lisp/emacspeak-jde.el
+++ trunk/emacspeak/lisp/emacspeak-jde.el
@@ -59,6 +59,18 @@
 
 ;;}}}
 ;;{{{ Advice interactive commands:
+(defadvice jde-complete-at-point (around emacspeak pre act)
+  "Say what you completed."
+  (let ((emacspeak-speak-messages nil)
+        (prior (point ))
+        (dtk-stop-immediately dtk-stop-immediately))
+    (when dtk-stop-immediately (dtk-stop))
+    ad-do-it
+    (when (> (point) prior)
+      (setq dtk-stop-immediately nil)
+      (tts-with-punctuations "all"
+                             (dtk-speak (buffer-substring prior (point )))))
+    ad-return-value))
 
 (defadvice jde-compile (after emacspeak pre act comp)
 "Provide auditory feedback"
