1f88cfb39901a509c20f23871c34cdb90db79769
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2001-02-16 03:58:07 +0000

    Updated with copyright info
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@56 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- emacspeak/servers/software-dtk/tcldtk.c
+++ emacspeak/servers/software-dtk/tcldtk.c
@@ -3,7 +3,26 @@
 
 
 /* <copyright*/
-
+/**
+*Copyright (C) 1995 -- 2000, T. V. Raman 
+*All Rights Reserved
+*
+* This file is not part of GNU Emacs, but the same permissions apply.
+*
+* GNU Emacs is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* GNU Emacs is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with GNU Emacs; see the file COPYING.  If not, write to
+* the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 /* > */
 /* <headers*/
 
