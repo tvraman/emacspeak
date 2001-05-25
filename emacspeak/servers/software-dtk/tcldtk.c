74a6c06484590eb366eb9ebf6c546206115a58b4
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2001-05-25 23:50:26 +0000

    updated

--- emacspeak/servers/software-dtk/tcldtk.c
+++ emacspeak/servers/software-dtk/tcldtk.c
@@ -1,7 +1,7 @@
 /*$Id$*/
 /* <copyright*/
 /**
-*Copyright (C) 1995 -- 2000, T. V. Raman 
+*Copyright (C) 1995 -- 2001, T. V. Raman 
 *All Rights Reserved
 *
 * This file is not part of GNU Emacs, but the same permissions apply.
