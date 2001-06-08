089f9985336982c86ebb53bae8eae1148cdc0ba9
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2001-06-08 23:11:37 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn@237 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/emacspeak/lisp/emacspeak-wizards.el
+++ trunk/emacspeak/lisp/emacspeak-wizards.el
@@ -703,8 +703,7 @@ end:\n\n")
 (loop for f in (frame-list)
                         collect
  (cons (frame-parameter f 'emacspeak-label)
-        (frame-parameter f 'emacspeak-label)))
-nil t))
+        (frame-parameter f 'emacspeak-label)))))
  
                         
 (defun emacspeak-frame-label-or-switch-to-labelled-frame (label
