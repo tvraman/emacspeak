bc6753ce097cf619ebdd494467a189f948448785
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2001-09-11 22:23:42 +0000

    *** empty log message ***

--- emacspeak/lisp/emacspeak-keymap.el
+++ emacspeak/lisp/emacspeak-keymap.el
@@ -166,6 +166,7 @@ field in the customization buffer.  You can use the notation
 (define-key emacspeak-keymap "V" 'emacspeak-speak-version)
 (define-key emacspeak-keymap "\C-v" 'view-mode)
 (define-key emacspeak-keymap "t" 'emacspeak-speak-time )
+(define-key emacspeak-keymap "T" 'emacspeak-view-emacspeak-tips )
 (define-key emacspeak-keymap "s" 'dtk-stop)
 (define-key emacspeak-keymap "r" 'emacspeak-speak-region)
 (define-key emacspeak-keymap "R" 'emacspeak-speak-rectangle)
