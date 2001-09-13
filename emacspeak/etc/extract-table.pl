c913d01f0e2cf7ed0b04d99964220a547e412e48
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2001-09-13 22:48:57 +0000

    *** empty log message ***

---
+++ emacspeak/etc/extract-table.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl -w
+#$Id$
+# Accepts a URI and table spec
+#returns a csv file
+use strict;
+use FileHandle;
+use LWP::UserAgent;
+use HTML::TableExtract;
+use IO::File;
+use Getopt::Long;
+use vars qw (%options);
+my %options = ();
+GetOptions (\%options,
+"task=s",
+           "url=s",
+           "depth=i",
+"count=i");
+my $input="/tmp/$options{task}.html";
+RetrieveURLToFile($options{url}, $input);
+my $te = new HTML::TableExtract( depth => 2, count=>7);
+$te->parse_file($input);
+my $output = new FileHandle (">  /tmp/$options{task}.csv");
+my $row;
+foreach $row ($te->rows) {
+  $output->print(  join(',', @$row),"\n");
+}
+$output->close();
+unlink ($input);
+# {{{  retrieve URL to file
+
+sub RetrieveURLToFile {
+  my ($url, $filename) = @_;
+  my $ua = new LWP::UserAgent;
+  # Create a request
+  my $req = new HTTP::Request( 'GET' => $url);
+  # Pass request to the user agent and get a response back
+  my $res = $ua->request($req, $filename);
+  if ($res->is_success()) {
+    warn"table: Retrieved $url to $filename\n";
+  } elsif ($res->is_error()) {
+    exit ("Retrieval for $url failed\n");
+  }
+}
+
+# }}}
+1;
