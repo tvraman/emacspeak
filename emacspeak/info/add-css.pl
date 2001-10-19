309022bac567da0944466b472566ef2d9df463ca
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2001-10-19 03:57:01 +0000

    *** empty log message ***

--- emacspeak/info/add-css.pl
+++ emacspeak/info/add-css.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl-w  -i 
 my $css='<link rel="http://www.w3.org/StyleSheets/Core/Modernist" type="text/css">';
-my $logo=qq(<p> <a href="http://emacspeak.sf.net"><img src="emacspeak.jpg" alt= "EMACSPEAK --Complete Audio Desktop"></a> </p>);
+my $logo=qq(<p> <a href="http://emacspeak.sf.net"><img width="150" height="216" src="emacspeak.jpg" alt= "EMACSPEAK --Complete Audio Desktop"></a> </p>);
 while ( <>) {
   s@<head>@<head>$css@igo;
 s@</address>@</address>$logo@igo;
