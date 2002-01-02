3a944952977839e35b812b3a709fc797ed60e940
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2002-01-02 18:53:15 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn@814 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/emacspeak/lisp/emacspeak-mspools.el
+++ trunk/emacspeak/lisp/emacspeak-mspools.el
@@ -16,7 +16,7 @@
 ;;}}}
 ;;{{{  Copyright:
 
-;;; Copyright (c) 1995 -- 2001, T. V. Raman
+;;; Copyright (c) 1995 -- 2002, T. V. Raman
 ;;; All Rights Reserved. 
 ;;;
 ;;; This file is not part of GNU Emacs, but the same permissions apply.
