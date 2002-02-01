fbd6d1448e58f5fa5d6470d9e8c8c930453aa01c
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2002-02-01 00:21:00 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn@902 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/emacspeak/lisp/emacspeak-speak.el
+++ trunk/emacspeak/lisp/emacspeak-speak.el
@@ -1539,16 +1539,19 @@ semantic to do the work."
 (defsubst ems-process-mode-line-format (spec)
   "Process mode line format spec."
   (cond
-   ((symbolp spec) (eval spec))
-   ((stringp spec) spec)
-   ((and (listp spec)
-         (stringp (car spec)))
-    (concat (car spec)
-            (ems-process-mode-line-format (cdr spec))))
-   ((and (listp spec)
-         (symbolp (car spec)))
-    (concat (ems-process-mode-line-format (car spec))
-            (ems-process-mode-line-format (cadr spec))))))
+ ((symbolp spec) (symbol-value  spec))
+ ((stringp spec) (eval spec))
+ ((and (listp spec)
+       (stringp (car spec)))
+  (concat 
+               (car spec)
+          (ems-process-mode-line-format (cdr spec))))
+ ((and (listp spec)
+       (symbolp (car spec)))
+  (concat
+               (symbol-value (car spec))
+  (ems-process-mode-line-format (cadr spec))))))
+
 
 (defun emacspeak-speak-mode-line ()
   "Speak the mode-line."
