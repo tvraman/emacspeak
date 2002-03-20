6608e5c672a9c7500388a38dd35f40e9eefc87be
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2002-03-20 04:56:48 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn@1012 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/emacspeak/lisp/emacspeak-analog.el
+++ trunk/emacspeak/lisp/emacspeak-analog.el
@@ -92,9 +92,125 @@
             (emacspeak-auditory-icon 'select-object))))))
 
 ;;}}}
+;;{{{ field navigation
+
+;;; You can add a fields property that holds a list of field start
+;;; positions 
+;;; in analog-entries-list
+;;; emacspeak will use this to navigate using the arrow keys.
+
+(defsubst emacspeak-analog-get-field-spec ()
+  "Returns field specification if one defined for current entry.
+Nil means no field specified."
+  (save-excursion
+    (let ((start (previous-single-property-change (point)
+                                                  'analog-entry-start)))
+      (when start
+        (analog-get-entry-property
+         (get-text-property
+          (1- start)
+          'analog-entry-start)
+         'fields)))))
+
+
+(defun emacspeak-analog-forward-field-or-char ()
+  "Move forward to next field if field specification is available.
+Otherwise move to next char.
+Speak field or char moved to."
+  (interactive)
+  (let ((fields (emacspeak-analog-get-field-spec)))
+    (cond
+     (fields (emacspeak-analog-next-field fields)
+             (emacspeak-analog-speak-field fields)
+             (emacspeak-auditory-icon 'large-movement))
+     (t (call-interactively 'emacspeak-forward-char)))))
+
+(defun emacspeak-analog-backward-field-or-char ()
+  "Move back to next field if field specification is available.
+Otherwise move to previous char.
+Speak field or char moved to."
+  (interactive)
+  (let ((fields (emacspeak-analog-get-field-spec)))
+    (cond
+     (fields (emacspeak-analog-previous-field fields)
+             (emacspeak-analog-speak-field fields)
+             (emacspeak-auditory-icon 'large-movement))
+     (t (call-interactively 'emacspeak-backward-char)))))
+
+
+(defun emacspeak-analog-speak-field (fields)
+  "Speak field containing point."
+  (save-excursion
+    (let ((col (current-column))
+          (start nil)
+          (end nil)
+          (prev 0)
+          (current  (first fields)))
+      (beginning-of-line)
+      (while (and fields 
+                  (< current col))
+        (setq prev current
+              current (pop fields)))
+        (forward-char prev)
+        (setq start (point))
+      (cond
+       ((= prev col)
+        (beginning-of-line)
+        (forward-char prev)
+        (setq start (point))
+        (beginning-of-line)
+        (forward-char (1- current))
+        (setq end (point)))
+       ((>= col current)
+        (beginning-of-line)
+        (forward-char current)
+        (setq start (point))
+        (end-of-line)
+        (setq end (point)))
+       (t (beginning-of-line)
+          (forward-char (1- current))
+          (setq end (point))))
+      (emacspeak-speak-region start end))))
+
+(defun emacspeak-analog-speak-this-field ()
+  "Speak current field."
+  (interactive)
+  (emacspeak-analog-speak-field (emacspeak-analog-get-field-spec)))
+
+(defun emacspeak-analog-next-field (fields)
+  "Move to next field."
+  (let ((col (current-column))
+        (end (first fields)))
+    (while (and fields 
+                (<= end col))
+      (setq end (pop fields)))  
+    (cond
+     ((> end col)
+      (beginning-of-line)
+      (forward-char end))
+     (t (emacspeak-auditory-icon 'error)))))
+
+(defun emacspeak-analog-previous-field (fields)
+  "Move to previous field."
+  (let ((col (current-column))
+        (start 0)
+        (prev 0)
+        (end (first fields)))
+    (while (and fields 
+                (< end col))
+      (setq prev start 
+            start end 
+            end (pop fields)))
+    (beginning-of-line)
+    (forward-char prev)))
+
+;;}}}
 ;;{{{ key bindings
+
 (declaim (special analog-mode-map))
-(define-key analog-mode-map "f" 'analog-find-entry-file)
+(define-key analog-mode-map '[left]
+  'emacspeak-analog-backward-field-or-char)
+(define-key analog-mode-map '[right] 'emacspeak-analog-forward-field-or-char)
 
 ;;}}}
 (provide 'emacspeak-analog)
