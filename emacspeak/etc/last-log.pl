20ed56e190d49786c0aedd26183160367651af45
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2002-07-24 16:10:46 +0000

    *** empty log message ***

--- emacspeak/etc/last-log.pl
+++ emacspeak/etc/last-log.pl
@@ -7,32 +7,25 @@ use strict;
 use constant  RECORD_SIZE => 292;
 use constant RECORD_FORMAT =>'l A32 A256';
 
-#build up a database of users 
-
-my ($name, $junk, $uid);
-my  %names;
-while (($name, $junk, $uid) = getpwent) {
-  $names{$name} = $uid;
-}
-endpwent;
-
 my $user = shift;
 $user ||= $ENV{LOGNAME};
-my $u = $names{$user};
+my $u =qx(id -u);
 die "User $user not found on this system" unless defined ($u);
+chomp($u);
 
 my $lastlog  = "/var/log/lastlog";
 open(LASTL, $lastlog) or die "Cannot read lastlog $!";
-
 my $offset = $u * RECORD_SIZE  ;
 seek (LASTL, $offset, 0);
 my $record;
 read(LASTL, $record, RECORD_SIZE);
-
 my ($time, $line, $host) = unpack(RECORD_FORMAT, $record);
+if ( $host) {
 my $home=$ENV{HOME};
 open (OUT, "> $home/.emacspeak/.current-remote-hostname") or die "Cannot write output $!";
 print OUT $host, "\n";
 close OUT;
+} else {
+  print STDERR "No host entry on lastlog.\n";
+}
 close LASTL;
-
