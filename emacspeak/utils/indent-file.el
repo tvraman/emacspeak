9df4e84d1768693c76d148310957a9bbec0c0021
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2002-08-25 15:40:31 +0000

    *** empty log message ***

--- emacspeak/utils/indent-file.el
+++ emacspeak/utils/indent-file.el
@@ -3,24 +3,22 @@
 
 (require 'cl)
 
-(defun batch-indent-files (file-list)
-  "Batch indent specified list of elisp files."
-  (loop for f in file-list
-	do
-	(find-file f)
-	(lisp-indent-region (point-min)
-			    (point-max))
-	(shell-command-on-region (point-min)
-				 (point-max)
-				 "cat -s"
-				 (current-buffer)
-				 'replace)
-        (save-buffer)))
+(defun batch-indent-files ()
+  "Batch indent  elisp files in directory.."
+  (let ((file-list (directory-files default-directory
+                                    nil
+                                    ".el$")))
+    (loop for f in file-list
+          do
+          (find-file f)
+          (lisp-indent-region (point-min)
+                              (point-max))
+          (shell-command-on-region (point-min)
+                                   (point-max)
+                                   "cat -s"
+                                   (current-buffer)
+                                   'replace)
+          (save-buffer))))
 
-(defun batch-indent-files-in-directory ()
-  "Batch indent all files in working directory."
-  (declare (special default-directory))
-  (batch-indent-files
-   (directory-files default-directory
-		    nil
-		    ".el$")))
+  
+(batch-indent-files)
