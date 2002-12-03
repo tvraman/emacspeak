da6b1e1f0e623caa3897615167fe282d792dbb8c
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2002-12-03 16:11:54 +0000

    *** empty log message ***

--- emacspeak/tvr/tnt-prepare.el
+++ emacspeak/tvr/tnt-prepare.el
@@ -1,9 +1,5 @@
 (augment-load-path "tnt")
 (load-library "tnt")
 
-(setq tnt-use-timestamps t)
 ;;; aim server 
 ;;; tvraman login.oscar.aol.com 5190
-(setq tnt-message-on-buddy-signonoff  nil)
-(setq tnt-send-idle-after 300)
-
