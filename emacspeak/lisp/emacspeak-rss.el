3757b62c5f9ebc0c341410cd8a173e3d8f44d048
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2002-12-22 04:34:29 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@1609 bb5eec90-091a-0410-bc89-db79c2cf2c7b

---
+++ emacspeak/lisp/emacspeak-rss.el
@@ -0,0 +1,98 @@
+;;; emacspeak-xslt.el --- Emacspeak RSS Wizard
+;;; $Id$
+;;; $Author$
+;;; Description:  RSS Wizard for the emacspeak desktop
+;;; Keywords: Emacspeak,  Audio Desktop RSS
+;;{{{  LCD Archive entry:
+
+;;; LCD Archive Entry:
+;;; emacspeak| T. V. Raman |raman@cs.cornell.edu
+;;; A speech interface to Emacs |
+;;; $Date$ |
+;;;  $Revision$ |
+;;; Location undetermined
+;;;
+
+;;}}}
+;;{{{  Copyright:
+;;;Copyright (C) 1995 -- 2002, T. V. Raman 
+;;; Copyright (c) 1994, 1995 by Digital Equipment Corporation.
+;;; All Rights Reserved.
+;;;
+;;; This file is not part of GNU Emacs, but the same permissions apply.
+;;;
+;;; GNU Emacs is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2, or (at your option)
+;;; any later version.
+;;;
+;;; GNU Emacs is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Emacs; see the file COPYING.  If not, write to
+;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;}}}
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;{{{  introduction
+
+;;; Simple RSS wizard for Emacspeak
+
+;;}}}
+;;{{{  Required modules
+
+(eval-when-compile (require 'cl))
+(declaim  (optimize  (safety 0) (speed 3)))
+(require 'custom)
+(require 'emacspeak-speak)
+(require 'emacspeak-xslt)
+(require 'emacspeak-w3)
+(require 'emacspeak-wizards)
+
+;;}}}
+;;{{{ RSS feed cache
+
+(defgroup emacspeak-rss nil
+  "RSS Feeds for the Emacspeak desktop."
+  :group 'emacspeak)
+
+(defcustom emacspeak-rss-feeds
+  '(
+    ("Wired News" ."http://www.wired.com/news_drop/netcenter/netcenter.rdf")
+("BBC News" . "http://www.bbc.co.uk/syndication/feeds/news/ukfs_news/front_page/rss091.xml")
+("CNet Tech News" . "http://rss.com.com/2547-12-0-5.xml")
+("XML.COM" . "http://www.xml.com/xml/news.rss")
+)
+"Table of RSS feeds."
+:type '(repeat
+(cons :tag "RSS Feed"
+      (string :tag "Title")
+      (string :tag "URI")))
+:group 'emacspeak-rss)
+
+;;}}}
+;;{{{  view feed
+(defun emacspeak-rss-browse (feed)
+  "Browse specified RSS feed."
+  (interactive
+   (list
+    (let ((completion-ignore-case t))
+    (completing-read "Feed:"
+                     emacspeak-rss-feeds))))
+  (let ((uri (cdr
+              (assoc feed emacspeak-rss-feeds))))))
+
+;;}}}
+(provide 'emacspeak-rss)
+;;{{{ end of file
+
+;;; local variables:
+;;; folded-file: t
+;;; byte-compile-dynamic: nil
+;;; end:
+
+;;}}}
