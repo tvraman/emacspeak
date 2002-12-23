d13deec4c2183b8fef7282a888820b18b84b53ca
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2002-12-23 16:54:49 +0000

    *** empty log message ***

--- emacspeak/lisp/emacspeak-pronounce.el
+++ emacspeak/lisp/emacspeak-pronounce.el
@@ -541,6 +541,7 @@ See http://oz.uc.edu/~solkode/smileys.html for a full list."
 
 (defcustom emacspeak-pronounce-common-xml-namespace-uri-pronunciations 
   '(
+    ("http://www.w3.org/1999/02/22-rdf-syntax-ns#" . "RDF Syntax")
     ("http://www.w3.org/2002/06/xhtml2" . " xhtml2 ")
     ("http://www.w3.org/1999/XSL/Transform" . " XSLT ")
     ("http://www.w3.org/2002/xforms/cr" . " XForms ")
