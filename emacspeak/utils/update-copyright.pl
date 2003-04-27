c6bbe270b251572500bc2bf8a59ae005a3069570
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2003-04-27 23:47:29 +0000

    *** empty log message ***

--- emacspeak/utils/update-copyright.pl
+++ emacspeak/utils/update-copyright.pl
@@ -1,10 +1,10 @@
-#!/usr/local/bin/perl -i.bak
+#!/usr/local/bin/perl -i
 #$Id$
 #Update Copyright notice
 #
 
-my $old = "1995 -- 2000, T. V. Raman";
-my $new = "1995 -- 2002, T. V. Raman";
+my $old = "1995 -- 2003, T. V. Raman";
+my $new = "1995 -- 2003, T. V. Raman";
 
 while (<>) {
     s/$old/$new/o;
