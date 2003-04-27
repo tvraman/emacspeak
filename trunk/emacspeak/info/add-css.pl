22c5d2132739ab51086efc3300aa449adca0fcca
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2003-04-27 23:04:52 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn@2019 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/emacspeak/info/add-css.pl
+++ trunk/emacspeak/info/add-css.pl
@@ -3,7 +3,7 @@ my $css='<link rel="http://www.w3.org/StyleSheets/Core/Modernist" type="text/css
 my $logo=qq(<p> <a href="http://emacspeak.sf.net"><img width="150" height="216" src="emacspeak.jpg" alt= "EMACSPEAK --Complete Audio Desktop"></a> </p>);
 while ( <>) {
   s@<head>@<head>$css@igo;
-s@</address>@</address>$logo@igo;
+s@<body>@<body>$logo@igo;
 s/^\s*\@title.*$//;
 s/^\s*\@kindex.*//;
 print;
