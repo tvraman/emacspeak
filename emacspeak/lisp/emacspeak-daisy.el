96161efd409be26bd9fab96291b4da638bd2baa6
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2003-06-27 03:22:56 +0000

    *** empty log message ***

--- emacspeak/lisp/emacspeak-daisy.el
+++ emacspeak/lisp/emacspeak-daisy.el
@@ -459,6 +459,7 @@ Here is a list of all emacspeak DAISY commands along with their key-bindings:
   'emacspeak-daisy-play-content-under-point)
 (define-key emacspeak-daisy-mode-map "n" 'next-line)
 (define-key emacspeak-daisy-mode-map "p" 'previous-line)
+(define-key emacspeak-daisy-mode-map "P" 'emacspeak-daisy-play-page-range)
 
 ;;}}}
 ;;{{{  open a book (entry point)
@@ -515,7 +516,9 @@ Here is a list of all emacspeak DAISY commands along with their key-bindings:
         (start (line-beginning-position))
         (end (line-end-position)))
     (cond
-     (viewer (switch-to-buffer viewer)
+     ((and viewer
+           (buffer-live-p viewer))
+(switch-to-buffer viewer)
              (emacspeak-auditory-icon 'select-object)
              (emacspeak-speak-mode-line))
      (content
