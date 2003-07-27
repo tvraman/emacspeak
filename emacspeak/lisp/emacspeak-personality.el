86a0b03581704b87328168c0ee01e948571eeaa9
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2003-07-27 15:27:04 +0000

    *** empty log message ***

--- emacspeak/lisp/emacspeak-personality.el
+++ emacspeak/lisp/emacspeak-personality.el
@@ -330,6 +330,47 @@ displayed in the messages area."
         (error nil))
       )))
 
+(defadvice set-text-properties (after emacspeak-personality  pre act) 
+  "Used by emacspeak to augment font lock."
+  (let ((start (ad-get-arg 0))
+        (end (ad-get-arg 1 ))
+        (properties (ad-get-arg 2))
+        (object (ad-get-arg 3))
+        (facep nil)
+        (voice nil)
+        (value nil))
+    (setq facep (emacspeak-personality-plist-face-p properties))
+    (when (and  emacspeak-personality-voiceify-faces
+		facep)
+      (setq value (second facep))
+      (condition-case nil
+          (progn
+            (cond
+             ((symbolp value)
+              (setq voice (voice-setup-get-voice-for-face   value)))
+             ((and (consp value)	;check for plain cons and pass
+                   (equal value (last value)))
+	      nil)
+             ( (listp value)
+               (setq voice
+                     (delete nil 
+                             (mapcar   #'voice-setup-get-voice-for-face value))))
+             (t (message "Got %s" value)))
+            (when voice
+              (funcall emacspeak-personality-voiceify-faces start end voice object))
+            (when (and emacspeak-personality-show-unmapped-faces
+                       (not voice))
+              (cond
+               ((listp value)
+                (mapcar #'(lambda (v)
+                            (puthash  v t emacspeak-personality-unmapped-faces))
+                        value))
+               (t (puthash  value t emacspeak-personality-unmapped-faces))))
+            )
+        (error nil))
+      )))
+
+
 (defadvice propertize (around emacspeak-personality  pre act) 
   "Used by emacspeak to augment font lock."
   (let ((string (ad-get-arg 0))
