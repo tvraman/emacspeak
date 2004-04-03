f061426d69e5e598260a37c7f7cc344b339851e3
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2004-04-03 00:58:23 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn@2778 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/emacspeak/lisp/emacspeak-ido.el
+++ trunk/emacspeak/lisp/emacspeak-ido.el
@@ -43,20 +43,94 @@
 (require 'emacspeak-preamble)
 
 ;;}}}
-;;{{{  Introduction:
-
-;;; Commentary:
-
-;;; speech-enable ido.el
-;;; speech-enabled along the same lines as emacspeak-iswitchb.el
-
-;;; Code:
-
-;;}}}
 ;;{{{ speech-enable feedback routines
+
+(defadvice ido-exhibit (after emacspeak pre act comp)
+  "Speak first of the displayed matches."
+  (dtk-speak
+   (format
+    "%s %d Choices: %s"
+    (car ido-matches)
+    (length ido-matches)
+    (or ido-text ""))))
 
 ;;}}}
 ;;{{{ speech-enable interactive commands:
+
+(defadvice ido-toggle-case (after emacspeak pre act comp)
+  "Provide auditory feedback."
+  (when (interactive-p)
+    (emacspeak-auditory-icon
+     (if ido-case-fold 'on 'off))
+    (dtk-speak
+     (format "Case %s"
+             (if ido-case-fold 'on 'off)))))
+
+(defadvice ido-toggle-regexp (after emacspeak pre act comp)
+  "Provide auditory feedback."
+  (when (interactive-p)
+    (emacspeak-auditory-icon
+     (if ido-enable-regexp 'on 'off))
+    (dtk-speak
+     (format "Case %s"
+             (if ido-enable-regexp 'on 'off)))))
+(defadvice ido-toggle-prefix (after emacspeak pre act comp)
+  "Provide auditory feedback."
+  (when (interactive-p)
+    (emacspeak-auditory-icon
+     (if ido-enable-prefix 'on 'off))
+    (dtk-speak
+     (format "Prefix %s"
+             (if ido-enable-prefix 'on 'off)))))
+
+(defadvice ido-toggle-ignore (after emacspeak pre act comp)
+  "Provide auditory feedback."
+  (when (interactive-p)
+    (emacspeak-auditory-icon
+     (if ido-process-ignore-lists 'on 'off))
+    (dtk-speak
+     (format "Case %s"
+             (if ido-process-ignore-lists 'on 'off)))))
+
+(defadvice ido-complete (after emacspeak pre act comp)
+  "Speak completion at the head of the list."
+  (when (interactive-p)
+    (dtk-speak (car ido-matches))))
+(defadvice  ido-find-file (after emacspeak pre act comp)
+  "Provide auditory feedback."
+  (when (interactive-p)
+    (emacspeak-auditory-icon 'open-object)
+    (emacspeak-speak-mode-line)))
+
+(defadvice  ido-switch-buffer (after emacspeak pre act comp)
+  "Provide auditory feedback."
+  (when (interactive-p)
+    (emacspeak-auditory-icon 'select-object)
+    (emacspeak-speak-mode-line)))
+
+;;; note that though these are after advice fragments,
+;;; ido-matches does not reflect the change at the time we
+;;; get called.
+;;; hence the off-by-one hack
+
+(defadvice ido-next-match (after emacspeak pre act comp)
+  "Speak match at the front of the list."
+  (when (interactive-p)
+    (emacspeak-auditory-icon 'select-object)
+    (dtk-speak
+     (second ido-matches))))
+
+(defadvice ido-prev-match (after emacspeak pre act comp)
+  "Speak match at the front of the list."
+  (when (interactive-p)
+    (emacspeak-auditory-icon 'select-object)
+    (dtk-speak
+     (car (last ido-matches)))))
+
+(defadvice ido-kill-buffer (after emacspeak pre act comp)
+  "Provide auditory icon."
+  (when (interactive-p)
+    (emacspeak-auditory-icon 'close-object)))
 
 ;;}}}
 (provide 'emacspeak-ido)
