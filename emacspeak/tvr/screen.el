752c26c554e12e554a571221892109c3aaf3c149
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2004-04-19 13:27:30 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@2852 bb5eec90-091a-0410-bc89-db79c2cf2c7b

---
+++ emacspeak/tvr/screen.el
@@ -0,0 +1,5 @@
+;;;keys for screen
+(define-key function-key-map "\e\e[A" [27 up])
+(define-key function-key-map "\e\e[B" [27 down])
+(define-key function-key-map "\e\e[D" [27 left])
+(define-key function-key-map "\e\e[C" [27 right])
