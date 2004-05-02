9d82d9571d44524c0f982e164ba60c3d520a0d47
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2004-05-02 17:22:16 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn@2873 bb5eec90-091a-0410-bc89-db79c2cf2c7b

---
+++ trunk/emacspeak/etc/ocr-client.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w
+#$Id$
+#Description: Invoke ocrxtr client. Pipe result to stdout
+#Usage: ocr-client.pl image-file [hostname]
+use strict;
+use File::Basename;
+
+my $OCR = 'ocrxtr';
+my $image =shift;
+die "No image specified" unless defined ($image);
+my $host =shift;
+my ($name,$path,$suffix) = fileparse($image,"\.tiff");
+my $input = "/tmp/$$-$name.tiff";
+my $output = "/tmp/ocr-output-$$.txt";
+$host ='localhost' unless defined ($host);
+if ( $host =~ m/localhost/) {
+  qx($OCR -out_text_name $output $image);
+  open (OUT, "cat -s $output |");
+  while ( <OUT>) {
+    print;
+  }
+  unlink $output;
+} else {
+  qx(scp $image  $host:$input);
+  qx(ssh $host $OCR -out_text_name $output $input 2>&1 > /dev/null);
+  open (OUT, "ssh $host cat -s $output |");
+  while (<OUT>) {
+    print;
+  }
+  qx(ssh $host rm $input $output);
+}
