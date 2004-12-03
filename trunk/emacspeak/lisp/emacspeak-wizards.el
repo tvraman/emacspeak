92a73e95eed19b4242a99f48d0b4b681a8facfdc
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2004-12-03 20:12:40 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn@3068 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/emacspeak/lisp/emacspeak-wizards.el
+++ trunk/emacspeak/lisp/emacspeak-wizards.el
@@ -559,7 +559,7 @@ default-directory after switching."
      (t (save-excursion
           (set-buffer buffer)
           (message (buffer-string)))))))
-
+;;;###autoload
 (defun emacspeak-wizards-tpctl-display-status ()
   "Show display status on thinkpads using tpctl."
   (interactive)
