6cfebace2ea5f613f5af2594618c72af2d56d7d8
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2005-04-30 16:05:38 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@3343 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- emacspeak/lisp/emacspeak-pronounce.el
+++ emacspeak/lisp/emacspeak-pronounce.el
@@ -628,7 +628,7 @@ See http://oz.uc.edu/~solkode/smileys.html for a full list."
   '(
     ("http://www.w3.org/1999/02/22-rdf-syntax-ns#" . "RDF Syntax")
     ("http://www.w3.org/2002/06/xhtml2" . " xhtml2 ") 
-    ("http://www.w3.org/2001/XInclude" . "xinclude")
+    ("http://www.w3.org/2003/XInclude" . "xinclude")
     ("http://www.w3.org/1999/XSL/Transform" . " XSLT ")
     ("http://www.w3.org/2002/xforms" . " XForms ")
     ("http://www.w3.org/2001/xml-events" . " XEvents ")
