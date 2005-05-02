1df5c997bc8bc70781d820584ee524706bfa141a
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2005-05-02 13:31:59 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn@3351 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/emacspeak/servers/tts-lib.tcl
+++ trunk/emacspeak/servers/tts-lib.tcl
@@ -189,7 +189,7 @@ proc queue_restore {} {
 #play a sound over the server
 proc p {sound} {
     global tts
-    catch "exec $tts(play) $sound 2>&1 >   /dev/null &" errCode
+    catch "exec $tts(play) $sound >/dev/null  &" errCode
     speech_task
 }
 
