c6a21fbd9d88fa42d6aa8b9f08cbeff1e18a9456
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2005-06-29 16:39:11 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@3372 bb5eec90-091a-0410-bc89-db79c2cf2c7b

---
+++ emacspeak/tvr/org-prepare.el
@@ -0,0 +1,13 @@
+;;;$Id$
+;;; load org mode
+(augment-load-path "org" "org")
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(autoload 'org-mode "org"
+  "Org Mode" t)
+(autoload 'org-diary "org" "Diary entries from Org mode")
+(autoload 'org-agenda "org" "Multi-file agenda from Org mode" t)
+(autoload 'org-store-link "org" "Store a link to the current location" t)
+(autoload 'orgtbl-mode "org" "Org tables as a minor mode" t)
+(autoload 'turn-on-orgtbl "org" "Org tables as a minor mode")
