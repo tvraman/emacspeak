49e2dcfd5e4ad0f903ffe92f256303863d17ca3b
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2005-06-30 17:40:46 +0000

    *** empty log message ***

---
+++ emacspeak/lisp/emacspeak-org.el
@@ -0,0 +1,67 @@
+;;; emacspeak-org.el --- Speech-enable org 
+;;; $Id$
+;;; $Author$
+;;; Description:  Emacspeak front-end for ORG 
+;;; Keywords: Emacspeak, org 
+;;{{{  LCD Archive entry:
+
+;;; LCD Archive Entry:
+;;; emacspeak| T. V. Raman |raman@cs.cornell.edu
+;;; A speech interface to Emacs |
+;;; $Date$ |
+;;;  $Revision$ |
+;;; Location undetermined
+;;;
+
+;;}}}
+;;{{{  Copyright:
+
+;;; Copyright (C) 1999 T. V. Raman <raman@cs.cornell.edu>
+;;; All Rights Reserved.
+;;;
+;;; This file is not part of GNU Emacs, but the same permissions apply.
+;;;
+;;; GNU Emacs is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2, or (at your option)
+;;; any later version.
+;;;
+;;; GNU Emacs is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Emacs; see the file COPYING.  If not, write to
+;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;}}}
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Commentary:
+;;{{{  Introduction:
+
+;;; Speech-enable org ---
+;;;  Org allows you to keep organized notes and todo lists.
+;;; Homepage: http://www.astro.uva.nl/~dominik/Tools/org/
+
+;;}}}
+;;{{{ required modules
+
+;;; Code:
+(require 'emacspeak-preamble)
+(require 'emacspeak-redefine)
+
+;;}}}
+
+
+
+(provide 'emacspeak-org)
+;;{{{ end of file
+
+;;; local variables:
+;;; folded-file: t
+;;; byte-compile-dynamic: t
+;;; end:
+
+;;}}}
