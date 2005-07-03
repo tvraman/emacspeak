b8af4fc905da1df2fd6cfc72d9ee3a1015fe3908
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2005-07-03 15:51:14 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@3375 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- emacspeak/tvr/org-prepare.el
+++ emacspeak/tvr/org-prepare.el
@@ -4,10 +4,15 @@
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
-(autoload 'org-mode "org"
-  "Org Mode" t)
-(autoload 'org-diary "org" "Diary entries from Org mode")
-(autoload 'org-agenda "org" "Multi-file agenda from Org mode" t)
-(autoload 'org-store-link "org" "Store a link to the current location" t)
-(autoload 'orgtbl-mode "org" "Org tables as a minor mode" t)
-(autoload 'turn-on-orgtbl "org" "Org tables as a minor mode")
+;;; need to fix foldout 
+(define-key outline-mode-map "\C-c\C-x" nil)
+
+(load-library "remember-prepare")
+;(autoload 'org-remember-annotation "org")
+;(autoload 'org-remember-handler "org")
+(setq org-directory "~/.org/")
+(setq org-default-notes-file 
+(expand-file-name "notes.org"  org-directory))
+(setq remember-annotation-functions '(org-remember-annotation))
+(setq remember-handler-functions '(org-remember-handler))
+
