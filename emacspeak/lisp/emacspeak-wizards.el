a75dfc6cfc6a9f937f8a3d8638d6e7db02f79dde
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2005-09-23 03:37:21 +0000

    *** empty log message ***

--- emacspeak/lisp/emacspeak-wizards.el
+++ emacspeak/lisp/emacspeak-wizards.el
@@ -1769,8 +1769,9 @@ Signals beginning  of buffer."
 (defun emacspeak-wizards-terminal (program)
   "Launch terminal and rename buffer appropriately."
   (interactive (list (read-from-minibuffer "Run program: ")))
+  (switch-to-buffer-other-frame
   (ansi-term program
-             (first (split-string program  )))
+             (first (split-string program  ))))
   (delete-other-windows)
   (emacspeak-auditory-icon 'open-object)
   (emacspeak-speak-mode-line))
