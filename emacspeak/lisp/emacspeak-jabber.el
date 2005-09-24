73985643bddb19af89cdf1dd6c879d55f3e0ab99
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2005-09-24 19:11:15 +0000

    *** empty log message ***

---
+++ emacspeak/lisp/emacspeak-jabber.el
@@ -0,0 +1,67 @@
+;;; emacspeak-jabber.el --- Speech-Enable jabber 
+;;; $Id$
+;;; $Author$
+;;; Description: speech-enable jabber 
+;;; Keywords: Emacspeak, jabber
+;;{{{  LCD Archive entry: 
+
+;;; LCD Archive Entry:
+;;; emacspeak| T. V. Raman |raman@cs.cornell.edu 
+;;; A speech interface to Emacs |
+;;; $Date$ |
+;;;  $Revision$ | 
+;;; Location undetermined
+;;;
+
+;;}}}
+;;{{{  Copyright:
+
+;;; Copyright (c) 1995 -- 2004, T. V. Raman
+;;; All Rights Reserved. 
+;;;
+;;; This file is not part of GNU Emacs, but the same permissions apply.
+;;;
+;;; GNU Emacs is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2, or (at your option)
+;;; any later version.
+;;;
+;;; GNU Emacs is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Emacs; see the file COPYING.  If not, write to
+;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;}}}
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;{{{ Introduction:
+
+;;; Commentary:
+;;; emacs-jabber.el implements a  jabber client for emacs
+;;; emacs-jabber is hosted at sourceforge.
+;;; I use emacs-jabber with my gmail.com account
+
+;;; Code:
+
+;;}}}
+;;{{{  Required modules
+
+(require 'emacspeak-preamble)
+
+;;}}}
+;;{{{ Advice interactive commands:
+
+;;}}}
+(provide 'emacspeak-jabber)
+;;{{{ end of file 
+
+;;; local variables:
+;;; folded-file: t
+;;; byte-compile-dynamic: t
+;;; end: 
+
+;;}}}
