205f9f880d64c113ec1133563f5d32d5385b1306
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2005-11-09 20:36:13 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn@3507 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/emacspeak/servers/dectalk.py
+++ trunk/emacspeak/servers/dectalk.py
@@ -17,7 +17,45 @@ __license__ = "GPL"
 import acss
 
 _defined_voices = {}
+#Will hold mapping from ACSS dimensions to Dectalk settings:
 
+_table ={}
+#family codes:
+
+_table['family'] = {
+    'paul' :  ':np',
+    'harry' :  ':nh',
+    'dennis' :  ':nd',
+    'frank' :  ':nf',
+    'betty' :  ':nb',
+    'ursula' :  ':nu',
+    'wendy' :  ':nw',
+    'rita' :  ':nr',
+    'kid' :  ':nk'
+    }
+
+# average-pitch :
+# Average pitch for standard male voice is 122hz --this is mapped to
+# a setting of 5.
+# Average pitch varies inversely with speaker head size --a child
+# has a small head and a higher pitched voice.
+# We change parameter head-size in conjunction with average pitch to
+# produce a more natural change on the Dectalk.
+
+#paul average pitch
+_table[('paul', 'average-pitch')] ={}
+for (setting, ap, hs)  in [(0, 96, 115),
+             (1, 101, 112),
+             (2, 108, 109),
+             (3, 112, 106),
+             (4, 118, 103, ),
+             (5, 122, 100),
+             (6, 128, 98),
+             (7, 134, 96),
+             (8, 140, 94),
+             (9, 147, 91)]:
+    _table[('paul', 'average-pitch')][setting] = "ap %s hs %s" % (ap, hs)
+             
 def getvoice(acss):
     """Memoized function that returns  synthesizer code for
     specified  ACSS setting.
@@ -27,5 +65,3 @@ def getvoice(acss):
     if name in _defined_voices: return _defined_voices[name]
     _defined_voices[name] =acss2voice(acss)
     return _defined_voices[name]
-    
-    
