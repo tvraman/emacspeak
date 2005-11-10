d8623b1a3d584076655889282633588dbcebca61
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2005-11-10 00:52:15 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@3508 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- emacspeak/servers/dectalk.py
+++ emacspeak/servers/dectalk.py
@@ -44,18 +44,36 @@ _table['family'] = {
 
 #paul average pitch
 _table[('paul', 'average-pitch')] ={}
-for (setting, ap, hs)  in [(0, 96, 115),
-             (1, 101, 112),
-             (2, 108, 109),
-             (3, 112, 106),
-             (4, 118, 103, ),
-             (5, 122, 100),
-             (6, 128, 98),
-             (7, 134, 96),
-             (8, 140, 94),
-             (9, 147, 91)]:
-    _table[('paul', 'average-pitch')][setting] = "ap %s hs %s" % (ap, hs)
-             
+
+for (s, ap, hs)  in [
+    (0, 96, 115),
+    (1, 101, 112),
+    (2, 108, 109),
+    (3, 112, 106),
+    (4, 118, 103, ),
+    (5, 122, 100),
+    (6, 128, 98),
+    (7, 134, 96),
+    (8, 140, 94),
+    (9, 147, 91)]:
+    _table[('paul', 'average-pitch')][s] = "ap %s hs %s" % (ap, hs)
+
+#Harry  has a big head --and a lower pitch for the middle setting
+_table[('harry', 'average-pitch')] ={}
+
+for (s, ap, hs)  in[
+    (0, 50, 125),
+    (1, 59, 123),
+    (2, 68, 121),
+    (3, 77, 120),
+    (4, 83, 118, ),
+    (5, 89, 115),
+    (6, 95, 112),
+    (7, 110, 105),
+    (8, 125, 100),
+    (9, 140, 95),]:
+    _table[('harry', 'average-pitch')][s] = "ap %s hs %s" % (ap, hs)
+
 def getvoice(acss):
     """Memoized function that returns  synthesizer code for
     specified  ACSS setting.
@@ -65,3 +83,26 @@ def getvoice(acss):
     if name in _defined_voices: return _defined_voices[name]
     _defined_voices[name] =acss2voice(acss)
     return _defined_voices[name]
+
+def acss2voice(acss):
+    """Return synthesizer code."""
+    code = ""
+    family ='paul'
+    if 'family'in acss:
+        family = acss['family']
+        code += _table[family]
+    if 'rate' in acss: code += " :ra %s" % (180 +4 * acss['rate'])
+    voice = ""
+    dv = ""
+    for d in ['average-pitch', 'pitch-range',
+              'richness', 'stress']:
+        if d in acss:voice += _table[(family, d)][acss[d]]
+    if voice: dv = " :dv %s" % voice
+    if code or voice: code = "[%s  %s]" % (code, dv)
+    return (code, "")
+
+    
+
+    
+    
+    
