18e0849eacd1ad68f95e20672fe38d8e251f628a
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2005-12-10 16:54:10 +0000

    *** empty log message ***

--- emacspeak/lisp/emacspeak.el
+++ emacspeak/lisp/emacspeak.el
@@ -442,6 +442,8 @@ functions for details.   "
     (setq default-enable-multibyte-characters nil))
   (emacspeak-export-environment)
   (require 'emacspeak-aumix)
+  (when (featurep 'ido)
+    (require 'emacspeak-ido))
   (require 'emacspeak-sounds)
   (dtk-initialize)
   (require 'emacspeak-personality)
