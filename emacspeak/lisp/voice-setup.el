959c8ca8da9994f09b06fe8dd618f25a5bd79f9c
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2006-02-14 14:14:15 +0000

    *** empty log message ***

--- emacspeak/lisp/voice-setup.el
+++ emacspeak/lisp/voice-setup.el
@@ -280,6 +280,13 @@ Optional arg GLOBAL means to replace all matches instead of only the first."
                    (symbol-name face))))))
     (eval
      `(def-voice-font ,personality ,voice  ',face  ,doc))))
+
+(defun voice-setup-add-map (fv-alist)
+  "Sets up face to voice mapping given in fv-alist."
+  (loop for fv in fv-alist
+        do
+        (voice-setup-map-face (first fv) (second fv))))
+
 ;;}}}
 ;;{{{  special form defvoice 
 
