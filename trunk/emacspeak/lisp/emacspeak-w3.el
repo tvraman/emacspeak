b4aab83a714cefabe577b613ef59276012024e90
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2006-04-19 23:40:19 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn@3872 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/emacspeak/lisp/emacspeak-w3.el
+++ trunk/emacspeak/lisp/emacspeak-w3.el
@@ -1606,7 +1606,7 @@ Note that this hook gets reset after it is used by W3 --and this is intentional.
 
 (defadvice w3-notify-when-ready (after emacspeak pre act comp)
   "Call w3 post-processor hook if set."
-  (when    emacspeak-w3-post-process-hook
+  (when (functionp emacspeak-w3-post-process-hook)
     (unwind-protect
         (run-hooks  'emacspeak-w3-post-process-hook)
       (setq emacspeak-w3-post-process-hook nil))))
