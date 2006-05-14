2324a42bdd45decc5192e7ec9bc766b8c8286cde
raman <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2006-05-14 00:17:52 +0000

    *** empty log message ***
    git-svn-id: https://emacspeak.googlecode.com/svn@3910 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/emacspeak/lisp/emacspeak-etable.el
+++ trunk/emacspeak/lisp/emacspeak-etable.el
@@ -49,14 +49,130 @@
 
 ;;}}}
 ;;{{{ Update command remap list.
+
 (eval-when '(load)
- (push (cons 'emacspeak-self-insert-command '*table--cell-self-insert-command) table-command-remap-alist))
- 
+ (push
+  (cons 'emacspeak-self-insert-command '*table--cell-self-insert-command)
+       table-command-remap-alist)
+ (loop for pair in
+       (list 
+         (cons [backtab] 'table-backward-cell)
+         (cons "\C-e ." 'emacspeak-etable-speak-cell))
+       do
+ (push pair table-cell-bindings))
+
+;;}}}
+;;{{{ Advice edit commands
+
+(defadvice *table--cell-delete-char (around emacspeak pre act)
+  "Speak character you're deleting."
+  (cond
+   ((interactive-p )
+    (dtk-tone 500 30 'force)
+    (and emacspeak-delete-char-speak-deleted-char
+         (emacspeak-speak-char t))
+    ad-do-it)
+   (t ad-do-it))
+  ad-return-value)
+
+(defadvice *table--cell-delete-backward-char (around emacspeak pre act)
+  "Speak character you're deleting."
+  (cond
+   ((interactive-p )
+    (dtk-tone 500 30 'force)
+    (emacspeak-speak-this-char (preceding-char ))
+    ad-do-it)
+   (t ad-do-it))
+  ad-return-value)
+
+(defadvice *table--cell-self-insert-command (after emacspeak pre act comp)
+  "Provide spoken output."
+  (when  (interactive-p)
+    (cond
+     ((and (= 32 last-input-char)
+           emacspeak-word-echo)
+      (save-excursion
+        (let ((orig (point)))
+          (table--finish-delayed-tasks)
+          (backward-word 1)
+          (emacspeak-speak-region orig (point)))))
+     (emacspeak-character-echo
+      (when dtk-stop-immediately-while-typing (dtk-stop))
+      (emacspeak-speak-this-char last-input-char )))))
+(defadvice *table--cell-quoted-insert  (after emacspeak pre act )
+  "Speak the character that was inserted."
+  (when (interactive-p)
+    (table--finish-delayed-tasks)
+    (emacspeak-speak-this-char (preceding-char ))))
+
+(defadvice *table--cell-newline (before emacspeak pre act)
+  "Speak the previous line if line echo is on.
+See command \\[emacspeak-toggle-line-echo].  Otherwise cue the user to
+the newly created blank line."
+  (declare (special emacspeak-line-echo ))
+  (when (interactive-p)
+    (table--finish-delayed-tasks)
+    (cond
+     (emacspeak-line-echo (emacspeak-speak-line ))
+     (t(if dtk-stop-immediately (dtk-stop))
+       (dtk-tone 225 120 'force   )))))
+
+(defadvice *table--cell-newline-and-indent (around emacspeak pre act)
+  "Speak the previous line if line echo is on.
+See command \\[emacspeak-toggle-line-echo].
+Otherwise cue user to the line just created."
+  (declare (special emacspeak-line-echo ))
+  (cond
+   ((interactive-p)
+    (cond
+     (emacspeak-line-echo
+      (emacspeak-speak-line )
+      ad-do-it)
+     (t ad-do-it
+        (dtk-speak-using-voice voice-annotate
+                               (format
+                                "indent %s"
+                                (current-column)))
+        (dtk-force))))
+   (t ad-do-it))
+  ad-return-value)
+
+(defadvice *table--cell-open-line (after emacspeak pre act )
+  "Provide auditory feedback."
+  (when (interactive-p)
+    (let ((count (ad-get-arg 0)))
+      (emacspeak-auditory-icon 'open-object)
+      (message "Opened %s blank line%s"
+               (if (= count 1) "a" count)
+               (if (= count 1 ) "" "s")))))
 
 ;;}}}
 
+;;{{{ speak cell contents:
 
+(defun emacspeak-etable-speak-cell ()
+  "Speak current cell."
+  (interactive)
+  (let ((cell (table--probe-cell 'no-error)))
+    (cond
+     (cell
+      (emacspeak-speak-rectangle
+        (car cell)
+        (cdr cell)))
+     (t (error "Cant identify cell.")))))
 
+(loop for f in
+      '(table-forward-cell table-backward-cell)
+      do
+      (eval
+       `(defadvice ,f (after emacspeak pre act comp)
+          "Provide auditory feedback by speaking current cell
+      contents."
+          (when (interactive-p)
+            (table--finish-delayed-tasks)
+            (emacspeak-etable-speak-cell)))))
+
+;;}}}
 (provide  'emacspeak-etable)
 ;;{{{  emacs local variables 
 
