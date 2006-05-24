2b6da8c1ba290fbab8050c0fe3f33cbfc7246e8f
raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b <raman@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2006-05-24 04:06:01 +0000

    *** empty log message ***

---
+++ emacspeak/lisp/emacspeak-webutils.el
@@ -0,0 +1,64 @@
+;;; emacspeak-webutils.el --- Common Web Utilities For Emacspeak
+;;; $Id$
+;;; $Author$
+;;; Description:  Emacspeak Webutils
+;;; Keywords: Emacspeak, web 
+;;{{{  LCD Archive entry:
+
+;;; LCD Archive Entry:
+;;; emacspeak| T. V. Raman |raman@cs.cornell.edu
+;;; A speech interface to Emacs |
+;;; $Date$ |
+;;;  $Revision$ |
+;;; Location undetermined
+;;;
+
+;;}}}
+;;{{{  Copyright:
+
+;;; Copyright (C) 1999 T. V. Raman <raman@cs.cornell.edu>
+;;; All Rights Reserved.
+;;;
+;;; This file is not part of GNU Emacs, but the same permissions apply.
+;;;
+;;; GNU Emacs is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2, or (at your option)
+;;; any later version.
+;;;
+;;; GNU Emacs is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Emacs; see the file COPYING.  If not, write to
+;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;}}}
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Commentary:
+;;{{{  Introduction:
+
+;;; This module provides common Web utilities for emacspeak.
+;;; This is to avoid duplication of code between emacspeak-w3.el
+;;;and emacspeak-w3m.el
+
+;;}}}
+;;{{{ required modules
+
+;;; Code:
+(require 'emacspeak-preamble)
+
+;;}}}
+
+(provide 'emacspeak-webutils)
+;;{{{ end of file
+
+;;; local variables:
+;;; folded-file: t
+;;; byte-compile-dynamic: t
+;;; end:
+
+;;}}}
