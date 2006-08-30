86c841faca977d4f30f8eed43e47effe471475c7
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2006-08-30 00:44:57 +0000

    Updated Copyright year
    git-svn-id: https://emacspeak.googlecode.com/svn@4151 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- trunk/utils/update-copyright.pl
+++ trunk/utils/update-copyright.pl
@@ -3,8 +3,8 @@
 #Update Copyright notice
 #
 
-my $old = "1995 -- 2003, T. V. Raman";
-my $new = "1995 -- 2004, T. V. Raman";
+my $old = "1995 -- 2004, T. V. Raman";
+my $new = "1995 -- 2006, T. V. Raman";
 
 while (<>) {
     s/$old/$new/o;
