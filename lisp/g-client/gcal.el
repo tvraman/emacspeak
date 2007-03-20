a756c94cc0d972441c07e3cf42aab4dc6cbc5b52
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-03-20 20:52:21 +0000

    added feed of feeds command to gcal.

--- lisp/g-client/gcal.el
+++ lisp/g-client/gcal.el
@@ -699,6 +699,33 @@ date under point."
    gcal-event-view))
 
 ;;}}}
+;;{{{ Feed of feeds:
+(defvar gcal-feeds-template-url
+  "http://www.google.com/calendar/feeds/%s"
+  "URL template for feed of feeds from calendar.")
+(defsubst gcal-feeds-url (userid)
+  "Return url for feed of feeds."
+  (declare (special gcal-feeds-template-url))
+  (format gcal-feeds-template-url userid))
+
+(defun gcal-calendars ()
+  "Retrieve and display feed of feeds after authenticating."
+  (interactive)
+  (declare (special gcal-auth-handle
+                    g-atom-view-xsl
+                    g-curl-program g-curl-common-options
+                    g-cookie-options))
+  (g-auth-ensure-token gcal-auth-handle)
+  (g-display-result
+   (format
+    "%s --location --header 'Authorization: GoogleLogin auth=%s' '%s' 2>/dev/null"
+    g-curl-program
+    (g-cookie "Auth" gcal-auth-handle)
+    (gcal-feeds-url
+     (g-url-encode (g-auth-email gcal-auth-handle))))
+   g-atom-view-xsl))
+
+;;}}}
 ;;{{{ Interfacing with Emacs Calendar:
 ;;;###autoload
 (define-prefix-command  'gcal-calendar-prefix-map)
