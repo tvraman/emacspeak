e1e9cc475d0fe3c73ec67842630dac5fdfbb74cc
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-04-08 21:46:35 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@4447 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- tvr/mspools-prepare.el
+++ tvr/mspools-prepare.el
@@ -1,22 +1,22 @@
+(defsubst mspools-compute-size (file)
+  "Count number of messages."
+  (cond
+   ((= 0 (nth  7 (file-attributes file))) 0)
+   (t (let ((buffer (get-buffer-create " *mspools-scratch*")))
+        (save-excursion
+          (set-buffer buffer)
+          (erase-buffer)
+          (shell-command (format "grep '^From ' %s | wc -l" file) buffer)
+          (read (buffer-string)))))))
 
-(defun mspools-compute-size (file)
-  (let ((buffer (get-buffer-create " *mspools-scratch*")))
-    (save-excursion
-      (set-buffer buffer)
-      (erase-buffer)
-      (shell-command (format "grep '^From ' %s" file) buffer)
-      (if (eq (current-buffer) buffer)
-          (count-lines (point-min) (point-max))  
-        0))))
-
-(defun mspools-size-folder (spool)
+(defsubst mspools-size-folder (spool)
   "Return (SPOOL . SIZE ) iff SIZE of spool file is non-zero."
-  (let* ((file (concat mspools-folder-directory spool))
+  (let* ((file (expand-file-name spool mspools-folder-directory))
          (size (mspools-compute-size file )))
     (setq file (or (file-symlink-p file) file))
                                         ;size could be nil if the sym-link points to a non-existent file
                                         ;so check this first.
     (if (and size  (> size 0))
- 	(cons spool  size)
+	(cons spool  size)
                                         ;else SPOOL is empty
       nil)))
