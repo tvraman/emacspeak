cbae9964bb47055ab44bcdca3a8187ea957a7cbc
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-05-02 13:54:45 +0000

    updated version number
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@4512 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-setup.el
+++ lisp/emacspeak-setup.el
@@ -100,7 +100,7 @@ pronunciation dictionaries are stored. ")
 ;;;###autoload
 (defconst emacspeak-version
   (format
-   "25.0 %s"
+   "26.0 %s"
    (cond
     ((file-exists-p emacspeak-readme-file)
      (let ((buffer (find-file-noselect emacspeak-readme-file))
