b459b4f8daad923ad8ab99fc0d929beb9bbcf765
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-05-02 00:13:58 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@4509 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- etc/ocropus.pl
+++ etc/ocropus.pl
@@ -10,7 +10,7 @@ my $output = "/tmp/ocr-output-$$.txt";
 my $image =shift;
 die "No image specified" unless defined ($image);
 qx($OCR  ocr $image > $output 2>/dev/null);
-qx(lynx -dump $output 2>/dev/null | OUTPUT);
+open(OUTPUT, "lynx -dump $output 2>/dev/null"  );
 while (<OUTPUT>) {
   print;
 }
