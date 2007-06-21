05d25d6920a0afa22d4a7b9b2def687fb2a233cb
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-06-21 21:35:56 +0000

    added dom inspector helpers
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@4653 bb5eec90-091a-0410-bc89-db79c2cf2c7b

---
+++ js/di.js
@@ -0,0 +1,24 @@
+#$Id:$
+# Description: DomInspector helpers
+
+repl.showAttributes = function(node) {
+  var map = node.attributes;
+  if (map  instanceof NamedNodeMap) {
+    for (i = 0; i < map.length; i++) {
+      repl.print (map[i].name +' = ' + map[i].value, false);
+      if (map[i].namespaceURI) {
+        repl.print("\tNamespace: " + map[i].namespaceURI);
+      } else {
+        repl.print("");
+      }
+    }
+  } else {
+    repl.print (node + " has no attributes.");
+  }
+}
+  
+repl.showErrors = function (m, u, l) {
+    repl.print("Message: " +m
+               + "URL: " +u
+               +"Line: " + l);
+  }
