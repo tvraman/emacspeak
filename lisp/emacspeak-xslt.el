2b7262da622ae73a627c184b23b0dbeb43dfd891
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-07-20 14:10:10 +0000

    fixed readers.

--- lisp/emacspeak-xslt.el
+++ lisp/emacspeak-xslt.el
@@ -302,7 +302,7 @@ part of the libxslt package."
   (interactive
    (list
     (emacspeak-xslt-read)
-    (read-string "URL: " (browse-url-url-at-point))
+    (emacspeak-we-read-this-url)
     current-prefix-arg))
   (let ((src-buffer
          (emacspeak-xslt-xml-url
