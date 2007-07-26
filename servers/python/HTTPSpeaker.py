c8d9fe520a01dc619d6d6b56495c5a44b3a8d2bb
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-07-26 22:44:24 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@4940 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- servers/python/HTTPSpeaker.py
+++ servers/python/HTTPSpeaker.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python2.4
+
 """HTTP wrapper around Emacspeak speech server.
 
 Speech server is launched on HTTP server startup.
@@ -21,9 +23,10 @@ __license__ = "LGPL"
 from speaker import Speaker
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 import sys
+import urllib
 
 class HTTPSpeaker (HTTPServer):
-    
+
     """Speech server via HTTP."""
 
     def __init__(self, address, handler,
@@ -50,12 +53,12 @@ class SpeakHTTPRequestHandler(BaseHTTPRequestHandler):
                 'reset',
                 'shutdown',
                 'version'             ]
-    
+
     def do_GET(self):
         """Produce speech."""
         cmd = None
         arg = None
-        
+
         # we either get a cmd or a cmd?arg
         q=self.path.find('?')
         if q == -1:
@@ -63,21 +66,21 @@ class SpeakHTTPRequestHandler(BaseHTTPRequestHandler):
         else:
             cmd = self.path[1:q]
             arg = self.path[q+1:].replace('+', ' ')
-        
+
         if cmd not in self.handlers:
             sys.stderr.write("cmd %s handlers %s" % (cmd, self.handlers))
             self.send_error(501, "unknown method %s" % cmd)
             return
-        
+
         if hasattr(self.server.speaker, cmd):
             method = getattr(self.server.speaker, cmd)
             if arg is None:
                 method()
             else:
-                method(arg)
+                method(urllib.unquote(arg))
             self.send_response(200, self.path)
         else: self.send_error(501, "Speaker error")
-    
+
 def start():
     if sys.argv[1:]:
         port = int(sys.argv[1])
@@ -86,8 +89,7 @@ def start():
     server_address = ('', port)
     httpd = HTTPSpeaker  (server_address,  SpeakHTTPRequestHandler)
     httpd.serve_forever()
-    
+
 
 if __name__ == '__main__':
     start()
-
