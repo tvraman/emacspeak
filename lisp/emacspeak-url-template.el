c6a8c24ee8b07dcad17b990a3febf105a5a3902e
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-07-26 00:38:23 +0000

    refactored
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@4931 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-url-template.el
+++ lisp/emacspeak-url-template.el
@@ -575,7 +575,7 @@ content."
  "http://www.google.com/views?q=%s+view:timeline&num=25"
  (list "Google For: ")
  #'(lambda nil
-     (emacspeak-websearch-post-process "Timeline View"
+     (emacspeak-webutils-post-process "Timeline View"
                                        'emacspeak-speak-rest-of-buffer))
  "Do a Google search and get a timeline view of results.")
  
