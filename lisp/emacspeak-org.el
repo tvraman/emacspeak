d51477f6c8d136509ecacddebf7e6143fdbece16
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-08-05 18:19:52 +0000

    More legacy code cleanups.
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@5070 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-org.el
+++ lisp/emacspeak-org.el
@@ -261,8 +261,7 @@
   (cond
    ((interactive-p )
     (dtk-tone 500 30 'force)
-    (and emacspeak-delete-char-speak-deleted-char
-         (emacspeak-speak-char t))
+    (emacspeak-speak-char t)
     ad-do-it)
    (t ad-do-it))
   ad-return-value)
