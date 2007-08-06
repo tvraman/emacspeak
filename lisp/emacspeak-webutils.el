5eaa02c3532dd2e7af51cb231dc378205fd9db55
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-08-06 18:38:18 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@5086 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-webutils.el
+++ lisp/emacspeak-webutils.el
@@ -226,7 +226,9 @@ ARGS specifies additional arguments to SPEAKER if any."
 
 (defsubst emacspeak-webutils-google-autocomplete (prompt)
   "Read user input using Google Suggest for auto-completion."
-  (let ((minibuffer-completing-file-name t)) ;; so we can type spaces
+  (let ((minibuffer-completing-file-name t) ;; so we can type
+        ;; spaces
+        (completion-ignore-case t))
   (completing-read prompt
      (dynamic-completion-table emacspeak-webutils-google-suggest))))
 
