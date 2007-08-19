afd0e42f83d0d7ea0f472c93a078f7248c64dfc4
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-08-19 00:10:46 +0000

    Added x.el for defining hyper super and alt consistent with emacspeak on the console.

---
+++ tvr/console-keymaps/x.el
@@ -0,0 +1,4 @@
+(define-key function-key-map [67108923] "\C-x@H")
+(define-key function-key-map [67108903] "\C-x@S")
+(define-key function-key-map [67108903] "\C-x@S")
+(define-key function-key-map [67108903] "\C-x@A")
