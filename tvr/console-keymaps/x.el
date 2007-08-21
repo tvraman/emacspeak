71748c02b3715b8ab8dda689ed219c8eb18b441f
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-08-21 23:55:15 +0000

    fixed org mode map.
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@5191 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- tvr/console-keymaps/x.el
+++ tvr/console-keymaps/x.el
@@ -2,3 +2,5 @@
 (define-key function-key-map [67108903] "\C-x@S")
 (define-key function-key-map [67108903] "\C-x@S")
 (define-key function-key-map [67108903] "\C-x@A")
+;;; Try
+(define-key function-key-map [(control \;)] "\C-x@H")
