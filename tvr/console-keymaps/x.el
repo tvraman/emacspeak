1147441904adb0f41736073a9477ee759bf6b303
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-08-22 03:03:41 +0000

    cleaned up x.el
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@5194 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- tvr/console-keymaps/x.el
+++ tvr/console-keymaps/x.el
@@ -1,6 +1,5 @@
-(define-key function-key-map [67108923] "\C-x@H")
-(define-key function-key-map [67108903] "\C-x@S")
-(define-key function-key-map [67108903] "\C-x@S")
-(define-key function-key-map [67108903] "\C-x@A")
-;;; Try
+(define-key function-key-map [(control .)] "\C-x@S")
+(define-key function-key-map [(control \')] "\C-x@S")
+(define-key function-key-map [(control ,)] "\C-x@A")
+
 (define-key function-key-map [(control \;)] "\C-x@H")
