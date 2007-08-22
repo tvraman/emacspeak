3cbcb6073aba40ad30b1b01debf3c2ce7fc131e4
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-08-22 07:40:51 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@5195 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- tvr/console-keymaps/x.el
+++ tvr/console-keymaps/x.el
@@ -1,5 +1,4 @@
 (define-key function-key-map [(control .)] "\C-x@S")
 (define-key function-key-map [(control \')] "\C-x@S")
 (define-key function-key-map [(control ,)] "\C-x@A")
-
 (define-key function-key-map [(control \;)] "\C-x@H")
