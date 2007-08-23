4b09ff78c648cba47b347e4f29bed4c4778a3aa1
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-08-23 22:29:51 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@5201 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- tvr/analog-prepare.el
+++ tvr/analog-prepare.el
@@ -1,11 +1,12 @@
-(load-library "analog")
+
+(autoload 'analog "analog" "View logs" t)
 (setq analog-entry-list
       '(
         ;; files in the WWW group
         ("/etc/httpd/logs/access_log"
          (group . "WWW")
          (lines . 10)
-         (fields . (33 66)))                  ; show 10 lines
+         (fields . (33 66)))            ; show 10 lines
         ("/etc/httpd/logs/error_log"
          (fields . (26 56))
          (group . "WWW")
@@ -15,7 +16,7 @@
          (fields . (16 41))
          (group . "Mail")
          (hide
-          . ("queue" "completed")))     ; hide lines matching queue/completed
+          . ("queue" "completed"))) ; hide lines matching queue/completed
         ;; commands in the Commands group
         ("df -h"
          (group . "Commands")
@@ -24,21 +25,22 @@
         ("free"
          (group . "Commands")
          (type . command)
-         (position . head)              ; monitor the head of the output
+         (position . head)            ; monitor the head of the output
          (lines . 6))
         ("last"
          (group . "Commands")
          (type . command)
-         (position . head)              ; monitor the head of the output
+         (position . head)            ; monitor the head of the output
          (lines . 6))
         ("ps aux"
          (group . "Commands")
          (type . command)
-         (keep . "tvraman")                ; keep lines matching matt
-         (hide . ("bash" "ssh" )) ; hide/keep can be lists
+         (keep . "tvraman")             ; keep lines matching matt
+         (hide . ("bash" "ssh" ))       ; hide/keep can be lists
          (lines . all))
         ;; files in the System group
         ("/var/log/messages"
          (group . "System")
          (hide . ("CRON" ))
          (fields . (16 24 32)))))
+
--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -18,16 +18,17 @@
 Path is resolved relative to `whence' which defaults to emacs-personal-library."
   (interactive "Denter directory name: ")
   (declare (special emacs-personal-library))
-  (unless (and library
-	       (locate-library library))
-    (add-to-list 'load-path
-		 (expand-file-name path
-				   (or whence
-				       (and (boundp
-					     'emacs-personal-library)
-					    emacs-personal-library)))
-		 at-end))
+  (unless (and library (locate-library library))
+    (add-to-list
+     'load-path
+     (expand-file-name
+      path
+      (or
+       whence
+       (and (boundp 'emacs-personal-library) emacs-personal-library)))
+     at-end))
   (when library (locate-library library)))
+
 (defsubst augment-auto-mode-alist (ext mode)
   "Add to auto-mode-alist."
   (declare (special auto-mode-alist))
