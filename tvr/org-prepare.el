338602d499e1ea8f61e55db80606814eceaa54f3
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-08-28 18:51:15 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@5231 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- tvr/org-prepare.el
+++ tvr/org-prepare.el
@@ -1,11 +1,10 @@
 ;;;$Id$
-(augment-load-path "org" "org")
+;;; See http://johnwiegley.com/org.mode.day.planner.html
 
 (load-library "org")
-
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-directory "~/.org/")
-(setq org-default-notes-file 
+(setq org-default-notes-file
       (expand-file-name "notes.org"  org-directory))
