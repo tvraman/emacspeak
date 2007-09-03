1422b74f6ac499823b805c79526ec708ff6ace5a
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2007-09-03 00:35:10 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@5247 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- tvr/org-prepare.el
+++ tvr/org-prepare.el
@@ -8,3 +8,19 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-directory "~/.org/")
 (setq org-default-notes-file (expand-file-name "notes.org"  org-directory))
+(define-prefix-command 'org-todo-state-map)
+
+     (define-key org-mode-map "\C-cx" 'org-todo-state-map)
+
+     (define-key org-todo-state-map "x"
+       #'(lambda nil (interactive) (org-todo "CANCELLED")))
+     (define-key org-todo-state-map "d"
+       #'(lambda nil (interactive) (org-todo "DONE")))
+     (define-key org-todo-state-map "f"
+       #'(lambda nil (interactive) (org-todo "DEFERRED")))
+     (define-key org-todo-state-map "l"
+       #'(lambda nil (interactive) (org-todo "DELEGATED")))
+     (define-key org-todo-state-map "s"
+       #'(lambda nil (interactive) (org-todo "STARTED")))
+     (define-key org-todo-state-map "w"
+       #'(lambda nil (interactive) (org-todo "WAITING")))
