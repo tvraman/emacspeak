caaf5d68c8517b64d44c52061dba223c3b82a15b
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2008-08-17 23:47:47 +0000

    use lynx to dump results for content
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@5761 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/g-client/gsearch.el
+++ lisp/g-client/gsearch.el
@@ -189,8 +189,11 @@ Optional interactive prefix arg refresh forces this cached URL to be refreshed."
       (pushnew (g-json-get "url" lucky) minibuffer-history)
       (set-buffer-modified-p modified-p)
       (message "%s %s"
-               (g-json-get "titleNoFormatting" lucky)
-               (g-json-get "content" lucky))))))
+	       (g-json-get "titleNoFormatting" lucky)
+	       (shell-command-to-string
+		(format
+		 "echo '%s' | lynx -dump -stdin 2>/dev/null"
+		 (g-json-get "content" lucky))))))))
 
 ;;}}}
 
