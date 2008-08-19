25b7940bdedc28d33bb59e40adf60a984615d563
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2008-08-19 03:31:59 +0000

    *** empty log message ***

--- lisp/g-client/gsearch.el
+++ lisp/g-client/gsearch.el
@@ -153,10 +153,10 @@
   (let* ((minibuffer-completing-file-name t) ;; accept spaces
          (completion-ignore-case t)
          (word (thing-at-point 'word))
-         (suggestions (when (and word
-                                 (> (length word) 0))
-                        (cons  word 
-                               (gsearch-suggest  word))))
+         (suggestions
+	  (when (and word (> (length word) 0))
+	    (set-text-properties 0 (length word) nil word)
+	    (cons  word (gsearch-suggest  word))))
          (query nil))
     (setq query
           (completing-read
