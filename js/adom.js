a009b2d00cb933f4566cdc0dbc7ffb3ef97a46c9
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2009-05-20 14:04:54 +0000

    integrated bug fixes from Sean for moz interaction
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@6158 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- js/adom.js
+++ js/adom.js
@@ -484,7 +484,7 @@ handleURLBarCommand();
  * Update adom pointer in repl to point to current document.
  * @return {ADom}
  */
-repl.prototype.updateADom = function ()  {
+repl.updateADom = function ()  {
     if (content.document.adom == undefined) {
         // constructor caches adom in content.document
         repl.adom = new ADom(content.document);
