5ade1663d070429d07e07e9a6a6a2a9f4d06c71c
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2009-09-30 21:35:28 +0000

    tts-lib.tcl --- added option -N to aplay. need to eventually stop supporting /usr/bin/play from sox

--- servers/tts-lib.tcl
+++ servers/tts-lib.tcl
@@ -183,7 +183,7 @@ proc queue_restore {} {
 #play a sound over the server
 proc p {sound} {
     global tts
-    catch "exec $tts(play) $sound 1>&- 2>&- & " errcode
+    catch "exec $tts(play) -N $sound 1>&- 2>&- & " errcode
     speech_task
 }
 
