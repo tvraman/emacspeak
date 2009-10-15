5351cb637b584267af6f5393b007400d248e414b
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2009-10-15 15:45:19 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@6320 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- webhead/java/src/net/emacspeak/web/Client.java
+++ webhead/java/src/net/emacspeak/web/Client.java
@@ -1,6 +1,7 @@
 package net.emacspeak.web;
 //< Imports:
 
+
 import java.io.IOException;
 import java.io.StringWriter;
 
@@ -14,29 +15,37 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class Client {
 
-// < main:
+    //< main:
 
     public static void main(String args[]) 
-    throws Exception {
+        throws Exception {
         final WebClient webClient = new WebClient();
         final HtmlPage page = webClient.getPage("file:./tests/00-test.html");
         writeContent(page);
         writeXml(page);
     }
 
+    //>
+    //< writeContent
 
     public static void writeContent (HtmlPage page) {
         System.out.println( page.asText());
     }
 
+    //>
+    //< writeXml
+
     public static void writeXml (HtmlPage page) {
         HtmlElement body = page.getFirstByXPath("/html");
         System.out.println(body.asXml());
     }
-// >
-
-    
+    //>
         
-               
-    
-}
+} // class Client
+
+//<End Of File:
+
+// local variables:
+// folded-file: t
+// end:
+//>
