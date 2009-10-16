edef56d6e6daa86b036aa13596cf857c8ea4d319
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2009-10-16 21:43:12 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@6326 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- webhead/java/src/net/emacspeak/web/Client.java
+++ webhead/java/src/net/emacspeak/web/Client.java
@@ -11,20 +11,45 @@ import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 //>
+/**
+ * @file   Client.java
+ * @author T.V Raman <rtv.raman.tv@gmail.com>
+ * @date   Fri Oct 9 14:30:21 2009
+ * 
+ * @brief  Implements a headless web client.
+ * class Client implements an interactive command-loop that:
+ *  Accepts commands on standard-input,
+ * Returns results on standard-output.
+ */
 
 public class Client {
 
-    //< main:
 
+    /** 
+     * Constructor: Initialize WebClient
+     * 
+     * 
+     * @return  Newly constructed Client
+     */
+
+    public Client () {
+        _client = new WebClient();
+    }
+
+    //< main:
+    /** 
+     *  
+     * 
+     * @param args String Args[] (not used)
+     */
     public static void main(String args[]) 
         throws Exception {
-        final WebClient webClient = new WebClient();
-        final HtmlPage page = webClient.getPage("file:./src/test/resources/00-test.html");
+        Client c = new Client();
+        final HtmlPage page = c._client.getPage("file:./src/test/resources/00-test.html"); 
         writeContent(page);
         writeXml(page);
     }
-
-    //>
+//>
     //< writeContent
 
     public static void writeContent (HtmlPage page) {
@@ -39,7 +64,11 @@ public class Client {
         System.out.println(body.asXml());
     }
     //>
-        
+
+//<Class members
+
+    private final WebClient _client;
+//>
 } // class Client
 
 //<End Of File:
