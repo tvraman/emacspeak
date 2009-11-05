66a50dffe1da3aede66e66cffe49b009756e57f4
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2009-11-05 01:04:31 +0000

    content.scala --- more concise

--- webhead/scala/content.scala
+++ webhead/scala/content.scala
@@ -1,16 +1,15 @@
 import  com.gargoylesoftware.htmlunit._
+import  com.gargoylesoftware.htmlunit.html._
 
 object Content {
-  val w = new com.gargoylesoftware.htmlunit.WebClient()
+  val w = new WebClient()
   w.setThrowExceptionOnScriptError(false);
+
   def content (url : String) {
-    val p:com.gargoylesoftware.htmlunit.html.HtmlPage  =
-      this.w.getPage(url)
+    val p:HtmlPage  = this.w.getPage(url)
     println(url)
     println(p.asText())
   }
 
-  def main(args: Array[String]) = {
-    args.map(content(_))
-  }
+  def main(args: Array[String]) = args.map(content(_))
 }
