31e9857cb583601c92fc03483f367c7727c91fec
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2010-04-14 14:24:28 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@6471 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-url-template.el
+++ lisp/emacspeak-url-template.el
@@ -411,7 +411,9 @@ dont-url-encode if true then url arguments are not url-encoded "
   "Take a cid particle, and invokes mplayer."
   (declare (special emacspeak-url-template-iplayer-convertor))
   (let ((handle (format emacspeak-url-template-iplayer-convertor (substring  cid 4))))
-    (emacspeak-m-player handle 'playlist)))
+    (kill-new handle)
+    (emacspeak-m-player handle 'playlist)
+    (message handle)))
 
 (emacspeak-url-template-define
  "BBC  iPlayer"
