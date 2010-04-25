55b6cba7d97417cb9dbb3ed4355ba002cb487218
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2010-04-25 03:35:25 +0000

    emacs 24 additions.
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@6480 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-advice.el
+++ lisp/emacspeak-advice.el
@@ -256,6 +256,17 @@ If you moved more than a line,
   "Speak the screenful."
   (when (interactive-p)
     (emacspeak-auditory-icon 'scroll)
+    (dtk-speak (emacspeak-get-window-contents))))e
+(defadvice scroll-up-command (after emacspeak pre act comp)
+  "Speak the next screenful."
+  (when (interactive-p)
+    (emacspeak-auditory-icon 'scroll)
+    (dtk-speak (emacspeak-get-window-contents))))
+
+(defadvice scroll-down-command (after emacspeak pre act comp)
+  "Speak the screenful."
+  (when (interactive-p)
+    (emacspeak-auditory-icon 'scroll)
     (dtk-speak (emacspeak-get-window-contents))))
 
 (defadvice  beginning-of-defun (after emacspeak pre act)
