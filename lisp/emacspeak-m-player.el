49a382159730fddb396ce559f98cc922cc1de275
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2010-06-01 03:38:38 +0000

    mplayer: info displays file name as well.

--- lisp/emacspeak-m-player.el
+++ lisp/emacspeak-m-player.el
@@ -454,7 +454,7 @@ A string of the form `<number> 1' sets volume as an absolute."
   "Display current position in track and its length."
   (interactive)
   (emacspeak-m-player-dispatch
-   "get_time_pos\nget_percent_pos\nget_time_length\n")
+   "get_time_pos\nget_percent_pos\nget_time_length\nget_file_name\n")
   (when (interactive-p)
     (emacspeak-auditory-icon 'select-object)))
 
