0c63c880b7093208b4ce3b7af16889c931555c48
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2010-08-16 02:47:30 +0000

    advice delete-forward-char
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@6563 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-advice.el
+++ lisp/emacspeak-advice.el
@@ -386,7 +386,15 @@ the words that were capitalized."
 (defadvice ucs-insert (after emacspeak pre act comp)
   "Speak char we inserted."
   (emacspeak-speak-char-name (ad-get-arg 0)))
-
+(defadvice delete-forward-char (around emacspeak pre act)
+  "Speak character you're deleting."
+  (cond
+   ((interactive-p )
+    (dtk-tone 500 30 'force)
+    (emacspeak-speak-char t)
+    ad-do-it)
+   (t ad-do-it))
+  ad-return-value)
 (defadvice delete-char (around emacspeak pre act)
   "Speak character you're deleting."
   (cond
