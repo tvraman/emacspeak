af6aaa31dfe1ace303dab3255608de69e2bdae1b
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2010-12-28 23:01:32 +0000

    forward decls in dtk-interp

--- lisp/amixer.el
+++ lisp/amixer.el
@@ -49,6 +49,9 @@
 (defstruct amixer-control
   numid iface name setting)
 
+(declare-function amixer-control-name  "amixer.el" (amixer))
+(declare-function amixer-control-numid  "amixer.el" (amixer))
+(declare-function amixer-control-iface  "amixer.el" (amixer))
 (defstruct amixer-control-setting
   type access values
   min max step
