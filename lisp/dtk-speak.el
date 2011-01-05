67043d848c4683c6219aeba61d8308621f4574e4
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2011-01-05 16:44:31 +0000

    added interactive command to  easily start async servers

--- lisp/dtk-speak.el
+++ lisp/dtk-speak.el
@@ -1596,6 +1596,39 @@ When called  interactively, The selected server is started immediately. "
     (when (interactive-p)
       (dtk-initialize))))
 
+(defvar dtk-async-server-process nil
+  "Process handle to async server.")
+
+
+;;;###autoload
+(defun dtk-async-server (program)
+  "Select and start an async  speech server interactively.
+Async servers allow external programs like self-voicing Web browsers to use
+the Emacspeak TTS server abstraction.
+Argument PROGRAM specifies the speech server program."
+  (interactive
+   (list
+    (completing-read
+     "Select speech server:"
+     (or dtk-servers-alist
+         (tts-setup-servers-alist))
+     nil
+     t  )))
+  (declare (special    dtk-servers-alist
+                       dtk-async-server-process
+                       emacspeak-servers-directory ))
+  (when (and
+         dtk-async-server-process
+             (eq 'run (process-status dtk-async-server-process)))
+    (kill-process dtk-async-server-process))
+  (setq dtk-async-server-process
+        (start-process
+         "AsyncTTS"
+         "*AsyncTTS*"
+         (executable-find "python")
+         (expand-file-name "python/HTTPSpeaker.py" emacspeak-servers-directory)
+         program)))
+
 ;;}}}
 ;;{{{  initialize the speech process
 
