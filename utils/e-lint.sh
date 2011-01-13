607fe87c2fbdb92565d5a182ca5009c3fabd1cc3
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2011-01-13 01:44:29 +0000

    linted websearch
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@6720 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- utils/e-lint.sh
+++ utils/e-lint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #$Iid:$
-LOAD="-l advice.el -l cl-macs.el -l emacspeak-load-path.el -l emacspeak-loaddefs.el"
+LOAD="-l advice.el -l cl-macs.el -l emacspeak-load-path.el \
+ -l emacspeak-loaddefs.el -L ./g-client"
 echo "$@" | \
 emacs -batch -q $LOAD -f elint-file 
