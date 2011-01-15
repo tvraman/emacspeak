7745234938d304a0d9d6187ca8d8f6f496e8b49f
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2011-01-15 19:46:14 +0000

    updated rg

--- lisp/emacspeak-org.el
+++ lisp/emacspeak-org.el
@@ -327,6 +327,8 @@
           ([(meta shift left)] org-shiftmetaleft)
           ([(meta shift right)] org-shiftmetaright)
           ([(meta shift return)] org-insert-todo-heading)
+	  ("\C-e" emacspeak-prefix-command)
+	  ("\C-ee" orrg-end-of-line)
           ("\C-j" org-insert-heading)
           ("\M-n" org-next-item)
           ("\M-p" org-previous-item)
@@ -336,6 +338,7 @@
   (loop for k in
         '(
           ( "\C-e" emacspeak-prefix-command)
+	  ("\C-ee" orrg-end-of-line)
           ( "\C-h" help-command))
         do
         (emacspeak-keymap-update  org-goto-map k)))
