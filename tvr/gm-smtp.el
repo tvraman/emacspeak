414e494ce590b593c8334ab5e7f78f50493abfe8
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2011-06-05 16:26:09 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@7049 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- tvr/gm-smtp.el
+++ tvr/gm-smtp.el
@@ -7,8 +7,8 @@
  smtpmail-default-smtp-server "smtp.gmail.com"
  smtpmail-smtp-server "smtp.gmail.com"
  smtpmail-smtp-service 587
- smtpmail-auth-credentials
- '(("smtp.gmail.com" 587 "username" "password"))
+ smtpmail-auth-credentials ; passwd set to nil  will be prompted
+ '(("smtp.gmail.com" 587 "username" nil))
  smtpmail-starttls-credentials
  '(("smtp.gmail.com" 587
     "/etc/ssl/certs/cacert.pem" "/etc/ssl/private/cakey.pem")))
