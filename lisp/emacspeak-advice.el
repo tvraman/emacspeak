463827d2ce8154d3863fe8e59d5efda1528c67fd
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2011-10-01 16:55:23 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@7233 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-advice.el
+++ lisp/emacspeak-advice.el
@@ -594,10 +594,10 @@ before the message is spoken."
   (let ((inhibit-read-only t))
     (voice-lock-mode t)
     ad-do-it
-    (setq emacspeak-last-message (ansi-color-apply ad-return-value ))
+    (setq emacspeak-last-message  ad-return-value )
     (when (and
-           emacspeak-speak-messages     ; speaking messages
-           ad-return-value              ;we really do have a message
+           emacspeak-speak-messages  ; speaking messages
+           ad-return-value           ;we really do have a message
            (/= emacspeak-lazy-message-time ;; previous message not recent
                (setq emacspeak-lazy-message-time (nth 1  (current-time)))))
       ;; so we really need to speak it
