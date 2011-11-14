c959986a511aedbe516a20363fcb82a58bad8386
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2011-11-14 04:18:38 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@7402 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/shr-url.el
+++ lisp/shr-url.el
@@ -1,18 +1,30 @@
 (require 'shr)
 
-(defun shr-url-callback (args)
+a(defun shr-url-callback (args)
   "Callback for url-retrieve."
   (goto-char (point-min))
-  (let* ((buffer (get-buffer-create "Web View"))
-         (start (re-search-forward "^$"))
-         (dom (libxml-parse-html-region start(point-max))))
+  (let* ((start (re-search-forward "^$"))
+         (dom (libxml-parse-html-region start(point-max)))
+         (buffer (get-buffer-create (shr-get-title-from-dom dom))))
     (with-current-buffer buffer
       (erase-buffer)
-    (shr-insert-document dom))
-    (switch-to-buffer buffer)))    
+    (shr-insert-document dom)
+    (set-buffer-modified-p nil)
+    (setq buffer-read-only t))
+    (switch-to-buffer buffer)
+    (emacspeak-speak-mode-line)))    
+
+(defsubst shr-get-title-from-dom (dom)
+  "Return Title."
+  (third
+   (find-if #'(lambda (e) (and (listp e)(eq 'title (car e))))
+            (third dom))))
 
 (defun shr-url (url)
   "Display web page."
-  (interactive "sURL: ")
+  (interactive
+   (list
+    (read-from-minibuffer "URL: "
+                          (get-text-property (point) 'shr-url))))
   (url-retrieve url 'shr-url-callback))
 
