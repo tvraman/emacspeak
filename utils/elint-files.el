0d2f611f1009b0a5fb2cbc39e6d1f5ea6d1ca570
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2012-04-15 00:09:12 +0000

    elint files tool added.
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@7687 bb5eec90-091a-0410-bc89-db79c2cf2c7b

---
+++ utils/elint-files.el
@@ -0,0 +1,12 @@
+;;;$Id: elint-files.el 7425 2011-11-22 01:55:17Z tv.raman.tv $
+
+(require 'cl)
+(require 'elint)
+(defun batch-elint-files ()
+  "Batch elint  elisp files in directory."
+  (let ((file-list (directory-files default-directory nil "\\.el\\'")))
+    (loop for f in file-list
+          do
+            (elint-file f))))
+            
+(batch-elint-files)
