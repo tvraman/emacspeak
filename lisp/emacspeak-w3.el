3bf80067047713e03fc68a4628771e9eeb8be7a4
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2012-04-25 02:27:31 +0000

    fixed bug in url-fix.

--- lisp/emacspeak-w3.el
+++ lisp/emacspeak-w3.el
@@ -852,8 +852,8 @@ Tue Apr 24 17:33:27 PDT 2012
 (defadvice url-retrieve-internal (before fix-bug pre act comp)
   "Fix bug in handling of google result urls."
   (let ((u (ad-get-arg 0)))
-    (when (string-prefix-p (emacspeak-google-result-url-prefix) u)
-      (ad-set-arg 0 (emacspeak-canonicalize-google-result-url u)))))
+    (when (string-prefix-p (emacspeak-w3-google-result-url-prefix) u)
+      (ad-set-arg 0 (emacspeak-w3-canonicalize-google-result-url u)))))
 
 (defun foo (x y)
   "show x"
