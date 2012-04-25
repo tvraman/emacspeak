d44371da6c59e419fc42964455adaa4cf12ee79e
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2012-04-25 00:14:26 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@7716 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/url-fixup.el
+++ lisp/url-fixup.el
@@ -6,6 +6,7 @@
   (let ((prefix-length (if emacspeak-websearch-google-use-https 29 28))
         (suffix-start (string-match "&sa=" url)))
     (substring url prefix-length suffix-start)))
+
 (defsubst emacspeak-google-result-url-prefix ()
   "Return prefix of result urls."
   (declare (special emacspeak-websearch-google-use-https))
@@ -17,8 +18,9 @@
   "Fix bug in handling of google result urls."
   (let ((url (ad-get-arg 0)))
     (when (string-prefix-p (emacspeak-google-result-url-prefix) url)
-      (ad-set-arg 0 (emacspeak-canonicalize-google-result-url)))
+      (ad-set-argument 0 (emacspeak-canonicalize-google-result-url)))
     ad-do-it
     ad-return-value))
+
     
 )
