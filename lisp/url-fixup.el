1365ecf5fa67fb48a14805c43e3d81b8cd2bcc5e
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2012-04-25 00:33:48 +0000

    fixed up url.el bug to handle google search results correctly.

--- lisp/url-fixup.el
+++ lisp/url-fixup.el
@@ -2,25 +2,27 @@
 ;;; prefix: http://www.google.com/url?q=
 ;;; Suffix: &sa=...
 
-(defsubst emacspeak-canonicalize-google-result-url (url)
+(defsubst emacspeak-w3-canonicalize-google-result-url (url)
   (let ((prefix-length (if emacspeak-websearch-google-use-https 29 28))
         (suffix-start (string-match "&sa=" url)))
     (substring url prefix-length suffix-start)))
 
-(defsubst emacspeak-google-result-url-prefix ()
+(defsubst emacspeak-w3-google-result-url-prefix ()
   "Return prefix of result urls."
   (declare (special emacspeak-websearch-google-use-https))
   (format "%s://www.google.com/url?q="
           (if emacspeak-websearch-google-use-https "https" "http")))
 
              
-(defadvice url-retrieve-internal (around fix-bug pre act comp)
+(defadvice url-retrieve-internal (before fix-bug pre act comp)
   "Fix bug in handling of google result urls."
-  (let ((url (ad-get-arg 0)))
-    (when (string-prefix-p (emacspeak-google-result-url-prefix) url)
-      (ad-set-argument 0 (emacspeak-canonicalize-google-result-url)))
-    ad-do-it
-    ad-return-value))
+  (let ((u (ad-get-arg 0)))
+    (when (string-prefix-p (emacspeak-google-result-url-prefix) u)
+      (ad-set-arg 0 (emacspeak-canonicalize-google-result-url u)))))
 
-    
-)
+(defun foo (x y)
+  "show x"
+  (message (concat x y)))
+
+(defadvice foo (before try pre act comp)
+  (ad-set-argument 0 (concat (ad-get-arg 0) " world")))
