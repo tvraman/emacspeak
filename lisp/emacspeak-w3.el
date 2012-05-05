d74c06b50e8e71253ee07a142fe2d634d802241e
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2012-05-05 22:08:54 +0000

    advised w3-fetch to fix up google search urls
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@7745 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-w3.el
+++ lisp/emacspeak-w3.el
@@ -859,9 +859,8 @@ Tue Apr 24 17:33:27 PDT 2012
 (defadvice w3-fetch (before fix-bug pre act comp)
   "Fix bug in handling of google result urls."
   (let ((u (ad-get-arg 0)))
-    (when
-        (and u(string-prefix-p (emacspeak-w3-google-result-url-prefix) u) )
-      (ad-set-arg 0 (emacspeak-w3-canonicalize-google-result-url u))))))
+    (when (and u(string-prefix-p (emacspeak-w3-google-result-url-prefix) u))
+      (ad-set-arg 0 (emacspeak-w3-canonicalize-google-result-url u)))))
 
 
 (defun foo (x y)
