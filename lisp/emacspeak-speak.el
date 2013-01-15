3bbd5927e2c2c770ee0c382e44d7eeafdbf86658
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2013-01-15 16:39:41 +0000

    fixed newly introduced bug in emacspeak-speak-line; call stop  where needed.
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@8073 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-speak.el
+++ lisp/emacspeak-speak.el
@@ -874,17 +874,22 @@ are indicated with auditory icon ellipses."
         (emacspeak-auditory-icon 'ellipses))
       (cond
        ((string-equal ""  line)
+        (when dtk-stop-immediately (dtk-stop))
         (dtk-tone 250   75 'force))
        ((string-match  emacspeak-speak-space-regexp  line) ;only white space
+        (when dtk-stop-immediately (dtk-stop))
         (dtk-tone 300   120 'force))
        ((and (not (eq 'all dtk-punctuation-mode))
              (string-match  emacspeak-horizontal-rule line))
+        (when dtk-stop-immediately (dtk-stop))
         (dtk-tone 350   100 t))
        ((and (not (eq 'all dtk-punctuation-mode))
-             (string-match  emacspeak-decoration-rule line) )
+             (string-match  emacspeak-decoration-rule line))
+        (when dtk-stop-immediately (dtk-stop))
         (dtk-tone 450   100 t))
        ((and (not (eq 'all dtk-punctuation-mode))
              (string-match  emacspeak-unspeakable-rule line))
+        (when dtk-stop-immediately (dtk-stop))
         (dtk-tone 550   100 t))
        (t
         (let*
