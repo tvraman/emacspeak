a83b50853246d46279f9eacaac2eefd9af37fe46
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2013-01-15 02:00:47 +0000

    nuked redundant stop in speak-line

--- lisp/emacspeak-speak.el
+++ lisp/emacspeak-speak.el
@@ -850,7 +850,6 @@ are indicated with auditory icon ellipses."
        ((null arg))
        ((> arg 0) (setq start orig))
        (t (setq end orig)))
-      (when dtk-stop-immediately (dtk-stop))
       (setq line
             (if emacspeak-show-point
                 (ems-set-personality-temporarily
