b5fba132266a4ccd6fd5680d8538843858bc4a93
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2013-05-08 15:41:47 +0000

    updating release bits for versioning.

--- lisp/emacspeak-setup.el
+++ lisp/emacspeak-setup.el
@@ -99,7 +99,7 @@ pronunciation dictionaries are stored. ")
 ;;;###autoload
 (defconst emacspeak-version
   (format
-   "37.0 %s"
+   "38.0 %s"
    (cond
     ((file-exists-p emacspeak-readme-file)
      (let ((buffer (find-file-noselect emacspeak-readme-file))
@@ -122,7 +122,7 @@ pronunciation dictionaries are stored. ")
 
 ;;;###autoload
 (defconst emacspeak-codename
-  "SolidDog"
+  "FreeDog"
   "Code name of present release.")
 
 ;;}}}
