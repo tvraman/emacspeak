01664a37298e844861ffb5bb58f66fdc3f75ff5c
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2013-10-10 23:51:11 +0000

    auditory icon from async event handlers

--- lisp/emacspeak-advice.el
+++ lisp/emacspeak-advice.el
@@ -738,6 +738,7 @@ Handle end-of-buffer and beginning-of-buffer specially."
                     emacspeak-speak-messages))
   (let ((emacspeak-speak-messages nil)
         (emacspeak-speak-errors nil))
+    (emacspeak-auditory-icon 'progress)
     ad-do-it))
 
 ;;}}}
