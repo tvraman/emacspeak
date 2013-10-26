8a20c218807f90be7ed1190d02e848be0c577af2
tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2013-10-26 16:31:11 +0000

    string-equal in testing for empty strings

--- tvr/org-prepare.el
+++ tvr/org-prepare.el
@@ -5,19 +5,5 @@
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
-(define-key global-map "\C-cb" 'org-switchb)(setq org-directory "~/.org/")
+(setq org-directory "~/.org/")
 (setq org-default-notes-file (expand-file-name "notes.org"  org-directory))
-(define-prefix-command 'org-todo-state-map)
-(define-key org-mode-map "\C-cx" 'org-todo-state-map)
-(define-key org-todo-state-map "x"
-  #'(lambda nil (interactive) (org-todo "CANCELLED")))
-(define-key org-todo-state-map "d"
-  #'(lambda nil (interactive) (org-todo "DONE")))
-(define-key org-todo-state-map "f"
-  #'(lambda nil (interactive) (org-todo "DEFERRED")))
-(define-key org-todo-state-map "l"
-  #'(lambda nil (interactive) (org-todo "DELEGATED")))
-(define-key org-todo-state-map "s"
-  #'(lambda nil (interactive) (org-todo "STARTED")))
-(define-key org-todo-state-map "w"
-  #'(lambda nil (interactive) (org-todo "WAITING")))
