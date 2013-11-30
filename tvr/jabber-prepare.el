7303bc72b4cfeb3b33c0c31cf6ece47f62777264
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2013-11-30 15:42:52 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@8623 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- tvr/jabber-prepare.el
+++ tvr/jabber-prepare.el
@@ -1,11 +1,18 @@
-(augment-load-path "jabber")
+(augment-load-path "emacs-jabber")
+
+(load-library "jabber-autoloads")
 (load-library "jabber")
 (load-library "ssl")
 (load-library "nm")
 (add-hook 'nm-connected-hook 'jabber-connect-all)
+(setq fsm-debug nil)
 (add-hook 'nm-disconnected-hook 'jabber-disconnect)
 (nm-enable)
-(setq fsm-debug nil)
-(add-hook 'jabber-roster-mode-hook
-          #'(lambda nil
-              (setq buffer-undo-list t)))
+
+
+
+
+
+(require 'tls)
+;(setq tls-program '("gnutls-cli --insecure -p %p %h"))
+;(setq tls-program '("gnutls-cli -p %p %h"))
