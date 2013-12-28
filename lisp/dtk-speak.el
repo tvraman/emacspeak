52c3da17ba08dc21751cc993e641a265b6e73f76
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2013-12-28 17:18:00 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@8743 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/dtk-speak.el
+++ lisp/dtk-speak.el
@@ -143,7 +143,7 @@ Do not set this variable by hand, use command  `dtk-toggle-split-caps'
 (defcustom dtk-cleanup-patterns
   (list
    "." "_" "-"  "=" "/"  "+" "*" ":" ";" "%"
-   "{" "}" "~" "$" ")" "#" "/\\" "<>" )
+   "‑""{" "}" "~" "$" ")" "#" "/\\" "<>" )
   "List of repeating patterns to clean up.
 You can use  command  `dtk-add-cleanup-pattern'
  bound to \\[dtk-add-cleanup-pattern]  to add more patterns.
@@ -1973,6 +1973,7 @@ Optional argument group-count specifies grouping for intonation."
 ;;{{{  emacs local variables
 
 ;;; local variables:
+;;; coding: utf-8
 ;;; folded-file: t
 ;;; byte-compile-dynamic: nil
 ;;; end:
