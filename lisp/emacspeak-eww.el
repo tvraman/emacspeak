56ba6eb915bb57c3bd5f6cda5e7cc5c6894251be
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2014-05-12 20:37:43 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@9188 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-eww.el
+++ lisp/emacspeak-eww.el
@@ -1006,7 +1006,7 @@ Warning, this is fragile, and depends on a stable id for the
     (error "This doesn't look like a Google results page."))
   (let*
       ((emacspeak-eww-rename-result-buffer nil)
-       (value "kno-result")
+       (value "rhs")
        (media "rg_meta")
        (inhibit-read-only t)
        (dom
