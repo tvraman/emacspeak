0a4839fd7b526085b92bb06d7a0d328b4fa313b9
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2014-07-12 15:55:29 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@9262 bb5eec90-091a-0410-bc89-db79c2cf2c7b

---
+++ bash-utils/prompt.sh
@@ -0,0 +1,12 @@
+# Set up prompts for bash:
+SOUNDDIR=${HOME}/emacs/lisp/emacspeak/sounds/chimes-stereo
+SUCCESS=$SOUNDDIR/item.wav
+FAIL=$SOUNDDIR/warn-user.wav
+function prompt () {
+case "$?" in
+0) aplay -N -q   ${SUCCESS}  1>&- 2>&-;;
+*) aplay  -N -q  ${FAIL} 1>&- 2>&-  ;;
+esac
+}
+
+export PROMPT_COMMAND=prompt
