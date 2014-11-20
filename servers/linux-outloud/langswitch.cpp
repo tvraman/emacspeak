c50a740cda9ea91c7e4101eb9adbf44ae7f2b74b
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2014-11-20 19:06:54 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@9581 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- servers/linux-outloud/langswitch.cpp
+++ servers/linux-outloud/langswitch.cpp
@@ -19,48 +19,46 @@ struct langInfo {
   const char *label;
 };
 
-enum {
-  LANGUAGE_MAX_LABEL = 30
-}; // max size of the label field
+enum { LANGUAGE_MAX_LABEL = 30 };  // max size of the label field
 
 static struct langInfo TheLanguages[] = {
-  { NODEFINEDCODESET, NULL, NULL, NULL, NULL },
-  { eciGeneralAmericanEnglish, "en_US", "iso8859-1", "1.0", "American" },
-  { eciBritishEnglish, "en_GB", "iso8859-1", "1.1", "British" },
-  { eciCastilianSpanish, "es_ES", "iso8859-1", "2.0", "Español" },
-  { eciMexicanSpanish, "es_MX", "iso8859-1", "2.1", "Mexicano" },
-  { eciStandardFrench, "fr_FR", "iso8859-1", "3.0", "Français" },
-  { eciCanadianFrench, "fr_CA", "iso8859-1", "3.1", "Français Canadien" },
-  { eciStandardGerman, "de_DE", "iso8859-1", "4.0", "Deutsch" },
-  { eciStandardItalian, "it_IT", "iso8859-1", "5.0", "Italiano" },
-  { eciMandarinChineseGB, "zh_CN", "gb2312", "6.0", "Chinese Simplified" },
-  { eciMandarinChinesePinYin, "zh_CN", "gb2312", "6.0.1",
-    "Chinese Simplified" },
-  { eciMandarinChineseUCS, "zh_CN", "UCS2", "6.0.8", "Chinese Simplified" },
-  { eciTaiwaneseMandarinBig5, "zh_HK", "big5", "6.1", "Chinese Traditional" },
-  { eciTaiwaneseMandarinZhuYin, "zh_HK", "big5", "6.1.1",
-    "Chinese Traditional" },
-  { eciTaiwaneseMandarinPinYin, "zh_HK", "big5", "6.1.2",
-    "Chinese Traditional" },
-  { eciTaiwaneseMandarinUCS, "zh_HK", "UCS2", "6.1.8", "Chinese Traditional" },
-  { eciBrazilianPortuguese, "pt_BR", "iso8859-1", "7.0",
-    "Brazilian Portuguese" },
-  { eciStandardJapaneseSJIS, "ja_JP", "shiftjis", "8.0", "Japanese" },
-  { eciStandardJapaneseUCS, "ja_JP", "UCS2", "8.0.8", "Japanese" },
-  { eciStandardFinnish, "fi_FI", "iso8859-1", "9.0", "Finnish" },
-  { eciStandardKoreanUHC, NULL, NULL, NULL },
-  { eciStandardKoreanUCS, NULL, NULL, NULL, NULL },
-  { eciStandardCantoneseGB, NULL, NULL, NULL, NULL },
-  { eciStandardCantoneseUCS, NULL, NULL, NULL, NULL },
-  { eciHongKongCantoneseBig5, NULL, NULL, NULL, NULL },
-  { eciHongKongCantoneseUCS, NULL, NULL, NULL, NULL },
-  { eciStandardDutch, NULL, NULL, NULL, NULL },
-  { eciStandardNorwegian, NULL, NULL, NULL, NULL },
-  { eciStandardSwedish, NULL, NULL, NULL, NULL },
-  { eciStandardDanish, NULL, NULL, NULL, NULL },
-  { eciStandardReserved, NULL, NULL, NULL, NULL },
-  { eciStandardThai, NULL, NULL, NULL, NULL },
-  { eciStandardThaiTIS, NULL, NULL, NULL, NULL },
+    {NODEFINEDCODESET, NULL, NULL, NULL, NULL},
+    {eciGeneralAmericanEnglish, "en_US", "iso8859-1", "1.0", "American"},
+    {eciBritishEnglish, "en_GB", "iso8859-1", "1.1", "British"},
+    {eciCastilianSpanish, "es_ES", "iso8859-1", "2.0", "Español"},
+    {eciMexicanSpanish, "es_MX", "iso8859-1", "2.1", "Mexicano"},
+    {eciStandardFrench, "fr_FR", "iso8859-1", "3.0", "Français"},
+    {eciCanadianFrench, "fr_CA", "iso8859-1", "3.1", "Français Canadien"},
+    {eciStandardGerman, "de_DE", "iso8859-1", "4.0", "Deutsch"},
+    {eciStandardItalian, "it_IT", "iso8859-1", "5.0", "Italiano"},
+    {eciMandarinChineseGB, "zh_CN", "gb2312", "6.0", "Chinese Simplified"},
+    {eciMandarinChinesePinYin, "zh_CN", "gb2312", "6.0.1",
+     "Chinese Simplified"},
+    {eciMandarinChineseUCS, "zh_CN", "UCS2", "6.0.8", "Chinese Simplified"},
+    {eciTaiwaneseMandarinBig5, "zh_HK", "big5", "6.1", "Chinese Traditional"},
+    {eciTaiwaneseMandarinZhuYin, "zh_HK", "big5", "6.1.1",
+     "Chinese Traditional"},
+    {eciTaiwaneseMandarinPinYin, "zh_HK", "big5", "6.1.2",
+     "Chinese Traditional"},
+    {eciTaiwaneseMandarinUCS, "zh_HK", "UCS2", "6.1.8", "Chinese Traditional"},
+    {eciBrazilianPortuguese, "pt_BR", "iso8859-1", "7.0",
+     "Brazilian Portuguese"},
+    {eciStandardJapaneseSJIS, "ja_JP", "shiftjis", "8.0", "Japanese"},
+    {eciStandardJapaneseUCS, "ja_JP", "UCS2", "8.0.8", "Japanese"},
+    {eciStandardFinnish, "fi_FI", "iso8859-1", "9.0", "Finnish"},
+    {eciStandardKoreanUHC, NULL, NULL, NULL},
+    {eciStandardKoreanUCS, NULL, NULL, NULL, NULL},
+    {eciStandardCantoneseGB, NULL, NULL, NULL, NULL},
+    {eciStandardCantoneseUCS, NULL, NULL, NULL, NULL},
+    {eciHongKongCantoneseBig5, NULL, NULL, NULL, NULL},
+    {eciHongKongCantoneseUCS, NULL, NULL, NULL, NULL},
+    {eciStandardDutch, NULL, NULL, NULL, NULL},
+    {eciStandardNorwegian, NULL, NULL, NULL, NULL},
+    {eciStandardSwedish, NULL, NULL, NULL, NULL},
+    {eciStandardDanish, NULL, NULL, NULL, NULL},
+    {eciStandardReserved, NULL, NULL, NULL, NULL},
+    {eciStandardThai, NULL, NULL, NULL, NULL},
+    {eciStandardThaiTIS, NULL, NULL, NULL, NULL},
 };
 
 // >
@@ -109,8 +107,7 @@ enum ECILanguageDialect initLanguage(Tcl_Interp *interp,
     char buffer_j[3];
 
     for (aLang = 0; aLang < LANG_INFO_MAX; aLang++) {
-      if (TheLanguages[aLang].lang == aLanguages[i])
-        break;
+      if (TheLanguages[aLang].lang == aLanguages[i]) break;
     }
 
     if ((aLang == LANG_INFO_MAX) || (TheLanguages[aLang].code == NULL)) {
