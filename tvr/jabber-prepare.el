62e7f6a5afb8f190063bcfeb19caee22bb989ecd
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2015-01-22 22:55:27 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@9878 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- tvr/jabber-prepare.el
+++ tvr/jabber-prepare.el
@@ -1,7 +1,7 @@
 (augment-load-path "jabber")
 (load-library "jabber")
 (load-library "ssl")
-;(load-library "nm")
+(load-library "nm")
 (add-hook 'nm-connected-hook 'jabber-connect-all)
 (add-hook 'nm-disconnected-hook 'jabber-disconnect)
 (nm-enable)
@@ -9,3 +9,10 @@
 (add-hook 'jabber-roster-mode-hook
           #'(lambda nil
               (setq buffer-undo-list t)))
+
+;;; terse modeline
+(setq jabber-mode-line-string (list " "
+              'jabber-mode-line-presence
+              ;" "
+              ;'jabber-mode-line-contacts
+              ))
