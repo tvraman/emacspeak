c3152e8b8aa785f58d042bca93815d1c45c32370
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2015-02-06 21:37:58 +0000

    snd-edit is now just plain sox
    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@9963 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-sox.el
+++ lisp/emacspeak-sox.el
@@ -1,8 +1,8 @@
-;;; emacspeak-snd-edit.el --- Speech-enable SND-EDIT
-;;; $Id: emacspeak-snd-edit.el 4797 2007-07-16 23:31:22Z tv.raman.tv $
+;;; emacspeak-sox.el --- Speech-enable SOX
+;;; $Id: emacspeak-sox.el 4797 2007-07-16 23:31:22Z tv.raman.tv $
 ;;; $Author: tv.raman.tv $
-;;; Description:  Speech-enable SND-EDIT An Emacs Interface to snd-edit
-;;; Keywords: Emacspeak,  Audio Desktop snd-edit
+;;; Description:  Speech-enable SOX An Emacs Interface to sox
+;;; Keywords: Emacspeak,  Audio Desktop sox
 ;;{{{  LCD Archive entry:
 
 ;;; LCD Archive Entry:
@@ -28,7 +28,7 @@
 ;;;
 ;;; GNU Emacs is distributed in the hope that it will be useful,
 ;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNSND-EDIT FOR A PARTICULAR PURPOSE.  See the
+;;; MERCHANTABILITY or FITNSOX FOR A PARTICULAR PURPOSE.  See the
 ;;; GNU General Public License for more details.
 ;;;
 ;;; You should have received a copy of the GNU General Public License
@@ -41,8 +41,7 @@
 ;;{{{  introduction
 
 ;;; Commentary: This module defines a convenient speech-enabled
-;;; interface for editting mp3 and wav files. It uses
-;;; command-line tools  sox and 
+;;; interface for editting mp3 and wav files using SoX.
 ;;; Launching this module creates a special interaction buffer 
 ;;; that provides single keystroke commands for editing and applying effects to a selected sound file.
 
@@ -57,10 +56,10 @@
 ;;}}}
 ;;{{{ Define Special Mode
 
-(defun emacspeak-snd-edit-draw-effect (effect)
+(defun emacspeak-sox-draw-effect (effect)
   "Insert a representation of specified effect at point."
-  (let ((name (emacspeak-snd-edit-effect-name effect))
-        (params (emacspeak-snd-edit-effect-params effect)))
+  (let ((name (emacspeak-sox-effect-name effect))
+        (params (emacspeak-sox-effect-params effect)))
     (insert (propertize  name 'face  'font-lock-keyword-face))
     (loop
      for p in params do
@@ -72,94 +71,89 @@
      
 
 
-(defun emacspeak-snd-edit-redraw (context)
-  "Redraws snd-edit buffer."
+(defun emacspeak-sox-redraw (context)
+  "Redraws sox buffer."
   (let ((inhibit-read-only t)
         (orig (point-min))
-        (file (emacspeak-snd-edit-context-file context))
-        (start (emacspeak-snd-edit-context-start context))
-        (end (emacspeak-snd-edit-context-end context))
-        (effects (emacspeak-snd-edit-context-effects context)))
+        (file (emacspeak-sox-context-file context))
+        (effects (emacspeak-sox-context-effects context)))
     (goto-char orig)
     (erase-buffer)    
     (insert (propertize "Audio File:  " 'face font-lock-doc-face))
-    (when  file
-      (insert  (propertize file 'face font-lock-keyword-face)))
+    (when  file (insert  (propertize file 'face font-lock-keyword-face)))
     (insert "\n")
-    (when effects (mapc #'emacspeak-snd-edit-draw-effect effects))))
+    (when effects (mapc #'emacspeak-sox-draw-effect effects))))
 
-(define-derived-mode emacspeak-snd-edit-mode special-mode
+(define-derived-mode emacspeak-sox-mode special-mode
                      "Interactively manipulate audio files."
   "An audio workbench for the Emacspeak desktop."
-  (declare (special emacspeak-snd-edit-context))
-  (unless emacspeak-snd-edit-context
-    (setq emacspeak-snd-edit-context (make-emacspeak-snd-edit-context)))
-  (emacspeak-snd-edit-redraw emacspeak-snd-edit-context)
+  (declare (special emacspeak-sox-context))
+  (unless emacspeak-sox-context
+    (setq emacspeak-sox-context (make-emacspeak-sox-context)))
+  (emacspeak-sox-redraw emacspeak-sox-context)
   (setq buffer-read-only t)
   (setq header-line-format "Audio Workbench"))
 
-(defvar emacspeak-snd-edit-buffer "Audio WorkBench"
+(defvar emacspeak-sox-buffer "Audio WorkBench"
   "Buffer name of workbench.")
 
 ;;;###autoload
-(defun emacspeak-snd-edit ()
+(defun emacspeak-sox ()
   "Create a new Audio Workbench or switch to an existing workbench."
   (interactive)
-  (declare (special emacspeak-snd-edit-buffer))
-  (unless (get-buffer emacspeak-snd-edit-buffer)
-    (let ((buffer (get-buffer-create emacspeak-snd-edit-buffer)))
+  (declare (special emacspeak-sox-buffer))
+  (unless (get-buffer emacspeak-sox-buffer)
+    (let ((buffer (get-buffer-create emacspeak-sox-buffer)))
     (with-current-buffer buffer
-      (emacspeak-snd-edit-mode)
-      (emacspeak-snd-edit-setup-keys))))
-  (switch-to-buffer emacspeak-snd-edit-buffer)
+      (emacspeak-sox-mode)
+      (emacspeak-sox-setup-keys))))
+  (switch-to-buffer emacspeak-sox-buffer)
     (emacspeak-auditory-icon 'open-object)
     (emacspeak-speak-header-line))
     
     
-(defgroup emacspeak-snd-edit nil
+(defgroup emacspeak-sox nil
   "Audio workbench for the Emacspeak Audio Desktop."
   :group 'emacspeak)
 
-(defun emacspeak-snd-edit-setup-keys ()
-  "Set up snd-edit keymap."
-  (declare (special emacspeak-snd-edit-mode-map))
+(defun emacspeak-sox-setup-keys ()
+  "Set up sox keymap."
+  (declare (special emacspeak-sox-mode-map))
   (loop
  for k in
  '(
-   ("e" emacspeak-snd-edit-set-effect)
-   ("f" emacspeak-snd-edit-file)
-   ("p" emacspeak-snd-edit-play)
-   ("[" emacspeak-snd-edit-set-start)
-   ("]" emacspeak-snd-edit-set-end)
+   ("e" emacspeak-sox-set-effect)
+   ("f" emacspeak-sox-file)
+   ("p" emacspeak-sox-play)
    )
         do
-        (emacspeak-keymap-update  emacspeak-snd-edit-mode-map k)))
+        (emacspeak-keymap-update  emacspeak-sox-mode-map k)))
 
 ;;}}}
 ;;{{{ Top-level Context:
 
-(defstruct emacspeak-snd-edit-effect
+(defstruct emacspeak-sox-effect
   name ; effect name
   params ; list of effect param/value pairs 
   )
 
-(defstruct emacspeak-snd-edit-context
+(defstruct emacspeak-sox-context
   file ; file being manipulated 
   effects ; list of effects with params
 )
 
-(defvar emacspeak-snd-edit-context
+(defvar emacspeak-sox-context
   nil
-  "Buffer-local handle to snd-edit context.")
+  "Buffer-local handle to sox context.")
 
-(make-variable-buffer-local 'emacspeak-snd-edit-context)
+(make-variable-buffer-local 'emacspeak-sox-context)
 
-(defcustom emacspeak-snd-edit-wave
+(defcustom emacspeak-sox-wave
   (executable-find "sox")
   "Location of SoX utility."
   :type 'file)
 
-(defcustom emacspeak-snd-edit-play 
+(defcustom emacspeak-sox-play 
   (executable-find "play")
   "Location of play from SoX utility."
   :type 'file)
@@ -171,7 +165,7 @@
 ;;}}}
 ;;{{{ Common Commands 
 
-(defsubst emacspeak-snd-edit-sound-p (snd-file)
+(defsubst emacspeak-sox-sound-p (snd-file)
   "Predicate to test if we can edit this file."
   (let ((case-fold-search t))
     (or 
@@ -180,72 +174,58 @@
      (string-match "\\.wav$" snd-file))))
      
 
-(defun emacspeak-snd-edit-file (snd-file)
+(defun emacspeak-sox-file (snd-file)
   "Open specified snd-file on the Audio Workbench."
   (interactive "fSound File: ")
-  (declare (special emacspeak-snd-edit-context))
-  (unless emacspeak-snd-edit-context
+  (declare (special emacspeak-sox-context))
+  (unless emacspeak-sox-context
     (error "Audio Workbench not initialized."))
   (let ((inhibit-read-only t)
-        (type (emacspeak-snd-edit-sound-p snd-file)))
+        (type (emacspeak-sox-sound-p snd-file)))
     (unless type (error "%s does not look like a sound file." snd-file))
-    (setf (emacspeak-snd-edit-context-file emacspeak-snd-edit-context) snd-file))
-  (emacspeak-snd-edit-redraw emacspeak-snd-edit-context)
+    (setf (emacspeak-sox-context-file emacspeak-sox-context) snd-file))
+  (emacspeak-sox-redraw emacspeak-sox-context)
   (message "Selected file %s" snd-file)
   (emacspeak-auditory-icon 'select-object))
 
-(defun emacspeak-snd-edit-read-timestamp (prompt)
+(defun emacspeak-sox-read-timestamp (prompt)
   "Read timestamp hh:mm:ss.sss."
-  (declare (special emacspeak-snd-edit-context))
+  (declare (special emacspeak-sox-context))
   (let ((timestamp (read-from-minibuffer  prompt)))
-    (unless emacspeak-snd-edit-context
+    (unless emacspeak-sox-context
       (error "Audio Workbench not initialized."))
     (unless (string-match "[0-9:.]+" timestamp)
       (error "%s does not look like a valid timestamp." timestamp))
     timestamp))
 
-(defun emacspeak-snd-edit-set-start (timestamp)
-  "Set start time."
-  (interactive
-   (list
-    (emacspeak-snd-edit-read-timestamp "Start: ")))
-  (setf (emacspeak-snd-edit-context-start emacspeak-snd-edit-context) timestamp)
-  (emacspeak-snd-edit-redraw emacspeak-snd-edit-context)
-  (message "Set start to %s" timestamp))
 
-(defun emacspeak-snd-edit-set-end (timestamp)
-  "Set end time."
-  (interactive
-   (list
-    (emacspeak-snd-edit-read-timestamp "End: ")))
-  (setf (emacspeak-snd-edit-context-end emacspeak-snd-edit-context) timestamp)
-  (emacspeak-snd-edit-redraw emacspeak-snd-edit-context)
-  (message "Set end to %s" timestamp))
+
+
 
 
      
 
-(defun emacspeak-snd-edit-play ()
+(defun emacspeak-sox-play ()
   "Play wave file from specified context."
   (interactive )
-  (declare (special emacspeak-snd-edit-context
-            emacspeak-snd-edit-play))
-  (let* ((c emacspeak-snd-edit-context)
-         (file (emacspeak-snd-edit-context-file c))
-        (effects (emacspeak-snd-edit-context-effects c))
+  (declare (special emacspeak-sox-context
+            emacspeak-sox-play))
+  (let* ((c emacspeak-sox-context)
+         (file (emacspeak-sox-context-file c))
+        (effects (emacspeak-sox-context-effects c))
         (command nil)
         (options nil))
     (loop
      for e in effects  do
      (push 
       (funcall
-       (intern (format "emacspeak-snd-edit-get-%s-options" (emacspeak-snd-edit-effect-name e)))
+       (intern (format "emacspeak-sox-get-%s-options" (emacspeak-sox-effect-name e)))
        e)
       options))
     (setq options (mapconcat #'identity  options " "))
     (setq command
           (format "%s %s %s &"
-                          emacspeak-snd-edit-play file options))
+                          emacspeak-sox-play file options))
     (call-process shell-file-name nil nil nil shell-command-switch command)
     command))
     
@@ -261,27 +241,27 @@
 ;;}}}
 ;;{{{ SoX Commands:
 
-(defconst emacspeak-snd-edit-effects
+(defconst emacspeak-sox-effects
   '("trim"
     "echo"
     )
   "Table of implemented effects.")
 
 
-(defun emacspeak-snd-edit-set-effect (name)
+(defun emacspeak-sox-set-effect (name)
   "Set effect."
   (interactive
-   (list (completing-read "SoX Effect: " emacspeak-snd-edit-effects)))
-  (declare (special emacspeak-snd-edit-context  emacspeak-snd-edit-effects))
-      (setf (emacspeak-snd-edit-context-effects emacspeak-snd-edit-context)
-            (list  (funcall (intern (format  "emacspeak-snd-edit-get-%s-effect"  name)))))
-    (emacspeak-snd-edit-redraw emacspeak-snd-edit-context)
+   (list (completing-read "SoX Effect: " emacspeak-sox-effects)))
+  (declare (special emacspeak-sox-context  emacspeak-sox-effects))
+      (setf (emacspeak-sox-context-effects emacspeak-sox-context)
+            (list  (funcall (intern (format  "emacspeak-sox-get-%s-effect"  name)))))
+    (emacspeak-sox-redraw emacspeak-sox-context)
   (message "Set effect  %s" name))
 
-(defun emacspeak-snd-edit-get-trim-effect ()
+(defun emacspeak-sox-get-trim-effect ()
   "Read needed params for effect trim,
 and return a suitable effect structure."
-  (make-emacspeak-snd-edit-effect
+  (make-emacspeak-sox-effect
    :name "trim"
    :params
    (let ((s (read-from-minibuffer "Time Offset: "))
@@ -293,9 +273,9 @@ and return a suitable effect structure."
      
    
 
-(defun emacspeak-snd-edit-get-trim-options   (effect)
+(defun emacspeak-sox-get-trim-options   (effect)
   "Construct options  portion of commandline for this trim effect."
-  (let ((params (emacspeak-snd-edit-effect-params effect)))
+  (let ((params (emacspeak-sox-effect-params effect)))
     (format "trim %s"
             (mapconcat #'second params " "))))
     
@@ -303,7 +283,7 @@ and return a suitable effect structure."
 
 ;;}}}
 
-(provide 'emacspeak-snd-edit)
+(provide 'emacspeak-sox)
 ;;{{{ end of file
 
 ;;; local variables:
