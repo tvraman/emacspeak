b97a6e482a90ec88632f9abecaab4912572b5953
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2015-02-19 01:26:39 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@10091 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-m-player.el
+++ lisp/emacspeak-m-player.el
@@ -636,10 +636,11 @@ A string of the form `<number> 1' sets volume as an absolute."
 (defun emacspeak-m-player-display-position ()
   "Display current position in track and its length."
   (interactive)
-  (message "%s"
-           (replace-regexp-in-string "ans_" ""
-                                     (emacspeak-m-player-dispatch
-                                      "get_time_pos\nget_percent_pos\nget_time_length\nget_file_name\n"))))
+  (let ((result
+         (emacspeak-m-player-dispatch
+          "get_time_pos\nget_percent_pos\nget_time_length\nget_file_name\n")))
+    (setq result (if result (replace-regexp-in-string "ans_" "" result) ""))
+    (message "%s" result)))
 
 (defun emacspeak-m-player-load-file(f)
   "Load specified file."
