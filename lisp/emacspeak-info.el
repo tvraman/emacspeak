425a63b485d05ed909f14f6e58e7154f631795df
tv.raman.tv <tv.raman.tv@bb5eec90-091a-0410-bc89-db79c2cf2c7b>
2015-03-03 00:25:18 +0000

    git-svn-id: https://emacspeak.googlecode.com/svn/trunk@10215 bb5eec90-091a-0410-bc89-db79c2cf2c7b

--- lisp/emacspeak-info.el
+++ lisp/emacspeak-info.el
@@ -75,7 +75,7 @@
 ;;}}}
 ;;{{{ advice
 
-(defcustom  emacspeak-info-select-node-speak-chunk 'screenfull
+(defcustom  emacspeak-info-select-node-speak-chunk 'node
   "*Specifies how much of the selected node gets spoken.
 Possible values are:
 screenfull  -- speak the displayed screen
