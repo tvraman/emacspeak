b2c2939767d1f4b6082b7c7c397a059e07a9de82
raman <tv.raman.tv@gmail.com>
2015-04-08 17:21:19 -0700

    e-lint: load cl.el

--- utils/e-lint.sh
+++ utils/e-lint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #$Iid:$
-LOAD="-l advice.el -l cl-macs.el -l emacspeak-load-path.el \
+LOAD="-l advice.el -l cl-macs.el -l emacspeak-load-path.el -l cl.elc  \
  -l emacspeak-loaddefs.el -L ./g-client"
 echo "$@" | \
 emacs -batch -q $LOAD -f elint-file 
