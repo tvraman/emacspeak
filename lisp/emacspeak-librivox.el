c1be48ec9f5b2a252fc80b2b6944eb2d1354b313
raman <tv.raman.tv@gmail.com>
2015-05-21 13:38:49 -0700

    message empty results

--- lisp/emacspeak-librivox.el
+++ lisp/emacspeak-librivox.el
@@ -166,6 +166,7 @@ Optional arg `page-title' specifies page title."
                    g-curl-program g-curl-common-options url)))
          (books (g-json-get 'books result)))
     (assert books nil "No books found.")
+    (unless books (message "No results."))
     (when books
       (with-temp-buffer
         (insert "<title>" page-title "</title>\n")
