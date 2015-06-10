8bed0644c0144a1f6ae0ac6daa8c3e6aea9c011c
T. V. Raman <tv.raman.tv@gmail.com>
2015-06-10 10:34:08 -0700

    iplayer-prepare --- put channel in the header line

--- tvr/iplayer-prepare.el
+++ tvr/iplayer-prepare.el
@@ -3,3 +3,5 @@
 (push '("a" "BBC Asian Network") iplayer-presets)
 (define-key iplayer-mode-map "a" 'iplayer-preset)
 (define-key iplayer-mode-map "w" 'iplayer-preset)
+(defadvice iplayer-channel (after header-line pre act comp)
+  (setq header-line-format (format "%s" channel)))
