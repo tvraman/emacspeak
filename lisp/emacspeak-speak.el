8dadc73c1c218633120cfd427229207bcd42da16
T. V. Raman <tv.raman.tv@gmail.com>
2015-06-28 19:01:37 -0700

    lose lexical-let in ems-with-messages-silenced.

--- lisp/emacspeak-speak.el
+++ lisp/emacspeak-speak.el
@@ -238,13 +238,10 @@ Argument BODY specifies forms to execute."
 
 (defmacro ems-with-messages-silenced  (&rest body)
   "Evaluate body  after temporarily silencing auditory error feedback."
-  `(lexical-let ((emacspeak-speak-messages nil)
-                 (inhibit-message t)
-                 (emacspeak-use-auditory-icons nil))
-     (let ((emacspeak-speak-messages nil)
-           (inhibit-message t)
-           (emacspeak-use-auditory-icons nil))
-       ,@body)))
+  `(let ((emacspeak-speak-messages nil)
+         (inhibit-message t)
+         (emacspeak-use-auditory-icons nil))
+     ,@body))
 
 ;;}}}
 ;;{{{ getting and speaking text ranges
