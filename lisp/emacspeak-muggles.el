b003bf2bbf61c75a3a643398363d064f6d239377
T. V. Raman <tv.raman.tv@gmail.com>
2015-07-16 15:18:40 -0700

    added outliner hydra

--- lisp/emacspeak-muggles.el
+++ lisp/emacspeak-muggles.el
@@ -60,6 +60,7 @@
 ;;;@item m-player: Super-M Emacspeak-M-Player Commands
 ;;; @item hideshow C-c h hideshow
 ;;; @item toggle-option <C-c o> Toggle options
+;;; @item outliner <C-c #> outline-minor-mode
 ;;;@end itemize
 
 ;;; Code:
@@ -272,6 +273,51 @@ _w_ whitespace-mode:   %`whitespace-mode
   ("q" nil "quit"))
 
 ;;}}}
+;;{{{ Outliner:
+
+;;; Cloned from Hydra Wiki:
+
+(defhydra emacspeak-muggles-outliner 
+  (global-map "C-c #"
+              :pre (progn
+                     (outline-minor-mode 1)
+                     (emacspeak-muggles-pre))
+              :post emacspeak-muggles-post
+              :color pink :hint nil)
+  "
+^Hide^             ^Show^           ^Move
+^^^^^^------------------------------------------------------
+_q_: sublevels     _a_: all         _u_: up
+_t_: body          _e_: entry       _n_: next visible
+_o_: other         _i_: children    _p_: previous visible
+_c_: entry         _k_: branches    _f_: forward same level
+_l_: leaves        _s_: subtree     _b_: backward same level
+_d_: subtree
+
+"
+  ;; Hide
+  ("q" hide-sublevels)    ; Hide everything but the top-level headings
+  ("t" hide-body)         ; Hide everything but headings (all body lines)
+  ("o" hide-other)        ; Hide other branches
+  ("c" hide-entry)        ; Hide this entry's body
+  ("l" hide-leaves)       ; Hide body lines in this entry and sub-entries
+  ("d" hide-subtree)      ; Hide everything in this entry and sub-entries
+  ;; Show
+  ("a" show-all)          ; Show (expand) everything
+  ("e" show-entry)        ; Show this heading's body
+  ("i" show-children)     ; Show this heading's immediate child sub-headings
+  ("k" show-branches)     ; Show all sub-headings under this heading
+  ("s" show-subtree)      ; Show (expand) everything in this heading & below
+  ;; Move
+  ("u" outline-up-heading)                ; Up
+  ("n" outline-next-visible-heading)      ; Next
+  ("p" outline-previous-visible-heading)  ; Previous
+  ("f" outline-forward-same-level)        ; Forward - same level
+  ("b" outline-backward-same-level)       ; Backward - same level
+  ("z" nil "leave"))
+
+;;}}}
+
 (provide 'emacspeak-muggles)
 ;;{{{ end of file
 
