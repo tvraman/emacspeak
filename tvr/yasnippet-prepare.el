d795e9fca339c55ea9bc855598f390e11f270159
raman <tv.raman.tv@gmail.com>
2015-08-02 07:21:45 -0700

    updated yasnippet setup

--- tvr/yasnippet-prepare.el
+++ tvr/yasnippet-prepare.el
@@ -1,7 +1,10 @@
-(augment-load-path "yasnippet" "yasnippet")
+(setq yas-dir (augment-load-path "yasnippet" "yasnippet"))
 (load-library "yasnippet")
+(when yas-dir 
 (setq yas/root-directory
-      (expand-file-name "yasnippet/snippets"
-                        emacs-personal-library))
+      (expand-file-name "snippets"
+                        (file-name-directory yas-dir))))
 (yas/initialize)
 (yas/load-directory yas/root-directory)
+(yas/initialize)
+(yas/reload-all)
