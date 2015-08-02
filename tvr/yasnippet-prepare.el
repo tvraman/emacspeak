68fc7f19b2e37c56a1b5898940b61824235b5efc
raman <tv.raman.tv@gmail.com>
2015-08-02 07:27:16 -0700

    fixed obsolete var.

--- tvr/yasnippet-prepare.el
+++ tvr/yasnippet-prepare.el
@@ -1,7 +1,7 @@
 (setq yas-dir (augment-load-path "yasnippet" "yasnippet"))
 (load-library "yasnippet")
 (when yas-dir 
-(setq yas/root-directory
+(setq yas-snippet-dirs
       (expand-file-name "snippets"
                         (file-name-directory yas-dir))))
 (yas/initialize)
