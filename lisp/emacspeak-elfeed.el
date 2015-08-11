4cfeede9f21b227f45cc19d4df414eaf00a82d04
T. V. Raman <tv.raman.tv@gmail.com>
2015-08-11 14:27:57 -0700

    elfeed: w3 additions

--- lisp/emacspeak-elfeed.el
+++ lisp/emacspeak-elfeed.el
@@ -161,10 +161,36 @@
             (emacspeak-we-xslt-filter emacspeak-we-recent-xpath-filter link 'speak))
      (t (message "No link under point.")))))
 
+
+(defun emacspeak-elfeed-w3-filter-entry-at-point ()
+  "Display current article after filtering using W3.
+Work-around for async fetch bug in EWW."
+  (interactive)
+  (declare (special emacspeak-we-recent-xpath-filter))
+  (let* ((browse-url-browser-function 'browse-url-w3)
+         (entry (emacspeak-elfeed-entry-at-point))
+         (link(elfeed-entry-link entry)))
+    (cond
+     (entry (elfeed-untag  entry 'unread)
+            (emacspeak-we-xslt-filter emacspeak-we-recent-xpath-filter link 'speak))
+     (t (message "No link under point.")))))
+
 (defun emacspeak-elfeed-eww-entry-at-point ()
   "Display current article in EWW."
   (interactive)
   (let* ((entry (emacspeak-elfeed-entry-at-point))
+         (link(elfeed-entry-link entry)))
+    (cond
+     (entry (elfeed-untag  entry 'unread)
+            (eww link))
+     (t (message "No link under point.")))))
+
+
+(defun emacspeak-elfeed-w3-entry-at-point ()
+  "Display current article in W3."
+  (interactive)
+  (let* ((browse-url-browser-function browse-url-w3)
+         (entry (emacspeak-elfeed-entry-at-point))
          (link(elfeed-entry-link entry)))
     (cond
      (entry (elfeed-untag  entry 'unread)
@@ -189,12 +215,14 @@
 (defadvice elfeed-search-mode (after emacspeak pre act comp)
   "Set up Emacspeak commands."
   (declare (special elfeed-search-mode-map goal-column))
-  (setq goal-column 11) ; place point on entry title
+  (setq goal-column 11)                 ; place point on entry title
   (define-key elfeed-search-mode-map "n" 'emacspeak-elfeed-next-entry)
   (define-key elfeed-search-mode-map "p" 'emacspeak-elfeed-previous-entry)
   (define-key elfeed-search-mode-map "." 'emacspeak-elfeed-filter-entry-at-point)
   (define-key elfeed-search-mode-map "e" 'emacspeak-elfeed-eww-entry-at-point)
-  (define-key elfeed-search-mode-map " "'emacspeak-elfeed-speak-entry-at-point))
+  (define-key elfeed-search-mode-map " "'emacspeak-elfeed-speak-entry-at-point)
+  (define-key elfeed-search-mode-map "W" 'emacspeak-elfeed-w3-entry-at-point)
+  (define-key elfeed-search-mode-map "w" 'emacspeak-elfeed-w3-filter-entry-at-point))
 
 ;;}}}
 (provide 'emacspeak-elfeed)
