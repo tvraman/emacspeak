fe0fce069ce1a767c2ec2fb2b07a401f28a075f4
raman <tv.raman.tv@gmail.com>
2015-10-07 10:54:29 -0700

    dont touch shr-base

--- lisp/emacspeak-eww.el
+++ lisp/emacspeak-eww.el
@@ -745,7 +745,7 @@ for use as a DOM filter."
 (defun emacspeak-eww-view-helper  (filtered-dom)
   "View helper called by various filtering viewers."
   (declare (special emacspeak-eww-rename-result-buffer
-                    eww-shr-render-functions shr-base))
+                    eww-shr-render-functions ))
   (let ((emacspeak-eww-rename-result-buffer nil)
         (url (emacspeak-eww-current-url))
         (title  (format "%s: Filtered" (emacspeak-eww-current-title)))
@@ -754,7 +754,7 @@ for use as a DOM filter."
     (eww-save-history)
     (erase-buffer)
     (goto-char (point-min))
-    (setq shr-base (shr-parse-base url))
+    ;(setq shr-base (shr-parse-base url))
     (shr-insert-document filtered-dom)
     (emacspeak-eww-set-dom filtered-dom)
     (emacspeak-eww-set-url url)
