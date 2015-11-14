f1e3375002bbab152855429f71c6f1f9ec91eacb
raman <tv.raman.tv@gmail.com>
2015-11-14 15:30:16 -0800

    nuke field process from tts-env -- not needed

--- lisp/tts-env.el
+++ lisp/tts-env.el
@@ -57,9 +57,8 @@
 ;;{{{ Structure Definition 
 
 (cl-defstruct tts-env
-  name process                               
-  default-voice default-speech-rate
-  speech-rate-step speech-rate-base 
+  name default-voice 
+  default-speech-rate speech-rate-step speech-rate-base 
   list-voices voice-defined-p                       
   get-voice-command define-voice-from-acss                
   )
