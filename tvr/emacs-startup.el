578635081376ea04b6ae908a7839dc8b1c7065de
raman <tv.raman.tv@gmail.com>
2015-11-23 17:16:42 -0800

    set text-quoting-style to 'grave

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -79,6 +79,7 @@ Path is resolved relative to `whence' which defaults to emacs-personal-library."
     (tool-bar-mode -1)
     (scroll-bar-mode -1)
     (fringe-mode 1)
+    (setq text-quoting-style 'grave)
     (setq outline-minor-mode-prefix "\C-x@h")
     (when (file-exists-p  emacs-private-library)
       (augment-load-path emacs-private-library ))
