6c523508c31b67602e1ba7ff84cb3866b5fa18eb
raman <tv.raman.tv@gmail.com>
2015-11-27 19:23:13 -0800

    round panner when checking bounds.

--- lisp/emacspeak-m-player.el
+++ lisp/emacspeak-m-player.el
@@ -1419,7 +1419,7 @@ Check first if current buffer is in emacspeak-m-player-mode."
     (emacspeak-m-player-dispatch  "af_del pan, channels")
     (emacspeak-m-player-dispatch (format "af_add pan=2:%s:%s" pan pan))
     (setq emacspeak-m-player-panner (+ 0.1  emacspeak-m-player-panner))
-    (when (= 1 emacspeak-m-player-panner) (setq emacspeak-m-player-panner -1))
+    (when (= 1 (round emacspeak-m-player-panner)) (setq emacspeak-m-player-panner -1))
     (message "Panned  to %.1f %.1f" (- 1 this) this)))
 
 ;;}}}
