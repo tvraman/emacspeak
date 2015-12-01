fc98ba553de3c907def68096a15b9bdf9b1f4fcb
raman <tv.raman.tv@gmail.com>
2015-11-30 16:12:26 -0800

    adding theme pan-chimes

---
+++ sounds/pan-chimes/define-theme.el
@@ -0,0 +1,6 @@
+;;; Wave icons using chimes as a theme
+;;; Icons from theme Chimes with effect autopan applied using mplayer and ladspa
+
+(emacspeak-sounds-define-theme
+ (expand-file-name "pan-chimes/" emacspeak-sounds-directory)
+ ".wav")
