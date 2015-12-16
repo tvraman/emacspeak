ef69b2f6e5daf2263eeb368a97c4675ef3561e7c
raman <tv.raman.tv@gmail.com>
2015-12-16 08:49:56 -0800

    smarter update hook

--- lisp/soundscape.el
+++ lisp/soundscape.el
@@ -297,10 +297,8 @@ Do not set this by hand, use command \\[soundscape-toggle].")
   "Hook function to update Soundscape automatically."
   (declare (special soundscape-auto soundscape-last-mode))
   (when (and soundscape-auto
-             ;(not (eq major-mode soundscape-last-mode))
-             ;(not (eq 'minibuffer-inactive-mode major-mode))
-             ;(not (string-match "^ \\*temp*" (buffer-name )))
-             )
+             (not (eq major-mode soundscape-last-mode))
+             (not (eq 'minibuffer-inactive-mode major-mode)))
     (setq soundscape-last-mode major-mode)
     (soundscape-activate major-mode)))
 
