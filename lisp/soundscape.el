5de5d09a5e9bc3abad2d246688fd7147aa2ec122
raman <tv.raman.tv@gmail.com>
2015-12-16 20:05:26 -0800

    updated web mood  --- use background waves with vol turned down

--- lisp/soundscape.el
+++ lisp/soundscape.el
@@ -254,7 +254,7 @@ See  \\{soundscape-default-theme} for details."
     ("LightWind" (comint-mode elfeed-search-mode))
     ("Steady" (calendar-mode diary-mode))
     ( "Cavern" (prog-mode))
-    ("WaterFlow"  ,soundscape-web-modes)
+    ("BackgroundWaves"  ,soundscape-web-modes)
     ( "Still" (text-mode))
     ("Water"  (dired-mode))
     ("BuddhaLoop" (fundamental-mode))
