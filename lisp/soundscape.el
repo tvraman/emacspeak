993e92af070b6afe04dd1be8fe8905268544ad91
raman <tv.raman.tv@gmail.com>
2015-12-20 15:03:01 -0800

    add ChangingLoops for emacspeak-m-player-mode

--- lisp/soundscape.el
+++ lisp/soundscape.el
@@ -269,6 +269,7 @@ See  \\{soundscape-default-theme} for details."
 
 (defconst soundscape-default-theme
   `(
+    ("ChangingLoops" (emacspeak-m-player-mode))
     ("NoStormYet"  ( fundamental-mode))
     ("TonkSpace" (tabulated-list-mode))
     ("LightWind" (comint-mode elfeed-search-mode))
