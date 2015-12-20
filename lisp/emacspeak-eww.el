7d631f68cbf5f397405cf16249c45a4d7b439eed
raman <tv.raman.tv@gmail.com>
2015-12-20 09:02:09 -0800

    eww: dynamically set soundscape-auto to nil in eww-follow since eww is asynchronous

--- lisp/emacspeak-eww.el
+++ lisp/emacspeak-eww.el
@@ -518,15 +518,16 @@ Retain previously set punctuations  mode."
 
 (defadvice eww-follow-link (around emacspeak pre act comp)
   "Respect emacspeak-we-url-executor if set."
-  (emacspeak-auditory-icon 'button)
-  (cond
+  (let ((soundscape-auto nil))
+    (emacspeak-auditory-icon 'button)
+    (cond
    ((and (ems-interactive-p)
          (boundp 'emacspeak-we-url-executor)
          (fboundp emacspeak-we-url-executor))
     (let ((url (get-text-property (point) 'shr-url)))
       (unless url (error "No URL  under point"))
       (funcall emacspeak-we-url-executor url)))
-   (t ad-do-it)))
+   (t ad-do-it))))
 
 ;;}}}
 ;;{{{ xslt transform on request:
