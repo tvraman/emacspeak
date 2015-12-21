d01c6d248783041738944523678df491f9703323
T. V. Raman <tv.raman.tv@gmail.com>
2015-12-21 13:23:23 -0800

    wip

---
+++ scapes/listen/main.py
@@ -0,0 +1,43 @@
+# org.emacspeak.listen
+
+from boopak.package import *
+from boodle import agent, builtin
+from boopak.argdef import *
+
+
+class Catalog(agent.Agent):
+  """Recieves a list of agents.
+  Accepts remote commands to start and stop these received agents."""
+  _args = ArgList(ArgExtra(ListOf(Wrapped(agent.Agent))))
+  selected_event = 'remote'
+
+  def init(self, *agents):
+    if (len(agents) == 0):
+      raise Exception('Catalog requires at least one agent argument')
+    self.classlist = agents
+    self.fadetime = 2.0
+    self.pos = 0
+    self.workagent = None
+
+  def run(self):
+    self.workagent = Agents(self.fadetime)
+    self.post_listener_agent(self.workagent, hold=True)
+    self.listen()
+    self.workagent.trigger(self.classlist[self.pos])
+
+
+  def receive(self, event):
+    key = event.split('.')[-1]
+    newpos = self.pos
+    count = len(self.classlist)
+    if (key == 'chanup'):
+      newpos = ((self.pos+1) % count)
+    elif (key == 'chandown'):
+      newpos = ((self.pos+count-1) % count)
+    elif (key in ['num1', 'num2', 'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'num9']):
+      val = int(key[-1]) - 1
+      if (val >= 0 and val < count):
+        newpos = val
+        if (newpos != self.pos):
+          self.pos = newpos
+          self.workagent.trigger(self.classlist[self.pos])
