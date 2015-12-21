21b810053c87dee805f81ccb7c8b355343226a8d
T. V. Raman <tv.raman.tv@gmail.com>
2015-12-21 14:03:28 -0800

    wip

--- scapes/listen/main.py
+++ scapes/listen/main.py
@@ -6,59 +6,56 @@ from boopak.argdef import *
 
 
 class Agents(agent.Agent):
-  selected_event = 'agent'
+    selected_event = 'agent'
 
-  def init(self, fadetime=2.0):
-    self.fadetime = fadetime
-    self.prevchannel = None
+    def init(self, fadetime=2.0):
+        self.fadetime = fadetime
+        self.prevchannel = None
 
-  def run(self):
-    self.listen(hold=True)
+    def run(self):
+        self.listen(hold=True)
 
-  def receive(self, event, *arglist):
-    clas = self.load_described(arglist)
-    self.trigger(clas)
+    def receive(self, event, *arglist):
+        clas = self.load_described(arglist)
+        self.trigger(clas)
 
-  def trigger(self, clas):
-    if (self.prevchannel != None and self.prevchannel.active):
-      self.sched_agent(builtin.FadeOutAgent(self.fadetime), chan=self.prevchannel)
-      self.prevchannel = self.new_channel(0)
-      self.prevchannel.set_volume(1, self.fadetime)
-      self.sched_agent(clas(), chan=self.prevchannel)
+    def trigger(self, clas):
+        if (self.prevchannel != None and self.prevchannel.active):
+            self.sched_agent(builtin.FadeOutAgent(
+                self.fadetime), chan=self.prevchannel)
+            self.prevchannel = self.new_channel(0)
+            self.prevchannel.set_volume(1, self.fadetime)
+            self.sched_agent(clas(), chan=self.prevchannel)
+
 
 class Catalog(agent.Agent):
-  """Recieves a list of agents.
-  Accepts remote commands to start and stop these received agents."""
-  _args = ArgList(ArgExtra(ListOf(Wrapped(agent.Agent))))
-  selected_event = 'remote'
 
-  def init(self, *agents):
-    if (len(agents) == 0):
-      raise Exception('Catalog requires at least one agent argument')
-    self.classlist = agents
-    self.fadetime = 2.0
-    self.pos = 0
-    self.workagent = None
+    """Recieves a list of agents.
+    Accepts remote commands to start and stop these received agents."""
+    _args = ArgList(ArgExtra(ListOf(Wrapped(agent.Agent))))
+    selected_event = 'remote'
 
-  def run(self):
-    self.workagent = Agents(self.fadetime)
-    self.post_listener_agent(self.workagent, hold=True)
-    self.listen()
-    self.workagent.trigger(self.classlist[self.pos])
+    def init(self, *agents):
+        if (len(agents) == 0):
+            raise Exception('Catalog requires at least one agent argument')
+        self.classlist = agents
+        self.fadetime = 2.0
+        self.pos = 0
+        self.workagent = None
 
+    def run(self):
+        self.workagent = Agents(self.fadetime)
+        self.post_listener_agent(self.workagent, hold=True)
+        self.listen()
+        self.workagent.trigger(self.classlist[self.pos])
 
-  def receive(self, event):
-    key = event.split('.')[-1]
-    newpos = self.pos
-    count = len(self.classlist)
-    if (key == 'chanup'):
-      newpos = ((self.pos+1) % count)
-    elif (key == 'chandown'):
-      newpos = ((self.pos+count-1) % count)
-    elif (key in ['num1', 'num2', 'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'num9']):
-      val = int(key[-1]) - 1
-      if (val >= 0 and val < count):
-        newpos = val
-        if (newpos != self.pos):
-          self.pos = newpos
-          self.workagent.trigger(self.classlist[self.pos])
+    def receive(self, event):
+        key = event.split('.')[-1]
+        newpos = self.pos
+        count = len(self.classlist)
+        val = int(key)
+        if (val >= 0 and val < count):
+          newpos = val
+          if (newpos != self.pos):
+            self.pos = newpos
+            self.workagent.trigger(self.classlist[self.pos])
