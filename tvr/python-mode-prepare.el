32fa946bf7405734fea0cc148c5736398ff2d044
raman <tv.raman.tv@gmail.com>
2015-12-22 15:20:55 -0800

    updated py setup

--- tvr/python-mode-prepare.el
+++ tvr/python-mode-prepare.el
@@ -7,6 +7,5 @@
 (add-hook
  'python-mode-hook
  #'(lambda ()
-     (when (locate-library "company")
-       (company-mode  1))
+     (when (locate-library "company") (company-mode  1))
      (when (locate-library "elpy") (elpy-enable))))
