dba855941637fbd51c03c01ab540b814a414a0a4
raman <tv.raman.tv@gmail.com>
2015-12-29 20:04:41 -0800

    Added nightscape Nocturnal

--- scapes/nature/main.py
+++ scapes/nature/main.py
@@ -12,6 +12,8 @@ water = bimport('org.boodler.sample.water')
 oldwater = bimport('org.boodler.old.water')
 wind = bimport('org.boodler.sample.wind')
 rain = bimport('org.boodler.old.water')
+cricket = bimport('org.boodler.old.insect')
+trill = bimport('org.boodler.sample.insect')
 
 ca_mocks = [
     birds.mocking_1, birds.mocking_2, birds.mocking_3,  # CA Mocking Bird
@@ -24,9 +26,6 @@ showers = [
     rain.rain_thin, rain.rain_splatter,
     rain.rain_splashy_low, rain.rain_on_leaves,
     rain.rain_med]
-    
-    
-    
 
 streams = [
     water.stream_rushing_1, water.stream_rushing_2, water.stream_rushing_3]
@@ -37,6 +36,24 @@ winds = [
     wind.gust_soft_1, wind.gust_soft_2, wind.gust_soft_3,
     wind.soft_whistly_1, wind.soft_whistly_2, wind.soft_whistly_3,
     wind.soft_rushy]
+
+bugs = [
+    cricket.complex_trilling_trig, cricket.everglades_conehead,
+    cricket.fastcalling_tree_cricket, cricket.pine_tree_cricket,
+    cricket.house_cricket, cricket.jamaican_field_cricket,
+    cricket.japanese_burrowing_cricket, cricket.melodious_ground_cricket,
+    cricket.robust_shieldback, cricket.sand_field_cricket,
+    cricket.seashore_meadow_katydid, cricket.slightly_musical_conehead,
+    cricket.southern_ground_cricket, cricket.syncopated_scaly_cricket,
+    cricket.texas_meadow_katydid, cricket.tinking_trig,
+    cricket.tinkling_ground_cricket, cricket.tropical_house_cricket,
+    cricket.vocal_field_cricket, trill.cricket_loud,
+    trill.cricket_ratchety, trill.cricket_scritchy,
+    trill.crickets_distant, trill.crickets_low,
+    trill.crickets_many_1, trill.cricket_solo_1,
+    trill.cricket_solo_2, trill.cricket_solo_3,
+    trill.crickets_one_swell,
+]
 
 
 # helper: Pendulum generator:
@@ -58,12 +75,12 @@ class GardenBackground (agent.Agent):
         self.pendulum = pendulum(30)
 
     def run(self):
-        count = self.pendulum.next() # [0, 30]
+        count = self.pendulum.next()  # [0, 30]
         gurgle = random.choice(streams)
         breeze = random.choice(winds)
-        vol =random.uniform(0.2, 0.6)
+        vol = random.uniform(0.2, 0.6)
         if (count < 7 or count > 22):
-            pitch = random.uniform(0.6,1.1)
+            pitch = random.uniform(0.6, 1.1)
         else:
             pitch = random.uniform(0.9, 1.4)
         pan = (count - 15) * 0.1  # [-1.5, 1.5]
@@ -71,8 +88,8 @@ class GardenBackground (agent.Agent):
         self.sched_note_pan(breeze, -1 * pan, pitch, vol,  self.time + d0)
         if ((count % 6) == 0):
             shower = random.choice(showers)
-            self.sched_note_pan(shower, -1.2 * pan, 1.0, vol*0.2,  self.time )
-        self.resched(d0  + random.uniform(-1.0, 0.1))
+            self.sched_note_pan(shower, -1.2 * pan, 1.0, vol * 0.2,  self.time)
+        self.resched(d0 + random.uniform(-1.0, 0.1))
 
 
 class FlMockingBirds(agent.Agent):
@@ -165,5 +182,46 @@ class ManyMockingBirds (agent.Agent):
             ag = FlMockingBirds(
                 7.0, 97.0,
                 0.15, 0.35,
+                1.2)
+            self.sched_agent(ag)
+
+
+class Crickets(agent.Agent):
+
+    _args = ArgList(Arg(type=float), Arg(type=float), Arg(type=float),
+                    Arg(type=float), Arg(type=float))
+
+    def init(self,
+             minDelay=0.0,
+             maxDelay=9.0,
+             minVol=0.1,
+             maxVol=1.0,
+             pan=1.0):
+        self.minDelay = minDelay
+        self.maxDelay = maxDelay
+        self.minVol = minVol
+        self.maxVol = maxVol
+        self.pan = pan
+
+    def run(self):
+        ag = play.IntermittentSoundsList(
+            self.minDelay, self.maxDelay,
+            1.0, 1.0,  # pitch
+            self.minVol, self.maxVol,
+            self.pan,
+            bugs)
+        self.sched_agent(ag)
+
+
+class Nocturnal (agent.Agent):
+
+    def run(self):
+        nature = GardenBackground(0.0)
+        self.sched_agent(nature)
+
+        for i in xrange(20):
+            ag = Crickets(
+                0.0, 90.0,
+                0.15, 0.5,
                 1.2)
             self.sched_agent(ag)
