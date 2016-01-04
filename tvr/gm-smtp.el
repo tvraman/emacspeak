cbe9c83fd3702a823b9054ddc8cbdb48128d798a
raman <tv.raman.tv@gmail.com>
2016-01-03 18:45:21 -0800

    gm-smtp: use async

--- tvr/gm-smtp.el
+++ tvr/gm-smtp.el
@@ -1,13 +1,15 @@
 ;;; Configuring Emacs to send GMail:
-;;; In addition:
-;;; authinfo.gpg should contain:
-;;; 1 line for outgoing smtp 
-;;; 1 line for incoming imap (for reading mail)
-(require 'smtpmail)(setq send-mail-function 'smtpmail-send-it)
+;;; Auth credentials are taken from .authinfo.gpg
+
+(require 'smtpmail)
+
 (setq
  send-mail-function 'smtpmail-send-it
- user-mail-address "username@gmail.com"
- smtpmail-default-smtp-server "smtp.gmail.com"
  smtpmail-smtp-server "smtp.gmail.com"
- smtpmail-smtp-service 587
- )
+ smtpmail-smtp-service 587)
+
+(when (locate-library "smtpmail-async")
+  (require 'smtpmail-async)
+  (setq send-mail-function 'async-smtpmail-send-it
+        message-send-mail-function 'async-smtpmail-send-it))
+(provide 'gm-smtp)
