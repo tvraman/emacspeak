c2e0b83c2d2f80a317a3fa9c7259b4b790742b5c
T. V. Raman <tv.raman.tv@gmail.com>
2016-01-08 11:30:18 -0800

    added soft-hyphen to replacement list

--- lisp/dtk-unicode.el
+++ lisp/dtk-unicode.el
@@ -79,6 +79,7 @@
     (?“ . "\"")                     ;LEFT DOUBLE QUOTATION MARK
     (?” . "\"")                     ; RIGHT DOUBLE QUOTATION MARK
     (?⋆ . "*")                      ; STAR OPERATOR
+    (?­ .  "-") ; soft-hyphen 
     (?‘ . " backquote  ")           ; LEFT SINGLE QUOTATION MARK
     (?’ . "'")                      ; right SINGLE QUOTATION MARK
     (?‐ . "-")                      ; hyphen
