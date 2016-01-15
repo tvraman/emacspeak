7f5c53bfba4f4ce9cd8ba5654e0cee38b22d4c17
T. V. Raman <tv.raman.tv@gmail.com>
2016-01-15 14:56:59 -0800

    tvr: setup  update

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -1,25 +1,30 @@
-;;{{{ Emacs initialization file for Raman:
+;;{{{ History: Emacs initialization file for Raman:
+
 ;;; $Id: emacs-startup.el 7753 2012-05-06 22:43:52Z tv.raman.tv $
 ;;; Segre March 22 1991
 ;;; July 15, 2001 finally cutting over to custom.
 ;;; August 12, 2007: Cleaned up for Emacs 22
+
 ;;}}}
 ;;{{{ personal lib
-(setq inhibit-startup-echo-area-message "raman")
-(defvar emacs-private-library
-  (expand-file-name "~/.elisp")
+
+(setq inhibit-startup-echo-area-message user-login-name)
+
+(defvar emacs-private-library (expand-file-name "~/.elisp")
   "Private personalization directory. ")
 
 (defvar emacs-personal-library
   (expand-file-name "~/emacs/lisp/site-lisp")
-  "Directory where we keep personal libraries")
+  "Directory where we keep personal libraries.
+These are exported to GitHub under emacspeak/tvr.")
 
 ;;}}}
 ;;{{{ helper functions:
 
+;;; This is for setting variables customized via custom.
+
 (defmacro csetq (variable value)
   `(funcall (or (get ',variable 'custom-set) 'set-default) ',variable ,value))
-
 
 (defsubst augment-load-path (path &optional library whence at-end)
   "add directory to load path.
@@ -70,30 +75,38 @@ Path is resolved relative to `whence' which defaults to emacs-personal-library."
 (defun start-up-my-emacs()
   "Start up emacs for me. "
   (declare (special emacs-personal-library emacs-private-library))
-  (let ((gc-cons-threshold 8000000)
-        (debug-on-quit t)
-        (debug-on-error t))
-(csetq tool-bar-mode nil)
-(csetq menu-bar-mode nil)
+  (let ((gc-cons-threshold 8000000))
+    ;;{{{ Basic Look And Feel:
 
-(csetq message-log-max 1024)
-(tooltip-mode -1)
+    (tooltip-mode -1)
     (menu-bar-mode -1)
     (tool-bar-mode -1)
     (scroll-bar-mode -1)
-    (fringe-mode 1)
+    (fringe-mode 0)
     (setq text-quoting-style 'grave)
     (setq outline-minor-mode-prefix "\C-x@h")
 
+    (put 'upcase-region 'disabled nil)
+    (put 'downcase-region 'disabled nil)
+    (put 'narrow-to-region 'disabled nil)
+    (put 'eval-expression 'disabled nil)
+
+    ;;}}}
+    ;;{{{ Augment Load Path:
+
     (when (file-exists-p  emacs-private-library)
       (augment-load-path emacs-private-library ))
-    
-(when (file-exists-p  emacs-personal-library)
+
+    (when (file-exists-p  emacs-personal-library)
       (augment-load-path emacs-personal-library))
+
+    ;;}}}
     ;;{{{ Load and customize emacspeak
 
     (unless (featurep 'emacspeak)
-      (load-file (expand-file-name "~/emacs/lisp/emacspeak/lisp/emacspeak-setup.el")))
+      (load-file
+       (expand-file-name "~/emacs/lisp/emacspeak/lisp/emacspeak-setup.el")))
+
     (when (featurep 'emacspeak) (emacspeak-sounds-select-theme "3d/"))
 
     ;;}}}
@@ -110,7 +123,7 @@ Path is resolved relative to `whence' which defaults to emacs-personal-library."
             ([f5] emacspeak-pianobar)
             ([pause] dtk-stop)
             ("\M--" undo)
-            ([delete]dtk-toggle-punctuation-mode)
+            ([f6] dtk-cloud)
             ( [f8]emacspeak-remote-quick-connect-to-server)
             ([f11]shell)
             ([f12]vm)
@@ -124,22 +137,16 @@ Path is resolved relative to `whence' which defaults to emacs-personal-library."
           (global-set-key (first key) (second key)))
 
 ;;; Smarten up ctl-x-map
-(define-key ctl-x-map "\C-n" 'forward-page)
-(define-key ctl-x-map "\C-p" 'backward-page)
+    (define-key ctl-x-map "\C-n" 'forward-page)
+    (define-key ctl-x-map "\C-p" 'backward-page)
 
     ;;}}}
-    ;;{{{  Basic Support Libraries 
+    ;;{{{  Basic Support Libraries
 
     (require 'dired-x)
     (require 'dired-aux)
-
-    (put 'upcase-region 'disabled nil)
-    (put 'downcase-region 'disabled nil)
-    (put 'narrow-to-region 'disabled nil)
-    (put 'eval-expression 'disabled nil)
-
     (dynamic-completion-mode)
-(unless enable-completion (completion-mode ))
+    (unless enable-completion (completion-mode ))
 
     ;;}}}
     ;;{{{  different mode settings
@@ -150,25 +157,24 @@ Path is resolved relative to `whence' which defaults to emacs-personal-library."
         "shell"
       '(progn
          (define-key shell-mode-map "\C-cr" 'comint-redirect-send-command)
-         (define-key shell-mode-map "\C-ch" '
-           emacspeak-wizards-refresh-shell-history)))
+         (define-key shell-mode-map "\C-ch" 'emacspeak-wizards-refresh-shell-history)))
 
     ;;}}}
     ;;{{{ Prepare needed libraries
 
     (package-initialize)
+
     (mapc
      #'load-library-if-available
      '(
        "my-functions"
-;;; Mail readers:
+;;; Mail:
        "vm-prepare" "gm-smtp" "gnus-prepare" "bbdb-prepare"
        "mspools-prepare" "sigbegone"
-;;; Web Browsers:
-       "w3-prepare"
+;;; Web:
+       "w3-prepare" "elfeed-prepare"
        ;;; Authoring:
        "auctex-prepare" "nxml-prepare" "folding-prepare"
-       "elfeed-prepare"
        "calc-prepare"
        "hydra-prepare"
        "tcl-prepare" "slime-prepare" "company-prepare" "python-mode-prepare"
@@ -176,36 +182,39 @@ Path is resolved relative to `whence' which defaults to emacs-personal-library."
                                         ;"jde-prepare" "ecb-prepare"
        "org-prepare"
        "erc-prepare" "jabber-prepare" "twittering-prepare"
-       "tramp-prepare" "fff-prepare" "fap-prepare"
+       "tramp-prepare"  "fap-prepare"
        "emms-prepare" "iplayer-prepare"
-"auto-correct-setup"
-"color-theme-prepare"
-       "local"
-       "emacspeak-dbus"))
+       "auto-correct-setup"
+       "color-theme-prepare"
+       "local"))
 
     ;;}}}
+;;{{{ turn on modes:
+
+(initialize-completions)
+(savehist-mode )
+     (save-place-mode)
+     (midnight-mode)
+(server-start)
+(bbdb-insinuate-vm)
+
+;;}}}
     ))                                  ; end defun
 ;;{{{  start it up
 
 (add-hook
  'after-init-hook
  #'(lambda ()
-     (savehist-mode )
-     (save-place-mode)
-     (midnight-mode)
      (emacspeak-tts-startup-hook)
      (soundscape-toggle)
-     (bbdb-insinuate-vm)
-     (server-start)
      (shell)
      (calendar)
-     (initialize-completions)
-     (load-library "emacspeak-m-player")
-     (shell-command "aplay ~/cues/highbells.au")
      (setq frame-title-format '(multiple-frames "%b" ( "Emacs")))
+     (shell-command "aplay ~/cues/highbells.au")
      (message "Successfully initialized Emacs")))
 (when (file-exists-p custom-file) (load-file custom-file))
 (start-up-my-emacs)
+
 ;;}}}
 (provide 'emacs-startup)
 ;;{{{  emacs local variables
