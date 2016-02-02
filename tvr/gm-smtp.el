66ed779530e1687e17e06273837159a7cb9bc2c0
raman <tv.raman.tv@gmail.com>
2016-02-01 17:45:17 -0800

    updated gm-smtp

--- tvr/gm-smtp.el
+++ tvr/gm-smtp.el
@@ -7,9 +7,10 @@
  send-mail-function 'smtpmail-send-it
  smtpmail-smtp-server "smtp.gmail.com"
  smtpmail-smtp-service 587)
+;;; Have had mail go awol with async (commenting out)
+;; (when (locate-library "smtpmail-async")
+;;   (require 'smtpmail-async)
+;;   (setq send-mail-function 'async-smtpmail-send-it
+;;         message-send-mail-function 'async-smtpmail-send-it))
 
-(when (locate-library "smtpmail-async")
-  (require 'smtpmail-async)
-  (setq send-mail-function 'async-smtpmail-send-it
-        message-send-mail-function 'async-smtpmail-send-it))
 (provide 'gm-smtp)
