66f6d0c3dd9cdba1e3ff334efbc89c68a535214f
raman <tv.raman.tv@gmail.com>
2016-02-05 12:58:18 -0800

    fixed bug in nightscape

--- scapes/nature/main.py
+++ scapes/nature/main.py
@@ -269,7 +269,7 @@ class Nightscape (agent.Agent):
             ag = Crickets(
                 0.0, 12.0,
                 0.1, 0.18, 1.2)
-            self.sched_agent(ag, i * 10, y)
+            self.sched_agent(ag, 0, nc)
 
 
 class BirdChorus (agent.Agent):
