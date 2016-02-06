8222565be82d524ebabcd4fac2b81e01ea11b448
raman <tv.raman.tv@gmail.com>
2016-02-05 20:32:05 -0800

    tightened durations, adjusted y

--- scapes/nature/main.py
+++ scapes/nature/main.py
@@ -313,12 +313,12 @@ class BirdSongs (agent.Agent):
             for j in xrange(8):
                 # compute y using i and j
                 # i = 0 approaches, i=1 no change, i=2 recedes
-                y = (i - 1) * (1.7 - j * 0.1)
+                y = (i - 1) * (1.4 - j * 0.05)
                 bc = self.new_channel_pan(
                     stereo.compose(stereo.scalexy(1.5), stereo.shiftxy(0, y)))
                 start = 15 * (i + j)
                 ag = self.agents[i](
-                    start, 60 + start - 5 * j,  # duration shortens through the loop
+                    start, 45 + start - 5 * j,  # duration shortens through the loop
                     0.35, 0.5,  # volume
                     1 + j * 0.05  # pan
                 )
