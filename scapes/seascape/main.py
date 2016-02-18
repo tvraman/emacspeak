ee1bded94b06282c5ef150503cb7001d8be4e1d8
raman <tv.raman.tv@gmail.com>
2016-02-17 19:54:26 -0800

    Smoother, but more varying seascape for web surfing

--- scapes/seascape/main.py
+++ scapes/seascape/main.py
@@ -4,17 +4,13 @@ from boodle import agent
 from boodle import builtin
 
 play = bimport('org.boodler.play')
-manage = bimport('org.boodler.manage')
+
 
 water = bimport('org.boodler.old.water')
 
 wavesounds = [
-    water.waves_lapping,
-    water.waves_light,
-    water.waves_rough,
-    water.waves_floopy,
-    water.water_rushing,
-    water.water_pouring,
+    water.waves_lapping, water.waves_light, water.waves_rough,
+    water.waves_floopy, water.water_rushing, water.water_pouring,
     water.water_rapids]
 
 
@@ -22,7 +18,7 @@ class SurfWaves(agent.Agent):
     """Orchestrate wave agents SurfWaveSounds and SurfBackgroundWaves"""
 
     def run(self):
-        for i in range(8):
+        for i in range(12):
             ag = SurfBackgroundWaves()
             self.sched_agent(ag)
         for i in range(16):
@@ -34,20 +30,19 @@ class SurfWaveSounds(agent.Agent):
 
     def run(self):
         ag = play.IntermittentSoundsList(
-            mindelay=1.0, maxdelay=5.0,
+            mindelay=1.0, maxdelay=15.0,
             minpitch=0.2, maxpitch=1.0,
-            minvol=0.1, maxvol=0.5,
+            minvol=0.02, maxvol=0.1,
             maxpan=1.5, sounds=wavesounds)
-        ag2 = manage.VolumeModulateAgent(ag, 0.025)
-        self.sched_agent(ag2)
+        self.sched_agent(ag)
 
 
 class SurfBackgroundWaves(agent.Agent):
 
     def run(self):
         p = random.uniform(0.2, 1.0)
-        v = random.uniform(0.05, 0.15)
-        d = random.uniform(0.3, 2.0)
+        v = random.uniform(0.01, 0.1)
+        d = random.uniform(0.3, 5.0)
         pan = random.uniform(-1.5, 1.5)
         dur = self.sched_note_pan(water.waves_light, pan, pitch=p, volume=v)
         self.resched(dur * d)
