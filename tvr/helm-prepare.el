e771d91e6ed721bfc7d4fdfd73c1ffe3cc936810
raman <tv.raman.tv@gmail.com>
2016-03-02 17:24:26 -0800

    updated helm-prepare

--- tvr/helm-prepare.el
+++ tvr/helm-prepare.el
@@ -1,2 +1,25 @@
+;;; Taken from http://tuhdo.github.io/helm-intro.html
+
+(require 'helm)
 (require 'helm-config)
+
+;;; Use alt-h as the helm prefix 
+(global-set-key (kbd "C-x @ah") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
 (helm-mode 1)
+
+
