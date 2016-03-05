2b4793f2d37f2b4e863cddd00666d27b8fbc6b42
T. V. Raman <tv.raman.tv@gmail.com>
2016-03-04 16:56:29 -0800

    dont turn on helm by default.

--- tvr/helm-prepare.el
+++ tvr/helm-prepare.el
@@ -19,10 +19,6 @@
       helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
       helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
       helm-ff-file-name-history-use-recentf t)
-
-(helm-mode 1)
-
-
 (global-set-key (kbd "C-x @h;;") 'helm-mode)
 (global-set-key (kbd "C-x @h; M-:") 'helm-eval-expression-with-eldoc)
 (define-key helm-map (kbd "C-s") 'helm-toggle-suspend-update)
