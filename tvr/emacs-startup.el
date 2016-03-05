eb1ac8263c75c9f50d50d0b9764711d32b8e015e
T. V. Raman <tv.raman.tv@gmail.com>
2016-03-04 16:57:31 -0800

    load helm-prepare

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -180,6 +180,7 @@ Path is resolved relative to `whence' which defaults to emacs-personal-library."
        "calc-prepare"
        "hydra-prepare"
        "tcl-prepare" "slime-prepare" "company-prepare" "python-mode-prepare"
+       "helm-prepare"
                                         ; jde and ecb will pull in cedet.
                                         ;"jde-prepare" "ecb-prepare"
        "org-prepare"
