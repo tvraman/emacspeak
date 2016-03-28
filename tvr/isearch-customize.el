6e37b2dc38edfab54bc167b8d4ded7ca17bd0633
raman <tv.raman.tv@gmail.com>
2016-03-27 17:23:01 -0700

    added isearch customize in tvr

---
+++ tvr/isearch-customize.el
@@ -0,0 +1,10 @@
+
+(define-key isearch-mode-map [(control return)]
+  #'isearch-exit-other-end)
+
+(defun isearch-exit-other-end ()
+  "Exit isearch, at the opposite end of the string."
+  (interactive)
+  (isearch-exit)
+  (goto-char isearch-other-end))
+
