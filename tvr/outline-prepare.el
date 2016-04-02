43fe3ccbfbd0583eb16f7497377a7c8f229c3aee
raman <tv.raman.tv@gmail.com>
2016-04-02 11:28:35 -0700

    updated outline setup

--- tvr/outline-prepare.el
+++ tvr/outline-prepare.el
@@ -1,23 +1,23 @@
 ; Outline-minor-mode key map
-(define-prefix-command 'cm-map nil "Outline-")
+(define-prefix-command 'tvr-outline-map nil "Outline-")
 ; HIDE
-(define-key cm-map "q" 'hide-sublevels)    ; Hide everything but the top-level headings
-(define-key cm-map "t" 'hide-body)         ; Hide everything but headings (all body lines)
-(define-key cm-map "o" 'hide-other)        ; Hide other branches
-(define-key cm-map "c" 'hide-entry)        ; Hide this entry's body
-(define-key cm-map "l" 'hide-leaves)       ; Hide body lines in this entry and sub-entries
-(define-key cm-map "d" 'hide-subtree)      ; Hide everything in this entry and sub-entries
+(define-key tvr-outline-map "q" 'hide-sublevels)    ; Hide everything but the top-level headings
+(define-key tvr-outline-map "t" 'hide-body)         ; Hide everything but headings (all body lines)
+(define-key tvr-outline-map "o" 'hide-other)        ; Hide other branches
+(define-key tvr-outline-map "c" 'hide-entry)        ; Hide this entry's body
+(define-key tvr-outline-map "l" 'hide-leaves)       ; Hide body lines in this entry and sub-entries
+(define-key tvr-outline-map "d" 'hide-subtree)      ; Hide everything in this entry and sub-entries
 ; SHOW
-(define-key cm-map "a" 'show-all)          ; Show (expand) everything
-(define-key cm-map "e" 'show-entry)        ; Show this heading's body
-(define-key cm-map "i" 'show-children)     ; Show this heading's immediate child sub-headings
-(define-key cm-map "k" 'show-branches)     ; Show all sub-headings under this heading
-(define-key cm-map "s" 'show-subtree)      ; Show (expand) everything in this heading & below
+(define-key tvr-outline-map "a" 'show-all)          ; Show (expand) everything
+(define-key tvr-outline-map "e" 'show-entry)        ; Show this heading's body
+(define-key tvr-outline-map "i" 'show-children)     ; Show this heading's immediate child sub-headings
+(define-key tvr-outline-map "k" 'show-branches)     ; Show all sub-headings under this heading
+(define-key tvr-outline-map "s" 'show-subtree)      ; Show (expand) everything in this heading & below
 ; MOVE
-(define-key cm-map "u" 'outline-up-heading)                ; Up
-(define-key cm-map "n" 'outline-next-visible-heading)      ; Next
-(define-key cm-map "p" 'outline-previous-visible-heading)  ; Previous
-(define-key cm-map "f" 'outline-forward-same-level)        ; Forward - same level
-(define-key cm-map "b" 'outline-backward-same-level)       ; Backward - same level
-(global-set-key "\M-\C-o" cm-map)
+(define-key tvr-outline-map "u" 'outline-up-heading)                ; Up
+(define-key tvr-outline-map "n" 'outline-next-visible-heading)      ; Next
+(define-key tvr-outline-map "p" 'outline-previous-visible-heading)  ; Previous
+(define-key tvr-outline-map "f" 'outline-forward-same-level)        ; Forward - same level
+(define-key tvr-outline-map "b" 'outline-backward-same-level)       ; Backward - same level
+(global-set-key "\C-o" tvr-outline-map)
 
