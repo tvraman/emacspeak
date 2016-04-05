4ccaee919ad6046303138f1073b9e50684d0cbab
raman <tv.raman.tv@gmail.com>
2016-04-04 20:03:38 -0700

    turn up birds in the background.

--- scapes/seascape/main.py
+++ scapes/seascape/main.py
@@ -23,7 +23,7 @@ class SurfWaves(agent.Agent):
             stereo.compose(stereo.scalexy(1.2), stereo.shiftxy(0, 1.25)))
         ag = nature.Nightingales(
             0, 25,  # Duration
-            0.05, 0.75,  # volume
+            0.15, 0.75,  # volume
             1)
         self.sched_agent(ag, 0, bc)
 
@@ -31,7 +31,7 @@ class SurfWaves(agent.Agent):
             stereo.compose(stereo.scalexy(1.2), stereo.shiftxy(0, -1.25)))
         ag = nature.Cuckoos(
             0, 25,  # Duration
-            0.05, 0.75,  # volume
+            0.15, 0.75,  # volume
             1)
         self.sched_agent(ag, 0, bc)
         for i in range(16):
