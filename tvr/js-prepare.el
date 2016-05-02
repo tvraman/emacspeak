34bdd0b410a3ea4701eac423b15b26802935ab2a
raman <tv.raman.tv@gmail.com>
2016-05-01 19:27:01 -0700

    js setup

---
+++ tvr/js-prepare.el
@@ -0,0 +1,6 @@
+(when (locate-library "js2-mode")
+  (load "js2-mode-autoloads"))
+(when (locate-library "tern")
+  (load-library "tern-autoloads")
+  (add-hook 'js-mode-hook #'tern-mode)
+  (add-hook 'js2-mode-hook #'tern-mode))
