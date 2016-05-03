d9af89e5b68c0d40ad42fd158cd4a5d49a2890ed
T. V. Raman <tv.raman.tv@gmail.com>
2016-05-03 09:54:13 -0700

    initializing epa support

---
+++ lisp/emacspeak-epa.el
@@ -0,0 +1,120 @@
+;;; emacspeak-epa.el --- Speech-enable EasyPG Assistant
+;;; $Author: tv.raman.tv $
+;;; Description:  Speech-enable EPA An Emacs Interface to epa
+;;; Keywords: Emacspeak,  Audio Desktop epa
+;;{{{  LCD Archive entry:
+
+;;; LCD Archive Entry:
+;;; emacspeak| T. V. Raman |raman@cs.cornell.edu
+;;; A speech interface to Emacs |
+;;; $Date: 2007-05-03 18:13:44 -0700 (Thu, 03 May 2007) $ |
+;;;  $Revision: 4532 $ |
+;;; Location undetermined
+;;;
+
+;;}}}
+;;{{{  Copyright:
+;;;Copyright (C) 1995 -- 2007, 2011, T. V. Raman
+;;; Copyright (c) 1994, 1995 by Digital Equipment Corporation.
+;;; All Rights Reserved.
+;;;
+;;; This file is not part of GNU Emacs, but the same permissions apply.
+;;;
+;;; GNU Emacs is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2, or (at your option)
+;;; any later version.
+;;;
+;;; GNU Emacs is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNEPA FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Emacs; see the file COPYING.  If not, write to
+;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;}}}
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;{{{  introduction
+
+;;; Commentary:
+;;; EPA == EasyPG Assistant
+;;; Integrate GPG functionality into Emacs.
+;;; Speech-enable all interactive commands.
+
+;;; Code:
+
+;;}}}
+;;{{{  Required modules
+
+(require 'cl)
+(declaim  (optimize  (safety 0) (speed 3)))
+(require 'emacspeak-preamble)
+
+;;}}}
+;;{{{ Map Faces:
+
+;; 86:(defface epa-validity-high
+;;      92:(defface epa-validity-medium
+;;      98:(defface epa-validity-low
+;;     103:(defface epa-validity-disabled
+;;     108:(defface epa-string
+;;     116:(defface epa-mark
+;;     123:(defface epa-field-name
+;;     129:(defface epa-field-body
+
+;;}}}
+;;{{{ Advice Interactive Commands:
+
+;; ("epa-decrypt-armor-in-region"
+;; "epa-decrypt-file"
+;; "epa-decrypt-region"
+;; "epa-delete-keys"
+;; "epa-dired-do-decrypt"
+;; "epa-dired-do-encrypt"
+;; "epa-dired-do-sign"
+;; "epa-dired-do-verify"
+;; "epa-encrypt-file"
+;; "epa-encrypt-region"
+;; "epa-exit-buffer"
+;; "epa-export-keys"
+;; "epa-file-disable"
+;; "epa-file-enable"
+;; "epa-file-name-regexp-update"
+;; "epa-file-select-keys"
+;; "epa-global-mail-mode"
+;; "epa-import-armor-in-region"
+;; "epa-import-keys"
+;; "epa-import-keys-region"
+;; "epa-info-mode"
+;; "epa-insert-keys"
+;; "epa-key-list-mode"
+;; "epa-key-mode"
+;; "epa-list-keys"
+;; "epa-list-secret-keys"
+;; "epa-mail-decrypt"
+;; "epa-mail-encrypt"
+;; "epa-mail-import-keys"
+;; "epa-mail-mode"
+;; "epa-mail-sign"
+;; "epa-mail-verify"
+;; "epa-mark-key"
+;; "epa-sign-file"
+;; "epa-sign-region"
+;; "epa-unmark-key"
+;; "epa-verify-cleartext-in-region"
+;; "epa-verify-file"
+;; "epa-verify-region")
+
+;;}}}
+(provide 'emacspeak-epa)
+;;{{{ end of file
+
+;;; local variables:
+;;; folded-file: t
+;;; byte-compile-dynamic: t
+;;; end:
+
+;;}}}
