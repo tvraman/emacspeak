62a8b18b74f90a39d8a1cb423cb16d6b2d984960
raman <tv.raman.tv@gmail.com>
2016-05-14 12:58:10 -0700

    tuned seascape

--- scapes/seascape/main.py
+++ scapes/seascape/main.py
@@ -37,14 +37,14 @@ class SurfWaves(agent.Agent):
         for i in range(16):
             y = 1 + i * 0.025
             sc = self.new_channel_pan(
-                stereo.compose(stereo.scalexy(1.2), stereo.shiftxy(0, y)))
+                stereo.compose(stereo.scalexy(1.4), stereo.shiftxy(0, y)))
             ag = SurfBackgroundWaves()
             self.sched_agent(ag, i * 2, sc)
 
         for i in range(16):
             y = 1 + i * 0.025
             sc = self.new_channel_pan(
-                stereo.compose(stereo.scalexy(1.2), stereo.shiftxy(0, -y)))
+                stereo.compose(stereo.scalexy(1.4), stereo.shiftxy(0, y)))
             ag = SurfWaveSounds()
             self.sched_agent(ag, i * 8, sc)
 
@@ -53,7 +53,7 @@ class SurfWaveSounds(agent.Agent):
 
     def run(self):
         ag = play.IntermittentSoundsList(
-            mindelay=1.0, maxdelay=10.0,
+            mindelay=1.0, maxdelay=8.0,
             minpitch=0.2, maxpitch=1.0,
             minvol=0.02, maxvol=0.25,
             maxpan=1.25, sounds=wavesounds)
@@ -65,7 +65,7 @@ class SurfBackgroundWaves(agent.Agent):
     def run(self):
         p = random.uniform(0.2, 1.0)
         v = random.uniform(0.01, 0.1)
-        d = random.uniform(0.3, 8.0)
+        d = random.uniform(0.3, 12.0)
         pan = random.uniform(-1.25, 1.25)
         dur = self.sched_note_pan(water.waves_light, pan, pitch=p, volume=v)
         self.resched(dur * d)
