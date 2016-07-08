f0ff14fedadcd0cd4b5a5ceeb88c917f0f325c5c
T. V. Raman <tv.raman.tv@gmail.com>
2016-07-08 16:53:06 -0700

    restoring quotes using libfinance

--- etc/quotes.pl
+++ etc/quotes.pl
@@ -1,10 +1,8 @@
 #!/usr/bin/perl -w
 #$Id$
 use Finance::Quote;
-#my @symbols = <>;
-#chomp (@symbols);
-my @symbols =qw/ibm msft adbe goog googl fb/;
-
+my @symbols = <>;
+chomp (@symbols);
 my @labels=qw(
   ask avg_vol bid cap
 close currency date day_range
@@ -25,7 +23,7 @@ my $cl='';
 print join(",", @labels), "\n";
 foreach $sym (@symbols) {
   foreach $l (@labels) {
-my $v = $quotes{$sym.$cl.$l},;
+my $v = $quotes{$sym, $l},;
     print $v, ","  if defined ($v);
   }
   print "\n";
