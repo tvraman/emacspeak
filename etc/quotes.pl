109c16c5554be9ef849d74603c798de715c186c7
T. V. Raman <tv.raman.tv@gmail.com>
2016-07-08 16:57:00 -0700

    use strict

--- etc/quotes.pl
+++ etc/quotes.pl
@@ -1,30 +1,28 @@
 #!/usr/bin/perl -w
-#$Id$
+use strict;
 use Finance::Quote;
 my @symbols = <>;
 chomp (@symbols);
 my @labels=qw(
   ask avg_vol bid cap
-close currency date day_range
-div div_date div_yield eps
-ex_div high isodate last
-low method name net
-open p_change pe price
-success symbol time volume
-year_range);
+  close currency date day_range
+  div div_date div_yield eps
+  ex_div high isodate last
+  low method name net
+  open p_change pe price
+  success symbol time volume
+  year_range);
 
 $q = Finance::Quote->new;
-#$q->require_labels(@labels);
+$q->require_labels(@labels);
 %quotes  = $q->fetch("usa",@symbols);
-#print join (", ", @labels ),"\n";
-my $sep = q(",");
 
-my $cl='';
 print join(",", @labels), "\n";
 foreach $sym (@symbols) {
   foreach $l (@labels) {
 my $v = $quotes{$sym, $l},;
-    print $v, ","  if defined ($v);
+    print $v  if defined ($v);
+print ", ";
   }
   print "\n";
 }
