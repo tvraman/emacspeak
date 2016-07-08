4d56a1222c8b394e31208d200b6a82f7684a7ed0
T. V. Raman <tv.raman.tv@gmail.com>
2016-07-08 16:26:58 -0700

    wip: hard-wired tickers for now

--- etc/obsolete/quotes.pl
+++ etc/obsolete/quotes.pl
@@ -1,46 +1,32 @@
 #!/usr/bin/perl -w
 #$Id$
-#use Finance::YahooQuote;
 use Finance::Quote;
-my @symbols = <>;
-chomp (@symbols);
-my @labels=qw(
-              Symbol
-              Company
-              Last-Price
-              Last-Trade-Date
-              Last-Trade-Time
-              Change
-              Percent-Change
-              Volume
-              Average-Daily-Vol
-              Bid
-              Ask
-              Close
-              Open
-              Day-Range
-              52-Week-Range
-              EPS
-              P/E
-              Dividend-Pay-Date
-              Dividend-per-Share
-              Dividend-Yield
-              Market-Capitalization
-              Stock-Exchange
-             );
+#my @symbols = <>;
+#chomp (@symbols);
+my @symbols =qw/ibm msft adbe goog googl fb/;
 
-#@quotes = getquote @symbols;
+my @labels=qw(
+  ask avg_vol bid cap
+close currency date day_range
+div div_date div_yield eps
+ex_div high isodate last
+low method name net
+open p_change pe price
+success symbol time volume
+year_range);
+
 $q = Finance::Quote->new;
 #$q->require_labels(@labels);
-$quotes  = $q->fetch("usa",@symbols);
+%quotes  = $q->fetch("usa",@symbols);
 #print join (", ", @labels ),"\n";
 my $sep = q(",");
 
-
-  foreach $sym (@symbols) {
-    print "Sym: $sym\n";
-     foreach $key (keys $quotes->{$sym}) {
-  print "$quotes{$key},, ";
+my $cl='';
+print join(",", @labels), "\n";
+foreach $sym (@symbols) {
+  foreach $l (@labels) {
+my $v = $quotes{$sym.$cl.$l},;
+    print $v, ","  if defined ($v);
   }
-     print "\n";
+  print "\n";
 }
