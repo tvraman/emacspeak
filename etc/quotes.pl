80c4623e3c53d37a17b76ca8574cb4fd6a1e4f10
raman <tv.raman.tv@gmail.com>
2016-07-08 18:05:01 -0700

    eliminated   fields isodate method

--- etc/quotes.pl
+++ etc/quotes.pl
@@ -7,22 +7,21 @@ my @labels=qw(
   ask avg_vol bid cap
   close currency date day_range
   div div_date div_yield eps
-  ex_div high isodate last
-  low method name net
+  ex_div high  last
+  low  name net
   open p_change pe price
-  success symbol time volume
-  year_range);
+  success symbol time volume year_range
+    );
 
 my $q = Finance::Quote->new;
-$q->require_labels(@labels);
 my %quotes  = $q->fetch("usa",@symbols);
 
 print join(",", @labels), "\n";
 foreach my $sym (@symbols) {
   foreach my $l (@labels) {
-my $v = $quotes{$sym, $l},;
-    print $v  if defined ($v);
-print ", ";
+my $v = $quotes{$sym, $l};
+    $v ="N/A" unless defined($v);
+print $v, ", ";
   }
   print "\n";
 }
