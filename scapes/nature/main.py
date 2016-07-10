e84438c792e74e03694359e81a59a381c3ec27be
raman <tv.raman.tv@gmail.com>
2016-07-09 20:03:14 -0700

    tuned nightscape

--- scapes/nature/main.py
+++ scapes/nature/main.py
@@ -409,10 +409,8 @@ class Nightscape (agent.Agent):
         nc = self.new_channel_pan(stereo.shiftxy(0, -1.3))  # in back
         self.sched_agent(nature, 0, nc)
 
-bc = self.new_channel_pan(
-                stereo.compose(stereo.scalexy(1.2), stereo.shiftxy(0, 1.0)))
-            ag = Nightingales(0.0, 30, 0.1, 1.0, 1.0)
-            self.sched_agent(ag, 0, bc)
+            ag = Nightingales(0.0, 30, 0.1, 1.0, 1.3)
+            self.sched_agent(ag)
 
         for i in xrange(12):
             y = 1 + i * 0.025
