6672813493d3da50353331a6d99a953364afa46b
raman <tv.raman.tv@gmail.com>
2016-07-14 19:54:46 -0700

    updated progress

--- sounds/pan-chimes/apply-pan.sh
+++ sounds/pan-chimes/apply-pan.sh
@@ -51,7 +51,7 @@ function swapchan () {
 # pan on.wav 1
 # pan open-object.wav 2
 # pan paragraph.wav 1
-# pan_depth progress.wav 1 60
+# pan_depth progress.wav 1 50
 # pan right.wav 1
 # pan save-object.wav 2
 # pan scroll.wav 1
