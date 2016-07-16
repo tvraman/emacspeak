6d67e1025acd858c5bca32e0543d28ccecb0bee1
raman <tv.raman.tv@gmail.com>
2016-07-16 16:10:50 -0700

    updated warn-user

--- sounds/pan-chimes/apply-pan.sh
+++ sounds/pan-chimes/apply-pan.sh
@@ -63,7 +63,7 @@ function swapchan () {
 # pan task-done.wav 1
 # pan unmodified-object.wav 2
 # pan voice-mail.wav 1
-# pan warn-user.wav 12
+# pan_depth warn-user.wav 2 45 
 # pan window-resize.wav 1
 # pan working.wav 1
 # pan yank-object.wav 5
