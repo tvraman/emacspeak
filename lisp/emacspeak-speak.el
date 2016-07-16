de7b33acc3492cc6b38692475d7fed148fddbbb3
raman <tv.raman.tv@gmail.com>
2016-07-16 14:17:16 -0700

    tick-tock -> time

--- lisp/emacspeak-speak.el
+++ lisp/emacspeak-speak.el
@@ -1904,7 +1904,7 @@ Timezone is specified using minibuffer completion.
 Second interactive prefix sets clock to new timezone."
   (interactive "P")
   (declare (special emacspeak-speak-time-format-string))
-  (emacspeak-auditory-icon 'tick-tock)
+  (emacspeak-auditory-icon 'time)
   (cond
    (world (call-interactively 'emacspeak-speak-world-clock))
    (t
