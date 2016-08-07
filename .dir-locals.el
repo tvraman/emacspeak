ee0b80e510f08603c7d15f75079a9aa5fc3bd37c
raman <tv.raman.tv@gmail.com>
2016-08-06 19:58:07 -0700

    added directory locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,19 @@
-(
- (c-mode . ((c-file-style . "GNU")))
- (log-edit-mode . ((log-edit-font-lock-gnu-style . t)
-                   (log-edit-setup-add-author . t)))
- (change-log-mode . ((add-log-time-zone-rule . t)
-		     (fill-column . 74)))
- (diff-mode . ((mode . whitespace)))
- (emacs-lisp-mode . ((indent-tabs-mode . nil))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode
+  (folded-file . t)
+  (c-file-style . "GNU"))
+ (change-log-mode
+  (add-log-time-zone-rule . t)
+  (fill-column . 74))
+ (diff-mode
+  (mode . whitespace))
+ (emacs-lisp-mode
+  (folded-file . t)
+  (indent-tabs-mode))
+ (log-edit-mode
+  (log-edit-font-lock-gnu-style . t)
+  (log-edit-setup-add-author . t))
+ )
+
