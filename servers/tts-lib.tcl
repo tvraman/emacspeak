99040c1b5d9543975cee7bb95bcc5ed99ac21657
raman <tv.raman.tv@gmail.com>
2016-08-15 08:30:21 -0700

    remove redundant check in proc beep

--- servers/tts-lib.tcl
+++ servers/tts-lib.tcl
@@ -211,15 +211,12 @@ proc beep_initialize {} {
 }
 
 proc beep {{freq 523} {length 100}} {
-    global tts
-    if {[info exists tts(beep)]
-        && $tts(beep) == 1}  {
-        set l  [expr $length / 1000.0]
-        set f  [expr $freq + 2]
+    set l  [expr $length / 1000.0]
+    set f  [expr $freq + 2]
         # equal 10ms fade at start and end:
         exec play -q -n synth $l sin $freq sin $f fade  0.01    0 channels 2 2>&1 > /dev/null &
-    }
 }
+
 
 # }}}
 # {{{self test 
