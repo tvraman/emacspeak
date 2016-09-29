ba31625631d17f1c76672706dc196ca4b362a3f2
T. V. Raman <tv.raman.tv@gmail.com>
2016-09-29 11:46:56 -0700

    added autoload cookie

--- lisp/emacspeak-aumix.el
+++ lisp/emacspeak-aumix.el
@@ -153,7 +153,7 @@ after saving the settings to have them take effect."
      (expand-file-name "forms/aumix-rc.el" emacspeak-etc-directory))
     (emacspeak-auditory-icon 'open-object)
     (emacspeak-forms-speak-field)))
-
+;;;###autoload 
 (defun emacspeak-aumix ()
   "Setup output parameters of the auditory display.
  Launch this tool while you have auditory output on
