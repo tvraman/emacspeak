31f94c4ccc947b4fa81b2b704b56880995a07cf7
raman <tv.raman.tv@gmail.com>
2016-10-12 17:19:32 -0700

    updated js setup

--- tvr/js-prepare.el
+++ tvr/js-prepare.el
@@ -1,6 +1,7 @@
 ;; -*- lexical-binding: t; -*-
 (when (locate-library "js2-mode")
-  (load "js2-mode-autoloads"))
+  (load "js2-mode-autoloads")
+  (augment-auto-mode-alist "\\.js$" 'js2-mode))
 (when (locate-library "tern")
   (load-library "tern-autoloads")
   (add-hook 'js-mode-hook #'tern-mode)
