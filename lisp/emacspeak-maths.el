590fa6172908758065abfb613440f11a19855dfb
T. V. Raman <tv.raman.tv@gmail.com>
2016-10-19 14:02:59 -0700

    updated/fixed docs, added smart enter for maths

--- lisp/emacspeak-maths.el
+++ lisp/emacspeak-maths.el
@@ -53,14 +53,23 @@
 ;;; expressions using the emacspeak-maths-navigator described below.
 ;;;
 ;;; Invoke the Navigator using s-spc --- this is the <windows> key on
-;;; Linux. Now you can use these keys: @itemize @item Enter: <SPC>
-;;; Enter a LaTeX expression. @item Alt-Text <a> Process alt-text
-;;; under point as LaTeX. @item Down <down> Move down a level. @item
-;;; Up <up> Move up a level. @item Left <left> Move left. @item Right
-;;; <right> Move right. @item Exit <any other key> Exit
+;;; Linux. Now you can use these keys: 
+;;; @itemize 
+;;; @item Enter: <SPC>
+;;; Enter a LaTeX expression. 
+;;; @item Alt-Text <a> Process alt-text
+;;; under point as LaTeX. 
+;;; @item Down <down> Move down a level. 
+;;; @item
+;;; Up <up> Move up a level. 
+;;; @item Left <left> Move left. 
+;;; @item Right
+;;; <right> Move right. 
+;;; @item Exit <any other key> Exit
 ;;; navigator.
  ;;;
-;;;@end itemize
+;;;
+;;; @end itemize
 ;;;The current expression is spoken after
 ;;; each of the above commands. It is also displayed in a special
 ;;; buffer *Spoken Math*. That buffer holds all previously generated
@@ -432,6 +441,19 @@ Set calc-language to tex to use this feature."
         (mark-active
          (buffer-substring (region-beginning)(region-end))))))
 
+(defun emacspeak-maths-enter-guess ()
+  "Send the guessed  LaTeX expression to Maths server.
+Guess is based on context."
+  (interactive)
+  (emacspeak-maths-guess-input)         ;guess based on context
+  (declare (special emacspeak-maths))
+  (emacspeak-maths-ensure-server)
+  (process-send-string
+   (emacspeak-maths-client-process emacspeak-maths)
+   (format "enter: %s"
+           (emacspeak-maths-input emacspeak-maths))))
+
+
 (defun emacspeak-maths-enter (latex)
   "Send a LaTeX expression to Maths server.
 Tries to guess default based on context.
@@ -518,6 +540,7 @@ For use on Wikipedia pages  for example."
                 :pre emacspeak-muggles-pre
                 :post emacspeak-muggles-post)
      "Spoken Math"
+     ("RET" emacspeak-maths-enter-guess)
      ("SPC" emacspeak-maths-enter "enter")
      ("a" emacspeak-maths-speak-alt "Alt Text")
      ("d" emacspeak-maths-depth "Depth")
