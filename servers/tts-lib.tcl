94b418197486b769ecef2de0b696c2c808114e8d
raman <tv.raman.tv@gmail.com>
2016-12-24 16:40:06 -0800

    speak-line: tone adjust for fade

--- servers/tts-lib.tcl
+++ servers/tts-lib.tcl
@@ -213,7 +213,7 @@ proc beep_initialize {} {
 proc beep {{freq 523} {length 100}} {
     set l  [expr $length / 1000.0]
         # equal 10ms fade at start and end:
-        exec play -q -n  synth $l sin $freq  fade  h 0.05 0     0  channels 2 > /dev/null &
+        exec play -q -n  synth $l sin $freq  fade  l 0.05 0 channels 2 > /dev/null &
 }
 
 
