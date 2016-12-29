d5b8eb5f910e41d2a7425fe2207bc0dad4582e3b
raman <tv.raman.tv@gmail.com>
2016-12-28 20:10:11 -0800

    clang-format

--- js/node/math-server.js
+++ js/node/math-server.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//Commentary:
+// Commentary:
 
 
 /**
@@ -40,10 +40,8 @@ var net = require('net');
 // Initialize Math rendering system.
 var mjx = require('mathjax-node');
 var sre = require('speech-rule-engine');
-sre.setupEngine({markup: 'acss',
-                 domain: 'emacspeak',
-                 rules: ['EmacspeakRules']
-                });
+sre.setupEngine(
+    {markup: 'acss', domain: 'emacspeak', rules: ['EmacspeakRules']});
 
 // Auxiliary methods for error handling.
 var errorGen = {};
@@ -53,11 +51,7 @@ errorGen.parseError = function(error) {
 };
 
 errorGen.mathjaxErrors = function(errors, socket) {
-  socket.write(
-    errors
-      .map(errorGen.parseError)
-      .join(' ')
-  );
+  socket.write(errors.map(errorGen.parseError).join(' '));
 };
 
 // table of request handlers:
@@ -68,62 +62,72 @@ var handlers = {};
 
 // Accept a LaTeX math expression:
 handlers.enter = function(expr, socket) {
-  mjx.config({
-    displayErrors: false
+  mjx.config({displayErrors: false});
+  mjx.typeset({math: expr, format: 'TeX', mml: true}, function(data) {
+    (data.errors && data.errors.length) ?
+        errorGen.mathjaxErrors(data.errors, socket) :
+        socket.write(sre.walk(data.mml));
   });
-  mjx.typeset({math: expr,
-               format: 'TeX',
-               mml: true},
-              function(data) {
-                (data.errors && data.errors.length) ?
-                  errorGen.mathjaxErrors(data.errors, socket) :
-                  socket.write(sre.walk(data.mml)); }
-             );
 };
 
 // Implement Handlers:
-handlers.up = function(expr, socket) {socket.write(sre.move('UP'));};
-handlers.down = function(expr, socket) {socket.write(sre.move('DOWN'));};
-handlers.left = function(expr, socket) {socket.write(sre.move('LEFT'));};
-handlers.right = function(expr, socket) {socket.write(sre.move('RIGHT'));};
-handlers.repeat = function(expr, socket) {socket.write(sre.move('TAB'));};
-handlers.depth = function(expr, socket) {socket.write(sre.move('SPACE'));};
-handlers.root = function(expr, socket) {socket.write(sre.move('HOME'));};
+handlers.up = function(expr, socket) {
+  socket.write(sre.move('UP'));
+};
+handlers.down = function(expr, socket) {
+  socket.write(sre.move('DOWN'));
+};
+handlers.left = function(expr, socket) {
+  socket.write(sre.move('LEFT'));
+};
+handlers.right = function(expr, socket) {
+  socket.write(sre.move('RIGHT'));
+};
+handlers.repeat = function(expr, socket) {
+  socket.write(sre.move('TAB'));
+};
+handlers.depth = function(expr, socket) {
+  socket.write(sre.move('SPACE'));
+};
+handlers.root = function(expr, socket) {
+  socket.write(sre.move('HOME'));
+};
 
 // Start a TCP Server on port 5000
 net.createServer(function(socket) {
-  // Identify this client
-  socket.name = socket.remoteAddress + ':' + socket.remotePort;
+     // Identify this client
+     socket.name = socket.remoteAddress + ':' + socket.remotePort;
 
-  // Method: respond
-  function respond(message, sender) {
-    // message is of the form:
-    //cmd: args, args, args
-    var request = message.toString();
-    var cmd = request.split(':', 1)[0];
-    var args = request.slice(cmd.length + 1);
-    var handler = handlers[cmd];
-    if (handler !== undefined) {
-      handler.call(null, args, socket);
-    } else {
-      process.stdout.write('Handler for ' + request[0] + ' not defined.\n');
-    }
-  }
+     // Method: respond
+     function respond(message, sender) {
+       // message is of the form:
+       // cmd: args, args, args
+       var request = message.toString();
+       var cmd = request.split(':', 1)[0];
+       var args = request.slice(cmd.length + 1);
+       var handler = handlers[cmd];
+       if (handler !== undefined) {
+         handler.call(null, args, socket);
+       } else {
+         process.stdout.write('Handler for ' + request[0] + ' not defined.\n');
+       }
+     }
 
-  // Announce yourself:
-  socket.write('(welcome  \"Maths Speech  Server! \") ');
-  // Handle incoming messages from Emacs:
-  socket.on('data', function(data) {
-    respond(data, socket);
-  });
+     // Announce yourself:
+     socket.write('(welcome  \"Maths Speech  Server! \") ');
+     // Handle incoming messages from Emacs:
+     socket.on('data', function(data) {
+       respond(data, socket);
+     });
 
-  // Shutdown server on disconnect:
-  socket.on('end', function() {
-    socket.destroy();
-    process.exit();
-  });
+     // Shutdown server on disconnect:
+     socket.on('end', function() {
+       socket.destroy();
+       process.exit();
+     });
 
-}).listen(5000);
+   })
+    .listen(5000);
 
 // Put a friendly message on the terminal of the server.
 console.log('Math server running at port 5000\n');
