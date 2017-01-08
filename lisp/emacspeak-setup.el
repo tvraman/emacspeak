210400d1dd5f9cd5d93c3323ee71fea808774cd4
raman <tv.raman.tv@gmail.com>
2017-01-08 13:04:56 -0800

    added opus to media extensions

--- lisp/emacspeak-setup.el
+++ lisp/emacspeak-setup.el
@@ -111,7 +111,7 @@ such as pronunciation dictionaries are stored. ")
 (defvar emacspeak-media-extensions
   (let
       ((ext
-        '("wma" "wmv" "flv" "m4a" "m4b"  "flac" "aiff" "aac"
+        '("wma" "wmv" "flv" "m4a" "m4b"  "flac" "aiff" "aac" "opus"
           "ogv" "oga""ogg" "mp3"  "mp4" "webm" "wav")))
     (concat
      "\\."
