ef358242d8cfb677c3d25e2f6d820b9ea97520c2
raman <tv.raman.tv@gmail.com>
2017-01-16 10:22:05 -0800

    tuned seascapes

--- scapes/seascape/main.py
+++ scapes/seascape/main.py
@@ -22,29 +22,29 @@ class SurfWaves(agent.Agent):
         bc = self.new_channel_pan(
             stereo.compose(stereo.scalexy(1.1), stereo.shiftxy(0, 1.25)))
         ag = nature.Nightingales(
-            0, 10,  # Duration
-            0.2, 1.0,  # volume
+            0, 30,  # Duration
+            0.1, 1.0,  # volume
             1)
         self.sched_agent(ag, 0, bc)
 
         bc = self.new_channel_pan(
             stereo.compose(stereo.scalexy(1.3), stereo.shiftxy(0, -1.25)))
         ag = nature.Cuckoos(
-            0, 35,  # Duration
+            0, 60,  # Duration
             0.05, 0.75,  # volume
             1)
 
         self.sched_agent(ag, 0, bc)
         for i in range(8):
-            y = 1 + i * 0.025
+            y = 1 + i * 0.05
             sc = self.new_channel_pan(
                 stereo.compose(stereo.scalexy(1.4), stereo.shiftxy(0, y)))
             ag = SurfBackgroundWaves()
-            self.sched_agent(ag, i * 2, sc)
+            self.sched_agent(ag, i * 5, sc)
             sc = self.new_channel_pan(
-                stereo.compose(stereo.scalexy(1.4), stereo.shiftxy(0, y)))
+                stereo.compose(stereo.scalexy(1.4), stereo.shiftxy(0, -y)))
             ag = SurfWaveSounds()
-            self.sched_agent(ag, i * 8, sc)
+            self.sched_agent(ag, i * 10, sc)
 
 
 class SurfWaveSounds(agent.Agent):
@@ -62,7 +62,7 @@ class SurfBackgroundWaves(agent.Agent):
 
     def run(self):
         p = random.uniform(0.2, 1.0)
-        v = random.uniform(0.01, 0.2)
+        v = random.uniform(0.01, 0.5)
         d = random.uniform(0.3, 12.0)
         pan = random.uniform(-1.25, 1.25)
         dur = self.sched_note_pan(water.waves_light, pan, pitch=p, volume=v)
