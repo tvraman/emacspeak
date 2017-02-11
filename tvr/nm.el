73221a8f17f0f4e91cfb52d1a7e28a535edc2e4d
raman <tv.raman.tv@gmail.com>
2017-02-11 09:56:54 -0800

    nm updated

--- tvr/nm.el
+++ tvr/nm.el
@@ -22,29 +22,43 @@
 (defvar nm-disconnected-hook nil
   "A hook variable which runs when network is disconnected.")
 
-(defun nm-is-connected()
-  "Returns t if NetworkManager is connected, nil otherwise."
-  (equal 70
-         (dbus-get-property
-          :system
-          "org.freedesktop.NetworkManager" "/org/freedesktop/NetworkManager"
-          "org.freedesktop.NetworkManager" "State")))
+(defun nm-service-p()
+  "Checks whether NetworkManager service is available on system bus."
+  (member "org.freedesktop.NetworkManager" (dbus-list-known-names :system)))
+
+
+(defun nm-connected-p()
+  "Returns nil if NetworkManager service indicates that network
+is down, t otherwise. This means that this function will return t
+both when NetworkManager service is *not* available on system
+bus (network is assumed to be up) or when available service
+indicates network is up."
+  (or (not (nm-service-p))
+      (equal 70 (dbus-get-property
+            :system
+            "org.freedesktop.NetworkManager" "/org/freedesktop/NetworkManager"
+            "org.freedesktop.NetworkManager" "State"))))
 
 (defvar nm-dbus-registration nil
   "Record if registered with NM.")
 
+
 (defun nm-enable()
-  "Enable integration with NetworkManager. Does nothing if already enabled."
+  "Enable integration with NetworkManager. Does nothing if
+already enabled or service is not available."
   (interactive)
   (declare (special nm-dbus-registration))
-  (when (not nm-dbus-registration)
-    (setq nm-dbus-registration
-          (dbus-register-signal
-           :system
-           "org.freedesktop.NetworkManager" "/org/freedesktop/NetworkManager"
-           "org.freedesktop.NetworkManager" "StateChanged"
-           'nm-state-dbus-signal-handler))
-    (message "Enabled integration with NetworkManager.")))
+  (cond
+   ((not (nm-service-p))
+    (message "NetworkManager service not available.") nil)
+   ((not nm-dbus-registration)
+    (setq nm-dbus-registration (dbus-register-signal
+                                :system
+                                "org.freedesktop.NetworkManager" "/org/freedesktop/NetworkManager"
+                                "org.freedesktop.NetworkManager" "StateChanged"
+                                'nm-state-dbus-signal-handler))
+    (message "Enabled integration with NetworkManager."))
+   ((message "Integration with NetworkManager already enabled."))))
 
 (defun nm-disable()
   "Disable integration with NetworkManager. Does nothing if already disabled."
@@ -55,12 +69,34 @@
     (setq nm-dbus-registration nil)
     (message "Disabled integration with NetworkManager.")))
 
+;; NM_STATE numbers and meanings:
+;; NM_STATE_UNKNOWN = 0
+;;     Networking state is unknown. 
+;; NM_STATE_ASLEEP = 10
+;;     Networking is inactive and all devices are disabled. 
+;; NM_STATE_DISCONNECTED = 20
+;;     There is no active network connection. 
+;; NM_STATE_DISCONNECTING = 30
+;;     Network connections are being cleaned up. 
+;; NM_STATE_CONNECTING = 40
+;;     A network device is connecting to a network and there is no other available network connection. 
+;; NM_STATE_CONNECTED_LOCAL = 50
+;;     A network device is connected, but there is only link-local connectivity. 
+;; NM_STATE_CONNECTED_SITE = 60
+;;     A network device is connected, but there is only site-local connectivity. 
+;; NM_STATE_CONNECTED_GLOBAL = 70
+;;     A network device is connected, with global network connectivity. 
+;;
+;; See:
+;; https://developer.gnome.org/NetworkManager/unstable/spec.html#type-NM_STATE
+
 (defun nm-state-dbus-signal-handler (nmstate)
   "Handles NetworkManager signals and runs appropriate hooks."
   (cond
-   ((or (= 4 nmstate) (= 1 nmstate))
+   ((or (= 10 nmstate) (= 20 nmstate) (= 30 nmstate) (= 50 nmstate) (= 60 nmstate))
     (run-hooks 'nm-disconnected-hook))
    ((= 70 nmstate)
     (run-hooks 'nm-connected-hook))))
+
 (nm-enable)
 (provide 'nm)
