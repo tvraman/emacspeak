c513a554bac0dcc62244aaf915edaba3f8f216a8
raman <tv.raman.tv@gmail.com>
2017-02-25 08:02:18 -0800

    cleanup

--- etc/bootstrap.sh
+++ etc/bootstrap.sh
@@ -28,12 +28,8 @@ fi
 cd "emacspeak-${v}"
 make config && make
 (cd servers/linux-espeak &&  make )
-echo `pwd`
-
 #Run out of this directory.
 # Default to using  espeak unless DTK_PROGRAM is set.
 #
 :${DTK_PROGRAM} ? "Using ${DTK_PROGRAM}" : export DTK_PROGRAM="espeak"
 emacs -q -l ./lisp/emacspeak-setup.el -l $HOME/.emacs
-
-
