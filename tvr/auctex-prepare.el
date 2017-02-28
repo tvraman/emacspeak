ea01ed2a5e9f9a80b0c198152fd50e0eb2a8f737
raman <tv.raman.tv@gmail.com>
2017-02-28 08:55:29 -0800

    fix typo in auctex-prepare

--- tvr/auctex-prepare.el
+++ tvr/auctex-prepare.el
@@ -1,5 +1,5 @@
 ;; -*- lexical-binding: t; -*-
-(load-library "tex-site")  -*- lexical-binding: t; -*-
+(load-library "tex-site")
 (load-library "auctex-autoloads")
 (load-library "reftex-prepare")
 (add-hook 'LaTeX-mode-hook 'reftex-mode)
