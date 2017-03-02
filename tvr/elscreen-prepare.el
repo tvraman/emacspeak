3aa9d8c16d21966ea7101efff550cfae41c98a93
raman <tv.raman.tv@gmail.com>
2017-03-01 17:14:58 -0800

    dont show tabs

--- tvr/elscreen-prepare.el
+++ tvr/elscreen-prepare.el
@@ -6,6 +6,7 @@
     (global-set-key   [67108911] copy-map)
     (global-set-key [C-tab] 'elscreen-toggle)))
 (elscreen-persist-mode)
+(setq elscreen-display-tab nil)
 (condition-case nil
     (elscreen-persist-restore)
   (error "Failed to restore elscreen."))
