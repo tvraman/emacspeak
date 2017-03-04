566ec2f34055fe1b4ef4b89d63820622e1082549
raman <tv.raman.tv@gmail.com>
2017-03-04 09:31:14 -0800

    declaim->cl-declaim

--- lisp/emacspeak-eww.el
+++ lisp/emacspeak-eww.el
@@ -379,8 +379,8 @@
 ;;}}}
 ;;{{{ Required modules
 
-(require 'cl)
-(declaim (optimize (safety 0) (speed 3)))
+(require 'cl-lib)
+(cl-declaim (optimize (safety 0) (speed 3)))
 
 (eval-when-compile (require 'eww "eww" 'no-error))
 (require 'dom)
