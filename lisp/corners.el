d20195064b1cfd44684f76502df0b2273fd51095
raman <tv.raman.tv@gmail.com>
2017-04-04 17:52:07 -0700

    cleanup

--- lisp/corners.el
+++ lisp/corners.el
@@ -7,23 +7,22 @@
       ((fw (frame-width))
        (fh (1- (frame-height)))
        (tr 0)
-       (mr (/ (frame-height) 2))
+       (mr (/ fh 2))
        (br fh)
        (lc 0)
-       (mc (/ (frame-width) 2))
+       (mc (/ fw 2))
        (rc fw)
        (count (length (window-list))))
-    (print
-    (cond
-     ((equal corners `(,lc ,tr ,mc ,br)) 'left-half)
-     ((equal corners `(,mc ,tr ,rc, br)) 'right-half)
-     ((equal corners `(,lc ,tr ,rc ,mr)) 'top-half)
-     ((equal corners `(,lc ,mr ,rc ,br)) 'bottom-half)
-     ((equal corners `(,lc ,tr ,mc ,mr)) 'top-left)
-     ((equal corners `(,mc ,tr ,rc ,mr)) 'top-right)
-     ((equal corners `(,lc ,mr ,mc ,br)) 'bottom-left)
-     ((equal corners `(,mc ,mr ,rc ,br)) 'bottom-right)
-(t 'no-match)))))
+     (cond
+      ((equal corners `(,lc ,tr ,mc ,br)) 'left-half)
+      ((equal corners `(,mc ,tr ,rc, br)) 'right-half)
+      ((equal corners `(,lc ,tr ,rc ,mr)) 'top-half)
+      ((equal corners `(,lc ,mr ,rc ,br)) 'bottom-half)
+      ((equal corners `(,lc ,tr ,mc ,mr)) 'top-left)
+      ((equal corners `(,mc ,tr ,rc ,mr)) 'top-right)
+      ((equal corners `(,lc ,mr ,mc ,br)) 'bottom-left)
+      ((equal corners `(,mc ,mr ,rc ,br)) 'bottom-right)
+      (t 'no-match))))
 
 
 
