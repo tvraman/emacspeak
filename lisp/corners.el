ab1a83e500b6b51789f8a3f262e2d3e960d4719e
raman <tv.raman.tv@gmail.com>
2017-04-04 17:49:08 -0700

    tested corners.

--- lisp/corners.el
+++ lisp/corners.el
@@ -1,19 +1,29 @@
-;;; Locate A Window  -*- lexical-binding: t; -*-
-(defun  loc (&optional corners)
+;;; Locate A Window -*- lexical-binding: t; -*-
+(defun loc (&optional corners)
   "describe location"
+  (interactive "P")
   (or corners (setq corners (window-edges)))
   (let*
       ((fw (frame-width))
        (fh (1- (frame-height)))
        (tr 0)
-       (mr (/ fh 2))
+       (mr (/ (frame-height) 2))
        (br fh)
        (lc 0)
-       (mc (/ fw 2))
+       (mc (/ (frame-width) 2))
        (rc fw)
        (count (length (window-list))))
+    (print
     (cond
      ((equal corners `(,lc ,tr ,mc ,br)) 'left-half)
-     ((equal  corners `(,mc ,tr ,rc, br)) 'right-half)
-     ((equal  corners `(,lc ,tr ,rc,mr))'top-half)
-     ((equal  corners  `(,lc ,mr ,rc,br)) 'bottom-half))))
+     ((equal corners `(,mc ,tr ,rc, br)) 'right-half)
+     ((equal corners `(,lc ,tr ,rc ,mr)) 'top-half)
+     ((equal corners `(,lc ,mr ,rc ,br)) 'bottom-half)
+     ((equal corners `(,lc ,tr ,mc ,mr)) 'top-left)
+     ((equal corners `(,mc ,tr ,rc ,mr)) 'top-right)
+     ((equal corners `(,lc ,mr ,mc ,br)) 'bottom-left)
+     ((equal corners `(,mc ,mr ,rc ,br)) 'bottom-right)
+(t corners))))
+
+
+)
