4d90e33dab0458242c07a00f1397b4298ce2e29b
raman <tv.raman.tv@gmail.com>
2017-04-13 19:34:57 -0700

    wip

--- lisp/emacspeak-ivy.el
+++ lisp/emacspeak-ivy.el
@@ -169,7 +169,7 @@ ivy-youtube
   `(defadvice ,f (after emacspeak pre act comp)
      "Speak selection."
      (when (ems-interactive-p)
-       (dtk-speak (elt ivy--all-candidates ivy--index))
+       (dtk-speak (ivy-state-current ivy-last))
        (emacspeak-auditory-icon 'select-object)))))
 
 ;;}}}
