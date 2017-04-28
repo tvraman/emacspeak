7408889e69dcd3b9d761b5163a7f67f5b0c0acfd
raman <tv.raman.tv@gmail.com>
2017-04-27 18:17:06 -0700

    wip: emacspeak-pkg.el

--- emacspeak-pkg.el
+++ emacspeak-pkg.el
@@ -1,5 +1,9 @@
 (define-package
   "emacspeak"
-"46.0"
-"Emacspeak: The Complete Audio Desktop"
-)
+  "46.0"
+  "Emacspeak: The Complete Audio Desktop"
+  '((emacs "25.1"))
+  :url "http://github.com/tvraman/emacspeak"
+  :keywords '("Accessibility"  "Audio Desktop" "Eyes-Free Interaction")
+  )
+;; no-byte-compile: t
