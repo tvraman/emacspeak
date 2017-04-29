a0a8ebff9bdfef4aff22d4f6587618a02f0be765
raman <tv.raman.tv@gmail.com>
2017-04-29 07:46:02 -0700

    beep use two channels and set 10ms delay

--- servers/tts-lib.tcl
+++ servers/tts-lib.tcl
@@ -213,7 +213,8 @@ proc beep_initialize {} {
 proc beep {{freq 523} {length 100}} {
     set l  [expr $length / 1000.0]
         # equal 10ms fade at start and end:
-        exec play -q -n  synth $l sin $freq  fade  h 0.01 0  channels 2 > /dev/null &
+        exec play -q -n  synth $l sin $freq  sin $freq \
+        fade  h 0.01 0  delay 0 0.01 channels 2 > /dev/null &
 }
 
 
