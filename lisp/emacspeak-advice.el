9273d5b052a54cce8d7410241c649a88281c82fc
raman <tv.raman.tv@gmail.com>
2017-06-17 15:52:45 -0700

    remove redundant emacspeak-minibuffer-enter-auditory-icon

--- lisp/emacspeak-advice.el
+++ lisp/emacspeak-advice.el
@@ -2437,18 +2437,13 @@ Produce auditory icons if possible."
 ;;}}}
 ;;{{{ setup minibuffer hooks:
 
-(defvar emacspeak-minibuffer-enter-auditory-icon t
-  "Produce auditory icon when entering the minibuffer.")
-
 (defun emacspeak-minibuffer-setup-hook ()
   "Actions to take when entering the minibuffer with emacspeak running."
-  (declare (special emacspeak-minibuffer-enter-auditory-icon
-                    minibuffer-exit-hook minibuffer-default))
+  (declare (special minibuffer-exit-hook minibuffer-default))
   (let ((inhibit-field-text-motion t))
     (unless (memq 'emacspeak-minibuffer-exit-hook minibuffer-exit-hook)
       (add-hook 'minibuffer-exit-hook #'emacspeak-minibuffer-exit-hook))
-    (when emacspeak-minibuffer-enter-auditory-icon
-      (emacspeak-auditory-icon 'open-object))
+    (emacspeak-auditory-icon 'open-object)
     (when minibuffer-default (emacspeak-auditory-icon 'help))
     (tts-with-punctuations 'all (emacspeak-speak-buffer))))
 
