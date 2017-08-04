86de667738347471e64d120089fa3182198d9ecc
raman <tv.raman.tv@gmail.com>
2017-08-04 10:50:33 -0700

    cleanup

--- tvr/org-prepare.el
+++ tvr/org-prepare.el
@@ -2,7 +2,7 @@
 
 (load-library "org-autoloads")
 
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (define-key global-map "\C-cb" 'org-switchb)
