221098f8929e2db59dfb3b561459af735f8207ef
raman <tv.raman.tv@gmail.com>
2017-08-05 19:19:25 -0700

    cleanup

--- tvr/fap-prepare.el
+++ tvr/fap-prepare.el
@@ -1,11 +1,8 @@
 ;;; File At Point  -*- lexical-binding: t; -*-
 
-(require 'ffap)
-(declare (special  ffap-bindings))
-(setq ffap-bindings 
-      '(
-        (global-set-key  "\M-M" 'ffap-menu)
-        (global-set-key "\M-L" 'ffap-next)))
-(ffap-bindings)
+
+(eval-after-load 'ffap
+  `(progn
+(ffap-bindings)))
 
     
