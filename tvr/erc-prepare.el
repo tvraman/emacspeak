8ed9c5d1aa7a793792702c5632c7f56e639355dd
raman <tv.raman.tv@gmail.com>
2017-08-05 19:16:55 -0700

    cleanup

--- tvr/erc-prepare.el
+++ tvr/erc-prepare.el
@@ -1,32 +1,14 @@
-;(augment-load-path "erc" "erc")  -*- lexical-binding: t; -*-
-;(load-library "erc")
 (defun erc-w3 ()
   "Join W3C IRC server."
   (interactive)
   (erc  :server "irc.w3.org"
-:port 6665
-:nick "raman"
- ))
+        :port 6665
+        :nick "raman"
+        ))
 
 (defun erc-cricket ()
   "Join cricinfo IRC server."
   (interactive)
   (erc  :server "irc.cricket.org"
-:port 6667
-:nick "raman"))
-
-(defun erc-bubbles ()
-  "Join Bubbles"
-  (interactive)
-  (erc  "bubbles.almaden.ibm.com"
-6667
-"raman"
-"T. V. Raman" t ))
-
-(defun erc-handhelds ()
-  "Join Handhelds IRC server."
-  (interactive)
-  (erc  "irc.openprojects.net"
-6667
-"raman"
-"T. V. Raman" t ))
+        :port 6667
+        :nick "raman"))
