a8ca868cc4a9c7519a1b81770257481060eb6112
raman <tv.raman.tv@gmail.com>
2017-08-06 07:53:06 -0700

    projectile: wrap in eval-after-load

--- tvr/projectile-prepare.el
+++ tvr/projectile-prepare.el
@@ -1,10 +1,12 @@
 ;;; -*- lexical-binding: t; -*-
 (load-library "projectile-autoloads")
 ;(projectile-global-mode)
+(eval-after-load 'projectile
+  `(progn 
 (global-set-key (kbd "C-c ;") 'projectile-command-map)
 (csetq 
  projectile-mode-line
  '(:eval 
    (if(not (string= "-"  (projectile-project-name)))
        (format "[%s]" (projectile-project-name))
-     "")))
+     "")))))
