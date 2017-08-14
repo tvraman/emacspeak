4370b42d4bf23862365bcefbcef1b1ec1d9156f8
raman <tv.raman.tv@gmail.com>
2017-08-14 08:23:07 -0700

    keep alternative without \b

--- etc/typo.pl
+++ etc/typo.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/perl -i
 #fix typo 
 my $orig = shift || die "What should I change?";
+print STDERR "orig: $orig\n";
 my $new = shift || die "What should I change $orig  to?";
+print STDERR "new: $new \n";
 while ( <>) {
-  s/\b$orig\b/$new/go;
+    s/\b$orig\b/$new/go;
+  #s/$orig/$new/go;
   print;
 }
