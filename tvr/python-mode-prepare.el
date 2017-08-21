e5fc8feae2a343b06b0f44d50d252b279213b04b
raman <tv.raman.tv@gmail.com>
2017-08-21 15:54:52 -0700

    py-clean

--- tvr/python-mode-prepare.el
+++ tvr/python-mode-prepare.el
@@ -2,9 +2,6 @@
 (when (locate-library "python-mode-autoloads")
   (load-library "python-mode-autoloads"))
 
-(when (locate-library "ipython-autoloads")
-  (load-library "ipython-autoloads"))
-
 (add-hook
  'python-mode-hook
  #'(lambda ()
