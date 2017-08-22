7c3b5a5c555b7f5429974bf0982e621db06de287
raman <tv.raman.tv@gmail.com>
2017-08-22 14:11:22 -0700

    eval-after-load

--- tvr/python-mode-prepare.el
+++ tvr/python-mode-prepare.el
@@ -1,10 +1,11 @@
 ;; -*- lexical-binding: t; -*-
 (when (locate-library "python-mode-autoloads")
   (load-library "python-mode-autoloads"))
-
-(add-hook
- 'python-mode-hook
- #'(lambda ()
-     (when (locate-library "company") (company-mode  1))
-     ;(when (locate-library "elpy") (elpy-enable))
-     ))
+(eval-after-load "python-mode"
+  `(progn
+     (add-hook
+      'python-mode-hook
+      #'(lambda ()
+          (when (locate-library "company") (company-mode  1))
+                                        ;(when (locate-library "elpy") (elpy-enable))
+          ))))
