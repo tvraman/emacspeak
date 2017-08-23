a5a7c19d0b5e8d67b0677776193544f8b1e1dee8
raman <tv.raman.tv@gmail.com>
2017-08-23 09:05:18 -0700

    stable

--- tvr/js-prepare.el
+++ tvr/js-prepare.el
@@ -1,12 +1,13 @@
 ;; -*- lexical-binding: t; -*-
-(when (locate-library "js2-mode")
-  (load "js2-mode-autoloads")
-  (augment-auto-mode-alist "\\.js$" 'js2-mode))
-(when (locate-library "tern")
-  (load-library "tern-autoloads")
-  (add-hook 'js-mode-hook #'tern-mode)
-  (add-hook 'js2-mode-hook #'tern-mode))
-(when (locate-library "xref-js2")
-  (load-library "xref-js2-autoloads"))
-(when (locate-library "js2-refactor")
-  (load-library "js2-refactor-autoloads"))
+
+(load "js2-mode-autoloads")
+(eval-after-load "js2-mode"
+  `(progn  
+     (when (locate-library "tern")
+       (load-library "tern-autoloads")
+       (add-hook 'js2-mode-hook #'tern-mode))
+     (when (locate-library "xref-js2")
+       (load-library "xref-js2-autoloads"))
+     (when (locate-library "js2-refactor")
+       (load-library "js2-refactor-autoloads"))
+     ))
