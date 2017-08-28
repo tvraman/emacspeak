bf16286ec5c8d51cde039bfe1d4841f6131f6c5e
T. V. Raman <tv.raman.tv@gmail.com>
2017-08-28 10:58:35 -0700

    ws fixup

--- lisp/emacspeak-jabber.el
+++ lisp/emacspeak-jabber.el
@@ -162,8 +162,7 @@
   (cond
    (emacspeak-jabber-speak-presence-alerts ad-do-it)
    (t
-    (ems-with-messages-silenced
-     ad-do-it)))
+    (ems-with-messages-silenced ad-do-it)))
   ad-return-value)
 
 ;;;this is what I use as my jabber alert function:
