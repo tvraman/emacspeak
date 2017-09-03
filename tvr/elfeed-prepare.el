995b1fc6a1e8f41dd558699e701e115802bdd8f7
raman <tv.raman.tv@gmail.com>
2017-09-03 07:56:09 -0700

    elfeed: run once an hour

--- tvr/elfeed-prepare.el
+++ tvr/elfeed-prepare.el
@@ -3,7 +3,7 @@
 ;(load-library "elfeed-autoloads")
 (eval-after-load "elfeed"
   `(progn
-;; Run once an hour
-;(setq my-elfeed-timer 
-      ;(run-at-time t (* 60 60) #'elfeed-update))
-))
+     ;; Run once an hour
+     (setq my-elfeed-timer 
+           (run-at-time t (* 60 60) #'elfeed-update))
+     ))
