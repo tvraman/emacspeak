f37de9cdb614ccee20ffbe9f95d465b8400df640
raman <tv.raman.tv@gmail.com>
2017-09-14 18:18:32 -0700

    clean-up

--- tvr/tvr-functions.el
+++ tvr/tvr-functions.el
@@ -8,33 +8,6 @@
   (insert
    (format "\n Thanks, \n --%s\n" (user-full-name))))
 
-(defun tex-tie-current-word(n)
-  (interactive "P")
-  "Tie the next n  words."
-  (or n (setq n 1))
-  (while
-      (> n 0)
-    (setq n (- n 1))
-    (forward-word 1)
-    (delete-horizontal-space)
-    (insert-char 126 1)
-    )
-  (forward-word 1))
-
-(defun end-of-word(arg)
-  "move to end of word"
-  (interactive "P")
-  (if arg
-      (forward-word arg)
-    (forward-word 1)))
-
-(defun comma-at-end-of-word()
-  (interactive)
-  "Move point to end of word and put a comma."
-  (forward-word 1)
-  (insert-char
-   (string-to-char ",") 1))
-
 (defun lacheck-buffer-file()
   (interactive)
   "Lacheck file visited in current buffer"
