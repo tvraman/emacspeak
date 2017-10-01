84e87daeb75fc73f5b4489b3a90faaf284d7008e
raman <tv.raman.tv@gmail.com>
2017-10-01 12:15:08 -0700

    trial bindings

--- tvr/sp-prepare.el
+++ tvr/sp-prepare.el
@@ -6,4 +6,7 @@
      (define-key  smartparens-mode-map (kbd "C-M-a") 'beginning-of-defun)
      (define-key  smartparens-mode-map (kbd "C-M-e") 'end-of-defun)
      (define-key  smartparens-mode-map (kbd "C-M-k") 'kill-sexp)
+     (define-key smartparens-mode-map (kbd "M-a") 'sp-backward-down-sexp)
+     (define-key smartparens-mode-map (kbd "M-e") 'sp-up-sexp)
+     (define-key smartparens-mode-map (kbd "M-k") 'sp-kill-sexp)
      ))
