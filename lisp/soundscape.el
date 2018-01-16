81db90cb8940ec70a4df45fb64b2d75d39709c1e
T.V Raman <tv.raman.tv@gmail.com>
2018-01-16 08:15:53 -0800

    restore tighter test in soundscape-update so we dont
    contantly poke the remote from the idle-timer

--- lisp/soundscape.el
+++ lisp/soundscape.el
@@ -494,6 +494,7 @@ Optional interactive prefix arg `force' skips optimization checks."
   (cl-declare (special  soundscape--last-mode))
   (unless
       (or
+       (eq major-mode soundscape--last-mode)
        (eq 'minibuffer-inactive-mode major-mode)
        (string-prefix-p " " (buffer-name))
        (string-prefix-p " *Minibuf-" (buffer-name))
