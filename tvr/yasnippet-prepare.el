99b796100e209c6b8e5e86c2b88969a310934473
T.V Raman <tv.raman.tv@gmail.com>
2018-02-10 20:35:05 -0800

    ws-yas

--- tvr/yasnippet-prepare.el
+++ tvr/yasnippet-prepare.el
@@ -1,11 +1,12 @@
 ;; -*- lexical-binding: nil; -*-
 
-;(load-library "yasnippet-autoloads")
-(eval-after-load 'yasnippet
+(eval-after-load
+    'yasnippet
   `(progn 
-(yas-reload-all)
-(setq yas-verbosity 0)))
-(add-hook
- 'prog-mode-hook
- #'(lambda nil
-     (yas-minor-mode 1)))
+     (yas-reload-all)
+     (setq yas-verbosity 0)))
+;; (add-hook
+;;  'prog-mode-hook
+;;  #'(lambda nil
+;;      (yas-minor-mode 1)))
+
