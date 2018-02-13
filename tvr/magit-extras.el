1b0482ed8aac6b2f10c24c0182c14389ca90dd8f
T.V Raman <tv.raman.tv@gmail.com>
2018-02-13 08:32:41 -0800

    added-magit-extra

---
+++ tvr/magit-extras.el
@@ -0,0 +1,10 @@
+(autoload 'org-read-date "org")
+
+(defun magit-org-read-date (prompt &optional _default)
+  (org-read-date 'with-time nil nil prompt))
+
+(magit-define-popup-option 'magit-log-popup
+  ?s "Since date" "--since=" #'magit-org-read-date)
+
+(magit-define-popup-option 'magit-log-popup
+  ?u "Until date" "--until=" #'magit-org-read-date)
