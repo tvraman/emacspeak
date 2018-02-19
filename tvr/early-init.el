a8508fc2312610b243d80bc2820f8b4d95497ed0
T.V Raman <tv.raman.tv@gmail.com>
2018-02-19 09:40:16 -0800

    early-init

---
+++ tvr/early-init.el
@@ -0,0 +1,6 @@
+(make-thread
+     #'(lambda ()
+         (let ((file-name-handler-alist nil)
+               (gc-cons-threshold 64000000)
+               (load-source-file-function nil))
+           (package-initialize))))
