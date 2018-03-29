8b951fcd608c208f5b60fc4f4afc6ada5fa8bb07
T. V. Raman <tv.raman.tv@gmail.com>
2018-03-29 10:59:23 -0700

    x-wait-event-timeout set to 0

--- tvr/laptop-local.el
+++ tvr/laptop-local.el
@@ -1,5 +1,7 @@
 ;; -*- lexical-binding: nil; -*-
-(setq pre-redisplay-function nil)
+(setq pre-redisplay-function nil
+      x-wait-for-event-timeout 0)
+
 
 (load-library "touchpad-disable")
 (load-library "xbacklight")
