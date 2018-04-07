aae2a55327ab9762bbb5daff9c436809628e4354
T.V Raman <tv.raman.tv@gmail.com>
2018-04-07 09:05:43 -0700

    enable yasnippet in prog modes

--- tvr/yasnippet-prepare.el
+++ tvr/yasnippet-prepare.el
@@ -5,6 +5,7 @@
   `(progn 
      (yas-reload-all)
      (setq yas-verbosity 0)
+     (add-hook 'prog-mode-hook #'yas-minor-mode)
      (run-with-idle-timer
       5 nil
       #'yas-global-mode 1)))
