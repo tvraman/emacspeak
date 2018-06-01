bc63fa09cd59199ab172f63e8b7a2d28163b77b2
T.V Raman <tv.raman.tv@gmail.com>
2018-05-31 19:42:30 -0700

    startup-cleanup

--- tvr/python-mode-prepare.el
+++ tvr/python-mode-prepare.el
@@ -1,11 +1,10 @@
 ;; -*- lexical-binding: nil; -*-
 
-;(load-library "python-mode-autoloads")
+
 (eval-after-load "python-mode"
   `(progn
      (add-hook
       'python-mode-hook
       #'(lambda ()
-           (company-mode  1)
            ;(elpy-enable)
            ))))
