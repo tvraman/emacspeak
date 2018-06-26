308e0c687bf30f7ffb7ce32cafbe360b7c8cac71
T.V Raman <tv.raman.tv@gmail.com>
2018-06-25 17:12:09 -0700

    typofix

--- lisp/emacspeak-personality.el
+++ lisp/emacspeak-personality.el
@@ -444,7 +444,7 @@ Append means place corresponding personality at the end."
         (save-current-buffer
           (set-buffer (overlay-buffer overlay))
           (condition-case nil 
-          (put-text-property startend 'personality nil)
+          (put-text-property start end 'personality nil)
           (error nil)))))))
 
 (defvar emacspeak-personality-advice-move-overlay t
