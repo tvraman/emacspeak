daf1d756340d5d89133a065573156c2d9aff6c67
T.V Raman <tv.raman.tv@gmail.com>
2018-06-30 18:58:25 -0700

    refactor

--- etc/clock.pl
+++ etc/clock.pl
@@ -7,20 +7,21 @@ use strict;
 my $sounds="$ENV{EMACSPEAK_DIR}/sounds/clock/";
 my ($sec,$min,$hour,$mDay,$mon,$year,$wDay,$yDay,$isdst) = localtime();
 my %chimes =(
-             0 => [qw(Sun Mon Tue Wed Thu Fri Sat)],
-             15  => [qw(gf-15.mp3 wm-15.mp3 chime-15.mp3 bigben-15.mp3)],
-             30 =>[qw (gf-30.mp3 wm-30.mp3 chime-30.mp3 bigben-30.mp3)],
-             45 => [qw(gf-45.mp3 wm-45.mp3 chime-45.mp3 bigben-45.mp3)]
-            );
+    0 => [qw(Sun Mon Tue Wed Thu Fri Sat)],
+    15  => [qw(gf-15.mp3 wm-15.mp3 chime-15.mp3 bigben-15.mp3)],
+    30 =>[qw (gf-30.mp3 wm-30.mp3 chime-30.mp3 bigben-30.mp3)],
+    45 => [qw(gf-45.mp3 wm-45.mp3 chime-45.mp3 bigben-45.mp3)]
+    );
 my $chime;
+$min=0;
 exit unless defined ($chimes{$min});
 if ($min == 0 ) {
-  my $c = $chimes{0}[$wDay];
-  $hour %= 12;
-  $hour = 12 if ($hour == 0);
-  $chime = "$sounds/$c/$hour.mp3";
-  qx(mplayer -af bs2b $chime  2>/dev/null);
+    my $c = $chimes{0}[$wDay];
+    $hour %= 12;
+    $hour = 12 if ($hour == 0);
+    $chime = "$sounds/$c/$hour.mp3";
 } else {
-  $chime = $chimes{$min}[$hour  % 4];
-  qx(mplayer -af bs2b   $sounds/$chime 2>/dev/null);
+    $chime = "$sounds/". ($chimes{$min}[$hour  % 4]);
 }
+print $chime,"\n";
+qx(mplayer -af bs2b $chime  2>&1  >/dev/null);
