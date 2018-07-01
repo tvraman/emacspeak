69fd30942480e5ec8b53bbe2eebf44fe0092a3e8
T.V Raman <tv.raman.tv@gmail.com>
2018-06-30 19:06:22 -0700

    cleanup
    Author:

--- etc/clock.pl
+++ etc/clock.pl
@@ -1,10 +1,8 @@
-#!/usr/bin/perl -w
-#$Id: clock.pl,v 1.14 2006/05/14 21:26:59 tvraman Exp $
-#chime the time
-#Switching to MP3
+#!/usr/bin/perl -w #chime the time
+
 use strict;
-# Update ${EMACSPEAK_DIR} below to match your installation:
-my $sounds="$ENV{EMACSPEAK_DIR}/sounds/clock/";
+# Update  sounds location  to match your installation:
+my $sounds="$ENV{HOME}/emacs/lisp/emacspeak/sounds/clock";
 my ($sec,$min,$hour,$mDay,$mon,$year,$wDay,$yDay,$isdst) = localtime();
 my %chimes =(
     0 => [qw(Sun Mon Tue Wed Thu Fri Sat)],
@@ -13,7 +11,7 @@ my %chimes =(
     45 => [qw(gf-45.mp3 wm-45.mp3 chime-45.mp3 bigben-45.mp3)]
     );
 my $chime;
-$min=0;
+
 exit unless defined ($chimes{$min});
 if ($min == 0 ) {
     my $c = $chimes{0}[$wDay];
@@ -21,7 +19,8 @@ if ($min == 0 ) {
     $hour = 12 if ($hour == 0);
     $chime = "$sounds/$c/$hour.mp3";
 } else {
-    $chime = "$sounds/". ($chimes{$min}[$hour  % 4]);
+    my $c =  ($chimes{$min}[$hour  % 4]);
+    $chime = "$sounds/$c";
 }
-print $chime,"\n";
 qx(mplayer -af bs2b $chime  2>&1  >/dev/null);
+
