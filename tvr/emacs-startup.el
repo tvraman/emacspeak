3b6d1015f63f24c5ab4e92ae7fc1020ab4cddf12
T.V Raman <tv.raman.tv@gmail.com>
2018-07-08 19:05:34 -0700

    switching to folding.el from elpa --- ancient version (dates back to Cornell) renamed

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -30,7 +30,7 @@
     "kbd-setup"
     "vm-prepare" "gnus-prepare"  "bbdb-prepare" "elfeed-prepare"
     "sp-prepare" ;"vdiff-prepare"
-    "auctex-prepare"  "folding-prepare" "org-prepare"
+    "auctex-prepare"   "org-prepare"
                                         ;"use-emms"
     "calc-prepare" "helm-prepare"
     "js-prepare" "tcl-prepare" "slime-prepare" "yasnippet-prepare"
@@ -292,6 +292,18 @@
 (provide 'emacs-startup)
 ;;{{{  emacs local variables
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values '((folded-file . t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 ;;;local variables:
 ;;;folded-file: t
 ;;;end:
