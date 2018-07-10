d836419b0d0f5ccd32f3afe136bdc6fb01bb3b9a
T. V. Raman <tv.raman.tv@gmail.com>
2018-07-10 09:50:03 -0700

    recover our keys

--- tvr/lispy-prepare.el
+++ tvr/lispy-prepare.el
@@ -2,4 +2,6 @@
     "lispy"
   `(progn
      (define-key lispy-mode-map (kbd "M-m") nil)
+     (define-key lispy-mode-map (kbd "C-,") nil)
+     (define-key lispy-mode-map-lispy (kbd "C-,") nil)
      ))
