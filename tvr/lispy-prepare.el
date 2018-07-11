5c45cfe745946ff6c762f80c3ccb5816091122bb
T. V. Raman <tv.raman.tv@gmail.com>
2018-07-11 09:40:51 -0700

    lispy: update header line

--- tvr/lispy-prepare.el
+++ tvr/lispy-prepare.el
@@ -4,4 +4,13 @@
      (define-key lispy-mode-map (kbd "M-m") nil)
      (define-key lispy-mode-map (kbd "C-,") nil)
      (define-key lispy-mode-map-lispy (kbd "C-,") nil)
+     (add-hook
+      'lispy-mode-hook
+      #'(lambda nil
+          (setq
+           header-line-format
+           '((:eval
+              (format  "%s %s"
+                       (buffer-name)
+                       "Emacs Lispy"))))))
      ))
