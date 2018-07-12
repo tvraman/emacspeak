18496dc5645e1fc7249723290dc63b0953f05db9
T.V Raman <tv.raman.tv@gmail.com>
2018-07-11 17:19:44 -0700

    bugfix

--- lisp/emacspeak-preamble.el
+++ lisp/emacspeak-preamble.el
@@ -51,8 +51,8 @@
 (require 'emacspeak-keymap)
 (eval-when-compile
   (condition-case nil
-      (let ((inhibit-message t))
-        (message-log-max nil)
+      (let ((inhibit-message t)
+            (message-log-max nil))
         (package-initialize))
     (error "Check your elpa packages --- something is broken")))
 
