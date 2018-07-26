ecf8ae13ce1c33f941c88b1911cdc37e689915c9
T. V. Raman <tv.raman.tv@gmail.com>
2018-07-26 16:10:46 -0700

    rename

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -38,7 +38,7 @@
     "js-prepare" "tcl-prepare" "slime-prepare" "yasnippet-prepare"
     "python-mode-prepare" ;; "iplayer-prepare"
     "erc-prepare" "jabber-prepare" "twittering-prepare"
-    "auth-source-pass" "auth-source-xoauth2")
+    "auth-source-pass" "auth-source-xoauth2" "file-xoauth2")
   "Libraries to load.")
 
 ;;}}}
