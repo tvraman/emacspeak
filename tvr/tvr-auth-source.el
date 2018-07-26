799e9cae7ff66f91e5bf65628787a08879c66c46
T. V. Raman <tv.raman.tv@gmail.com>
2018-07-26 13:06:23 -0700

    file solution doesn't work (yet)

--- tvr/tvr-auth-source.el
+++ tvr/tvr-auth-source.el
@@ -42,85 +42,85 @@ on load.")
       (error "Could not find OAuth2 secrets store")))
   (gethash user auth-xoauth2-tvr-creds))
 
-;; (setq auth-source-xoauth2-creds  #'tvr-auth-source-oauth)
+(setq auth-source-xoauth2-creds  #'tvr-auth-source-oauth)
 (setq auth-source-xoauth2-creds (expand-file-name "~/.xoauth2-creds.gpg"))
 (auth-source-xoauth2-enable)
 (add-to-list 'smtpmail-auth-supported 'xoauth2)
 
-;;}}}
-;;{{{ Tests:
+;; ;;}}}
+;; ;;{{{ Tests:
 
-;;; (auth-source-xoauth2--search nil nil nil "raman@google.com" nil)
-;;; (auth-source-search :host "smtp.gmail.com" :user "raman@google.com" :type 'xoauth2 :max 1)
+;; ;;; (auth-source-xoauth2--search nil nil nil "raman@google.com" nil)
+;; ;;; (auth-source-search :host "smtp.gmail.com" :user "raman@google.com" :type 'xoauth2 :max 1)
 
-;;}}}
-;;{{{ Sending Mail:
+;; ;;}}}
+;; ;;{{{ Sending Mail:
 
-;;  Set send-mail-function via custom.
-(setq
- ;; mail-from-style 'angles
-                                        ;smtpmail-debug-info t
-                                        ;smtpmail-debug-verb t
- smtpmail-stream-type 'ssl
- smtpmail-smtp-user user-mail-address
- smtpmail-smtp-server "smtp.gmail.com"
- smtpmail-smtp-service 465)
+;; ;;  Set send-mail-function via custom.
+;; (setq
+;;  ;; mail-from-style 'angles
+;;                                         ;smtpmail-debug-info t
+;;                                         ;smtpmail-debug-verb t
+;;  smtpmail-stream-type 'ssl
+;;  smtpmail-smtp-user user-mail-address
+;;  smtpmail-smtp-server "smtp.gmail.com"
+;;  smtpmail-smtp-service 465)
 
-;;}}}
-;;{{{GMail Using xoauth2  and Gnus:
-(cl-declaim (special gnus-select-method gnus-secondary-select-methods))
-(setq
- gnus-select-method
- `(nnimap
-   "gmail"
-   (nnimap-address "imap.gmail.com")
-   (nnimap-server-port 993)
-   (nnimap-user ,user-mail-address)
-   (nnimap-authenticator xoauth2)
-   (nnimap-fetch-partial-articles "text/")
-   (nnimap-expunge always)
-   (nnmail-expiry-wait immediate)
-   (nnimap-streaming t)
-   (nnimap-stream ssl)))
+;; ;;}}}
+;; ;;{{{GMail Using xoauth2  and Gnus:
+;; (cl-declaim (special gnus-select-method gnus-secondary-select-methods))
+;; (setq
+;;  gnus-select-method
+;;  `(nnimap
+;;    "gmail"
+;;    (nnimap-address "imap.gmail.com")
+;;    (nnimap-server-port 993)
+;;    (nnimap-user ,user-mail-address)
+;;    (nnimap-authenticator xoauth2)
+;;    (nnimap-fetch-partial-articles "text/")
+;;    (nnimap-expunge always)
+;;    (nnmail-expiry-wait immediate)
+;;    (nnimap-streaming t)
+;;    (nnimap-stream ssl)))
 
-(defun gm-user-to-nnimap (user)
-  "Return nnimap select method for sspecified user."
-  `(nnimap
-    ,user
-    (nnimap-user ,(format "%s@gmail.com" user))
-    (nnimap-authenticator xoauth2)
-    (nnimap-address "imap.gmail.com")
-    (nnimap-server-port 993)
-    (nnimap-fetch-partial-articles "text/")
-    (nnmail-expiry-wait immediate)
-    (nnimap-streaming t)
-    (nnimap-stream ssl)))
+;; (defun gm-user-to-nnimap (user)
+;;   "Return nnimap select method for sspecified user."
+;;   `(nnimap
+;;     ,user
+;;     (nnimap-user ,(format "%s@gmail.com" user))
+;;     (nnimap-authenticator xoauth2)
+;;     (nnimap-address "imap.gmail.com")
+;;     (nnimap-server-port 993)
+;;     (nnimap-fetch-partial-articles "text/")
+;;     (nnmail-expiry-wait immediate)
+;;     (nnimap-streaming t)
+;;     (nnimap-stream ssl)))
 
-(setq gnus-secondary-select-methods
-      (mapcar #'gm-user-to-nnimap
-              '( "tv.raman.tv" "emacspeak")))
+;; (setq gnus-secondary-select-methods
+;;       (mapcar #'gm-user-to-nnimap
+;;               '( "tv.raman.tv" "emacspeak")))
 
-;;}}}
-;;{{{ Utils:
+;; ;;}}}
+;; ;;{{{ Utils:
 
-(defun google-py-oauth2-cli (user app-secret)
-  "generate command-line for pasting into a shell."
-  (format
-   "python oauth2.py --user %s --client_id %s --client_secret %s   --generate_oauth2_token"
-   user
-   (plist-get app-secret :client-id)
-   (plist-get app-secret :client-secret)))
-;;; Usage:
-                                        ;(google-py-oauth2-cli "tv.raman.tv@gmail.com" tvr-app-secrets)
-                                        ;(google-py-oauth2-cli "emacspeak@gmail.com" tvr-app-secrets)
+;; (defun google-py-oauth2-cli (user app-secret)
+;;   "generate command-line for pasting into a shell."
+;;   (format
+;;    "python oauth2.py --user %s --client_id %s --client_secret %s   --generate_oauth2_token"
+;;    user
+;;    (plist-get app-secret :client-id)
+;;    (plist-get app-secret :client-secret)))
+;; ;;; Usage:
+;;                                         ;(google-py-oauth2-cli "tv.raman.tv@gmail.com" tvr-app-secrets)
+;;                                         ;(google-py-oauth2-cli "emacspeak@gmail.com" tvr-app-secrets)
 
-;;}}}
-(provide 'tvr-auth-source)
-;;{{{ end of file
+;; ;;}}}
+;; (provide 'tvr-auth-source)
+;; ;;{{{ end of file
 
-;;; local variables:
-;;; folded-file: t
-;;; byte-compile-dynamic: t
-;;; end:
+;; ;;; local variables:
+;; ;;; folded-file: t
+;; ;;; byte-compile-dynamic: t
+;; ;;; end:
 
-;;}}}
+;; ;;}}}
