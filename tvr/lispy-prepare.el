c0d8dbb5080a88a57622cb3362a7712202cd9c08
T.V Raman <tv.raman.tv@gmail.com>
2018-07-30 17:11:51 -0700

    lispy

--- tvr/lispy-prepare.el
+++ tvr/lispy-prepare.el
@@ -4,6 +4,7 @@
      (cl-declare (special lispy-mode-map lispy-mode-map-lispy))
      (define-key lispy-mode-map (kbd "M-m") nil)
      (define-key lispy-mode-map (kbd ";") 'self-insert-command)
+     (define-key lispy-mode-map (kbd "M-;") 'lispy-comment)
      (define-key lispy-mode-map (kbd "C-d") 'delete-char)
      (define-key lispy-mode-map (kbd "M-C-d") 'lispy-delete)
      
