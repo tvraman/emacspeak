c8366aac56aeb55b2fb19770fff51b11dcbf84ef
T.V Raman <tv.raman.tv@gmail.com>
2018-09-03 16:57:20 -0700

    cleanup

--- tvr/sp-prepare.el
+++ tvr/sp-prepare.el
@@ -1,6 +1,7 @@
-(require 'smartparens-config)
+
 (eval-after-load "smartparens"
   `(progn
+     (require 'smartparens-config)
      (sp-use-smartparens-bindings)
      (define-key  smartparens-mode-map (kbd "C-M-a") 'beginning-of-defun)
      (define-key  smartparens-mode-map (kbd "C-M-e") 'end-of-defun)
