a0ab0197b345a8839ac5a867d85cadbf8de3111a
T.V Raman <tv.raman.tv@gmail.com>
2018-09-03 17:08:32 -0700

    cleanup

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -38,7 +38,7 @@
     "js-prepare" "tcl-prepare" "slime-prepare" "yasnippet-prepare"
     "python-mode-prepare"
     "erc-prepare" "jabber-prepare" "twittering-prepare"
-    "auth-source-pass" "auth-source-xoauth2" )
+    "auth-source-xoauth2" )
   "Libraries to load.")
 
 ;;}}}
