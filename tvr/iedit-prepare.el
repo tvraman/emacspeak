0d2db5ee6691b217c35aca68df49df4117141805
T.V Raman <tv.raman.tv@gmail.com>
2018-11-23 09:35:41 -0800

    use iedit

---
+++ tvr/iedit-prepare.el
@@ -0,0 +1,10 @@
+;;; Setup iedit keybinding without sacrificing hyper == C-;
+
+(defvar tvr-iedit-prefix (kbd "C-z")
+  "Personal iedit prefix key.")
+
+(define-key global-map tvr-iedit-prefix 'iedit-mode)
+(define-key isearch-mode-map tvr-iedit-prefix 'iedit-mode-from-isearch)
+      (define-key esc-map tvr-iedit-prefix 'iedit-execute-last-modification)
+      (define-key help-map tvr-iedit-prefix 'iedit-mode-toggle-on-function)
+      (message "Iedit default key binding is %s" (key-description tvr-iedit-prefix))
