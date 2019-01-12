6778ec98389e261402b0bfcd2e808c380cc75932
T.V Raman <tv.raman.tv@gmail.com>
2019-01-12 08:08:33 -0800

    xcape: updated

--- tvr/xcape.sh
+++ tvr/xcape.sh
@@ -3,10 +3,12 @@
 #XCape ==xcape -- reassign modifier keys etc.
 #Control by itself gives emacspeak modifier.
 #Shift_L gives open paren
+#Shift_R gives Escape
 
 TM=150 #timeout in ms for keyup
 
 KEYS="Shift_L=parenleft;\
+Shift_R=Escape;\
 Control_L=Control_L|e"
 
 xcape -t $TM -e  "$KEYS"
