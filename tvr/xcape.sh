1dfad72b5668bfe00934038642f9a67bab01bd22
T.V Raman <tv.raman.tv@gmail.com>
2019-01-12 09:25:20 -0800

    Added Multi_key on Control_R

--- tvr/xcape.sh
+++ tvr/xcape.sh
@@ -4,11 +4,12 @@
 #Control by itself gives emacspeak modifier.
 #Shift_L gives open paren
 #Shift_R gives Escape
-
+# Control_R gives Multi_key (compose)
 TM=150 #timeout in ms for keyup
 
 KEYS="Shift_L=parenleft;\
 Shift_R=Escape;\
+Control_R=Multi_key;\
 Control_L=Control_L|e"
 
 xcape -t $TM -e  "$KEYS"
