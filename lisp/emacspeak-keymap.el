68586ab715bd8c4d1a92897b855c5c65cd46e1b6
T. V. Raman <tv.raman.tv@gmail.com>
2019-03-28 09:49:45 -0700

    bound eww smart tabs

--- lisp/emacspeak-keymap.el
+++ lisp/emacspeak-keymap.el
@@ -619,6 +619,8 @@ interactive command that the key sequence executes."
 (defcustom emacspeak-alt-keys 
   '(
     ("," emacspeak-eldoc-speak-doc)
+    ("0" emacspeak-eww-smart-tabs-add)
+    ("1" emacspeak-eww-smart-tabs)
     ("a" emacspeak-feeds-atom-display)
     ("b" sox-binaural)
     ("c" emacspeak-wizards-view-buffers-filtered-by-this-mode)
