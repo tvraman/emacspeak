3e06ac7c76ef7d98254868a9d3c35e58009ea2be
T.V Raman <tv.raman.tv@gmail.com>
2019-04-04 13:13:08 -0700

    persist-var: quote if needed

--- lisp/emacspeak-speak.el
+++ lisp/emacspeak-speak.el
@@ -3672,6 +3672,7 @@ Arranges for `VAR' to be restored when `file' is loaded."
       (erase-buffer)
       (insert ";;; Auto-generated.\n\n")
       (insert (format "(setq %s \n" var))
+       (if (listp (symbol-value var)) (insert "'"))
       (pp (symbol-value var) (current-buffer))
       (insert (format ") ;;; set%s\n\n" var))
       (save-buffer))
