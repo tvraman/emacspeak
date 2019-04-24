9b03ea34ff9b03fa34bb745cb706f9111ced7a71
T. V. Raman <tv.raman.tv@gmail.com>
2019-04-24 16:25:15 -0700

    added github-explorer support

--- lisp/emacspeak.el
+++ lisp/emacspeak.el
@@ -214,6 +214,7 @@ that implements the speech-enabling extensions for `package' (a string)."
 (emacspeak-do-package-setup "forms" 'emacspeak-forms)
 (emacspeak-do-package-setup "generic" 'emacspeak-generic)
 (emacspeak-do-package-setup "geiser" 'emacspeak-geiser)
+(emacspeak-do-package-setup "github-explorer" 'emacspeak-gh-explorer)
 (emacspeak-do-package-setup "gtags" 'emacspeak-gtags)
 (emacspeak-do-package-setup "gnus" 'emacspeak-gnus)
 (emacspeak-do-package-setup "gnuplot" 'emacspeak-gnuplot)
