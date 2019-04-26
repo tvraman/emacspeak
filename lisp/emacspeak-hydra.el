8df14b21dd8ab8e5b07e719249102eb287fa5228
T.V Raman <tv.raman.tv@gmail.com>
2019-04-25 17:12:50 -0700

    cleanup-hydra

--- lisp/emacspeak-hydra.el
+++ lisp/emacspeak-hydra.el
@@ -96,7 +96,7 @@ Also turn on hydra-is-helpful if it was turned off."
 ;;; We use plain messages:
 
 (setq hydra-head-format "%s "
-      hydra-lv nil
+      hydra-hint-display-type nil
       hydra-hint-display-type #'message)
 
 (defun emacspeak-hydra-self-help (name)
