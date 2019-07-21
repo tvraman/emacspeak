f8c7354b5afd8750072501e02de3b33dcbe05c85
T.V Raman <tv.raman.tv@gmail.com>
2019-07-21 08:09:28 -0700

    google trends is gone

--- lisp/emacspeak-url-template.el
+++ lisp/emacspeak-url-template.el
@@ -300,14 +300,6 @@ dont-url-encode if true then url arguments are not url-encoded "
  "Google Trends"
  #'emacspeak-feeds-rss-display)
 
-(emacspeak-url-template-define
- "Google Related Trends"
- "https://www.google.com/trends/fetchComponent?hl=en-US&q=%s&geo=US&cid=RISING_QUERIES_0_0"
- (list "Comma Separated Keywords: ")
- nil
- "Display Related Query Trends."
- nil 'dont-escape)
-
 ;;}}}
 ;;{{{ utils:
 
