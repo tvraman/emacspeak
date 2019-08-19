b0c8fa0d078dea6b755bf79ed2add2724315de3b
T.V Raman <tv.raman.tv@gmail.com>
2019-08-18 19:26:46 -0700

    cleanup

--- tvr/early-init.el
+++ tvr/early-init.el
@@ -1,4 +1,6 @@
-(setq package-quickstart nil ; slime bug
-      gc-cons-threshold 64000000
-      load-source-file-function nil
-      file-name-handler-alist nil)
+(setq
+                                        ;package-quickstart nil                 ; slime bug
+ gc-cons-threshold 64000000
+                                        load-source-file-function nil
+                                        file-name-handler-alist nil
+ )
