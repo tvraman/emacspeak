a45d976bf42dd78247d1c6f48af8d107e36deee2
T.V Raman <tv.raman.tv@gmail.com>
2019-08-20 19:31:40 -0700

    cleanup

--- lisp/emacspeak-m-player.el
+++ lisp/emacspeak-m-player.el
@@ -426,7 +426,7 @@ The player is placed in a buffer in emacspeak-m-player-mode."
     current-prefix-arg))
   (cl-declare (special
                emacspeak-m-player-file-list emacspeak-m-player-current-directory
-               ido-work-directory-list emacspeak-media-directory-regexp
+               emacspeak-media-directory-regexp
                emacspeak-media-shortcuts-directory emacspeak-m-player-process
                emacspeak-m-player-program emacspeak-m-player-options))
   (when (and emacspeak-m-player-process
