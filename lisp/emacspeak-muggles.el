846fc81cea49c3d19ff9e65edec06e803ca4e45c
T.V Raman <tv.raman.tv@gmail.com>
2020-04-01 19:24:03 -0700

    bugfix

--- lisp/emacspeak-muggles.el
+++ lisp/emacspeak-muggles.el
@@ -770,7 +770,7 @@ Also generates global keybindings if any."
    (:body-pre
     (progn
       (when hydra-is-helpful (emacspeak-hydra-toggle-talkative))
-      (emacspeak-hydra-body-pre "Vuiet  Player"))
+      (emacspeak-hydra-body-pre "Vuiet  Explorer"))
     :pre emacspeak-hydra-pre :post emacspeak-hydra-post)
    (";" vuiet-playing-track-lyrics)
    ("," vuiet-seek-backward)
