f2714188268c098e2df9eb7b9b04cfff0850950e
T.V Raman <tv.raman.tv@gmail.com>
2020-04-03 09:03:45 -0700

    doc update

--- lisp/emacspeak-muggles.el
+++ lisp/emacspeak-muggles.el
@@ -68,7 +68,8 @@
 ;;; @item outliner: <C-c .> Bindings from outline-minor-mode.
 ;;;@item Info-Summary: <?> in Info Info Summary Muggle
 ;;; @item Repeatable-Yank: <C-y> Smart yank
-;;; @item SmartParens: <C-c ,> Smart Parens 
+;;; @item SmartParens: <C-c ,> Smart Parens
+;;; @item Vuiet Explorer: <s-v> Vuiet Music Explorer and Player
 ;;;@end itemize
 
 ;;; Emacspeak automatically speaks Hydra hints when displayed.
