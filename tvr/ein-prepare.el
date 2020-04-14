8ae715a4fa760b0d741d3213f90b1e4308e38c81
T.V Raman <tv.raman.tv@gmail.com>
2020-04-14 14:47:07 -0700

    ein

--- tvr/ein-prepare.el
+++ tvr/ein-prepare.el
@@ -1,5 +1,4 @@
 ;;; ein-prepare.el --- EIN Setup                     -*- lexical-binding: t; -*-
 (require 'ein)
-(require 'ein-loaddefs)
 (require 'ein-notebook)
-(require 'ein-subpackages)
+(load-library "ein-autoloads")
