c828b0ecba9fa1211046998152bacc4f757d1d52
T.V Raman <tv.raman.tv@gmail.com>
2020-04-23 14:56:24 -0700

    cleanup

--- tvr/early-init.el
+++ tvr/early-init.el
@@ -2,5 +2,4 @@
                                         package-quickstart t
  gc-cons-threshold 64000000
                                         load-source-file-function nil
-                                        file-name-handler-alist nil
  )
