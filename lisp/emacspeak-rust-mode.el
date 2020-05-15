9fbdeb4fdb0d5eaa3aa6a76aaf8461c7dc6f526c
T.V Raman <tv.raman.tv@gmail.com>
2020-05-14 18:50:02 -0700

    rust-mode: complete

--- lisp/emacspeak-rust-mode.el
+++ lisp/emacspeak-rust-mode.el
@@ -63,6 +63,17 @@
 ;;}}}
 ;;{{{ Interactive Commands:
 
+(cl-loop
+ for f in 
+ '(rust-compile  rust-run rust-test rust-run-clippy) do
+ (eval
+  `(defadvice ,f (after emacspeak pre act comp)
+     "Provide auditory feedback."
+     (when (ems-interactive-p)
+       (emacspeak-auditory-icon 'task-done))))) 
+
+do
+
 (defadvice rust-dbg-wrap-or-unwrap (after emacspeak pre act comp)
   "Provide auditory feedback."
   (when (ems-interactive-p)
