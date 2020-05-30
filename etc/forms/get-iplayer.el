be076114062f45bd56016203e480839060fbc27c
T.V Raman <tv.raman.tv@gmail.com>
2020-05-29 18:38:16 -0700

    cleanup

--- etc/forms/get-iplayer.el
+++ etc/forms/get-iplayer.el
@@ -1,8 +1,5 @@
 ;;; get-iplayer.el --- Speech friendly interface to get_iplayer cache   -*- lexical-binding: t; -*-
 ;;; Description:  Use forms-mode to look at iplayer cache
-;;; Keywords: forms
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;{{{  setup forms
 
 (setq forms-read-only t)
 (setq forms-file (expand-file-name "~/.get_iplayer/radio.cache"))
@@ -10,14 +7,6 @@
 (setq forms-field-sep "|")
 (setq forms-format-list
       (list
-       "Id" : 1 "Type"  : 2 "\n"
-       "Episode" : 4  "Description" : 12 "\n"
+       "Id: "  1 "Type: "   2 "\n"
+       "Episode: "  4  "Description: "  12 "\n"
        ))
-;;}}}
-;;{{{ end of file
-
-;;; local variables:
-;;; folded-file: t
-;;; end:
-
-;;}}}
