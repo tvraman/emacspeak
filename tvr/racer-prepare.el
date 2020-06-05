e86fbe74e542feaf4ea1fbf1f44a6b4a5c0fc7d1
T.V Raman <tv.raman.tv@gmail.com>
2020-06-05 09:16:45 -0700

    set up racer

---
+++ tvr/racer-prepare.el
@@ -0,0 +1,6 @@
+;;; Set up racer for completion etc in rust buffers.
+;;; See https://github.com/racer-rust/emacs-racer
+(require 'rust-mode)
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
