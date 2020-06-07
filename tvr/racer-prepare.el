ce7dafc58ebf27d937c50835aacb5d38b43272d8
T.V Raman <tv.raman.tv@gmail.com>
2020-06-07 09:20:46 -0700

    hooking into xref needs better documentation

--- tvr/racer-prepare.el
+++ tvr/racer-prepare.el
@@ -3,4 +3,18 @@
 (require 'rust-mode)
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'company-mode)
-(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(define-key rust-mode-map (kbd "TAB")
+  #'company-indent-or-complete-common)
+
+
+;;; xref and racer:
+(defun racer--xref-backend () 'racer)
+
+(add-hook 'xref-backend-functions #'racer--xref-backend)
+
+(cl-defmethod xref-backend-identifier-at-point ((_backend (eql racer)))
+  (racer-find-definition))
+
+
+(cl-defmethod xref-backend-identifier-completion-table ((_backend (eql racer)))
+  (completion-table-dynamic #'racer-complete))
