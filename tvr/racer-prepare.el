3f62ab5f25d348a8aa952672ce2eaab0e8a5c6b5
T.V Raman <tv.raman.tv@gmail.com>
2020-06-08 19:28:55 -0700

    cleanup

--- tvr/racer-prepare.el
+++ tvr/racer-prepare.el
@@ -1,7 +1,6 @@
 ;;; Set up racer for completion etc in rust buffers.
 ;;; See https://github.com/racer-rust/emacs-racer
 (require 'rust-mode)
-(add-hook 'rust-mode-hook #'eglot)
 (add-hook 'rust-mode-hook #'racer-mode)
 (add-hook 'racer-mode-hook #'company-mode)
 (define-key rust-mode-map (kbd "TAB")
