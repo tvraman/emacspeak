2e180118e7a31b532b0c4b7c00b769227e77c770
T.V Raman <tv.raman.tv@gmail.com>
2020-06-24 18:57:02 -0700

    setting outline-minor-mode here has no effect -- too late.

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -19,7 +19,6 @@
 (require 'cl-lib)
 (cl-declaim (optimize (safety 0) (speed 3)))
 (setq ad-redefinition-action 'accept)
-(defvar  outline-minor-mode-prefix "M-#")
 (defvar emacspeak-speak-messages)
 (defvar emacs-personal-library
   (expand-file-name "~/emacs/lisp/site-lisp")
