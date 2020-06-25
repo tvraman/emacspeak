85948b5d8a8dfc4a2ee02ed688588ce0a26922f1
T.V Raman <tv.raman.tv@gmail.com>
2020-06-24 18:51:39 -0700

    orgalist

---
+++ lisp/emacspeak-orgalist.el
@@ -0,0 +1,83 @@
+;;; emacspeak-orgalist.el --- Speech-enable ORGALIST  -*- lexical-binding: t; -*-
+;;; $Author: tv.raman.tv $
+;;; Description:  Speech-enable ORGALIST An Emacs Interface to orgalist
+;;; Keywords: Emacspeak,  Audio Desktop orgalist
+;;{{{  LCD Archive entry:
+
+;;; LCD Archive Entry:
+;;; emacspeak| T. V. Raman |raman@cs.cornell.edu
+;;; A speech interface to Emacs |
+;;; $Date: 2007-05-03 18:13:44 -0700 (Thu, 03 May 2007) $ |
+;;;  $Revision: 4532 $ |
+;;; Location undetermined
+;;;
+
+;;}}}
+;;{{{  Copyright:
+;;;Copyright (C) 1995 -- 2007, 2019, T. V. Raman
+;;; All Rights Reserved.
+;;;
+;;; This file is not part of GNU Emacs, but the same permissions apply.
+;;;
+;;; GNU Emacs is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2, or (at your option)
+;;; any later version.
+;;;
+;;; GNU Emacs is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNORGALIST FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Emacs; see the file COPYING.  If not, write to
+;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;}}}
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;{{{  introduction
+
+;;; Commentary:
+;;; Speech-enable orgalist --- create org-like lists everywhere.
+
+;;; Code:
+
+;;}}}
+;;{{{  Required modules
+
+(require 'cl-lib)
+(cl-declaim  (optimize  (safety 0) (speed 3)))
+(require 'emacspeak-preamble)
+
+;;}}}
+;;{{{ Interactive Commands:
+
+(cl-loop
+ for f in 
+ '(
+  orgalist--cycle-indentation orgalist-check-item orgalist-cycle-bullet
+orgalist-indent-item orgalist-indent-item-tree orgalist-insert-item
+orgalist-insert-radio-list orgalist-move-item-down orgalist-move-item-up
+orgalist-next-item orgalist-outdent-item orgalist-outdent-item-tree
+orgalist-previous-item
+)
+ do
+ (eval
+  `(defadvice ,f (after emacspeak pre act comp)
+     "Provide auditory feedback."
+     (when (ems-interactive-p)
+       (emacspeak-speak-line)
+       (emacspeak-auditory-icon 'select-object)))))
+
+
+
+;;}}}
+(provide 'emacspeak-orgalist)
+;;{{{ end of file
+
+;;; local variables:
+;;; folded-file: t
+;;; end:
+
+;;}}}
