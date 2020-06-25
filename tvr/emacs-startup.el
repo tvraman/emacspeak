9a919e5a3776bf5e43c96539053cbc8b8eb3f4e4
T.V Raman <tv.raman.tv@gmail.com>
2020-06-25 12:52:05 -0700

    outline minor mode prefix is C-o

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -19,7 +19,7 @@
 (require 'cl-lib)
 (cl-declaim (optimize (safety 0) (speed 3)))
 (setq ad-redefinition-action 'accept)
-(setq outline-minor-mode-prefix "\M-#")
+(setq outline-minor-mode-prefix (kbd "C-o"))
 (defvar emacspeak-speak-messages)
 (defvar emacs-personal-library
   (expand-file-name "~/emacs/lisp/site-lisp")
