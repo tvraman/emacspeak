eceb22af0514ccedb03ab51798fe9150b0814054
T.V Raman <tv.raman.tv@gmail.com>
2020-06-27 07:31:54 -0700

    keybinds need fixing

--- tvr/org-prepare.el
+++ tvr/org-prepare.el
@@ -3,6 +3,24 @@
 
 (eval-after-load "org"
   `(progn
+;;;  List creation commands:
+
+     (defun tvr-org-itemize ()
+       "Start a numbered  list."
+       (interactive)
+       (forward-line 0)
+       (insert "  -  "))
+
+     (defun tvr-org-enumerate ()
+       "Start a numbered  list."
+       (interactive)
+       (forward-line 0)
+       (insert "  1.  "))
+
+;;; add these to outline-minor-mode-map  
+     (define-key outline-minor-mode-map "i" 'tvr-org-itemize)
+     (define-key outline-minor-mode-map "e" 'tvr-org-enumerate)
+     
      (define-key global-map "\C-cl" 'org-store-link)
      (define-key global-map "\C-cb" 'org-switchb)
      (define-key global-map "\C-cc" 'org-capture)
