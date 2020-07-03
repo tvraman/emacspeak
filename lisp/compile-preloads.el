586acf8a955064ea243c8a8554eaa3d418a2b0b8
T.V Raman <tv.raman.tv@gmail.com>
2020-07-03 16:37:15 -0700

    attempt to silence load messages (fail)

--- lisp/compile-preloads.el
+++ lisp/compile-preloads.el
@@ -1,9 +1,14 @@
 (setq file-name-handler-alist nil
       gc-cons-threshold 64000000
       load-source-file-function  nil)
-(load-file "../lisp/emacspeak-load-path.el")
-(load-file "../lisp/emacspeak-loaddefs.el")
-(load-file "../lisp/emacspeak-cus-load.el")
-(load-file "../lisp/g-client/g-load-path.el")
-(load-file "../lisp/g-client/g-cus-load.el")
-(load-file "../lisp/g-client/g-loaddefs.el")
+(let ((message-log-max nil)
+      (inhibit-message  nil))
+  (mapcar
+   #'load-file
+   '(
+     "../lisp/emacspeak-load-path.el"
+     "../lisp/emacspeak-loaddefs.el"
+     "../lisp/emacspeak-cus-load.el"
+     "../lisp/g-client/g-load-path.el"
+     "../lisp/g-client/g-cus-load.el"
+     "../lisp/g-client/g-loaddefs.el")))
