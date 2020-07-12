cee8fde1fafeb343c8b3e6024df1e28ef8bafea3
T.V Raman <tv.raman.tv@gmail.com>
2020-07-12 10:59:17 -0700

    nyxt: start swank

--- nextalk/init.lisp
+++ nextalk/init.lisp
@@ -3,4 +3,7 @@
 (ql:quickload "tts")
 (tts:init)
 (tts:speak "Welcome To The Next In Browsers!")
+(swank:create-server :port 4006 :dont-close t)
+(tts:speak "Swank server started at port 4006")
 
+(tts:speak "Done loading init file!")
