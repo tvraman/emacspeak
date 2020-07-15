56fcaa25feb1dea594c2cbf75bc95b0b732343d4
T.V Raman <tv.raman.tv@gmail.com>
2020-07-14 17:19:20 -0700

    updated

--- nextalk/init.lisp
+++ nextalk/init.lisp
@@ -1,10 +1,7 @@
-(in-package :nyxt)
-
 (ql:quickload "tts")
 (tts:init)
 (tts:speak "Welcome To The Next In Browsers!")
-(swank:create-server :port 4006 :dont-close t)
-(tts:speak "Swank server started at port 4006")
 (define-configuration buffer
     ((default-modes (append '(emacs-mode) %slot-default))))
-(tts:speak "Done loading init file!")
+(swank:create-server :port 4006 :dont-close t)
+(tts:speak "Swank server started at port 4006")
