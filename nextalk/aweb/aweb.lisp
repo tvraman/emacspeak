5c6f3d6f18d8daf8b946b26282cdf5c7081ec478
T.V Raman <tv.raman.tv@gmail.com>
2020-07-16 09:04:42 -0700

    aweb

---
+++ nextalk/aweb/aweb.lisp
@@ -0,0 +1,51 @@
+;;;   -*- Syntax: Common-Lisp; Package: tts ; Mode: LISP -*-    ;;;
+;;; aweb.lisp -- Speech-Enable Nyxt Browser
+;;; $Author: tv.raman.tv $
+;;; Description: Speech-enable nyxt via  Common Lisp bridge to Emacspeak TTS servers
+;;; Keywords: Next, Emacspeak, Audio Desktop
+;;{{{ Copyright:
+
+;;; Copyright (C) -2020, T. V. Raman<tv.raman.tv@gmail.com>
+;;; All Rights Reserved.
+;;;
+;;; This file is not part of GNU Emacs, but the same permissions apply.
+;;;
+;;; GNU Emacs is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2, or (at your option)
+;;; any later version.
+;;;
+;;; GNU Emacs is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Emacs; see the file COPYING. If not, write to
+;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;}}}
+;;{{{ Introduction:
+
+;;; Commentary:
+;;; Speech-enable nyxt.
+
+;;}}}
+;;{{{Package:
+(proclaim '(optimize (compilation-speed 0) (safety 1) (speed 3)))
+(in-package :cl-user)
+
+(defpackage :aweb (:use :common-lisp :tts)
+            )
+
+(in-package :aweb)
+
+;;}}}
+;;{{{ end of file
+
+;;; local variables:
+;;; folded-file: t
+;;; byte-compile-dynamic: t
+;;; end:
+
+;;}}}
