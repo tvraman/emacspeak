b70baa96b99b66f060b5accbd9ed41b754aba6c2
T.V Raman <tv.raman.tv@gmail.com>
2020-07-19 17:12:56 -0700

    dired-omit

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,21 +1,15 @@
-;;; Directory Local Variables  -*- lexical-binding: t; -*-
+;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c-mode
-  (folded-file . t)
-  (c-file-style . "GNU"))
- (change-log-mode
-  (add-log-time-zone-rule . t)
-  (fill-column . 74))
- (diff-mode
-  (mode . whitespace))
- (emacs-lisp-mode
-  (folded-file . t)
-  (bug-reference-url-format
-   . "https://github.com/tvraman/emacspeak/issues/%s")
-  (indent-tabs-mode . nil))
- (log-edit-mode
-  (log-edit-font-lock-gnu-style . t)
-  (log-edit-setup-add-author . t))
- )
+((c-mode . ((folded-file . t)
+            (c-file-style . "GNU")))
+ (change-log-mode . ((add-log-time-zone-rule . t)
+                     (fill-column . 74)))
+ (diff-mode . ((mode . whitespace)))
+ (dired-mode . ((dired-omit-mode . t)))
+ (emacs-lisp-mode . ((folded-file . t)
+                     (bug-reference-url-format . "https://github.com/tvraman/emacspeak/issues/%s")
+                     (indent-tabs-mode . nil)))
+ (log-edit-mode . ((log-edit-font-lock-gnu-style . t)
+                   (log-edit-setup-add-author . t))))
 
