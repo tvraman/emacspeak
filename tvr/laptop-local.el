058f5ab84f9ec3e0ff02f67810df542a9cf44772
T.V Raman <tv.raman.tv@gmail.com>
2020-08-08 09:24:49 -0700

    laptop-local

--- tvr/laptop-local.el
+++ tvr/laptop-local.el
@@ -1,8 +1,9 @@
 ;; -*- lexical-binding: nil; -*-
-(setq pre-redisplay-function nil
-      x-wait-for-event-timeout 0)
 
+(setq pre-redisplay-function nil
+      x-wait-for-event-timeout 0
+      mail-host-address "google.com")
 (load-library "touchpad-disable")
 (load-library "xbacklight")
-(when emacspeak-mail-alert (emacspeak-toggle-mail-alert))
+(setq emacspeak-mail-alert nil)
 (provide 'laptop-local)
