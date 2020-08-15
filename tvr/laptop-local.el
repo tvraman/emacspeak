77fcb1fa30733fb1cbe4e9a0b41a9483f8ff6be6
T.V Raman <tv.raman.tv@gmail.com>
2020-08-15 13:06:01 -0700

    cleanup

--- tvr/laptop-local.el
+++ tvr/laptop-local.el
@@ -2,9 +2,8 @@
 
 (setq pre-redisplay-function nil
       x-wait-for-event-timeout 0
-      mail-host-address "google.com")
-user-mail-address "raman@google.com"
+      mail-host-address "google.com"
+      user-mail-address "raman@google.com")
 (load-library "touchpad-disable")
 (load-library "xbacklight")
-(setq emacspeak-mail-alert nil)
 (provide 'laptop-local)
