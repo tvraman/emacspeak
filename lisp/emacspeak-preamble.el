b9da907096e929bd42671426bb206492d8762197
T.V Raman <tv.raman.tv@gmail.com>
2020-08-16 08:33:42 -0700

    attempt to eliminate package-quickstart during compile

--- lisp/emacspeak-preamble.el
+++ lisp/emacspeak-preamble.el
@@ -54,7 +54,8 @@
 (require 'emacspeak-keymap)
 (eval-when-compile
   (condition-case nil
-      (package-initialize)
+      (progn (setq package-quickstart nil)
+             (package-initialize))
     (error "Check your elpa packages --- something is broken")))
 
 ;;}}}
