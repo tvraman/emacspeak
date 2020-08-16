d46492b3a641faea62a85786dbad5fb8abb79873
T.V Raman <tv.raman.tv@gmail.com>
2020-08-15 19:49:38 -0700

    restored shr renderer

--- tvr/vm-prepare.el
+++ tvr/vm-prepare.el
@@ -7,29 +7,35 @@
 (eval-after-load "vm"
   `(progn
      (load "vm-autoloads")
-(when (require 'bbdb) (bbdb-insinuate-vm))
+     (when (require 'bbdb) (bbdb-insinuate-vm))
 
-(global-set-key "\C-xm" 'vm-mail)
+     (global-set-key "\C-xm" 'vm-mail)
 
+
+(defun vm-mime-display-internal-shr-text/html (start end _layout)
+  "Use shr to inline HTML mails in the VM presentation buffer."
+    (shr-render-region start (1- end))
+    (put-text-property start end 'text-rendered-by-shr t))
+     
 ;;; has to be done indirectly
 ;;; Fake emacs-w3m, though we actually use shr
 
-(setq vm-mime-text/html-handler'emacs-w3m  )
-(defalias 'vm-mime-display-internal-emacs-w3m-text/html  'vm-mime-display-internal-shr-text/html)
+     (setq vm-mime-text/html-handler'emacs-w3m  )
+     (defalias 'vm-mime-display-internal-emacs-w3m-text/html  'vm-mime-display-internal-shr-text/html)
 
-(defun vm-chromium ()
-  "Run Chromium on current link."
-  (interactive)
-  (let ((url (browse-url-url-at-point)))
-    (unless url (error "No link here."))
-    (dtk-stop)
-    (browse-url-chrome url)
-    (message "Opening url with Chrome")))
+     (defun vm-chromium ()
+       "Run Chromium on current link."
+       (interactive)
+       (let ((url (browse-url-url-at-point)))
+         (unless url (error "No link here."))
+         (dtk-stop)
+         (browse-url-chrome url)
+         (message "Opening url with Chrome")))
 
-(define-key vm-mode-map "C" 'vm-chromium)
-(define-key vm-mode-map "o" 'mspools-show)
-(load-library "mspools")
-))
+     (define-key vm-mode-map "C" 'vm-chromium)
+     (define-key vm-mode-map "o" 'mspools-show)
+     (load-library "mspools")
+     ))
 
 (eval-after-load
     "mspools"
