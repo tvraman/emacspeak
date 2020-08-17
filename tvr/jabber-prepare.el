4879c5c524cecea212eeb2f6a5208822fac56601
T.V Raman <tv.raman.tv@gmail.com>
2020-08-16 17:47:25 -0700

    jabber-cleanup

--- tvr/jabber-prepare.el
+++ tvr/jabber-prepare.el
@@ -1,24 +1,25 @@
 ;; -*- lexical-binding: nil; -*-
+(define-key ctl-x-map (kbd "C-j") jabber-global-keymap)
+(eval-after-load "jabber")
+`(progn
+   (setq fsm-debug nil)
+   (add-hook 'jabber-roster-mode-hook
+             #'(lambda nil
+                 (setq jabber-mode-line-string
+                       (list " "
+                             'jabber-mode-line-presence
+                                        ;" "
+                                        ;'jabber-mode-line-contacts
+                             ))
+                 (defalias 'assoc-ignore-case 'assoc)
+                 (setq buffer-undo-list t)))
 
-(define-key ctl-x-map "\C-j" jabber-global-keymap)
-
-(setq fsm-debug nil)
-(add-hook 'jabber-roster-mode-hook
-          #'(lambda nil
-              (setq jabber-mode-line-string
-      (list " "
-              'jabber-mode-line-presence
-              ;" "
-              ;'jabber-mode-line-contacts
-              ))
-              (defalias 'assoc-ignore-case 'assoc)
-              (setq buffer-undo-list t)))
-
-(setq
- jabber-network-server "talk.google.com"
- jabber-account-list
- `((
-    ,(concat (user-login-name) "@" (or mail-host-address (system-name)))
-    (:network-server . "talk.google.com")
-    (:port . 5223)
-    (:connection-type . ssl))))
+   (setq
+    jabber-network-server "talk.google.com"
+    jabber-account-list
+    `((
+       ,(concat (user-login-name) "@" (or mail-host-address (system-name)))
+       (:network-server . "talk.google.com")
+       (:port . 5223)
+       (:connection-type . ssl))))
+   )
