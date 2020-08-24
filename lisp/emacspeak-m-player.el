8f300a93e776feaed73aecb2286b731143a60bb2
T.V Raman <tv.raman.tv@gmail.com>
2020-08-23 17:32:19 -0700

    mode-line: use initial pause of 60 ms

--- lisp/emacspeak-m-player.el
+++ lisp/emacspeak-m-player.el
@@ -167,7 +167,7 @@ This is set to nil when playing Internet  streams.")
      (let ((info (emacspeak-m-player-get-position)))
        (when info
          (concat
-          "At "
+          (propertize "At " 'pause 60)
           (ems--seconds-string-to-duration (cl-first info))
           (propertize " of " 'personality voice-smoothen-extra)
           (ems--seconds-string-to-duration (cl-third info))
