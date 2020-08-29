9ccc7f2aeca180572a45369ff5b66a420520a001
T.V Raman <tv.raman.tv@gmail.com>
2020-08-29 07:18:36 -0700

    cleanup

--- tvr/vm-prepare.el
+++ tvr/vm-prepare.el
@@ -1,7 +1,5 @@
 ;;; vm-prepare.l :  -*- lexical-binding: nil; -*-
-
 (with-eval-after-load "vm"
      (global-set-key (kbd "C-x m") 'vm-mail)
      (when (require 'bbdb) (bbdb-insinuate-vm))
      (load-library "mspools"))
-
