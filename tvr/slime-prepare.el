41411d0d5a818e64aaa080cdb16e9b008cdfa235
T.V Raman <tv.raman.tv@gmail.com>
2020-08-29 07:42:36 -0700

    cleanup

--- tvr/slime-prepare.el
+++ tvr/slime-prepare.el
@@ -1,18 +1,15 @@
 ;; -*- lexical-binding: nil; -*-
-(eval-after-load "slime"
-  `(progn
-     (setq inferior-lisp-program (executable-find "sbcl"))
-     (setq common-lisp-hyperspec-root
-           (if (file-exists-p "/usr/share/doc/hyperspec/")
-               "file:///usr/share/doc/hyperspec/"
-             "http://www.lispworks.com/reference/HyperSpec/"))
-     (global-set-key (kbd "C-c s") 'slime-selector)
-     (setq slime-contribs '(slime-fancy slime-hyperdoc slime-quicklisp))
-     (slime-setup)
-     (slime-autodoc--disable)
-     (setq slime-use-autodoc-mode nil)
-     (setq slime-lisp-implementations
-           `((sbcl ("sbcl"
-                    "--core"
-                    ,(expand-file-name "sbcl.core-for-slime" "~/.emacs.d")))))
-     ))
+(with-eval-after-load "slime"
+  (setq inferior-lisp-program (executable-find "sbcl"))
+  (setq common-lisp-hyperspec-root
+        (if (file-exists-p "/usr/share/doc/hyperspec/")
+            "file:///usr/share/doc/hyperspec/"
+          "http://www.lispworks.com/reference/HyperSpec/"))
+  (global-set-key (kbd "C-c s") 'slime-selector)
+  (setq slime-contribs '(slime-fancy slime-hyperdoc slime-quicklisp))
+  (slime-setup)
+  (slime-autodoc--disable)
+  (setq slime-use-autodoc-mode nil)
+  (setq
+   slime-lisp-implementations
+   `((sbcl ("sbcl" "--core" ,user-emacs-directory)))))
