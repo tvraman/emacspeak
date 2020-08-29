8c2a1b6b6f732241c98554409e69656af3be76d5
T.V Raman <tv.raman.tv@gmail.com>
2020-08-29 07:50:29 -0700

    cleanup

--- tvr/lispy-prepare.el
+++ tvr/lispy-prepare.el
@@ -1,48 +1,22 @@
-(eval-after-load
-    "lispy"
-  `(progn
-     (cl-declare (special lispy-mode-map lispy-mode-map-lispy))
-     (let ((cmd
-            (if tab-bar-mode 'tab-bar-select-tab 'digit-argument)))
-       (cl-loop
-        for i from 1 to 9 do
-        (define-key lispy-mode-map-lispy (vector (list 'control (+ i 1 ?0))) cmd)
-        (define-key lispy-mode-map (vector (list 'control (+ i 1 ?0))) cmd)))
-     (define-key lispy-mode-map (kbd "M-m") nil)
-     (define-key lispy-mode-map (kbd "C-y") 'emacspeak-muggles-yank-pop/yank)
-     (define-key lispy-mode-map (kbd ";") 'self-insert-command)
-     (define-key lispy-mode-map (kbd ":") 'self-insert-command)
-     (define-key lispy-mode-map (kbd "M-;") 'lispy-comment)
-     (define-key lispy-mode-map (kbd "C-d") 'delete-char)
-     (define-key lispy-mode-map (kbd "M-C-d") 'lispy-delete)
-     (define-key lispy-mode-map (kbd "M-d") 'kill-word)
-     (define-key lispy-mode-map (kbd "M-e") 'lispy-move-end-of-line)
-     (define-key lispy-mode-map "a" 'special-lispy-beginning-of-defun)
-     (define-key lispy-mode-map "A" 'special-lispy-ace-symbol)
-     (define-key lispy-mode-map (kbd "C-,") nil)
-     (define-key lispy-mode-map [(control left)] 'lispy-barf)
-     (define-key lispy-mode-map [(control right)] 'lispy-slurp)
-     (define-key lispy-mode-map-lispy (kbd "C-,") nil)
-     (add-hook
-      'lispy-mode-hook
-      #'(lambda nil
-          (setq
-           header-line-format
-           '((:eval
-              (format "%s %s %s"
-                      (buffer-name)
-                      " Lispy"
-                      (if vc-mode
-                          (concat
-                           vc-mode
-                           (format "%s" (vc-state (buffer-file-name))))
-                        "")))))))
-
-;;;###autoload
+(defun conditionally-enable-lispy ()
+    (when (memq this-command '(eval-expression emacspeak-wizards-show-eval-result))
+      (lispy-mode 1)))
+(with-eval-after-load "lispy"
+  (cl-declare (special lispy-mode-map lispy-mode-map-lispy))
+  (define-key lispy-mode-map (kbd "M-m") nil)
+  (define-key lispy-mode-map (kbd "C-y") 'emacspeak-muggles-yank-pop/yank)
+  (define-key lispy-mode-map (kbd ";") 'self-insert-command)
+  (define-key lispy-mode-map (kbd ":") 'self-insert-command)
+  (define-key lispy-mode-map (kbd "M-;") 'lispy-comment)
+  (define-key lispy-mode-map (kbd "C-d") 'delete-char)
+  (define-key lispy-mode-map (kbd "M-C-d") 'lispy-delete)
+  (define-key lispy-mode-map (kbd "M-d") 'kill-word)
+  (define-key lispy-mode-map (kbd "M-e") 'lispy-move-end-of-line)
+  (define-key lispy-mode-map "a" 'special-lispy-beginning-of-defun)
+  (define-key lispy-mode-map "A" 'special-lispy-ace-symbol)
+  (define-key lispy-mode-map (kbd "C-,") nil)
+  (define-key lispy-mode-map [(control left)] 'lispy-barf)
+  (define-key lispy-mode-map [(control right)] 'lispy-slurp)
+  (define-key lispy-mode-map-lispy (kbd "C-,") nil)
 ;;;  Lispy for eval-expression:
-
-     (defun conditionally-enable-lispy ()
-       (when (memq this-command '(eval-expression emacspeak-wizards-show-eval-result))
-         (lispy-mode 1)))
-     (add-hook 'minibuffer-setup-hook 'conditionally-enable-lispy)
-     ))
+  (add-hook 'minibuffer-setup-hook 'conditionally-enable-lispy))
--- tvr/sp-prepare.el
+++ tvr/sp-prepare.el
@@ -1,15 +1,13 @@
 
-(eval-after-load "smartparens"
-  `(progn
-     (require 'smartparens-config)
-     (sp-use-smartparens-bindings)
-     (define-key  smartparens-mode-map (kbd "C-M-a") 'beginning-of-defun)
-     (define-key  smartparens-mode-map (kbd "C-M-e") 'end-of-defun)
-     (define-key  smartparens-mode-map (kbd "C-M-k") 'kill-sexp)
-     (define-key  smartparens-mode-map (kbd "C-M-SPC") 'mark-sexp)
-     (define-key smartparens-mode-map (kbd "M-a") 'sp-backward-down-sexp)
-     (define-key smartparens-mode-map (kbd "M-e") 'sp-up-sexp)
-     (define-key smartparens-mode-map (kbd "M-k") 'sp-kill-sexp)
-     (define-key smartparens-mode-map (kbd "C-M-f") 'forward-sexp)
-     (define-key smartparens-mode-map (kbd "C-M-b") 'backward-sexp)
-     ))
+(with-eval-after-load "smartparens"
+  (require 'smartparens-config)
+  (sp-use-smartparens-bindings)
+  (define-key  smartparens-mode-map (kbd "C-M-a") 'beginning-of-defun)
+  (define-key  smartparens-mode-map (kbd "C-M-e") 'end-of-defun)
+  (define-key  smartparens-mode-map (kbd "C-M-k") 'kill-sexp)
+  (define-key  smartparens-mode-map (kbd "C-M-SPC") 'mark-sexp)
+  (define-key smartparens-mode-map (kbd "M-a") 'sp-backward-down-sexp)
+  (define-key smartparens-mode-map (kbd "M-e") 'sp-up-sexp)
+  (define-key smartparens-mode-map (kbd "M-k") 'sp-kill-sexp)
+  (define-key smartparens-mode-map (kbd "C-M-f") 'forward-sexp)
+  (define-key smartparens-mode-map (kbd "C-M-b") 'backward-sexp))
