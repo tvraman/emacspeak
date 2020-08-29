b962fcfc17b46f55e3b7a69aa190bfaf19c99ca7
T.V Raman <tv.raman.tv@gmail.com>
2020-08-29 07:59:41 -0700

    cleanup

--- tvr/jabber-prepare.el
+++ tvr/jabber-prepare.el
@@ -1,25 +1,15 @@
 ;; -*- lexical-binding: nil; -*-
+(defalias 'assoc-ignore-case 'assoc)
 (define-key ctl-x-map (kbd "C-j") jabber-global-keymap)
-(eval-after-load "jabber"
-  `(progn
-     (setq fsm-debug nil)
-     (add-hook 'jabber-roster-mode-hook
-               #'(lambda nil
-                   (setq jabber-mode-line-string
-                         (list " "
-                               'jabber-mode-line-presence
-                                        ;" "
-                                        ;'jabber-mode-line-contacts
-                               ))
-                   (defalias 'assoc-ignore-case 'assoc)
-                   (setq buffer-undo-list t)))
-
-     (setq
-      jabber-network-server "talk.google.com"
-      jabber-account-list
-      `((
-         "raman@google.com" ;;; hard-wired for now 
-         (:network-server . "talk.google.com")
-         (:port . 5223)
-         (:connection-type . ssl))))
-     ))
+(with-eval-after-load "jabber"
+  (setq fsm-debug nil)
+  (setq jabber-mode-line-string
+        (list " " 'jabber-mode-line-presence))
+  (setq
+   jabber-network-server "talk.google.com"
+   jabber-account-list
+   `((
+      "raman@google.com" ;;; hard-wired for now 
+      (:network-server . "talk.google.com")
+      (:port . 5223)
+      (:connection-type . ssl)))))
