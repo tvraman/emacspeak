c6168fa7738d5d6518126631897ede240b760e08
T.V Raman <tv.raman.tv@gmail.com>
2020-08-31 07:51:27 -0700

    slim down

--- tvr/laptop-local.el
+++ tvr/laptop-local.el
@@ -1,9 +1,32 @@
-;; -*- lexical-binding: nil; -*-
-
 (setq pre-redisplay-function nil
       x-wait-for-event-timeout 0
       mail-host-address "google.com"
       user-mail-address "raman@google.com")
-(load-library "touchpad-disable")
-(load-library "xbacklight")
+;;; To disable touchpad in all apps:
+
+
+(defvar touchpad-device "12"
+  "Device ID of touchpad.
+Set by locating it via xinput --list.")
+
+(defun turn-off-touchpad (&optional frame)
+  (interactive)
+  (declare (special touchpad-device))
+  (start-process "xinput" nil 
+    "xinput" "set-prop" touchpad-device "Device Enabled" "0")
+  (message "Disabled touchpad"))
+
+(defun turn-on-touchpad (&optional frame)
+  (interactive)
+  (declare (special touchpad-device))
+  (start-process "xinput" nil 
+    "xinput" "set-prop" touchpad-device "Device Enabled" "1")
+  (message "Enabled touchpad"))
+
+(when (fboundp 'global-disable-mouse-mode)
+  (global-disable-mouse-mode)
+  (setq disable-mouse-command 'dtk-stop))
+(turn-off-touchpad)
+
+
 (provide 'laptop-local)
