94e514101dd58a26f488197460dfe62979855bce
T.V Raman <tv.raman.tv@gmail.com>
2020-10-01 14:08:26 -0700

    require descr-text

--- lisp/dtk-unicode.el
+++ lisp/dtk-unicode.el
@@ -56,7 +56,7 @@
 
 (require 'cl-lib)
 (cl-declaim (optimize (safety 0) (speed 3)))
-(declare-function describe-char-unicode-data "descr-text" (char))
+(require 'descr-text)
 
 ;;}}}
 ;;{{{ Customizations
