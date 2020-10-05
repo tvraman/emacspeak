5c19a56cbe97b0cd34e7830a455f5d90b8f3bde5
T.V Raman <tv.raman.tv@gmail.com>
2020-10-05 12:23:27 -0700

    ws fixup

--- lisp/ems-kbd.el
+++ lisp/ems-kbd.el
@@ -1,9 +1,10 @@
 (defun ems-kbd (string )
   "Simplified and hopefully more robust kbd function."
-  (let ((case-fold-search nil)
-	(len (length string)) ; We won't alter string in the loop below.
-	(pos 0)
-	(res []))
+  (let
+      ((case-fold-search nil)
+       (len (length string)) ; We won't alter string in the loop below.
+       (pos 0)
+       (res []))
     (while
         (and (< pos len)
 	     (string-match "[^ \t\n\f]+" string pos))
