6135abb7d5d14c48bf2520c84a8bae431528cb49
T.V Raman <tv.raman.tv@gmail.com>
2020-10-06 10:23:04 -0700

    restore make-local-hook, avoid kbd

--- tvr/vm-prepare.el
+++ tvr/vm-prepare.el
@@ -3,5 +3,14 @@
 (autoload 'vm "vm" "vm mail reader" t nil)
 (autoload 'vm-visit-folder "vm" "Open VM folder" t nil)
 (with-eval-after-load "vm"
-  (global-set-key (kbd "C-x m") 'vm-mail)
+  (global-set-key "\C-xm" 'vm-mail)
   (when (require 'bbdb) (bbdb-insinuate-vm)))
+
+(defun make-local-hook (hook)
+  "compatibility"
+  (if (local-variable-p hook)
+      nil
+    (or (boundp hook) (set hook nil))
+    (make-local-variable hook)
+    (set hook (list t)))
+  hook)
