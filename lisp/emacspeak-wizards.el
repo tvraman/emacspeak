256911e50fb55bb5e73a02dc0a3f8e73d49adb91
T.V Raman <tv.raman.tv@gmail.com>
2020-10-07 17:14:01 -0700

    moved persisting of keybindings to org here

--- lisp/emacspeak-wizards.el
+++ lisp/emacspeak-wizards.el
@@ -3701,6 +3701,48 @@ Optional interactive prefix arg `best' picks best audio format."
        "Description of Problem:"))))
 
 ;;}}}
+;;{{{ Keymaps <-> Org (text) Files :
+
+;;; This makes it easy to consolidate personal bindings across machines.
+;;; It also protects against custom losing settings due to Custom accidents.
+;;;
+
+(defun emacspeak-wizards-bindings-from-org (variable filename)
+  "Load bindings from a specified file."
+  (interactive "vVariable: \nfFilename: ")
+  (let ((bindings nil))
+    (with-temp-buffer
+      "org-to-map"
+      (insert-file-contents filename)
+      (goto-char (point-min))
+      (while (not (eobp))
+        (let ((fields
+               (split-string
+                (buffer-substring-no-properties
+                 (line-beginning-position) (line-end-position))
+                " " 'omit-nulls)))
+          (push
+           (list (cl-first fields) (intern (cl-second fields)))
+           bindings))
+        (forward-line 1)))
+    (setq bindings (nreverse (copy-sequence bindings)))
+    (set variable  bindings)
+    (customize-save-variable variable bindings)))
+
+(defun emacspeak-wizards-bindings-to-org (variable filename)
+  "Persists mapping to org file."
+  (interactive "vVariable: \nfFilename: ")
+  (let ((buffer (find-file-noselect  filename)))
+    (with-current-buffer
+        buffer
+      (goto-char (point-max))
+      (cl-loop
+       for binding  in (symbol-value variable) do
+       (insert (format "%s %s\n" (cl-first binding) (cl-second binding))))
+      (save-buffer buffer))
+    (switch-to-buffer buffer)))
+
+;;}}}
 (provide 'emacspeak-wizards)
 ;;{{{ end of file
 
