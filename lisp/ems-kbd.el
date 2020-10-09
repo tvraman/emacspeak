de2b31acc01c53cac37ce6d4cabbdfaba0737430
T.V Raman <tv.raman.tv@gmail.com>
2020-10-09 13:46:32 -0700

    retain equal as the default assoc test

--- lisp/ems-kbd.el
+++ lisp/ems-kbd.el
@@ -44,17 +44,15 @@ Always returns a vector i.e. like passing need-vector to edmacro-parse-keys. "
              (cl-callf substring word 1))
            (when-let
                (found
-                (cdr
-                 (assoc word
-                        '(("NUL" . "\0")
-                          ("RET" . "\r")
-                          ("LFD" . "\n")
-                          ("TAB" . "\t")
-                          ("ESC" . "\e")
-                          ("SPC" . " ")
-                          ("DEL" . "\177"))
-                        #'string-equal)))
-             (setq word  found))
+                (assoc word
+                       '(("NUL" . "\0")
+                         ("RET" . "\r")
+                         ("LFD" . "\n")
+                         ("TAB" . "\t")
+                         ("ESC" . "\e")
+                         ("SPC" . " ")
+                         ("DEL" . "\177"))))
+             (setq word (cdr found)))
            (cond ;;; apply modifiers 
             ((= bits 0) (setq key word))
             ((/= (length word) 1)
