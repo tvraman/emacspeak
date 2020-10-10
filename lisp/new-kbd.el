61a6e2b00f3db90de963c17b9156248658b1cc5a
T.V Raman <tv.raman.tv@gmail.com>
2020-10-10 09:03:25 -0700

    use pcase

--- lisp/new-kbd.el
+++ lisp/new-kbd.el
@@ -45,15 +45,15 @@ Always returns a vector i.e. like passing need-vector to edmacro-parse-keys. "
              (cl-callf substring word 1))
            (when-let
                (found
-                (assoc word
-                       '(("NUL" . "\0")
-                         ("RET" . "\r")
-                         ("LFD" . "\n")
-                         ("TAB" . "\t")
-                         ("ESC" . "\e")
-                         ("SPC" . " ")
-                         ("DEL" . "\177"))))
-             (setq word (cdr found)))
+                (pcase word
+                  ("NUL"   "\0")
+                  ("RET"   "\r")
+                  ("LFD"   "\n")
+                  ("TAB"   "\t")
+                  ("ESC"   "\e")
+                  ("SPC"   " ")
+                  ("DEL"   "\177")))
+             (setq word  found))
            (cond ;;; apply modifiers 
             ((= bits 0) (setq key word))
             ((/= (length word) 1)
