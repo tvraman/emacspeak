4af977f8cce45398bb3f8a33fa39c8f5da703da2
T.V Raman <tv.raman.tv@gmail.com>
2020-10-10 09:09:59 -0700

    retain assoc and assq for now

--- lisp/new-kbd.el
+++ lisp/new-kbd.el
@@ -29,13 +29,14 @@ Always returns a vector i.e. like passing need-vector to edmacro-parse-keys. "
            (while ;;; calculate modifier bits
                (string-match "^[ACHMsS]-." word)
              (cl-incf bits
-                      (pcase (aref word 0)
-                        (?A   ?\A-\^@)
-                        (?C   ?\C-\^@)
-                        (?H   ?\H-\^@)
-                        (?M   ?\M-\^@)
-                        (?s   ?\s-\^@)
-                        (?S   ?\S-\^@)))
+                      (cdr
+                       (assq (aref word 0)
+                             '((?A . ?\A-\^@)
+                               (?C . ?\C-\^@)
+                               (?H . ?\H-\^@)
+                               (?M . ?\M-\^@)
+                               (?s . ?\s-\^@)
+                               (?S . ?\S-\^@)))))
              (cl-incf prefix 2)
              (cl-callf substring word 2))
            (when (string-match "^\\^.$" word)
@@ -44,15 +45,15 @@ Always returns a vector i.e. like passing need-vector to edmacro-parse-keys. "
              (cl-callf substring word 1))
            (when-let
                (found
-                (pcase word
-                  ("NUL"   "\0")
-                  ("RET"   "\r")
-                  ("LFD"   "\n")
-                  ("TAB"   "\t")
-                  ("ESC"   "\e")
-                  ("SPC"   " ")
-                  ("DEL"   "\177")))
-             (setq word  found))
+                (assoc word
+                       '(("NUL" . "\0")
+                         ("RET" . "\r")
+                         ("LFD" . "\n")
+                         ("TAB" . "\t")
+                         ("ESC" . "\e")
+                         ("SPC" . " ")
+                         ("DEL" . "\177"))))
+             (setq word (cdr found)))
            (cond ;;; apply modifiers 
             ((= bits 0) (setq key word))
             ((/= (length word) 1)
