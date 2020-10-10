a5e3f812eb871f404036d55787d9c4c65a107f17
T.V Raman <tv.raman.tv@gmail.com>
2020-10-10 11:19:59 -0700

    ws fixup

--- lisp/emacspeak-keymap.el
+++ lisp/emacspeak-keymap.el
@@ -106,17 +106,14 @@ edmacro-parse-keys. "
            (while ;;; calculate modifier bits
                (string-match "^[ACHMsS]-." word)
              (cl-incf bits
-                      (cdr
-                       (assq (aref word 0) ems--kbd-mod-table)))
+                      (cdr (assq (aref word 0) ems--kbd-mod-table)))
              (cl-incf prefix 2)
              (cl-callf substring word 2))
            (when (string-match "^\\^.$" word)
              (cl-incf bits ?\C-\^@)
              (cl-incf prefix)
              (cl-callf substring word 1))
-           (when-let
-               (found
-                (assoc word ems--kbd-char-table))
+           (when-let (found (assoc word ems--kbd-char-table))
              (setq word (cdr found)))
            (cond ;;; apply modifiers 
             ((= bits 0) (setq key word))
