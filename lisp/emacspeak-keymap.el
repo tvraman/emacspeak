cd4c72945a1bfe0cc6c460685e77dda412876f57
T.V Raman <tv.raman.tv@gmail.com>
2020-10-12 11:34:35 -0700

    fix typo

--- lisp/emacspeak-keymap.el
+++ lisp/emacspeak-keymap.el
@@ -80,7 +80,8 @@
   "AList mapping modifier names to modifier bit-values.")
 
 ;;;###autoload
-(defun(defun ems-kbd (string )
+
+(defun ems-kbd (string )
   "Simplified and hopefully more robust kbd function.
 Always returns a vector i.e. like passing need-vector to
   edmacro-parse-keys. "
@@ -124,51 +125,7 @@ Always returns a vector i.e. like passing need-vector to
             (t (setq key (list (+ bits (aref word 0)))))))))
 ;;; push key on to the result vector 
        (when key (cl-callf vconcat res key))))
-    res)) (string )
-  "Simplified and hopefully more robust kbd function.
-Always returns a vector i.e. like passing need-vector to
-  edmacro-parse-keys. "
-  (cl-declare (special ems--kbd-mod-table ems--kbd-char-table))
-  (let ((res [])
-        (modifier+angle-reg "^\\(\\([ACHMsS]-\\)*\\)<\\(.+\\)>$"))
-    (cl-loop
-     for word in (split-string string) do
-     (let ((key nil))
-       (cond 
-        ((string-match modifier+angle-reg word)  ;;; modifier+-<key> without DEL etc
-         (setq key
-               (list
-                (intern 
-                 (concat ;;; strip < and >
-                  (substring word (match-beginning 1) (match-end 1))
-                  (substring word (match-beginning 3) (match-end 3)))))))
-        (t
-         (let ((prefix 0)
-               (bits 0))
-           (while ;;; calculate modifier bits
-               (string-match "^[ACHMsS]-." word)
-             (cl-incf bits
-                      (cdr (assq (aref word 0) ems--kbd-mod-table)))
-             (cl-incf prefix 2)
-             (cl-callf substring word 2))
-           (when-let (found (assoc word ems--kbd-char-table))
-             (setq word (cdr found)))
-           (cond ;;; apply modifiers 
-            ((= bits 0) (setq key word))
-            ((/= (length word) 1)
-             (error "%s: Prefix  must precede a single character, not %s"
-                    string word))
-            ((and
-              (/= (logand bits ?\C-\^@) 0)
-              (string-match "[@-_a-z]" word))
-             (setq key
-                   (list (+ bits (- ?\C-\^@)
-                            (logand (aref word 0) 31)))))
-            (t (setq key (list (+ bits (aref word 0)))))))))
-;;; push key on to the result vector 
-       (when key (cl-callf vconcat res key))))
     res))
-
 
 
 ;;}}}
