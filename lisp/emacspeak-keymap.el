e23d6cd76c9d09313e23f3d7f6286282ff1778c7
T.V Raman <tv.raman.tv@gmail.com>
2020-10-12 11:55:10 -0700

    docfix

--- lisp/emacspeak-keymap.el
+++ lisp/emacspeak-keymap.el
@@ -93,7 +93,7 @@ Always returns a vector i.e. like passing need-vector to
      for word in (split-string string) do
      (let ((key nil))
        (cond 
-        ((string-match mod+angle-reg word)  ;;; modifier+-<key> without DEL etc
+        ((string-match mod+angle-reg word)  ;;; modifier+-<key> 
          (setq key
                (list
                 (intern 
@@ -116,7 +116,7 @@ Always returns a vector i.e. like passing need-vector to
             ((and
               (/= (logand bits ?\C-\^@) 0)
               (string-match "[@-_a-z]" word))
-             (setq key
+             (setq key ;;; C-a is 1 etc.
                    (list (+ bits (- ?\C-\^@)
                             (logand (aref word 0) 31)))))
             (t (setq key (list (+ bits (aref word 0)))))))))
