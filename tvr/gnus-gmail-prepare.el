f8b42753500077f8c685afa1348f839902e3622d
T.V Raman <tv.raman.tv@gmail.com>
2020-11-29 07:28:49 -0800

    *** empty log message ***

--- tvr/gnus-gmail-prepare.el
+++ tvr/gnus-gmail-prepare.el
@@ -111,6 +111,9 @@ This moves them into the Spam folder."
     ad-do-it
     ad-return-value))
 
+;;; For message.el in emacs 28
+(push 'signature message-shoot-gnksa-feet)
+
 (provide 'file-xoauth2)
 ;;; local variables:
 ;;; folded-file: t
