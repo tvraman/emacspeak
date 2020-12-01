cbd79a2740a7662be16debb1615dd80510d3ace0
T.V Raman <tv.raman.tv@gmail.com>
2020-12-01 13:35:55 -0800

    cleanup

--- lisp/emacspeak-ansi-color.el
+++ lisp/emacspeak-ansi-color.el
@@ -61,25 +61,22 @@
 
 (defun emacspeak-ansi-color-to-voice (face-spec)
   "Return a voice corresponding to specified face-spec."
-  (cl-declare (special ansi-color-names-vector ansi-color-faces-vector))
+  (cl-declare (special ansi-color-names-vector ))
   (condition-case nil
       (let* ((voice-name nil)
              (color (cdr (assq 'foreground-color  face-spec)))
              (color-index
               (when color
-                (cl-position  color ansi-color-names-vector
-                              :test #'string-equal)))
+                (cl-position  color ansi-color-names-vector :test #'string-equal)))
              (style nil)
-             (color-parameter nil))
-        (setq style (make-acss))
-        (setq color-parameter
-              (if color-index
+             (color-parameter (if color-index
                   (+ 1 color-index)
-                1))
+                1)))
+        (setq style (make-acss))
         (setf (acss-average-pitch style) color-parameter)
         (setf (acss-pitch-range style) color-parameter)
-        (setf (acss-richness style) color-parameter)
-        (setf (acss-stress style) color-parameter)
+        (setf (acss-richness style) (- 9 color-parameter))
+        (setf (acss-stress style) (- 9 color-parameter))
         (setq voice-name (acss-personality-from-speech-style  style))
         voice-name)
     (error nil)))
