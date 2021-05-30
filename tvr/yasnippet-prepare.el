c1127a92b306849fe096ddd5c18ea8585a396ef5
T.V Raman <tv.raman.tv@gmail.com>
2021-05-30 11:34:14 -0700

    yas

--- tvr/yasnippet-prepare.el
+++ tvr/yasnippet-prepare.el
@@ -1,8 +1,10 @@
 ;; -*- lexical-binding: nil; -*-
-
+(defvar tvr-yas-snippets-loaded nil)
 (eval-after-load
     'yasnippet
   `(progn
-     (run-with-idle-timer 1 nil #'yas-reload-all)
+     (unless tvr-yas-snippets-loaded
+       (run-with-idle-timer 1 nil #'yas-reload-all)
+       (setq tvr-yas-snippets-loaded t))
      (setq yas-verbosity 0)))
 
