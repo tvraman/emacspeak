58420d97b2b256f36dd933899c40d54a30aa49fc
T.V Raman <tv.raman.tv@gmail.com>
2021-08-10 08:24:37 -0700

    tvr: updated  eaf keybind

--- tvr/eaf-prepare.el
+++ tvr/eaf-prepare.el
@@ -3,4 +3,4 @@
 (eaf-setq eaf-browser-enable-adblocker "true")
 ;;; We'll handle pdf, epub etc via Emacspeak as usual --- no EAF
 (setq eaf-pdf-extension-list nil)
-(global-set-key (ems-kbd "C-; C-f") 'eaf-open-browser)
+(global-set-key (ems-kbd "C-; C-w") 'eaf-open-browser-with-history)
