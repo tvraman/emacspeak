c3e2ff5699bf8c760e0be906a15948b5fdc832c8
T.V Raman <tv.raman.tv@gmail.com>
2021-09-17 10:45:58 -0700

    doc update

--- lisp/dtk-speak.el
+++ lisp/dtk-speak.el
@@ -1367,7 +1367,8 @@ program. Port defaults to dtk-local-server-port"
   (eval-when-compile
     (or (getenv "ALSA_NOTIFY")
         (cl-first (split-string (shell-command-to-string  "aplay -L 2>/dev/null | grep mono")))))
-  "Virtual ALSA device to use for notifications stream."
+  "Virtual ALSA device to use for notifications stream. Set to nil to
+  disable a separate Notification TTS stream."
   :type 'string
   :group 'tts)
 
