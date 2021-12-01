0d3f6b9dff64751e1b71c0c9a75590e596585478
T.V Raman <tv.raman.tv@gmail.com>
2021-12-01 15:41:03 -0800

    nyxt/init.lisp now works

--- nyxttalk/init.lisp
+++ nyxttalk/init.lisp
@@ -1,9 +1,14 @@
+;;; The following lines added by ql:add-to-init-file:
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
 (ql:quickload :tts)
 (tts:init) ;;; (tts:init :engine "espeak" for espeak
 (tts:speak "Welcome To The Next In Browsers!")
-
-;; (define-configuration buffer
-;;     ((default-modes (append '(emacs-mode) %slot-default))))
-;; (swank:create-server :port 4006 :dont-close t)
-;; (tts:speak "Swank server started at port 4006")
-;; 
+(ql:quickload :aweb)
+(swank:create-server :port 4006 :dont-close t)
+(tts:speak "Swank server started at port 4006")
+ 
