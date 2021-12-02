17750d347be2ad995be4cb6a4972389ffbd3654b
T.V Raman <tv.raman.tv@gmail.com>
2021-12-01 19:14:40 -0800

    nyxt-init

--- nyxttalk/init.lisp
+++ nyxttalk/init.lisp
@@ -12,4 +12,6 @@
 
 (swank:create-server :port 4006 :dont-close t)
 (tts:speak "Swank server started at port 4006")
- 
+
+(define-configuration buffer
+  ((default-modes (append '(emacs-mode) %slot-default%))))
