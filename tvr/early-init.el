1d21034c77c70d5118a92adf6d15095cdc1057ad
T.V Raman <tv.raman.tv@gmail.com>
2021-12-20 11:08:11 -0800

    tvr-native-setup

--- tvr/early-init.el
+++ tvr/early-init.el
@@ -1,5 +1,6 @@
 (setq
- ;force-load-messages t
+ native-comp-async-report-warnings-errors nil
+                                        ;force-load-messages t
  package-quickstart t
  gc-cons-threshold 128000000
  )
