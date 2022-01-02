319fd601736853e70887e584c9556adaa8748b2e
T.V Raman <tv.raman.tv@gmail.com>
2022-01-02 07:19:32 -0800

    python-mode

--- tvr/python-mode-prepare.el
+++ tvr/python-mode-prepare.el
@@ -1,5 +1,6 @@
 ;; -*- lexical-binding: nil; -*-
-
+;;; setup package python-mode (melpa) --- not to be confused with
+;;;built-in package python.
 (eval-after-load "python-mode"
   `(progn
      (add-hook 'python-mode-hook #'elpy-enable)))
