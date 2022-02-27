2c4d52cbb721c994e6a2d6426a8c1085bad816e1
T.V Raman <tv.raman.tv@gmail.com>
2022-02-27 10:25:42 -0800

    cleanup

--- lisp/emacspeak-eww.el
+++ lisp/emacspeak-eww.el
@@ -1232,7 +1232,6 @@ for use as a DOM filter."
     (eww-save-history)
     (erase-buffer)
     (goto-char (point-min))
-;;;(setq shr-base (shr-parse-base url))
     (shr-insert-document filtered-dom)
     (emacspeak-eww-set-dom filtered-dom)
     (emacspeak-eww-set-url url)
