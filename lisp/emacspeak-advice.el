1523f8654b2dd7428e93896ff912ca6d600af7b9
T.V Raman <tv.raman.tv@gmail.com>
2022-03-26 08:52:12 -0700

    protect against null eldoc buffer

--- lisp/emacspeak-advice.el
+++ lisp/emacspeak-advice.el
@@ -794,10 +794,11 @@ When on a close delimiter, speak matching delimiter after a small delay. "
 (defun emacspeak-speak-eldoc (docs interactive)
   "Speak eldoc."
   (cl-declare (special eldoc--doc-buffer-docs eldoc--doc-buffer))
-  (with-current-buffer eldoc--doc-buffer
-    (unless (equal docs eldoc--doc-buffer-docs)
-      (emacspeak-auditory-icon 'doc))
-    (when interactive (dtk-speak (buffer-string)))))
+  (when eldoc--doc-buffer
+    (with-current-buffer eldoc--doc-buffer
+      (unless (equal docs eldoc--doc-buffer-docs)
+        (emacspeak-auditory-icon 'doc))
+      (when interactive (dtk-speak (buffer-string))))))
 
 (with-eval-after-load "eldoc"
   (remove-hook 'eldoc-display-functions #'eldoc-display-in-echo-area)
