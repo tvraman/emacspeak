ab10394a4c1ccd90c673c707fea517a819b8eff9
T.V Raman <tv.raman.tv@gmail.com>
2022-04-19 07:45:03 -0700

    pan chimes cleanup in index

--- sounds/pan-chimes/define-theme.el
+++
@@ -1,7 +0,0 @@
-;;; Wave icons using chimes as a theme  -*- lexical-binding: t; -*-
-;;; Icons from theme Chimes with effect autopan applied using mplayer and ladspa
-
-(require 'emacspeak-sounds)
-(emacspeak-sounds-define-theme
- (expand-file-name "pan-chimes/" emacspeak-sounds-directory)
- ".wav")
