b772147ba1d9eaa16634b55ca98029aa3c6cc26b
T.V Raman <tv.raman.tv@gmail.com>
2022-04-29 17:45:39 -0700

    utils: cleanup indent util

--- utils/indent-files.el
+++ utils/indent-files.el
@@ -12,19 +12,6 @@
        (find-file f)
        (emacs-lisp-mode)
        (goto-char (point-min))
-       (while (re-search-forward "( +" (point-max) 'no-error)
-         (unless
-             (or
-              (char-equal ?\\ (char-before (match-beginning 0)))
-              (char-equal ?? (char-before (match-beginning 0))))
-         (replace-match "(")))
-       (goto-char (point-min))
-       (while (re-search-forward " +)" (point-max) 'no-error)
-         (unless
-             (or
-              (char-equal ??  (char-before (match-beginning 0)))
-              (char-equal ?\\  (char-before (match-beginning 0))))
-           (replace-match ")")))
        (indent-region (point-min) (point-max))
        (shell-command-on-region (point-min) (point-max)
                                 "cat -s" (current-buffer) 'replace)
