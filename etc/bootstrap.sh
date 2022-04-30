4c4de889ea5f48f6da4914271ce35df23ee5d415
T.V Raman <tv.raman.tv@gmail.com>
2022-04-30 08:43:58 -0700

    update to 56.0

--- etc/bootstrap.sh
+++ etc/bootstrap.sh
@@ -8,7 +8,7 @@
 #  Prerequisites: espeak, libespeak libespeak-dev
 # libespeak-ng-libespeak-dev libespeak-ng-dev espeak-ng-espeak
 # Downloads,  builds and runs specified version.
-latest=55.0
+latest=56.0
 v=$1
 
 if [ ! -n "$1" ]
