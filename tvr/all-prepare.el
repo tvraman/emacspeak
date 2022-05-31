d00f378b70df1aab30a1323a9665cc2db7bd25f7
T.V Raman <tv.raman.tv@gmail.com>
2022-05-30 20:00:34 -0700

    revert previous change

--- tvr/all-prepare.el
+++ tvr/all-prepare.el
@@ -219,7 +219,7 @@ This moves them into the Spam folder."
   (setq
    slime-lisp-implementations
    `((sbcl ("sbcl" "--core"
-            ,(expand-file-name "sbcl-core-for-slime" user-emacs-directory))))))
+            ,(expand-file-name "sbcl.core-for-slime" user-emacs-directory))))))
 ;;;$Id: org-prepare.el 6727 2011-01-14 23:22:20Z tv.raman.tv $  -*- lexical-binding: nil; -*-
 
 (with-eval-after-load "org"
