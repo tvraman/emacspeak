7adf770ccbe3a1b7f52c6e89dd9e1f4e19332691
T.V Raman <tv.raman.tv@gmail.com>
2022-06-21 15:35:25 -0700

    nuked google stock market data

--- lisp/emacspeak-url-template.el
+++ lisp/emacspeak-url-template.el
@@ -301,15 +301,6 @@ dont-url-encode if true then url arguments are not url-encoded "
      (emacspeak-speak-rest-of-buffer))
  "Light-weight Google weather.")
 
-(emacspeak-url-template-define
- "stock market from google"
- "https://www.google.com/search?num=25&lite=90586&q=US+stock+market+summary"
- nil
- #'(lambda nil
-     (emacspeak-auditory-icon 'open-object)
-     (dtk-speak  "Press t to hear the stock market table"))
- "Light-weight stock market summary from Google.")
-
 ;;}}}
 ;;{{{ Calendar Mobile:
 
