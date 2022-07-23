d1e2cf3ad51c453c0e77765af91ae2ded748188b
T.V Raman <tv.raman.tv@gmail.com>
2022-07-22 17:15:57 -0700

    tvr-kbd-macro

--- tvr/kbd-macros.el
+++ tvr/kbd-macros.el
@@ -2,3 +2,6 @@
 (defalias 'emacs-github-log
    (kmacro "C-c 3 C-; d F u C-; d l l"))
 (global-set-key [24 11 48] 'emacs-github-log)
+(defalias 'tvr-morning
+   (kmacro "C-<tab> C-e g b h <tab> <return> n n e c"))
+(global-set-key [24 11 49] 'tvr-morning)
