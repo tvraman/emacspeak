35e57c6b4fbd72c38fc13c8b58b097e5fcfedfb8
T.V Raman <tv.raman.tv@gmail.com>
2022-07-22 17:19:06 -0700

    migrate kbd macros

--- tvr/all-prepare.el
+++ tvr/all-prepare.el
@@ -236,3 +236,10 @@ This moves them into the Spam folder."
   (define-key global-map "\C-cb" 'org-switchb)
   (define-key global-map  "\C-cc" 'org-capture)
   )
+;;; Jump to Emacs Git Logs At HEAD:
+(defalias 'emacs-github-log
+   (kmacro "C-c 3 C-; d F u C-; d l l"))
+(global-set-key [24 11 48] 'emacs-github-log)
+(defalias 'tvr-morning
+   (kmacro "C-<tab> C-e g b h <tab> <return> n n e c"))
+(global-set-key [24 11 49] 'tvr-morning)
