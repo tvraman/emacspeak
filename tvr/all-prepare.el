d498fc4a675e0a40aa36807b0a9cc951af6a5f91
T.V Raman <tv.raman.tv@gmail.com>
2022-07-23 06:23:44 -0700

    startup

--- tvr/all-prepare.el
+++ tvr/all-prepare.el
@@ -237,9 +237,9 @@ This moves them into the Spam folder."
   (define-key global-map  "\C-cc" 'org-capture)
   )
 ;;; Jump to Emacs Git Logs At HEAD:
-(defalias 'emacs-github-log
+(defalias 'tvr-emacs-log
    (kmacro "C-c 3 C-; d F u C-; d l l"))
-(global-set-key [24 11 48] 'emacs-github-log)
+(global-set-key [24 11 48] 'tvr-emacs-log)
 (defalias 'tvr-morning
    (kmacro "C-<tab> C-e g b h <tab> <return> n n e c"))
 (global-set-key [24 11 49] 'tvr-morning)
--- tvr/kbd-macros.el
+++ tvr/kbd-macros.el
@@ -1,7 +1,7 @@
 ;;; Jump to Emacs Git Logs At HEAD:
-(defalias 'emacs-github-log
+(defalias 'tvr-emacs-log
    (kmacro "C-c 3 C-; d F u C-; d l l"))
-(global-set-key [24 11 48] 'emacs-github-log)
+(global-set-key [24 11 48] 'tvr-emacs-log)
 (defalias 'tvr-morning
    (kmacro "C-<tab> C-e g b h <tab> <return> n n e c"))
 (global-set-key [24 11 49] 'tvr-morning)
