bbc76805ce5f649f4c2743b86c4a41a87e24dcc6
T.V Raman <tv.raman.tv@gmail.com>
2022-07-24 11:10:05 -0700

    ogg

--- lisp/emacspeak-sounds.el
+++ lisp/emacspeak-sounds.el
@@ -164,13 +164,10 @@ Use Serve when working with remote speech servers.")
                        emacspeak-sounds-themes-table
                        emacspeak-play-program emacspeak-sounds-directory))
   (when
-      (and 
-       (string= emacspeak-play-program (executable-find "pactl"))
-       (not
-        (member theme
-                (mapcar
-                 #'(lambda (th) (expand-file-name th emacspeak-sounds-directory))
-                 '("ogg-3d/" "ogg-chimes/")))))
+      (and (string= emacspeak-play-program (executable-find "pactl"))
+           (not
+            (member (file-relative-name theme emacspeak-sounds-directory)
+                    '("ogg-3d/" "ogg-chimes/"))))
     (error "%s: Only ogg-3d or ogg-chimes with Pulse Advanced" theme))
   (unless (file-directory-p theme)
     (setq theme  (file-name-directory theme)))
@@ -181,8 +178,7 @@ Use Serve when working with remote speech servers.")
   (when (string= (executable-find "pactl" emacspeak-play-program)
                  (shell-command
                   (format "%s load-sample-dir-lazy %s"
-                          (executable-find "pacmd")
-                          theme))))
+                          (executable-find "pacmd") theme))))
   t)
 
 
---
+++ sounds/ogg-3d/define-theme.el
@@ -0,0 +1,6 @@
+;;; Wave icons using chimes as a theme  -*- lexical-binding: t; -*-
+;;; Icons from theme Chimes with effect autopan applied using mplayer and ladspa
+
+(emacspeak-sounds-define-theme
+ (expand-file-name "ogg-3d/" emacspeak-sounds-directory)
+ ".ogg")
