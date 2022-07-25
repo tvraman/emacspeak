93295eddfb28dbad45adab1f34ecf358f707eb03
T.V Raman <tv.raman.tv@gmail.com>
2022-07-25 16:51:57 -0700

    emacspeak-outline: integrate repeat-mode support

--- lisp/emacspeak-outline.el
+++ lisp/emacspeak-outline.el
@@ -269,15 +269,17 @@ except that the outline section is  spoken"
                        outline-navigation-repeat-map))
   (unless (boundp 'outline-navigation-repeat-map)
     (setq outline-navigation-repeat-map (make-sparse-keymap)))
-  (cl-loop
-   for map in
-   '(outline-navigation-repeat-map outline-mode-prefix-map)
-   do
-   (define-key map "p" 'emacspeak-outline-speak-previous-heading)
-   (define-key map "n" 'emacspeak-outline-speak-next-heading)
-   (define-key map "b" 'emacspeak-outline-speak-backward-heading)
-   (define-key map "f" 'emacspeak-outline-speak-forward-heading)
-   (define-key map " " 'emacspeak-outline-speak-this-heading))
+  (define-key outline-mode-prefix-map "p" 'emacspeak-outline-speak-previous-heading)
+  (define-key outline-mode-prefix-map "n" 'emacspeak-outline-speak-next-heading)
+  (define-key outline-mode-prefix-map "b" 'emacspeak-outline-speak-backward-heading)
+  (define-key outline-mode-prefix-map "f" 'emacspeak-outline-speak-forward-heading)
+  (define-key outline-mode-prefix-map " "
+              'emacspeak-outline-speak-this-heading)
+  (define-key outline-navigation-repeat-map "p" 'emacspeak-outline-speak-previous-heading)
+  (define-key outline-navigation-repeat-map "n" 'emacspeak-outline-speak-next-heading)
+  (define-key outline-navigation-repeat-map "b" 'emacspeak-outline-speak-backward-heading)
+  (define-key outline-navigation-repeat-map "f" 'emacspeak-outline-speak-forward-heading)
+  (define-key outline-navigation-repeat-map " " 'emacspeak-outline-speak-this-heading)
   (mapc
    #'(lambda (cmd)
        (put cmd 'repeat-map 'outline-navigation-repeat-map))
