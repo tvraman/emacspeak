84ef94622ac06ed91069459f4cafe102ce749496
T.V Raman <tv.raman.tv@gmail.com>
2022-07-29 13:07:24 -0700

    turn off stop immediately in advice of read-key and friends
    -- otherwise ispell breaks
    Author:

--- lisp/emacspeak-advice.el
+++ lisp/emacspeak-advice.el
@@ -847,7 +847,8 @@ When on a close delimiter, speak matching delimiter after a small delay. "
   `(defadvice ,f (before emacspeak pre act comp)
      "Speak prompt"
      (ems-with-messages-silenced
-       (let ((prompt (ad-get-arg 0)))
+       (let ((prompt (ad-get-arg 0))
+             (dtk-stop-immediately nil))
          (emacspeak-auditory-icon 'item)
          (setq emacspeak-last-message prompt)
          (setq emacspeak-read-char-prompt-cache prompt)
