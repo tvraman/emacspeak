46a0f0c0fec7382caa9f6aaf945a9158938aeedb
T.V Raman <tv.raman.tv@gmail.com>
2022-07-29 17:18:06 -0700

    dont guess prompt for read-key and friends and  dont turn off
    dtk-stop-immediacy
    Author:

--- lisp/emacspeak-advice.el
+++ lisp/emacspeak-advice.el
@@ -847,16 +847,13 @@ When on a close delimiter, speak matching delimiter after a small delay. "
   `(defadvice ,f (before emacspeak pre act comp)
      "Speak prompt"
      (ems-with-messages-silenced
-       (let ((prompt (ad-get-arg 0))
-             (dtk-stop-immediately nil))
+       (let ((prompt (ad-get-arg 0)))
          (emacspeak-auditory-icon 'item)
          (setq emacspeak-last-message prompt)
          (setq emacspeak-read-char-prompt-cache prompt)
          (tts-with-punctuations
              'all
-           (dtk-speak
-            (or prompt
-                (substring ,(symbol-name f) 5)))))))))
+           (dtk-speak  prompt)))))))
 
 (defadvice read-char-choice (before emacspeak pre act comp)
   "Speak the prompt. "
