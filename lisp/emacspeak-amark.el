b8e467062a94bf952160898f2f7e5a75b6a69c5f
T.V Raman <tv.raman.tv@gmail.com>
2022-08-03 19:02:38 -0700

    cleanup

--- lisp/emacspeak-amark.el
+++ lisp/emacspeak-amark.el
@@ -8,30 +8,30 @@
 ;; LCD Archive Entry:
 ;; emacspeak| T. V. Raman |tv.raman.tv@gmail.com
 ;; A speech interface to Emacs |
-;; 
+;;
 ;;  $Revision: 4532 $ |
 ;; Location undetermined
-;; 
+;;
 
 ;;}}}
 ;;{{{  Copyright:
 
-;; Copyright (C) 1995 -- 2022, T. V. Raman 
+;; Copyright (C) 1995 -- 2022, T. V. Raman
 ;; Copyright (c) 1996 by T. V. Raman
 ;; All Rights Reserved.
-;; 
+;;
 ;; This file is not part of GNU Emacs, but the same permissions apply.
-;; 
+;;
 ;; GNU Emacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
-;; 
+;;
 ;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;; 
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs; see the file COPYING.  If not, write to
 ;; the Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,MA 02110-1301, USA.
@@ -45,7 +45,7 @@
 ;; Structure emacspeak-amark holds a bookmark into an mp3 file
 ;; path:  filename containing  marked
 ;; name: Bookmark tag
-;; Position: time offset from start 
+;; Position: time offset from start
 
 ;;  This library will be used from emacspeak-m-player,
 ;; to set and jump to bookmarks.
@@ -74,7 +74,7 @@
 ;;}}}
 ;;{{{ AMark Functions:
 
-(defun emacspeak-amark-names ()
+(defsubst emacspeak-amark-names ()
   "Return list of  amark names."
   (cl-declare (special emacspeak-amark-list))
   (cl-loop for a in emacspeak-amark-list collect (emacspeak-amark-name a)))
@@ -97,13 +97,14 @@ given name, it is updated with path and position."
        (amark                             ; exists, reposition
         (setf (emacspeak-amark-path amark) path
               (emacspeak-amark-position amark) position))
-       (t 
+       (t
         (push
          (make-emacspeak-amark :path path :name name :position position)
          emacspeak-amark-list))))))
 
 (defvar emacspeak-amark-file ".amarks.el"
   "Name of file used to save AMarks.")
+
 ;;;###autoload
 (defun emacspeak-amark-save ()
   "Save buffer-local AMarks in  currently playing directory."
@@ -115,11 +116,12 @@ given name, it is updated with path and position."
     (with-current-buffer buff
       (setq buffer-undo-list t)
       (erase-buffer)
-      (prin1  l (current-buffer)) 
+      (prin1 l (current-buffer))
       (save-buffer)
       (message "Saved AMarks in %s" (buffer-file-name))
       (kill-buffer buff)
       (emacspeak-auditory-icon 'save-object))))
+
 ;;;###autoload
 (defun emacspeak-amark-load (&optional dir)
   "Locate AMarks file from `dir' current  directory is default, and load it."
@@ -132,27 +134,14 @@ given name, it is updated with path and position."
          (locate-dominating-file dir emacspeak-amark-file)))
     (cond
      ((null where))
-     (t (setq buff
-              (find-file-noselect (expand-file-name emacspeak-amark-file where)))
-        (save-current-buffer
-          (set-buffer buff)
-          (goto-char (point-min))
-          (setq l (read buff))
-          (kill-buffer buff))
-        (setq emacspeak-amark-list l)))))
-
-;;}}}
-;;{{{Browse Amarks:
-
-(declare-function emacspeak-m-player-seek-absolute "emacspeak-m-player" (pos))
-
-(defun emacspeak-amark-play (amark)
-  "Play amark using m-player."
-  (emacspeak-m-player
-   (expand-file-name
-    (emacspeak-amark-path  amark)
-    default-directory))
-  (emacspeak-m-player-seek-absolute (emacspeak-amark-position amark)))
+     (t
+      (setq buff
+            (find-file-noselect (expand-file-name emacspeak-amark-file where)))
+      (with-current-buffer buff
+        (goto-char (point-min))
+        (setq l (read buff))
+        (kill-buffer buff))
+      (setq emacspeak-amark-list l)))))
 
 (defun emacspeak-amark-delete (amark)
   "Delete Amark and save."
@@ -168,13 +157,24 @@ given name, it is updated with path and position."
   (emacspeak-amark-browse)
   (message "Updated amarks"))
 
+(declare-function emacspeak-m-player-seek-absolute "emacspeak-m-player" (pos))
+
+(defun emacspeak-amark-play (amark)
+  "Play amark using m-player."
+  (emacspeak-m-player
+   (expand-file-name (emacspeak-amark-path  amark) default-directory))
+  (emacspeak-m-player-seek-absolute (emacspeak-amark-position amark)))
+
+;;}}}
+;;{{{Browse Amarks:
+
 ;;;###autoload
 (defun emacspeak-amark-browse ()
   "Browse  nearest amarks file."
   (interactive)
   (cl-declare (special emacspeak-amark-list))
   (let ((amarks
-         (or 
+         (or
           (emacspeak-amark-load)
           (error "No Amarks here")))
         (buff (get-buffer-create "*Amarks Browser"))
@@ -188,26 +188,22 @@ given name, it is updated with path and position."
       (setq buffer-undo-list t)
       (cl-loop
        for m in amarks do
-       (insert
-        (format
-         "%s\t" (emacspeak-amark-name m)))
+       (insert (format "%s\t" (emacspeak-amark-name m)))
        (insert-text-button
-        "Delete\t" 
+        "Delete\t"
         'mark m
         'action
         #'(lambda (b) (emacspeak-amark-delete (button-get b 'mark))))
        (insert (format "%s\t" (emacspeak-amark-position m)))
        (insert-text-button
-        (format "%s" (abbreviate-file-name (emacspeak-amark-path m)))
+        (format "%s" (emacspeak-amark-path m))
         'mark m
-        'action
-        #'(lambda (b) (emacspeak-amark-play (button-get b 'mark))))
+        'action #'(lambda (b) (emacspeak-amark-play (button-get b 'mark))))
        (insert "\n"))
       (emacspeak-speak-load-directory-settings)
       (goto-char (point-min))
       (forward-button 1))
     (funcall-interactively #'switch-to-buffer buff)))
-
 
 ;;}}}
 (provide  'emacspeak-amark)
