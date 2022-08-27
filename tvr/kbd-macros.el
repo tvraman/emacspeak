cc6bdd32153b0327564cab5cbdc1a07efac2f4f0
T.V Raman <tv.raman.tv@gmail.com>
2022-08-26 18:06:05 -0700

    startup

--- tvr/kbd-macros.el
+++ tvr/kbd-macros.el
@@ -1,7 +1,7 @@
 ;;; Jump to Emacs Git Logs At HEAD:
-(defalias 'tvr-emacs-log
+(defalias 'tvr-km-emacs-log
    (kmacro "C-c 3 C-; d F u C-; d l l M-< C-e C-q"))
-(global-set-key [24 11 48] 'tvr-emacs-log)
-(defalias 'tvr-morning
+(global-set-key [24 11 48] 'tvr-km-emacs-log)
+(defalias 'tvr-km-morning
    (kmacro "C-<tab> C-e g b h <tab> <return> n n e c"))
-(global-set-key [24 11 49] 'tvr-morning)
+(global-set-key [24 11 49] 'tvr-km-morning)
