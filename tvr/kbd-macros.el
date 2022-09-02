6b9ad06f86dcea1ff96c75aaf7711f0951c6611e
T.V Raman <tv.raman.tv@gmail.com>
2022-09-01 18:47:15 -0700

    silence chatter in emacs log viewer

--- tvr/kbd-macros.el
+++ tvr/kbd-macros.el
@@ -1,6 +1,6 @@
 ;;; Jump to Emacs Git Logs At HEAD:
 (defalias 'tvr-km-emacs-log
-   (kmacro "C-c 3 C-; d F u C-; d l l M-< C-e C-q n"))
+   (kmacro "C-c 3 C-; d F u C-; d l l C-e d q M-< C-e C-q C-e d q n"))
 (global-set-key [24 11 48] 'tvr-km-emacs-log)
 (defalias 'tvr-km-morning
    (kmacro "C-<tab> C-e g b h <tab> <return> n n e c"))
