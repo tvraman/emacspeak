aea82ffc04dd013d6310d485b7199ff17b77115e
T.V Raman <tv.raman.tv@gmail.com>
2022-09-14 09:30:57 -0700

    cleanup play-mark

--- lisp/emacspeak-amark.el
+++ lisp/emacspeak-amark.el
@@ -164,8 +164,7 @@ given name, it is updated with path and position."
   "Play amark using m-player."
   (emacspeak-m-player
    (expand-file-name (emacspeak-amark-path  amark) default-directory))
-  (sit-for 1)
-  (accept-process-output)
+  (sit-for 0.3)
   (emacspeak-m-player-seek-absolute (emacspeak-amark-position amark)))
 
 ;;}}}
