cf46b39b3b5f49a204c8a9a7f62f1237ebe795db
T.V Raman <tv.raman.tv@gmail.com>
2022-10-15 08:01:51 -0700

    in-flite

--- lisp/emacspeak-mpv.el
+++ lisp/emacspeak-mpv.el
@@ -70,6 +70,24 @@
        #'(lambda (s) (split-string s "="))
        (split-string (cdr (url-path-and-query u)) "&"))))))
 
+(defsubst ems--yt-set-time (url time)
+  "Return YT URL after updating   time offset in  YT URL."
+  (let ((u (url-generic-parse-url url))
+        (time (cadr
+               (assoc
+                "t"
+                (mapcar
+                 #'(lambda (s) (split-string s "="))
+                 (split-string (cdr (url-path-and-query u)) "&"))))))
+    (cond
+     ((null time)
+      (format "%s?t=%s" url time))
+     (t
+      (replace-regexp-in-string
+       "t=[0-9]+"
+       (format "t=%s" time)
+       url)))))
+
 ;;}}}
 ;;{{{ Interactive Commands:
 
