95b4d38c8530272750e7e2ac4b0f558a50b76f96
T.V Raman <tv.raman.tv@gmail.com>
2022-12-26 08:08:45 -0800

    typo

--- tvr/yes-prepare.el
+++ tvr/yes-prepare.el
@@ -7,7 +7,7 @@
      (let* ((ask (concat (ad-get-arg 0) " y/n "))
             (c (read-char ask)))
        (while (not (member c '(?n ?y)))
-              (setqc  (read-char ask)))
+              (setq c  (read-char ask)))
        (setq ad-return-value
              (cl-case c
                (?y t)
