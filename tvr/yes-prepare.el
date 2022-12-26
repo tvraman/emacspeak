8b1fb503b144bc5216ebfd536fb6223a27ed6f5a
T.V Raman <tv.raman.tv@gmail.com>
2022-12-26 08:25:30 -0800

    startup

--- tvr/yes-prepare.el
+++ tvr/yes-prepare.el
@@ -18,3 +18,18 @@
   ad-return-value)
 
 
+
+
+(defadvice y-or-n-p (around emacspeak pre act comp)
+  "speak."
+  (let* ((ask (concat (ad-get-arg 0) " y/n "))
+         (c (read-char ask)))
+    (while (not (member c '(?n ?y)))
+           (setq c  (read-char ask)))
+    (setq ad-return-value
+          (cl-case c
+                   (?y t)
+                   (?n nil))))
+  (emacspeak-auditory-icon
+   (if ad-return-value 'y-answer 'n-answer))
+  ad-return-value)
