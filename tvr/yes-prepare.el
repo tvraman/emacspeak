17f6a80d0bcc7e974ff6011cb83cd0cad5360c48
T.V Raman <tv.raman.tv@gmail.com>
2022-12-26 08:07:09 -0800

    clean

---
+++ tvr/yes-prepare.el
@@ -0,0 +1,20 @@
+
+
+(defadvice yes-or-no-p (around emacspeak pre act comp)
+  "speak."
+  (cond
+    (use-short-answers
+     (let* ((ask (concat (ad-get-arg 0) " y/n "))
+            (c (read-char ask)))
+       (while (not (member c '(?n ?y)))
+              (setqc  (read-char ask)))
+       (setq ad-return-value
+             (cl-case c
+               (?y t)
+               (?n nil)))))
+    (t ad-do-it))
+  (emacspeak-auditory-icon
+   (if ad-return-value 'y-answer 'n-answer))
+  ad-return-value)
+
+
