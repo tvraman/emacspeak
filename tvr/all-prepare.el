fc493bbaf12788196c392dc4d0e2f68b499c3fa0
T.V Raman <tv.raman.tv@gmail.com>
2022-12-26 08:10:20 -0800

    update-startup

--- tvr/all-prepare.el
+++ tvr/all-prepare.el
@@ -1,3 +1,23 @@
+
+
+(defadvice yes-or-no-p (around emacspeak pre act comp)
+  "speak."
+  (cond
+    (use-short-answers
+     (let* ((ask (concat (ad-get-arg 0) " y/n "))
+            (c (read-char ask)))
+       (while (not (member c '(?n ?y)))
+              (setq c  (read-char ask)))
+       (setq ad-return-value
+             (cl-case c
+               (?y t)
+               (?n nil)))))
+    (t ad-do-it))
+  (emacspeak-auditory-icon
+   (if ad-return-value 'y-answer 'n-answer))
+  ad-return-value)
+
+
 ;;; vm-prepare.l :  -*- lexical-binding: nil; -*-
 
 (autoload 'vm "vm" "vm mail reader" t nil)
