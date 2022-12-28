72b678745fe13d253557875636a7b2de4ae4f9c8
T.V Raman <tv.raman.tv@gmail.com>
2022-12-28 07:27:19 -0800

    yes-prepare

--- tvr/yes-prepare.el
+++ tvr/yes-prepare.el
@@ -1,14 +1,15 @@
 ;;; Emacs' yes-or-no-p and y-or-no-p have prompts hard-wired.
 ;; The advice forms below produce more succinct speech.
 
-(defadvice yes-or-no-p (around emacspeak pre act comp)
-  "speak."
+(defadvice yes-or-no-p (around tvr-fix pre act comp)
+  "Simplify Emacs' implementation."
   (cond
     (use-short-answers
-     (emacspeak-auditory-icon  'ask-question)
      (let* ((ask (concat (ad-get-arg 0) " y/n "))
             (c (read-char ask)))
        (while (not (member c '(?n ?y)))
+              (emacspeak-auditory-icon
+   (if ad-return-value 'y-answer 'n-answer))
               (setq c  (read-char ask))
               (emacspeak-auditory-icon  'ask-question))
        (setq ad-return-value
@@ -16,22 +17,17 @@
                       (?y t)
                       (?n nil)))))
     (t ad-do-it))
-  (emacspeak-auditory-icon
-   (if ad-return-value 'y-answer 'n-answer))
   ad-return-value)
 
-(defadvice y-or-n-p (around emacspeak pre act comp)
-  "speak."
-  (emacspeak-auditory-icon  'ask-question)
+(defadvice y-or-n-p (around tvr-fix pre act comp)
+  "Simplify Emacs' implementation.."
   (let* ((ask (concat (ad-get-arg 0) " y/n "))
          (c (read-char ask)))
     (while (not (member c '(?n ?y)))
-           (emacspeak-auditory-icon  'ask-question)
+           (emacspeak-auditory-icon  'ask-short-question)
            (setq c  (read-char ask)))
     (setq ad-return-value
           (cl-case c
                    (?y t)
                    (?n nil))))
-  (emacspeak-auditory-icon
-   (if ad-return-value 'y-answer 'n-answer))
   ad-return-value)
