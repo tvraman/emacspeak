5635e4854430134ad024405cc92e9e63d3ea2577
T.V Raman <tv.raman.tv@gmail.com>
2023-01-10 19:37:22 -0800

    exwm

--- tvr/all-prepare.el
+++ tvr/all-prepare.el
@@ -3,6 +3,25 @@
 (require 'exwm)
 (require 'exwm-config)
 (exwm-config-example)
+(exwm-config-ido)
+;;}}}
+;;{{{Look and feel:
+
+(setq exwm-workspace-number 2)
+
+;;; Name exwm buffers meaningfully:
+
+(add-hook 'exwm-update-title-hook
+          (lambda ()
+            (when (or (not exwm-instance-name)
+                      (string-prefix-p "sun-awt-X11-" exwm-instance-name)
+                      (string= "gimp" exwm-instance-name))
+              (exwm-workspace-rename-buffer exwm-title))))
+;;; hide echo-area when not used:
+(setq exwm-workspace-minibuffer-position 'bottom)
+
+;;}}}
+;;{{{Global Keys:
 
 ;;}}}
 ;;; Emacs' yes-or-no-p and y-or-no-p have prompts hard-wired.
