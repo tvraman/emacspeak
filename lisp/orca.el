81d196c110ca74e0890183d087c87d69b254da8e
T.V Raman <tv.raman.tv@gmail.com>
2023-01-10 20:14:46 -0800

    toggle orca for chrome

---
+++ lisp/orca.el
@@ -0,0 +1,12 @@
+;;; Easily start/stop orca for use with Chrome:
+
+(defvar orca-process nil
+  "Orca process handle")
+
+(defun orca-toggle ()
+  "Toggle state of orca."
+  (interactive)
+  (cond
+    (orca-process (delete-process orca-process)
+     (setq orca-process  nil))
+    (t (setq orca-process (start-process "Orca"nil "orca" "-r")))))
