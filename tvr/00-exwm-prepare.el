96c5e59f93637129059cd04be6d045c836c81992
T.V Raman <tv.raman.tv@gmail.com>
2023-01-10 20:36:25 -0800

    remove redundant update-class hook

--- tvr/00-exwm-prepare.el
+++ tvr/00-exwm-prepare.el
@@ -9,14 +9,9 @@
 
 (setq exwm-workspace-number 2)
 
-;;; Name exwm buffers meaningfully:
 
-(add-hook 'exwm-update-title-hook
-          (lambda ()
-            (when (or (not exwm-instance-name)
-                      (string-prefix-p "sun-awt-X11-" exwm-instance-name)
-                      (string= "gimp" exwm-instance-name))
-              (exwm-workspace-rename-buffer exwm-title))))
+
+
 ;;; hide echo-area when not used:
 (setq exwm-workspace-minibuffer-position 'bottom)
 
