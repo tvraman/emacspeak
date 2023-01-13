2e2d50d562c5ced205af92d93745ec764a7cc34c
T.V Raman <tv.raman.tv@gmail.com>
2023-01-13 08:29:09 -0800

    startup

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -116,7 +116,6 @@ Produce timing information as the last step."
 (defsubst tvr-tabs ()
   "Set up my tab-bar."
   (tab-bar-switch-to-tab "Books")
-  (tab-bar-switch-to-tab "Chrome")
   (tab-bar-switch-to-tab "XTerm")
   (tab-bar-switch-to-tab "Home"))
 
