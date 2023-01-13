478809acac4956c1ba18a629ae27101133d6e03f
T.V Raman <tv.raman.tv@gmail.com>
2023-01-13 08:09:47 -0800

    tab-bar

--- tvr/tab-bar-prepare.el
+++ tvr/tab-bar-prepare.el
@@ -1,13 +1,7 @@
-(defun tab-create (name)
-  "Create the NAME tab if it doesn't exist already."
-  (condition-case nil
-      (unless (equal (alist-get 'name (tab-bar--current-tab)) name)
-        (tab-bar-rename-tab-by-name name name))
-    (error (tab-new)
-           (tab-bar-rename-tab name))))
 
 (defun tvr-tabs ()
   "Set up my tab-bar."
-  (tab-create "Books")
-  (tab-create "EXWM")
-  (tab-create "XTerm"))
+  (tab-bar-switch-to-tab "Books")
+  (tab-bar-switch-to-tab "EXWM")
+  (tab-bar-switch-to-tab "XTerm")
+  (tab-bar-switch-to-tab "Home"))
