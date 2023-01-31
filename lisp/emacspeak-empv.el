997542c702a360a027c44836c0759ea092e025e9
T.V Raman <tv.raman.tv@gmail.com>
2023-01-31 10:53:51 -0800

    toggles

--- lisp/emacspeak-empv.el
+++ lisp/emacspeak-empv.el
@@ -115,14 +115,14 @@ empv-youtube-tabulated-last-results
 (cl-loop
   for f in 
   '(aempv-current-loop-off empv-current-loop-on
-empv-file-loop-off empv-file-loop-on
-empv-playlist-loop-off empv-playlist-loop-on) do
+    empv-toggle
+    empv-file-loop-off empv-file-loop-on
+    empv-playlist-loop-off empv-playlist-loop-on) do
   (eval
    `(defadvice ,f (after emacspeak pre act comp)
       "speak."
       (when (ems-interactive-p)
         (emacspeak-auditory-icon 'button)))))
-
 
 ;;}}}
 ;;{{{Setup:
