49c1b28c07c460d2cb4b02a82bcff45a8666e9b5
T.V Raman <tv.raman.tv@gmail.com>
2023-02-01 20:50:22 -0800

    ogg

--- sounds/ogg-3d/define-theme.el
+++ sounds/ogg-3d/define-theme.el
@@ -1,4 +1,4 @@
-;;; Wave icons using chimes as a theme  -*- lexical-binding: t; -*-
+;;; OGG icons using chimes as a theme  -*- lexical-binding: t; -*-
 ;;; Icons from theme Chimes with effect autopan applied using mplayer and ladspa
 
 (emacspeak-sounds-define-theme
