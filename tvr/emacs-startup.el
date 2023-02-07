36eb37dc2ff5a990f7a223805ef0e17ec82e27a8
T.V Raman <tv.raman.tv@gmail.com>
2023-02-07 11:50:12 -0800

    startup

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -218,8 +218,7 @@ Use Custom to customize where possible. "
   (diminish 'voice-lock-mode "")
   (diminish 'auto-fill-function "")
   (diminish 'abbrev-mode "")
-  (diminish 'auto-correct-mode "")
-  (load-library "yes-prepare"))
+  (diminish 'auto-correct-mode ""))
 
 (defun tvr-after-init ()
   "Actions to take after Emacs is up and ready."
