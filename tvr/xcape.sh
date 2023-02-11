6cd2116246cc1855af078ffc00d7d6d813c456c7
T.V Raman <tv.raman.tv@gmail.com>
2023-02-11 12:52:51 -0800

    xcape

--- tvr/xcape.sh
+++ tvr/xcape.sh
@@ -8,7 +8,7 @@
 TM=150 #timeout in ms for keyup
 
 KEYS="Shift_L=Escape;\
-Shift_R=parenleft;\
+Shift_R=Escape;\
 Control_L=Control_L|e"
 pidof xcape && kill -9 `pidof xcape`
 xcape -t $TM -e  "$KEYS" 2>&1 > /dev/null &
