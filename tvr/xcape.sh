fce4b0ad48bf93ef087c35f8c1c33492587ad9ce
T.V Raman <tv.raman.tv@gmail.com>
2023-02-11 11:30:40 -0800

    xcape

--- tvr/xcape.sh
+++ tvr/xcape.sh
@@ -5,10 +5,10 @@
 #Shift_L gives open paren
 #Shift_R gives Escape
 
-TM=250 #timeout in ms for keyup
+TM=150 #timeout in ms for keyup
 
-KEYS="Shift_L=parenleft;\
-Shift_R=Escape;\
+KEYS="Shift_L=Escape;\
+Shift_R=parenleft;\
 Control_L=Control_L|e"
 pidof xcape && kill -9 `pidof xcape`
-nohup xcape -t $TM -e  "$KEYS" 2>&1 > /dev/null
+xcape -t $TM -e  "$KEYS" 2>&1 > /dev/null &
