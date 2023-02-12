09a862a8c33f97dc59d4c25aad36d20b79cab523
T.V Raman <tv.raman.tv@gmail.com>
2023-02-12 11:16:50 -0800

    smarter header default

--- lisp/emacspeak-speak.el
+++ lisp/emacspeak-speak.el
@@ -1629,7 +1629,11 @@ Displays name of current buffer.")
        (emacspeak-auditory-icon 'item)
        (when (> window-count 1) (emacspeak--sox-multiwindow))
        (dtk-speak (format-mode-line header-line-format))))
-    (t (dtk-speak (format-time-string " %H:%M ")))))
+    (t
+     (dtk-speak
+      (concat
+       (propertize (buffer-name) 'personality voice-smoothen)
+       (format-time-string " %H:%M "))))))
 
 (defun emacspeak-toggle-header-line ()
   "Toggle Emacspeak's default header line."
