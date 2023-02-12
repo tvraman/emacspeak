f89bc26603b14272ad078a138d3bdc6590bcaec3
T.V Raman <tv.raman.tv@gmail.com>
2023-02-12 12:56:00 -0800

    xcape

--- tvr/xcape.sh
+++ tvr/xcape.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
-#XCape ==xcape -- reassign modifier keys etc.
+#XCape ==xcape -- reassign modifier keys
 #Control by itself gives emacspeak modifier.
 # See forthcoming blog article for rationale.
-
-
 
 TM=250 #timeout in ms for keyup
 
 KEYS="\
-Shift_R=Control_L|r;\
 Shift_L=Control_L|s;\
+Shift_R=Control_L|r;\
 Super_L=Control_L|c;\
 Alt_L=Control_L|x;\
 Alt_R=Control_L|x;\
 Control_R=Control_L|e;\
 Control_L=Control_L|e"
 pidof xcape && kill -9 `pidof xcape`
-xcape -t $TM -e  "$KEYS" 2>&1 > /dev/null &
+xcape -t $TM -e  "$KEYS" 2>&1 > /dev/null 
