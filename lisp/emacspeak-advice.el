0490a8a7f0f7096d49e3cbd4fd4c0ff2675358bc
T.V Raman <tv.raman.tv@gmail.com>
2023-03-14 08:49:59 -0700

    clean up fancy error handler

--- lisp/emacspeak-advice.el
+++ lisp/emacspeak-advice.el
@@ -837,12 +837,11 @@ When on a close delimiter, speak matching delimiter after a small delay. "
   (cl-declare (special ems--last-error-msg
                        ems--lazy-error-time))
   (let ((m (error-message-string data))
-        (fn (if caller (format "%s" caller) "")))
+        (fn (or caller "")))
     (when
         (and
          (<  (/ echo-keystrokes 20)
-             (float-time
-              (time-subtract (current-time) ems--lazy-msg-time))))
+             (float-time (time-subtract (current-time) ems--lazy-msg-time))))
       (setq ems--last-error-msg m
             ems--lazy-error-time (current-time) )
       (emacspeak-auditory-icon 'warn-user)
