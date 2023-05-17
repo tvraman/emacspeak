7848920d9fc8f8d2d11a6dd91b98ff531ad6e33b
T.V Raman <tv.raman.tv@gmail.com>
2023-05-17 11:38:07 -0700

    packages

---
+++ tvr/packages.el
@@ -0,0 +1,18 @@
+(setq
+ package-selected-packages
+ '(pass cdlatex fussy tldr noaa
+        yasnippet-snippets forge browse-at-remote devdocs
+        find-file-in-project company-fuzzy bash-completion org-translate
+        google-translate xelb exwm ido-completing-read+ pydoc
+        common-lisp-snippets calibredb haskell-mode diminish
+        slime-company csound-mode websearch arxiv-citation arxiv-mode
+        free-keys vdiff xkcd request-deferred exec-path-from-shell mines
+        js2-mode flx-ido clhs company-wordfreq elpy auth-source-xoauth2
+        company-statistics html2org sdcv orgalist clang-format eglot ein
+        chess list-unicode-display relint lispy 2048-game auto-correct
+        bbdb browse-kill-ring company-auctex auctex company-bibtex
+        company-c-headers company-flx company-prescient company-reftex
+        disable-mouse elfeed flyspell-correct-popup flyspell-correct
+        flyspell-popup folding go-complete go-eldoc go-mode html5-schema
+        company name-this-color nvm paradox rg sicp smartparens
+        ssh-config-mode stumpwm-mode threes twittering-mode yasnippet))
