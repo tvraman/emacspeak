8dd915662e5b38188972b300fd87d7a94146a13d
T.V Raman <tv.raman.tv@gmail.com>
2023-05-24 19:33:17 -0700

    swap c-x and c-c (experimental)

--- tvr/xcape.sh
+++ tvr/xcape.sh
@@ -3,6 +3,8 @@
 #XCape ==xcape -- reassign modifier keys
 #Control by itself gives emacspeak modifier.
 # See forthcoming blog article for rationale.
+# Swapping C-x and C-c from what is described in the blog.
+
 # Start with  timeout  250, then tune it down (suggest 10% every few
 #days)
 # Customize echo-keystrokes in Emacs: I have it as 0.001
@@ -13,9 +15,9 @@ TM=175 #timeout in ms for keyup
 KEYS="\
 Shift_L=Control_L|s;\
 Shift_R=Control_L|r;\
-Super_L=Control_L|c;\
-Alt_L=Control_L|x;\
-Alt_R=Control_L|x;\
+Super_L=Control_L|x;\
+Alt_L=Control_L|c;\
+Alt_R=Control_L|c;\
 Control_L=Control_L|e"
 pidof xcape && kill -9 `pidof xcape`
 xcape -t $TM -e  "$KEYS" 2>&1 > /dev/null 
