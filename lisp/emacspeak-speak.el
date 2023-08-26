245ec68d3c86970e1322cd348906c9b1bb5616eb
T.V Raman <tv.raman.tv@gmail.com>
2023-08-26 07:50:02 -0700

    line-to-top when reading rest of buffer

--- lisp/emacspeak-speak.el
+++ lisp/emacspeak-speak.el
@@ -1206,6 +1206,7 @@ Useful to listen to a buffer without switching  contexts."
   "Speak remainder of the buffer starting at point"
   (interactive)
   (emacspeak-auditory-icon 'select-object)
+  (recenter 0); line-to-top
   (emacspeak-speak-buffer 1))
 
 (defun emacspeak-speak-help (&optional arg)
