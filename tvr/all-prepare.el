0ad8749e05d033f0113d72dd294cab6b50defb3b
T.V Raman <tv.raman.tv@gmail.com>
2023-08-29 18:50:07 -0700

    cleanup startup

--- tvr/all-prepare.el
+++ tvr/all-prepare.el
@@ -158,6 +158,54 @@ This moves them into the Spam folder."
 (defalias 'tvr-km-morning
    (kmacro "C-<tab> C-e g b h <tab> <return> n n e c"))
 (define-key emacspeak-y-keymap "0" 'tvr-km-morning)
+(setq pre-redisplay-function nil
+      x-wait-for-event-timeout 0
+      mail-host-address "google.com"
+      user-mail-address "raman@google.com")
+(light-black)
+(defvar touchpad-device "10"
+  "Device ID of synaptics.
+Set by locating it via xinput list | grep -i touchpad ")
+
+(defun turn-off-touchpad (&optional frame)
+  (interactive)
+  (declare (special touchpad-device))
+  (start-process "xinput" nil 
+    "xinput" "set-prop" touchpad-device "Device Enabled" "0")
+  (message "Disabled touchpad"))
+
+(defun turn-on-touchpad (&optional frame)
+  (interactive)
+  (declare (special touchpad-device))
+  (start-process "xinput" nil 
+    "xinput" "set-prop" touchpad-device "Device Enabled" "1")
+  (message "Enabled touchpad"))
+
+;(turn-off-touchpad)
+
+(defun tvr-calendar ()
+  "Open Google Calendar in Chrome"
+  (interactive)
+  (browse-url-chrome "calendar/"))
+(define-key emacspeak-y-keymap "c" 'tvr-calendar)
+
+
+
+(defun tvr-chat ()
+  "Open Google Chat in Chrome"
+  (interactive)
+  (browse-url-chrome "chat/"))
+
+(define-key emacspeak-y-keymap " " 'tvr-chat)
+
+(defun tvr-mail ()
+  "Open Google Mail in Chrome"
+  (interactive)
+  (browse-url-chrome "chat/"))
+
+
+(define-key emacspeak-y-keymap "m" 'tvr-mail)
+(provide 'laptop-local)
 (define-key emacs-lisp-mode-map (ems-kbd "C-c e") 'macrostep-expand)
 (defun conditionally-enable-lispy ()
   (when (memq this-command '(eval-expression emacspeak-wizards-show-eval-result))
@@ -262,9 +310,8 @@ This moves them into the Spam folder."
 ;;{{{Weekday Colors:
 
 (defconst tvr-weekday-color-map
-  [; 0 is Sunday 
-   ("light sky blue" . "#6FBD87")                       ; light sky blue on silver tree
-   ("#FFF3FF" . "gold"); lavender blush on gold
+  [                                    ; 0 is Sunday 
+   ("light sky blue" . "#6FBD87")      ; light sky blue on silver tree
    ("royal blue" . "#FFD724")          ;RoyalBlue on pink
    ("#F4C430" . "sea green")           ; saffron on sea green
    ("#FFFFDA" . "royal blue")          ; light yellow on blue
