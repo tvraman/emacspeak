22ba6fb120872e32e9b8bc80d5ce7dfb1fb354ea
T.V Raman <tv.raman.tv@gmail.com>
2023-09-13 07:35:35 -0700

    yas

--- tvr/all-prepare.el
+++ tvr/all-prepare.el
@@ -342,12 +342,9 @@ Set by locating it via xinput list | grep -i touchpad ")
   hook)
 ;; -*- lexical-binding: nil; -*-
 (defvar tvr-yas-snippets-loaded nil)
-(with-eval-after-load
- 'yasnippet
- `(progn
-    (unless tvr-yas-snippets-loaded
-      (run-with-idle-timer 1 nil #'yas-reload-all)
-      (setq tvr-yas-snippets-loaded t))
-    (setq yas-verbosity 0))
- (diminish 'yas-minor-mode ""))
-
+(with-eval-after-load "yasnippet"
+  (unless tvr-yas-snippets-loaded
+    (run-with-idle-timer 1 nil #'yas-reload-all)
+    (setq tvr-yas-snippets-loaded t))
+  (setq yas-verbosity 0)
+  (diminish 'yas-minor-mode ""))
--- tvr/yasnippet-prepare.el
+++ tvr/yasnippet-prepare.el
@@ -1,11 +1,8 @@
 ;; -*- lexical-binding: nil; -*-
 (defvar tvr-yas-snippets-loaded nil)
-(with-eval-after-load
- 'yasnippet
- `(progn
-    (unless tvr-yas-snippets-loaded
-      (run-with-idle-timer 1 nil #'yas-reload-all)
-      (setq tvr-yas-snippets-loaded t))
-    (setq yas-verbosity 0))
- (diminish 'yas-minor-mode ""))
-
+(with-eval-after-load "yasnippet"
+  (unless tvr-yas-snippets-loaded
+    (run-with-idle-timer 1 nil #'yas-reload-all)
+    (setq tvr-yas-snippets-loaded t))
+  (setq yas-verbosity 0)
+  (diminish 'yas-minor-mode ""))
