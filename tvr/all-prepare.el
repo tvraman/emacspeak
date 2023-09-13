f131523946f8c53f33e5c9c5a65b68085100d659
T.V Raman <tv.raman.tv@gmail.com>
2023-09-12 20:26:45 -0700

    sdcv

--- tvr/all-prepare.el
+++ tvr/all-prepare.el
@@ -291,6 +291,28 @@ Set by locating it via xinput list | grep -i touchpad ")
   (define-key smartparens-mode-map "\C-\M-b" 'backward-sexp)
   (diminish 'smartparens-mode "" )
   )
+(with-eval-after-load
+    "sdcv"
+
+  (defun tvr-sdcv-update-dictionary-list ()
+    "Update sdcv dictionary lists if necessary by examining
+/usr/share/sdcv/dict"
+    (cl-declare (special sdcv-dictionary-complete-list
+                         sdcv-dictionary-simple-list))
+    (let ((installed (split-string (shell-command-to-string "sdcv -l") "\n"))
+          (dictionaires nil))
+      (pop installed)                   ; nuke header line
+      (setq dictionaires
+            (cl-loop
+             for d in installed collect
+             (split-string d " " 'omit-nulls " ")))
+      (setq sdcv-dictionary-simple-list
+            (cl-loop
+             for d in dictionaires collect
+             (mapconcat #'identity (butlast d) " ")))))
+  (tvr-sdcv-update-dictionary-list)
+  )
+
 ;;; vm-prepare.l :  -*- lexical-binding: nil; -*-
 
 (autoload 'vm "vm" "vm mail reader" t nil)
