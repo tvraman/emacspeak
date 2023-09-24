df7f48069609e37e56f3000fb0156f5312ba15db
T.V Raman <tv.raman.tv@gmail.com>
2023-09-23 20:27:38 -0700

    promote org key bindings from org-prepare.el to emacspeak-org

--- tvr/all-prepare.el
+++ tvr/all-prepare.el
@@ -213,23 +213,11 @@ Uses the go oauth tool found in the xoauth git repo."
 ;;;$Id: org-prepare.el 6727 2011-01-14 23:22:20Z tv.raman.tv $  -*- lexical-binding: nil; -*-
 
 (with-eval-after-load "org"
-  (cl-declaim (special org-mode-map org-multi-keymap))
+
   (require 'org-tempo)
   (add-hook 'org-mode-hook #'turn-on-org-cdlatex)
   (require 'ol-eww)
   (require 'ox-md)
-  (define-prefix-command 'org-multi-keymap)
-  (define-key org-mode-map (ems-kbd "C-'") 'org-multi-keymap)
-  (define-key org-multi-keymap "n" #'org-next-link)
-  (define-key org-multi-keymap "'" #'org-open-at-point)
-  (define-key org-multi-keymap ";" #'emacspeak-org-amarks-play)
-  (define-key org-multi-keymap "p" #'org-previous-link)
-  (define-key org-mode-map (ems-kbd "C-,") 'emacspeak-alt-keymap)
-  (define-key org-mode-map (ems-kbd "C-c m") 'org-md-export-as-markdown)
-  (define-key global-map (ems-kbd "C-c i") 'org-insert-link)
-  (define-key global-map (ems-kbd "C-c l") 'org-store-link)
-  (define-key global-map (ems-kbd "C-c b") 'org-switchb)
-  (define-key global-map  (ems-kbd "C-c c") 'org-capture)
   )
 (with-eval-after-load "orgalist"
   (diminish 'orgalist-mode ""))
