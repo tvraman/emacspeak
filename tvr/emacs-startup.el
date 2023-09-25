fde4fdeaa9912ff39dd5145585a63ca2787dae16
T.V Raman <tv.raman.tv@gmail.com>
2023-09-25 09:21:31 -0700

    diminish

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -175,7 +175,8 @@ Use Custom to customize where possible. "
      #'(lambda (m) (diminish m ""))
      '(
        outline-minor-mode reftex-mode voice-lock-mode company-mode hs-minor-mode
-       yas-minor-mode  auto-fill-function abbrev-mode auto-correct-mode)))
+       org-cdlatex-mode yas-minor-mode  auto-fill-function
+       abbrev-mode auto-correct-mode)))
   (setq  global-mode-string '("" display-time-string battery-mode-line-string))
   (bash-completion-setup)
   (load-theme 'modus-vivendi-tinted t))
