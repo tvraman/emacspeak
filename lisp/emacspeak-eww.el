5e6f2f369dd5b7613ad13823464aee0b757c952d
T.V Raman <tv.raman.tv@gmail.com>
2023-09-28 19:55:28 -0700

    updated ua masquerade

--- lisp/emacspeak-eww.el
+++ lisp/emacspeak-eww.el
@@ -549,7 +549,7 @@
   (format "User-Agent: %s\r\n"
           "Mozilla/5.0 (X11; Linux x86_64) \
 AppleWebKit/537.36 (KHTML, like Gecko) \
-Chrome/100.0.4240.75 \
+Chrome/117.0.0.0 \
 Safari/537.36"
           )
   "User Agent string sent when masquerading.")
