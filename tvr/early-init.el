e3af78038b8d2712afe15d1b82357909c0550b9d
T.V Raman <tv.raman.tv@gmail.com>
2023-10-20 12:33:33 -0700

    read-process-output-max (* 1024 512)

--- tvr/early-init.el
+++ tvr/early-init.el
@@ -1,6 +1,7 @@
 (setq
  native-comp-async-report-warnings-errors nil
                                         ;force-load-messages t
+ read-process-output-max (* 1024 512)
  package-quickstart t
  gc-cons-threshold 128000000
  )
