13af80b40ec938fbe995cfc9fe0407c186165a26
T.V Raman <tv.raman.tv@gmail.com>
2023-11-14 08:33:09 -0800

    Adding eat support for terminal emulation

---
+++ lisp/emacspeak-eat.el
@@ -0,0 +1,127 @@
+;;; emacspeak-eat.el --- Speech-enable EAT  -*- lexical-binding: t; -*-
+;;; $Author: tv.raman.tv $
+;;; Keywords: Emacspeak,  Audio Desktop eat
+;;;   LCD Archive entry:
+
+;;; LCD Archive Entry:
+;;; emacspeak| T. V. Raman |raman@cs.cornell.edu
+;;; A speech interface to Emacs |
+;;;  $Revision: 4532 $ |
+;;; Location undetermined
+;;;
+
+;;;   Copyright:
+
+;; Copyright (C) 1995 -- 2022, T. V. Raman
+;; Copyright (c) 1994, 1995 by Digital Equipment Corporation.
+;; All Rights Reserved.
+;; 
+;; This file is not part of GNU Emacs, but the same permissions apply.
+;; 
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;; 
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;;; Commentary:
+;; EAT ==  Emacs Terminal Emulator
+
+;;; Code:
+
+;;;   Required modules
+
+(eval-when-compile  (require 'cl-lib))
+(cl-declaim  (optimize  (safety 0) (speed 3)))
+(require 'emacspeak-preamble)
+(eval-when-compile (require 'eat "eat" 'no-error))
+;;;  Map Faces:
+
+
+
+
+(voice-setup-add-map 
+ '(
+   (eat-shell-prompt-annotation-failure voice-lighten)
+   (eat-shell-prompt-annotation-running voice-monotone)
+   (eat-shell-prompt-annotation-success voice-animate)
+   (eat-term-bold voice-bolden)
+   (eat-term-italic voice-smoothen)))
+;;;  Interactive Commands:
+
+
+
+'(
+  
+eat-blink-mode
+eat-char-mode
+eat-compile-terminfo
+eat-emacs-mode
+eat-eshell-char-mode
+eat-eshell-emacs-mode
+eat-eshell-mode
+eat-eshell-semi-char-mode
+eat-eshell-visual-command-mode
+eat-input-char
+eat-kill-process
+eat-line-delchar-or-eof
+eat-line-find-input
+eat-line-history-isearch-backward
+eat-line-history-isearch-backward-regexp
+eat-line-load-input-history-from-file
+eat-line-mode
+eat-line-next-input
+eat-line-next-matching-input
+eat-line-next-matching-input-from-input
+eat-line-previous-input
+eat-line-previous-matching-input
+eat-line-previous-matching-input-from-input
+eat-line-restore-input
+eat-line-send-input
+eat-line-send-interrupt
+eat-mode
+eat-mouse-yank-primary
+eat-mouse-yank-secondary
+eat-narrow-to-shell-prompt
+eat-next-shell-prompt
+eat-other-window
+eat-previous-shell-prompt
+eat-project
+eat-project-other-window
+eat-quoted-input
+eat-reload
+eat-reset
+eat-self-input
+eat-semi-char-mode
+eat-send-password
+eat-trace-mode
+eat-trace-replay
+eat-trace-replay-mode
+eat-trace-replay-next-frame
+eat-xterm-paste
+eat-yank
+eat-yank-from-kill-ring
+)
+
+(defadvice eat (after emacspeak pre act comp)
+  "speak."
+  (when (ems-interactive-p)
+    (emacspeak-auditory-icon 'open-object)
+    (emacspeak-speak-mode-line)))
+
+
+(provide 'emacspeak-eat)
+;;;  end of file
