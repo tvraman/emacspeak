a3982abb2c2f42ff460564c524d31baa2c1d5fd4
T.V Raman <tv.raman.tv@gmail.com>
2023-11-15 11:34:24 -0800

    code name and version update

--- lisp/emacspeak-speak.el
+++ lisp/emacspeak-speak.el
@@ -1738,7 +1738,7 @@ Seconds value is also placed in the kill-ring."
 
 ;;;  Codenames etc.
 (defvar emacspeak-codename
-  (propertize "ErgoDog" 'face 'bold)
+  (propertize "VirtualDog" 'face 'bold)
   "Code name of present release.")
 
 (defun emacspeak-setup-get-revision ()
@@ -1752,7 +1752,7 @@ Seconds value is also placed in the kill-ring."
       "")))
 
 (defvar emacspeak-version
-  (concat "58.0,   " emacspeak-codename)
+  (concat "59.0,   " emacspeak-codename)
   "Version number for Emacspeak.")
 
 (defun emacspeak-speak-version (&optional speak-rev)
