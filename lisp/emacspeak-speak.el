04c3d5f8790ca53f9b1e7f60d6219e3cda3a0985
T.V Raman <tv.raman.tv@gmail.com>
2023-11-20 10:28:50 -0800

    brief time: use main speech channel

--- lisp/emacspeak-speak.el
+++ lisp/emacspeak-speak.el
@@ -1684,7 +1684,7 @@ Optional second arg `set' sets the TZ environment variable as well."
   "Time in brief"
   (interactive)
   (cl-declare (special emacspeak-speak-time-brief-format))
-  (dtk-notify-say
+  (dtk-say
    (format-time-string emacspeak-speak-time-brief-format)))
 
 (defun emacspeak-speak-time (&optional world)
