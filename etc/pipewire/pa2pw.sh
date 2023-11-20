33fcf5b49609d7e283538113092c80e7908b022f
T.V Raman <tv.raman.tv@gmail.com>
2023-11-20 09:46:08 -0800

    add doc link

--- etc/pipewire/pa2pw.sh
+++ etc/pipewire/pa2pw.sh
@@ -4,6 +4,7 @@ pactl load-module module-ladspa-sink sink_name=binaural  sink_master=@DEFAULT_SI
 # Use binaural as the default sink:
 pactl #set-default-sink  binaural 
 # A tts_right and tts_left device
+# See https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/User/Modules/#module-remap-sink
 # Emacspeak will use the first as the notification device.
 pactl load-module module-remap-sink sink_name=tts_right  master=binaural channels=2 master_channel_map=front-right,front-right, channel_map=front-left,front-right remix=no
 pactl load-module module-remap-sink sink_name=tts_left  master=binaural channels=2 master_channel_map=front-left,front-left, channel_map=front-left,front-right remix=no
