ae3690c8efcd7ffe9e8dcc71d97afe0577e75e8c
T.V Raman <tv.raman.tv@gmail.com>
2023-11-20 10:03:53 -0800

    doc link: pw-link examples

--- etc/pipewire/pa2pw.sh
+++ etc/pipewire/pa2pw.sh
@@ -9,5 +9,7 @@ pactl #set-default-sink  binaural
 pactl load-module module-remap-sink sink_name=tts_right  master=binaural channels=2 master_channel_map=front-right,front-right, channel_map=front-left,front-right remix=no
 pactl load-module module-remap-sink sink_name=tts_left  master=binaural channels=2 master_channel_map=front-left,front-left, channel_map=front-left,front-right remix=no
 
+# Above can likely be done with pw-link:
+#https://www.thegeekdiary.com/pw-link-command-examples-in-linux/
 # device: snoop -- to record and play
 pactl load-module module-combine-sink sink_name=snoop  sink_properties=device.description="Record-and-Play"
