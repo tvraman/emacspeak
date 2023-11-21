29ad105bcbbade684a18fbec1b2db7c2f31e25d3
T.V Raman <tv.raman.tv@gmail.com>
2023-11-21 10:52:28 -0800

    hrtf

--- etc/pipewire/pw-hrtf.sh
+++ etc/pipewire/pw-hrtf.sh
@@ -9,12 +9,12 @@
 pactl load-module module-ladspa-sink sink_name=FULeft plugin=ZamHeadX2-ladspa label=ZamHeadX2 control=45,45,2.5 
 pactl load-module module-ladspa-sink sink_name=RULeft plugin=ZamHeadX2-ladspa label=ZamHeadX2 control=135,45,2.5 
 pactl load-module module-ladspa-sink sink_name=RURight plugin=ZamHeadX2-ladspa label=ZamHeadX2 control=225,45,2.5 
-pactl load-module module-ladspa-sink sink_name=RURight plugin=ZamHeadX2-ladspa label=ZamHeadX2 control=315,45,2.5
+pactl load-module module-ladspa-sink sink_name=FURight plugin=ZamHeadX2-ladspa label=ZamHeadX2 control=315,45,2.5
 
 
 # Lower 4
 pactl load-module module-ladspa-sink sink_name=FLLeft plugin=ZamHeadX2-ladspa label=ZamHeadX2 control=45,-45,2.5 
 pactl load-module module-ladspa-sink sink_name=RLLeft plugin=ZamHeadX2-ladspa label=ZamHeadX2 control=135,-45,2.5 
 pactl load-module module-ladspa-sink sink_name=RLRight plugin=ZamHeadX2-ladspa label=ZamHeadX2 control=225,-45,2.5 
-pactl load-module module-ladspa-sink sink_name=RLRight plugin=ZamHeadX2-ladspa label=ZamHeadX2 control=315,-45,2.5
+pactl load-module module-ladspa-sink sink_name=FLRight plugin=ZamHeadX2-ladspa label=ZamHeadX2 control=315,-45,2.5
 
