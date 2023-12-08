00c5926b5592f582abc6a9addf9b087ea43dc100
T.V Raman <tv.raman.tv@gmail.com>
2023-12-08 09:11:47 -0800

    km

--- tvr/all-prepare.el
+++ tvr/all-prepare.el
@@ -144,8 +144,7 @@ Uses the go oauth tool found in the xoauth git repo."
 ;;; Jump to Emacs Git Logs At HEAD:
 (defalias 'tvr-km-emacs-log
   (kmacro
-   "C-c 3 g i t SPC p <return> C-; d l l C-a  C-e C-q "))
-
+   "C-c 3 g i t SPC p <return> C-; d l l C-a "))
 (define-key emacspeak-y-keymap "3" 'tvr-km-emacs-log)
 (defalias 'tvr-km-morning
   (kmacro "C-<tab> C-e g b h <tab> <return> n n e c"))
--- tvr/kbd-macros.el
+++ tvr/kbd-macros.el
@@ -1,7 +1,7 @@
 ;;; Jump to Emacs Git Logs At HEAD:
 (defalias 'tvr-km-emacs-log
   (kmacro
-   "C-c 3 g i t SPC p <return> C-; d l l C-e | <return> <escape> < C-e s"))
+   "C-c 3 g i t SPC p <return> C-; d l l C-a "))
 (define-key emacspeak-y-keymap "3" 'tvr-km-emacs-log)
 (defalias 'tvr-km-morning
   (kmacro "C-<tab> C-e g b h <tab> <return> n n e c"))
