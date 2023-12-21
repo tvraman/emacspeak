311f7f0558d16952ec0efc5e24c878b2171a1a9c
T.V Raman <tv.raman.tv@gmail.com>
2023-12-21 13:46:21 -0800

    no-gptel

--- tvr/gptel-prepare.el
+++
@@ -1,8 +0,0 @@
-(setq-default gptel-backend
-      (gptel-make-ollama
-       "Ollama"
-       :host "localhost:11434"
-       :models '("zephyr" "llama2")
-       :stream t))
-
-(setq-default gptel-model "zephyr")
