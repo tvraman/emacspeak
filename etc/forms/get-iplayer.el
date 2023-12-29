e11165dd9d903e3027934a9bbae190cc737976af
T.V Raman <tv.raman.tv@gmail.com>
2023-12-29 14:03:54 -0800

    cleanup

--- etc/forms/get-iplayer.el
+++ etc/forms/get-iplayer.el
@@ -21,9 +21,10 @@
 (defun ems--get-iplayer-download ()
   (interactive)
   (let ((default-directory (expand-file-name "~/Downloads")))
-    (shell-command
-     (format "youtube-dl  %s " (nth  1 forms-fields)))))
+    (async-shell-command
+     (format "youtube-dl %s " (nth  13 forms-fields)))))
 
+(require 'forms)
 (when (and forms-mode-map (keymapp forms-mode-map))
   (define-key forms-mode-map "d" 'ems--get-iplayer-download)
   (define-key forms-mode-map "p" 'ems--get-iplayer-play))
