909f67bab3de43739b2119fa2de53ff7c59cc94f
Robert Melton <robert@robertmelton.com>
2024-01-02 09:29:38 -0500

    cleaned up pure play tone
    Of course after I mainline the project, I wake up witha  way better way
    to handle the tone generation that is simplier and cleaner and gets rid
    of an entire async queue

--- servers/mac-swiftmac/Sources/SwiftMacPackage/playpuretone.swift
+++ servers/mac-swiftmac/Sources/SwiftMacPackage/playpuretone.swift
@@ -5,11 +5,8 @@ import Foundation
 
 let audioPlayer = AVAudioPlayerNode()
 let audioEngine = AVAudioEngine()
-let toneQueue = DispatchQueue(
-  label: "org.emacspeak.server.swiftmac.tone", qos: .userInteractive)
 let semaphore = DispatchSemaphore(value: 1)
 
-/* Generates a tone in pure swift */
 func playPureTone(
   frequencyInHz: Int, amplitude: Float,
   durationInMillis: Int
@@ -17,63 +14,62 @@ func playPureTone(
   #if DEBUG
     debugLogger.log("in playPureTone")
   #endif
-  toneQueue.async {
-    semaphore.wait()
-    audioEngine.attach(audioPlayer)
-    let mixer = audioEngine.mainMixerNode
-    let sampleRateHz = Float(mixer.outputFormat(forBus: 0).sampleRate)
+  semaphore.wait()
+  audioEngine.attach(audioPlayer)
+  let mixer = audioEngine.mainMixerNode
+  let sampleRateHz = Float(mixer.outputFormat(forBus: 0).sampleRate)
 
-    guard
-      let format = AVAudioFormat(
-        commonFormat: AVAudioCommonFormat.pcmFormatFloat32,
-        sampleRate: Double(sampleRateHz),
-        channels: AVAudioChannelCount(1), interleaved: false)
-    else {
-      return
-    }
+  guard
+    let format = AVAudioFormat(
+      commonFormat: AVAudioCommonFormat.pcmFormatFloat32,
+      sampleRate: Double(sampleRateHz),
+      channels: AVAudioChannelCount(1), interleaved: false)
+  else {
+    semaphore.signal()
+    return
+  }
 
-    audioEngine.connect(audioPlayer, to: mixer, format: format)
+  audioEngine.connect(audioPlayer, to: mixer, format: format)
 
-    let numberOfSamples = AVAudioFrameCount(
-      (Float(durationInMillis)
-        / 1000 * sampleRateHz))
+  let numberOfSamples = AVAudioFrameCount(
+    (Float(durationInMillis)
+      / 1000 * sampleRateHz))
 
-    guard
-      let buffer = AVAudioPCMBuffer(
-        pcmFormat: format,
-        frameCapacity: numberOfSamples)
-    else {
-      return
-    }
-    buffer.frameLength = numberOfSamples
+  guard
+    let buffer = AVAudioPCMBuffer(
+      pcmFormat: format,
+      frameCapacity: numberOfSamples)
+  else {
+    semaphore.signal()
+    return
+  }
+  buffer.frameLength = numberOfSamples
 
-    let channels = UnsafeBufferPointer(
-      start: buffer.floatChannelData, count: Int(format.channelCount))
-    let floats = UnsafeMutableBufferPointer<Float>(
-      start: channels[0], count: Int(numberOfSamples))
+  let channels = UnsafeBufferPointer(
+    start: buffer.floatChannelData, count: Int(format.channelCount))
+  let floats = UnsafeMutableBufferPointer<Float>(
+    start: channels[0], count: Int(numberOfSamples))
 
-    let angularFrequency = Float(frequencyInHz * 2) * .pi
+  let angularFrequency = Float(frequencyInHz * 2) * .pi
 
-    for i in 0..<Int(numberOfSamples) {
-      let waveComponent =
-        sinf(Float(i) * angularFrequency / sampleRateHz)
-      floats[i] = waveComponent * amplitude
-    }
-    do {
-      try audioEngine.start()
-    } catch {
-      debugPrint("Error: Engine start failure")
-      return
-    }
+  for i in 0..<Int(numberOfSamples) {
+    let waveComponent =
+      sinf(Float(i) * angularFrequency / sampleRateHz)
+    floats[i] = waveComponent * amplitude
+  }
+  do {
+    try audioEngine.start()
+  } catch {
+    debugPrint("Error: Engine start failure")
+    semaphore.signal()
+    return
+  }
 
-    audioPlayer.play()
-    audioPlayer.scheduleBuffer(buffer, at: nil, options: .interrupts) {
-      toneQueue.async {
-        semaphore.signal()
-      }
-    }
-    semaphore.wait()
-
+  audioPlayer.play()
+  audioPlayer.scheduleBuffer(buffer, at: nil, options: .interrupts) {
     semaphore.signal()
   }
+
+  semaphore.wait()
+  semaphore.signal()
 }
