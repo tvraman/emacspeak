ac0c26c9527c41cc007311776ecc2b7cee6271e9
T.V Raman <tv.raman.tv@gmail.com>
2024-01-04 13:13:23 -0800

    improved read-passwd advice

--- lisp/emacspeak-advice.el
+++ lisp/emacspeak-advice.el
@@ -945,9 +945,8 @@ When on a close delimiter, speak matching delimiter after a small delay. "
 
 ;; read-password--hide-password
 (defadvice read-password--hide-password (after emacspeak pre act comp)
-  "speak."
-  (emacspeak-auditory-icon 'repeat-active)
-  (dtk-say " dot " ))
+  "Icon."
+  (emacspeak-auditory-icon 'repeat-active))
 
 
 ;;;  advice completion functions to speak:
