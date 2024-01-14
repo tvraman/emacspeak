a976174ac736a344181c637b2f9cfd3841580ec2
T.V Raman <tv.raman.tv@gmail.com>
2024-01-13 20:00:40 -0800

    waiting on possible upstream bug in gptel

--- lisp/emacspeak.el
+++ lisp/emacspeak.el
@@ -88,9 +88,7 @@ the Emacspeak desktop.")
 ;; DocView
 (declare-function doc-view-open-text "doc-view")
 (with-eval-after-load "gptel"
-  (add-hook
-   'gptel-post-response-hook
-   #'(lambda (_ _) (emacspeak-speak-rest-of-buffer))))
+  (add-hook 'gptel-post-response-hook #'emacspeak-speak-region))
 
 (with-eval-after-load "doc-view"
   (add-hook 'doc-view-mode-hook #'doc-view-open-text))
