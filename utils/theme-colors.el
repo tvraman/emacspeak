b63c14cf6f01dba94a0fe41a0b8592f2f9147399
T.V Raman <tv.raman.tv@gmail.com>
2024-01-16 08:49:25 -0800

    now with completion

--- utils/theme-colors.el
+++ utils/theme-colors.el
@@ -4,13 +4,20 @@
 
 (defun tvr-theme-colors (palette)
   "Display colors in  palette"
-  (interactive "SPalette:")
+  (interactive
+   (list
+    (intern
+     (completing-read
+      "Palette: "
+      (cl-loop
+       for s being the symbols
+       when (string-match ".*palette$" (symbol-name s)) collect s)))))
   (with-output-to-temp-buffer   "*Colors*"
     (print palette)
     (cl-loop
      for p in  (symbol-value palette) 
      when (stringp (cl-second p)) do
-     (prin1 (car p))
+     (prin1 (cl-first p))
      (prin1    (ntc-name-this-color (cl-second p)))
      (terpri))))
 
