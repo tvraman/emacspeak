8bb9e1ffad0a889667658b54b78709711b2aa80b
T.V Raman <tv.raman.tv@gmail.com>
2024-01-16 14:12:59 -0800

    clean

--- utils/theme-colors.el
+++ utils/theme-colors.el
@@ -1,6 +1,6 @@
 ;;; colors-theme.el -*- lexical-binding: t -*-
 (require 'cl-lib)
-(require 'name-this-color)
+
 
 (defun tvr-theme-colors (palette)
   "Display colors in  palette"
@@ -10,11 +10,12 @@
      (completing-read
       "Palette: "
       (cl-loop
-       for s being the symbols when
-       (and (boundp s)
-            (not (functionp s))
-            (string-match ".*palette$" (symbol-name s))) collect s)))))
-  (with-current-buffer (get-buffer-create    "*Colors*")
+       for s being the symbols
+       when (and (boundp s)
+                 (not (functionp s))
+                 (string-match ".*palette$" (symbol-name s))) collect
+                 s))))) ; done reading input
+  (with-current-buffer (get-buffer-create    "*Colors*") ; produce output
     (let ((inhibit-read-only  t))
       (erase-buffer)
       (insert (format "%s\n" palette))
@@ -22,7 +23,9 @@
        for p in  (symbol-value palette) 
        when (stringp (cl-second p)) do
        (let ((c (ems--color-name (cl-second p))))
-         (insert (format "%s:\t%s\t%s\n" (cl-first p) c (cl-second p)))))))
+         (insert (format "%s:\t%s\t%s\n" (cl-first p) c (cl-second p))))))
+    (setq buffer-read-only t)
+    (special-mode))
   (emacspeak-auditory-icon 'open-object)
   (funcall-interactively #'switch-to-buffer "*Colors*")
   (goto-char (point-min))
