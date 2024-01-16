3cc521fc00a2a0e16be0da8eb570bcc0a87e474b
T.V Raman <tv.raman.tv@gmail.com>
2024-01-16 09:42:48 -0800

    snapshot

--- utils/theme-colors.el
+++ utils/theme-colors.el
@@ -10,20 +10,25 @@
      (completing-read
       "Palette: "
       (cl-loop
-       for s being the symbols
-       when
+       for s being the symbols when
        (and (boundp s)
             (not (functionp s))
             (string-match ".*palette$" (symbol-name s))) collect s)))))
-  (with-output-to-temp-buffer   "*Colors*"
+  (with-output-to-temp-buffer   *Colors*
     (print palette)
     (cl-loop
      for p in  (symbol-value palette) 
      when (stringp (cl-second p)) do
-     (prin1 (cl-first p))
-     (prin1    (ntc-name-this-color (cl-second p)))
-     (terpri)))
+     (let ((c (ems--color-name (cl-second p))))
+       (set-text-properties 0 (length c) nil c)
+       (prin1 (cl-first p))
+       (prin1    c)
+       (terpri))))
+  (with-current-buffer "*Colors*"
+    (goto-char (point-min))
+    (save-excursion
+      (while (search-forward (format  %c 34)) nil t (replace-match  ))))
   (emacspeak-auditory-icon 'open-object)
-  (funcall-interactively #'switch-to-buffer "*Colors*"))
+  (funcall-interactively #'switch-to-buffer *Colors*))
 
 (provide 'theme-colors)
