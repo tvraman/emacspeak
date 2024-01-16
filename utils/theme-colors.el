c170634425e78aa9457c47d260d23b15b6a65878
T.V Raman <tv.raman.tv@gmail.com>
2024-01-16 10:56:27 -0800

    cleaned up

--- utils/theme-colors.el
+++ utils/theme-colors.el
@@ -15,21 +15,23 @@
             (not (functionp s))
             (string-match ".*palette$" (symbol-name s))) collect s)))))
   (with-current-buffer (get-buffer-create    "*Colors*")
-    (insert (format "%s\n" palette))
-    (cl-loop
-     for p in  (symbol-value palette) 
-     when (stringp (cl-second p)) do
-     (let ((c (ems--color-name (cl-second p))))
-       (set-text-properties 0 (length c) nil c)
-       (insert (format "%s:\t%s\t%s\n" (cl-first p) c (cl-second p)))
-       )))
-  (with-current-buffer "*Colors*"
     (let ((inhibit-read-only  t))
-      (save-excursion
-        (goto-char (point-min))
-        (while (search-forward (format  "%c" 34)) nil t (replace-match "" )))))
+      (erase-buffer)
+      (insert (format "%s\n" palette))
+      (cl-loop
+       for p in  (symbol-value palette) 
+       when (stringp (cl-second p)) do
+       (let ((c (ems--color-name (cl-second p))))
+         (set-text-properties 0 (length c) nil c)
+         (insert (format "%s:\t%s\t%s\n" (cl-first p) c (cl-second p))))))
+    (save-excursion
+      (goto-char (point-min))
+      (while
+          (search-forward (format  "%c" 34) nil t)  (replace-match "" ))))
   (emacspeak-auditory-icon 'open-object)
-  (funcall-interactively #'switch-to-buffer "*Colors*"))
+  (funcall-interactively #'switch-to-buffer "*Colors*")
+  (goto-char (point-min))
+  (emacspeak-speak-line))
 
 (declare-function emacspeak-auditory-icon "emacspeak-sounds" (icon))
 (declare-function ems--color-name "emacspeak-wizards" (color))
