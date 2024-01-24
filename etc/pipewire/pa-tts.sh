03ed3a183115fba6031fdcda3084515a5380fa31
T.V Raman <tv.raman.tv@gmail.com>
2024-01-24 15:11:01 -0800

    pa-tts

--- etc/pipewire/pa-tts.sh
+++ etc/pipewire/pa-tts.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-#Create our tts_mono_left and tts_mono_right and avoid .asoundrc
+#Create our tts_left and tts_right and avoid .asoundrc
 
 
 
 
-# A tts_mono_right and tts_mono_left device
+# A tts_right and tts_left device
 # See https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/User/Modules/#module-remap-sink
 # Emacspeak will use the first as the notification device.
-pactl load-module module-remap-sink sink_name=tts_mono_right  master=binaural channels=2 master_channel_map=front-right,front-right, channel_map=front-left,front-right remix=no
-pactl load-module module-remap-sink sink_name=tts_mono_left  master=binaural channels=2 master_channel_map=front-left,front-left, channel_map=front-left,front-right remix=no
+pactl load-module module-remap-sink sink_name=tts_right  master=binaural channels=2 master_channel_map=front-right,front-right, channel_map=front-left,front-right remix=no
+pactl load-module module-remap-sink sink_name=tts_left  master=binaural channels=2 master_channel_map=front-left,front-left, channel_map=front-left,front-right remix=no
 
 # Above can likely be done with pw-link:
 #https://www.thegeekdiary.com/pw-link-command-examples-in-linux/
