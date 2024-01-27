ec1b2d3c7491634c6c0957d9468a77efc5fe823b
T.V Raman <tv.raman.tv@gmail.com>
2024-01-27 08:54:45 -0800

    cleanup

--- etc/pipewire/pa-tts.sh
+++ etc/pipewire/pa-tts.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-#Create our tts_left and tts_right and avoid .asoundrc
+#Create our tts_mono_left and tts_mono_right and avoid .asoundrc
 
 
 
 
-# A tts_right and tts_left device
+# A tts_mono_right and tts_mono_left device
 # See https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/User/Modules/#module-remap-sink
 # Emacspeak will use the first as the notification device.
-pactl load-module module-remap-sink sink_name=tts_right  master=binaural channels=2 master_channel_map=front-right,front-right, channel_map=front-left,front-right remix=no
-pactl load-module module-remap-sink sink_name=tts_left  master=binaural channels=2 master_channel_map=front-left,front-left, channel_map=front-left,front-right remix=no
+pactl load-module module-remap-sink sink_name=tts_mono_right  master=binaural channels=2 master_channel_map=front-right,front-right, channel_map=front-left,front-right remix=no
+pactl load-module module-remap-sink sink_name=tts_mono_left  master=binaural channels=2 master_channel_map=front-left,front-left, channel_map=front-left,front-right remix=no
 
 # Above can likely be done with pw-link:
 #https://www.thegeekdiary.com/pw-link-command-examples-in-linux/
 # device: snoop -- to record and play
-pactl load-module module-combine-sink sink_name=snoop  sink_properties=device.description="Record-and-Play"
+# pactl load-module module-combine-sink sink_name=snoop  sink_properties=device.description="Record-and-Play"
+
+# to undo, use pactl unload-module <module name>
