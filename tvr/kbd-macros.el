563127267319a3094e1765837b59563ed2b10ef4
T.V Raman <tv.raman.tv@gmail.com>
2024-02-02 19:14:26 -0800

    km

--- tvr/kbd-macros.el
+++ tvr/kbd-macros.el
@@ -4,5 +4,5 @@
    "C-c 3 g i t SPC p <return> C-; d l l C-a "))
 (define-key emacspeak-y-keymap "3" 'tvr-km-emacs-log)
 (defalias 'tvr-km-morning
-  (kmacro "C-<tab> C-e g b h <tab> <return> n n e "))
+  (kmacro "C-<tab> C-e g b h <tab> <return> n n e c"))
 (define-key emacspeak-y-keymap "0" 'tvr-km-morning)
