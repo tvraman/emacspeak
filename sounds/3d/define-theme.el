2006000ed736bb66d18a8978ec8d971cd76faddd
T.V Raman <tv.raman.tv@gmail.com>
2024-02-07 20:32:22 -0800

    moving wav themes to attic

--- sounds/3d/define-theme.el
+++
@@ -1,5 +0,0 @@
-;;; Sound theme generated using CSound to produce  spatially located icons.  -*- lexical-binding: t; -*-
-
-(emacspeak-sounds-define-theme
- (expand-file-name "3d/" emacspeak-sounds-dir)
- ".wav")
--- sounds/pan-chimes/define-theme.el
+++
@@ -1,6 +0,0 @@
-;;; Wave icons using chimes as a theme  -*- lexical-binding: t; -*-
-;;; Icons from theme Chimes with effect autopan applied using mplayer and ladspa
-
-(emacspeak-sounds-define-theme
- (expand-file-name "pan-chimes/" emacspeak-sounds-dir)
- ".wav")
