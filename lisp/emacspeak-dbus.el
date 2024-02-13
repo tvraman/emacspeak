71c47e7a01fb888ac92477f1b8a4c5c91744b570
T.V Raman <tv.raman.tv@gmail.com>
2024-02-12 20:42:48 -0800

    dbus cleanup

--- lisp/emacspeak-dbus.el
+++ lisp/emacspeak-dbus.el
@@ -162,19 +162,13 @@ Stop apps that use the network."
 ;;;###autoload
 (defun emacspeak-dbus-screensaver-check ()
   "Check  and fix Emacs DBus Binding to gnome-screensaver"
-  (when (file-exists-p "/usr/bin/gnome-screensaver")
+  (when (and (file-exists-p "/usr/bin/gnome-screensaver")
+             (zerop (shell-command "pidof gnome-screensaver ")))
     (ems-with-messages-silenced
-     (condition-case nil
-         (dbus-call-method
+      (dbus-call-method
           :session
           "org.gnome.ScreenSaver" "/org/gnome/ScreenSaver"
           "org.gnome.ScreenSaver" "GetActive")
-       (error
-        (progn
-          (shell-command
-           "pidof gnome-screensaver \
- && kill -9 `pidof gnome-screensaver` 2>&1 > /dev/null")
-          (start-process "screen-saver" nil "gnome-screensaver"))))
      t)))
 
 (defvar emacspeak-dbus-sleep-registration nil
