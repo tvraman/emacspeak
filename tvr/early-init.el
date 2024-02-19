002c13d62ad52234d9a076d9a9872eac62bfb257
T.V Raman <tv.raman.tv@gmail.com>
2024-02-19 07:30:10 -0800

    empv

--- tvr/early-init.el
+++ tvr/early-init.el
@@ -2,6 +2,6 @@
  native-comp-async-report-warnings-errors nil
                                         ;force-load-messages t
  read-process-output-max (* 256 256)
- package-quickstart t
+ ;package-quickstart t
  gc-cons-threshold 128000000
  )
