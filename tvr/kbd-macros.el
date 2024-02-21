3e754be9e1907be3142e6b58337e13bd9ddaaf1e
T.V Raman <tv.raman.tv@gmail.com>
2024-02-21 14:28:06 -0800

    km

--- tvr/kbd-macros.el
+++ tvr/kbd-macros.el
@@ -1,7 +1,6 @@
 ;;; Jump to Emacs Git Logs At HEAD:
 (defalias 'tvr-km-emacs-log
-  (kmacro
-   "C-c 3 g i t SPC p <return> C-; d l l C-a "))
+  (kmacro "C-c 3 C-; d F u C-; d l l n"))
 (define-key emacspeak-y-keymap "3" 'tvr-km-emacs-log)
 (defalias 'tvr-km-morning
   (kmacro "C-<tab> C-e g b h <tab> <return> n n e c"))
