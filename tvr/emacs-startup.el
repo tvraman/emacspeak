b712b485b2b127a5d0e5010f2c790723879a507d
T.V Raman <tv.raman.tv@gmail.com>
2024-03-01 07:40:39 -0800

    one more empv binding

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -120,6 +120,7 @@ Use Custom to customize where possible. "
   (global-set-key (kbd "C-l") ctl-x-map)
   (global-set-key[remap dabbrev-expand] 'hippie-expand)
   (global-set-key (kbd "<insert>") empv-map)
+  (global-set-key (kbd "s-;") empv-map)
   (cl-loop ;; global key-bindings
    for key in
    '(
