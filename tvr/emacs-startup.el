7339a32e71c6ff41454ae02375790bd56459a187
T.V Raman <tv.raman.tv@gmail.com>
2024-03-02 17:50:02 -0800

    tvr: try setting unsplittable t on main frame

--- tvr/emacs-startup.el
+++ tvr/emacs-startup.el
@@ -170,7 +170,8 @@ Use Custom to customize where possible. "
        abbrev-mode auto-correct-mode)))
   (setq  global-mode-string '("" display-time-string battery-mode-line-string))
   (bash-completion-setup)
-  (load-library "llm-prepare"))
+  (load-library "llm-prepare")
+  (set-frame-parameter nil 'unsplittable t))
 
 (defun tvr-after-init ()
   "Actions to take after Emacs is up and ready."
