ee56c11b80ffff9b6fd7dff94803da607355c3e9
T.V Raman <tv.raman.tv@gmail.com>
2024-03-04 14:34:32 -0800

    cleanup

--- lisp/emacspeak-ebuku.el
+++ lisp/emacspeak-ebuku.el
@@ -35,7 +35,7 @@
 
 ;;; Code:
 
-;;;   Required modules
+;;   Required modules
 
 (eval-when-compile  (require 'cl-lib))
 (cl-declaim  (optimize  (safety 0) (speed 3)))
@@ -103,6 +103,7 @@
   (when (ems-interactive-p)
     (emacspeak-speak-mode-line)
     (emacspeak-icon 'open-object)))
+
 ;;; Additional Keybindings:
 
 (cl-declaim (special ebuku-mode-map))
