3a4ee529eeedcba7003ecb6d0a20104c5b77b1b6
T.V Raman <tv.raman.tv@gmail.com>
2024-03-12 19:57:39 -0700

    fixed typo

--- servers/piper/pipspeak.el
+++ servers/piper/pipspeak.el
@@ -4,15 +4,18 @@
 
 (defun pipspeak-start ()
   "Start the Piper process"
+  (interactive)
   (cl-declare (special  pipspeak-pip))
   (unless (process-live-p pipspeak-pip)
     (let ((process-connection-type nil))
-  (setq  pipspeak-pip
-         (start-process  "pip" "*pip*" (executable-find "pipspeak"))))))
+      (setq  pipspeak-pip
+             (start-process  "pip" "*pip*" (executable-find
+                                            "pipspeak")))))
+  (pipspeak-speak "Piper is running!"))
 
 (defun pipspeak-speak (text)
   "Speak text"
   (interactive "sText:")
   (cl-declare (special pipspeak-pip))
   (cl-assert (process-live-p pipspeak-pip) t "Start Piper first")
-  (process-send-string pip  (format "%s\n" text)))
+  (process-send-string pipspeak-pip  (format "%s\n" text)))
