43cc3c229586852e7438ef975e6cb355c87f2fed
T.V Raman <tv.raman.tv@gmail.com>
2024-03-18 07:45:55 -0700

    subdirs walker

--- lisp/emacspeak-m-player.el
+++ lisp/emacspeak-m-player.el
@@ -442,10 +442,7 @@ If a dynamic playlist exists, just use it."
             (or
              filename                   ; short-circuit expensive call
              (if prefix
-                 (cl-remove-if
-                  #'(lambda (f) (or (not (file-directory-p f))
-                                    (string-match "\\.git" f)))
-                  (directory-files-recursively dir "." t)) ;list dirs
+                 (ems--subdirs-recursively (expand-file-name dir)) ;list dirs
                (directory-files-recursively dir emacspeak-media-extensions)))))
       (or filename
           (completing-read "Media: "  collection))))))
