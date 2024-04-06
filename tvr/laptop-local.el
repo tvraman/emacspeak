3e4759e4719d7e90a9322b36dcfe079021a71d2c
T.V Raman <tv.raman.tv@gmail.com>
2024-04-06 10:30:04 -0700

    cleanup

--- tvr/laptop-local.el
+++ tvr/laptop-local.el
@@ -1,7 +1,8 @@
-(setq pre-redisplay-function nil
-      x-wait-for-event-timeout 0
-      mail-host-address "google.com"
-      user-mail-address "raman@google.com")
+(setq
+                                        ;pre-redisplay-function nil
+ x-wait-for-event-timeout 0
+ mail-host-address "google.com"
+ user-mail-address "raman@google.com")
 (light-black)
 
 (defun tvr-calendar ()
@@ -22,9 +23,7 @@
   (interactive)
   (browse-url-chrome "https://mail//"))
 
-
 (define-key emacspeak-v-keymap "m" 'tvr-mail)
-
 
 (defun tvr-snippets ()
   "Open Google Snippets in Chrome"
@@ -33,15 +32,11 @@
 
 (define-key emacspeak-v-keymap "s" 'tvr-snippets)
 
-
-
-
 (defun tvr-time-off ()
   "Open time-off in Chrome"
   (interactive)
   (browse-url-chrome "go/tom"))
 
 (define-key emacspeak-v-keymap "t" 'tvr-time-off)
-
 
 (provide 'laptop-local)
