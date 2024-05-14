9f4a3876e2bb7e024cb95d02129fbc78289f15e6
T.V Raman <tv.raman.tv@gmail.com>
2024-05-13 19:30:37 -0700

    add orig url

--- etc/hack-xkb.sh
+++ etc/hack-xkb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #beware: didn't work with xcape
 #keeping it here for the future.
+# Taken from http://www.emacs.dyerdwelling.family/emacs/20240309130457-emacs--kmonad-sway-kbd-map-locking/
 DST=$HOME/.config
 
 # Reset keyboard layout (to your preferred language) 
