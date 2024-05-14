730156ebdb6d552e3f8f99d285dc0fff976cbbc6
T.V Raman <tv.raman.tv@gmail.com>
2024-05-14 16:08:58 -0700

    chimes

--- etc/clock.pl
+++ etc/clock.pl
@@ -12,9 +12,9 @@ my $sounds="$ENV{HOME}/emacs/lisp/emacspeak/sounds/clock";
 my ($sec,$min,$hour,$mDay,$mon,$year,$wDay,$yDay,$isdst) = localtime();
 my %chimes =(
     0 => [qw(Sun Mon Tue Wed Thu Fri Sat)],
-    15  => [qw(gf-15.mp3 wm-15.mp3 chime-15.mp3 bigben-15.mp3)],
-    30 =>[qw (gf-30.mp3 wm-30.mp3 chime-30.mp3 bigben-30.mp3)],
-    45 => [qw(gf-45.mp3 wm-45.mp3 chime-45.mp3 bigben-45.mp3)]
+    15  => [qw(gf-15.ogg wm-15.ogg chime-15.ogg bigben-15.ogg)],
+    30 =>[qw (gf-30.ogg wm-30.ogg chime-30.ogg bigben-30.ogg)],
+    45 => [qw(gf-45.ogg wm-45.ogg chime-45.ogg bigben-45.ogg)]
     );
 my $chime;
 exit unless defined ($chimes{$min});
@@ -22,7 +22,7 @@ if ($min == 0 ) {
     my $c = $chimes{0}[$wDay];
     $hour %= 12;
     $hour = 12 if ($hour == 0);
-    $chime = "$sounds/$c/$hour.mp3";
+    $chime = "$sounds/$c/$hour.ogg";
 } else {
     my $c = $chimes{$min}[$hour % 4];
     $chime = "$sounds/$c";
