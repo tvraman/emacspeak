e7c6a52ad590cbf9eb9bbca8b7a925fde9d4534c
T.V Raman <tv.raman.tv@gmail.com>
2024-07-07 19:19:38 -0700

    consult-setup

--- tvr/all-prepare.el
+++ tvr/all-prepare.el
@@ -1,3 +1,61 @@
+(defvar  emacspeak-consult-keymap nil "Emacspeak consult keymap")
+
+(define-prefix-command 'emacspeak-consult-keymap)
+
+(global-set-key (kbd "C-/") 'emacspeak-consult-keymap)
+
+(cl-loop
+ for b in
+ '(
+   ("M-x" consult-mode-command)
+   ("h" consult-history)
+   ("k" consult-kmacro)
+   ("m" consult-man)
+   ("i" consult-info)
+   ;; C-x bindings in `ctl-x-map'
+   ("C-x M-:" consult-complex-command) ;; orig. repeat-complex-command
+   ("C-x b" consult-buffer)            ;; orig. switch-to-buffer
+   ("C-x 4 b" consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+   ("C-x 5 b" consult-buffer-other-frame) ;; orig. switch-to-buffer-other-frame
+   ("C-x t b" consult-buffer-other-tab) ;; orig. switch-to-buffer-other-tab
+   ("C-x r b" consult-bookmark)         ;; orig. bookmark-jump
+   ("C-x p b" consult-project-buffer) ;; orig. project-switch-to-buffer
+   ;; Custom M-# bindings for fast register access
+   ("M-#" consult-register-load)
+   ("M-'" consult-register-store) ;; orig. abbrev-prefix-mark (unrelated)
+   ("C-M-#" consult-register)
+   ;; Other custom bindings
+   ("M-y" consult-yank-pop) ;; orig. yank-pop
+   ;; M-g bindings in `goto-map'
+   ("M-g e" consult-compile-error)
+   ("M-g f" consult-flymake)     ;; Alternative: consult-flycheck
+   ("M-g g" consult-goto-line)   ;; orig. goto-line
+   ("M-g M-g" consult-goto-line) ;; orig. goto-line
+   ("M-g o" consult-outline)     ;; Alternative: consult-org-heading
+   ("M-g m" consult-mark)
+   ("M-g k" consult-global-mark)
+   ("M-g i" consult-imenu)
+   ("M-g I" consult-imenu-multi)
+   ;; M-s bindings in `search-map'
+   ("M-s d" consult-find) ;; Alternative: consult-fd
+   ("M-s c" consult-locate)
+   ("M-s g" consult-grep)
+   ("M-s G" consult-git-grep)
+   ("M-s r" consult-ripgrep)
+   ("M-s l" consult-line)
+   ("M-s L" consult-line-multi)
+   ("M-s k" consult-keep-lines)
+   ("M-s u" consult-focus-lines)
+   ;; Isearch integration
+   ("M-s e" consult-isearch-history)
+   ("M-e" consult-isearch-history)   ;; orig. isearch-edit-string
+   ("M-s e" consult-isearch-history) ;; orig. isearch-edit-string
+   ("M-s l" consult-line) ;; needed by consult-line to detect isearch
+   ("M-s L" consult-line-multi) ;; needed by consult-line to detect isearch
+   ("M-s" consult-history) ;; orig. next-matching-history-element
+   ("M-r" consult-history))
+ do
+ (define-key  emacspeak-consult-keymap (car b) (cadr b)))
 ;;;  Gnus Setup For GMail imap:  -*- lexical-binding: t; -*-
 ;; Read GMailusing gnus  with 2-factor (Oauth2) authentication.
 ;; Uses auth-source-xoauth2:
