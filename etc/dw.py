87939ec27809f969e2c5320ccc7236d461199276
T.V Raman <tv.raman.tv@gmail.com>
2024-07-17 11:58:24 -0700

    works, prints lots of annoying newlines

--- etc/dw.py
+++ etc/dw.py
@@ -3,7 +3,6 @@ import os, sys
 with open(sys.argv[1], encoding='utf-8') as f:
     words  = f.read().lower().split(" ");
 
-    for i in range(0,len(words)-1):
-        if  (words[i] == words[i+1]): print (words[i])
-
-
+for i in range(0,len(words)-1):
+        if  (words[i] == words[i+1]): 
+print(words[i])
