db95aa8535cc2fe3f33756389dfaba218f7525b7
T.V Raman <tv.raman.tv@gmail.com>
2024-07-17 11:45:30 -0700

    dw: quick hack

---
+++ etc/dw.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import os, sys
+with open(sys.argv[1], encoding='utf-8') as f:
+    words  = f.read().lower().split(" ");
+
+    for i in range(0,len(words)-1):
+        if  (words[i] == words[i+1]): print (words[i])
+
+
