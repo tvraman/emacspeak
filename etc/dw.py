32b6da8a9984b4a7859b85125f579ffbd935560a
T.V Raman <tv.raman.tv@gmail.com>
2024-07-17 17:48:58 -0700

    cleanup

--- etc/dw.py
+++ etc/dw.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-import os, sys
-with open(sys.argv[1], encoding='utf-8') as f:
-    words  = f.read().lower().split(" ");
+import sys
 
-for i in range(0,len(words)-1):
-        if  (words[i] == words[i+1]): 
-            print(words[i])
+with open(sys.argv[1], encoding="utf-8") as f:
+    words = f.read().lower().split(None)
+
+for i in range(0, len(words) - 1):
+    if words[i] == words[i + 1]:
+        print(words[i])
