e41f80640cc64f9fe86756703b8f01b204a15025
T.V Raman <tv.raman.tv@gmail.com>
2024-07-17 18:03:27 -0700

    print count if non 0

--- etc/dw.py
+++ etc/dw.py
@@ -4,6 +4,10 @@ import sys
 with open(sys.argv[1], encoding="utf-8") as f:
     words = f.read().lower().split(None)
 
+count = 0
 for i in range(0, len(words) - 1):
     if words[i] == words[i + 1]:
         print(words[i])
+        count = count + 1
+if count > 0:
+    print("%s duplicate words", count)
